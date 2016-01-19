f1d1a6717dcd8890d9af55b421c538410a1a009d
Romulus Crisan <cromica@gmail.com>
2016-01-19 10:11:43 +0200

    - changed version to 3.8

--- ApplyStudioProjectTemplate/Sdl.Community.ApplyStudioProjectTemplate/Properties/AssemblyInfo.cs
+++ ApplyStudioProjectTemplate/Sdl.Community.ApplyStudioProjectTemplate/Properties/AssemblyInfo.cs
@@ -31,12 +31,12 @@ using System.Runtime.InteropServices;
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.7.0.0")]
-[assembly: AssemblyFileVersion("3.7.0.0")]
+[assembly: AssemblyVersion("3.8.0.0")]
+[assembly: AssemblyFileVersion("3.8.0.0")]
