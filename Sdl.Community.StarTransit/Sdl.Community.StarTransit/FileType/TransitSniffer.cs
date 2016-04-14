df9a69bd2ccb01eccca17c0c1fd8e1ab9140fca7
andreaGhisa <aghisa@sdl.com>
2016-04-14 09:47:20 +0300

    Added languages to file type.

--- Sdl.Community.StarTransit/Sdl.Community.StarTransit/FileType/TransitSniffer.cs
+++ Sdl.Community.StarTransit/Sdl.Community.StarTransit/FileType/TransitSniffer.cs
@@ -100,200 +100,264 @@ namespace Sdl.Community.StarTransit.FileType
             fileExtension = fileExtension.ToUpper();
             switch (fileExtension)
             {
-                    case "DEU":
+                case "DEU":
                     return "de-DE";
-                    case "AFK":
+                case "AFK":
                     return "af-ZA";
-                    case "SQI":
+                case "AMH":
+                    return "am-ET";
+                case "SQI":
                     return "sq-AL";
-                    case "ARG":
+                case "ARG":
                     return "ar-DZ";
-                    case "ARH":
+                case "ARH":
                     return "ar-BH";
-                    case "ARE":
+                case "ARE":
                     return "ar-EG";
-                    case "ARI":
+                case "ARI":
                     return "ar-IQ";
-                    case "ARJ":
+                case "ARJ":
                     return "ar-JO";
-                    case "ARK":
+                case "ARK":
                     return "ar-KW";
-                    case "ARB":
+                case "ARB":
                     return "ar-LB";
-                    case "ARL":
+                case "ARL":
                     return "ar-LY";
-                    case "ARM":
+                case "ARM":
                     return "ar-MA";
-                    case "ARO":
+                case "ARO":
                     return "ar-OM";
-                    case "ARQ":
+                case "ARQ":
                     return "ar-QA";
-                    case "ARA":
+                case "ARA":
                     return "ar-SA";
-                    case "ARS":
+                case "ARS":
                     return "ar-SY";
-                    case "ART":
+                case "ART":
                     return "ar-TN";
-                    case "ARU":
+                case "ARU":
                     return "ar-AE";
-                    case "ARY":
+                case "ARY":
                     return "ar-YE";
-                    case "EUQ":
+                case "EUQ":
                     return "eu-ES";
-                    case "BEL":
+                case "BEL":
                     return "be-BY";
-                    case "BGR":
+                case "BGR":
                     return "bg-BG";
-                    case "CAT":
+                case "CAT":
                     return "ca-ES";
-                    case "CHS":
+                case "CHS":
                     return "zh-CN";
-                    case "ZHH":
+                case "ZHH":
                     return "zh-HK";
-                    case "ZHI":
+                case "ZHI":
                     return "zh-SG";
-                    case "CHT":
+                case "CHT":
                     return "zh-TW";
-                    case "HRV":
+                case "HRV":
                     return "hr-HR";
-                    case "CSY":
+                case "CSY":
                     return "cs-CZ";
-                    case "DAN":
+                case "DAN":
                     return "da-DK";
-                    case "NLB":
+                case "NLB":
                     return "nl-BE";
-                    case "NLD":
+                case "NLD":
                     return "nl-NL";
-                    case "ENA":
+                case "ENA":
                     return "en-AU";
-                    case "ENL":
+                case "ENL":
                     return "en-BZ";
-                    case "ENC":
+                case "ENC":
                     return "en-CA";
-                    case "ENI":
+                case "ENI":
                     return "en-IE";
-                    case "ENJ":
+                case "ENJ":
                     return "en-JM";
-                    case "ENZ":
+                case "ENZ":
                     return "en-NZ";
-                    case "ENS":
+                case "ENS":
                     return "en-ZA";
-                    case "ENT":
+                case "ENT":
                     return "en-TT";
-                    case "ENG":
+                case "ENG":
                     return "en-GB";
-                    case "ENU":
+                case "ENU":
                     return "en-US";
-                    case "ETI":
+                case "ETI":
                     return "et-EE";
-                    case "FOS":
+                case "FOS":
                     return "fo-FO";
-                    case "FAR":
+                case "FAR":
                     return "fa-IR";
-                    case "FIN":
+                case "FIN":
                     return "fi-FI";
-                    case "FRB":
+                case "FRB":
                     return "fr-BE";
-                    case "FRC":
+                case "FRC":
                     return "fr-CA";
-                    case "FRL":
+                case "FRL":
                     return "fr-LU";
-                    case "FRS":
+                case "FRS":
                     return "fr-CH";
-                    case "DEA":
+                case "DEA":
                     return "de-AT";
-                    case "DEC":
+                case "DEC":
                     return "de-LI";
-                    case "DEL":
+                case "DEL":
                     return "de-LU";
-                    case "DES":
+                case "DES":
                     return "de-CH";
-                    case "ELL":
+                case "ELL":
                     return "el-GR";
-                    case "HEB":
+                case "HEB":
                     return "he-IL";
-                    case "HIN":
+                case "HIN":
                     return "hi-IN";
-                    case "HUN":
+                case "HUN":
                     return "hu-HU";
-                    case "ISL":
+                case "ISL":
                     return "is-IS";
-                    case "ITA":
+                case "ITA":
                     return "it-IT";
-                    case "ITS":
+                case "ITS":
                     return "it-CH";
-                    case "JPN":
+                case "JPN":
                     return "ja-JP";
-                    case "KOR":
+                case "KOR":
                     return "ko-KR";
-                    case "LVI":
+                case "LVI":
                     return "lv-LV";
-                    case "LTH":
+                case "LTH":
                     return "lt-LT";
-                    case "MKD":
+                case "MKD":
                     return "mk-MK";
-                    case "PLK":
+                case "PLK":
                     return "pl-PL";
-                    case "PTB":
+                case "PTB":
                     return "pt-BR";
-                    case "ROM":
+                case "ROM":
                     return "ro-RO";
-                    case "RUS":
+                case "RUS":
                     return "ru-RU";
-                    case "SKY":
+                case "SKY":
                     return "sk-SK";
-                    case "SLV":
+                case "SLV":
                     return "sl-SI";
-                    case "ESS":
+                case "ESS":
                     return "es-AR";
-                    case "ESB":
+                case "ESB":
                     return "es-BO";
-                    case "ESL":
+                case "ESL":
                     return "es-CL";
-                    case "ESO":
+                case "ESO":
                     return "es-CO";
-                    case "ESC":
+                case "ESC":
                     return "es-CR";
-                    case "ESD":
+                case "ESD":
                     return "es-DO";
-                    case "ESF":
+                case "ESF":
                     return "es-EC";
-                    case "ESE":
+                case "ESE":
                     return "es-SV";
-                    case "ESG":
+                case "ESG":
                     return "es-GT";
-                    case "ESH":
+                case "ESH":
                     return "es-HN";
-                    case "ESM":
+                case "ESM":
                     return "es-MX";
-                    case "ESI":
+                case "ESI":
                     return "es-NI";
-                    case "ESA":
+                case "ESA":
                     return "es-PA";
-                    case "ESZ":
+                case "ESZ":
                     return "es-PY";
-                    case "ESR":
+                case "ESR":
                     return "es-PE";
-                    case "ES":
+                case "ES":
                     return "es-PR";
-                    case "ESP":
+                case "ESP":
                     return "es-ES";
-                    case "ESY":
+                case "ESY":
                     return "es-UY";
-                    case "ESV":
+                case "ESV":
                     return "es-VE";
-                    case "SVF":
+                case "SVF":
                     return "sv-FI";
-                    case "THA":
+                case "THA":
                     return "th-TH";
-                    case "TRK":
+                case "TRK":
                     return "tr-TR";
-                    case "UKR":
+                case "UKR":
                     return "uk-UA";
-                    case "URD":
+                case "URD":
                     return "ur-PK";
-                    case "VIT":
+                case "VIT":
                     return "vi-VN";
+                case "AZC":
+                    return "az-Cyrl-AZ";
+                case "AZE":
+                    return "az-Cyrl-AZ";
+                case "ENN":
+                    return "en-IN";
+                case "ENM":
+                    return "en-MY";
+                case "ENP":
+                    return "en-PH";
+                case "FRA":
+                    return"fr-FR";
+                case "FRM":
+                    return "fr-MC";
+                case "FRO":
+                    return "fo-FO";
+                case "FRY":
+                    return "fy-NL";
+                case "GAL":
+                    return "gl-ES";
+                case "GRC":
+                    return "el-GR";
+                case "IBO":
+                    return "ig-NG";
+                case "IND":
+                    return "id-ID";
+                case "KAZ":
+                    return "kk-KZ";
+                case "MNG":
+                    return "mn-MN";
+                case "MSB":
+                    return "ms-BN";
+                case "MSL":
+                    return "ms-MY";
+                case "NON":
+                    return "nn-NO";
+                case "NOR":
+                    return "nb-NO";
+                case "NSO":
+                    return "nso-ZA";
+                case "PTG":
+                    return "pt-PT";
+                case "SRL":
+                    return "sr";
+                case "SVE":
+                    return "sv-SE";
+                case "SRB":
+                    return "sr-Latn";
+                case "SWK":
+                    return "sw-KE";
+                case "TKM":
+                    return "tk-TM";
+                case "UZB":
+                    return "es-VE";
+                case "VEN":
+                    return "uz-Cyrl-UZ";
+                case "ZHM":
+                    return "zh-MO";
+                case "ZUL":
+                    return "zu-ZA";
+                case "ROU":
+                    return "ro-RO";
 
                 default:
                     return "";
