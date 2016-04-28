aaee2b0e57b101d4d4c3cd99d918e5dd17149d7c
andreaGhisa <aghisa@sdl.com>
2016-04-28 12:12:38 +0300

    InSource

---
+++ InSource/Insights/UserSessionInitializer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+using Microsoft.ApplicationInsights.DataContracts;
+using Microsoft.ApplicationInsights.Extensibility;
+
+namespace Sdl.Community.InSource.Insights
+{
+    public class UserSessionInitializer : IContextInitializer
+    {
+        public void Initialize(TelemetryContext context)
+        {
+            context.User.Id = Environment.UserName;
+            context.User.UserAgent = "Studio plugin";
+            context.Session.Id = Guid.NewGuid().ToString();
+            context.Device.OperatingSystem = Environment.OSVersion.ToString();
+            context.Device.Language = CultureInfo.CurrentCulture.DisplayName;
+            context.Component.Version = typeof(UserSessionInitializer)
+                .Assembly
+                .GetName()
+                .Version
+                .ToString();
+
+            context.InstrumentationKey = "fdbba35b-c4ab-4049-a952-5caba827b86a";
+
+
+        }
+    }
+}
