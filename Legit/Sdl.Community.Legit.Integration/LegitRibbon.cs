4aecc70e4e5a24078acca5fecc05aca4802a1527
andreaGhisa <aghisa@sdl.com>
2016-08-23 14:39:03 +0300

    Converted Sdl Legit! installer in sdlplugin.

---
+++ Legit/Sdl.Community.Legit.Integration/LegitRibbon.cs
@@ -0,0 +1,25 @@
+﻿using Sdl.Desktop.IntegrationApi;
+using Sdl.Desktop.IntegrationApi.DefaultLocations;
+using Sdl.Desktop.IntegrationApi.Extensions;
+
+
+namespace Sdl.Community.Legit.Integration
+{
+    [RibbonGroup("Sdl.Community.Legit", Name = "LegIt!")]
+    [RibbonGroupLayout(LocationByType = typeof(StudioDefaultRibbonTabs.AddinsRibbonTabLocation))]
+    class LegitRibbon: AbstractRibbonGroup
+    {
+    }
+
+    [Action("Sdl.Community.Legit", Name = "LegIt!", Icon = "LegIt! - 32", Description = "LegIt!")]
+    [ActionLayout(typeof(LegitRibbon), 20, DisplayType.Large)]
+    class LegitViewPartAction : AbstractAction
+    {
+        protected override void Execute()
+        {
+            
+           var main = new MainForm();
+            main.ShowDialog();
+        }
+    }
+}
