69e583514ee396d1890d35565d7347cd6a7c0e6e
andreaGhisa <aghisa@sdl.com>
2016-08-23 15:06:10 +0300

    Converted YourStudio installer to sdlplugin.

---
+++ YourStudio/Sdl.Community.YourStudio.Integration/YourStudioRibbon.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sdl.Desktop.IntegrationApi;
+using Sdl.Desktop.IntegrationApi.DefaultLocations;
+using Sdl.Desktop.IntegrationApi.Extensions;
+
+namespace Sdl.Community.YourStudio.Integration
+{
+    [RibbonGroup("Sdl.Community.YourStudio", Name = "YourStudio")]
+    [RibbonGroupLayout(LocationByType = typeof(StudioDefaultRibbonTabs.AddinsRibbonTabLocation))]
+    class YourStudioRibbon : AbstractRibbonGroup
+    {
+    }
+
+    [Action("Sdl.Community.YourStudio", Name = "YourStudio", Icon = "yoursdlstudio", Description = "YourStudio")]
+    [ActionLayout(typeof(YourStudioRibbon), 20, DisplayType.Large)]
+    class YourStudioViewPartAction:AbstractAction
+    {
+        protected override void Execute()
+        {
+            var yourStudio = new Form1();
+            yourStudio.ShowDialog();
+        }
+    }
+}
