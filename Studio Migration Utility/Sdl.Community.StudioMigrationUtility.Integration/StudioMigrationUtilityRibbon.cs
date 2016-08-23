82e2914d449476962d7c7a44be02fd0b0d429dc0
andreaGhisa <aghisa@sdl.com>
2016-08-23 15:32:44 +0300

    Converted Studio Migration Utility installer to sdlplugin.

---
+++ Studio Migration Utility/Sdl.Community.StudioMigrationUtility.Integration/StudioMigrationUtilityRibbon.cs
@@ -0,0 +1,25 @@
+﻿using Sdl.Community.StudioMigrationUtility.Services;
+using Sdl.Desktop.IntegrationApi;
+using Sdl.Desktop.IntegrationApi.DefaultLocations;
+using Sdl.Desktop.IntegrationApi.Extensions;
+
+namespace Sdl.Community.StudioMigrationUtility.Integration
+{
+    [RibbonGroup("Sdl.Community.StudioMigrationUtility", Name = "Studio Migration Utility")]
+    [RibbonGroupLayout(LocationByType = typeof(StudioDefaultRibbonTabs.AddinsRibbonTabLocation))]
+    class StudioMigrationUtilityRibbon : AbstractRibbonGroup
+    {
+    }
+
+    [Action("Sdl.Community.StudioMigrationUtility", Name = "Studio Migration Utility", Icon = "migrate", Description = "Studio Migration Utility")]
+    [ActionLayout(typeof(StudioMigrationUtilityRibbon), 20, DisplayType.Large)]
+    class StudioMigrationUtilityViewPartAction:AbstractAction
+    {
+        protected override void Execute()
+        {
+            var migrateUtility = new MigrateUtility(new StudioVersionService());
+            migrateUtility.ShowDialog();
+
+        }
+    }
+}
