fae83c2f45ecd2e338b90cbdee6b27bcd582bf9f
andreaGhisa <aghisa@sdl.com>
2016-08-23 14:53:20 +0300

    Converted TMRepair installer to sdlplugin.

---
+++ SDLTMRepair/Sdl.Community.TMRepair.Integration/TMRepairRibbon.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sdl.Desktop.IntegrationApi;
+using Sdl.Desktop.IntegrationApi.DefaultLocations;
+using Sdl.Desktop.IntegrationApi.Extensions;
+
+namespace Sdl.Community.TMRepair.Integration
+{
+    [RibbonGroup("Sdl.Community.TMRepair", Name = "TMRepair")]
+    [RibbonGroupLayout(LocationByType = typeof(StudioDefaultRibbonTabs.AddinsRibbonTabLocation))]
+    class TmRepairRibbon : AbstractRibbonGroup
+    {
+    }
+
+    [Action("Sdl.Community.TMRepair", Name = "TMRepair",Icon = "Repair", Description = "TMRepair")]
+    [ActionLayout(typeof(TmRepairRibbon), 20, DisplayType.Large)]
+    class TmRepairViewPartAction:AbstractAction
+    {
+        protected override void Execute()
+        {
+            var tmRepair = new SDLTMRepair();
+            tmRepair.ShowDialog();
+        }
+    }
+}
