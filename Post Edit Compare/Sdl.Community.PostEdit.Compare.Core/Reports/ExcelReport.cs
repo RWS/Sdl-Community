f11a633eec93a365443d454c65299eda85f80397
andreaGhisa <aghisa@sdl.com>
2017-09-26 15:28:00 +0300

    Create table refactoring.

--- Post Edit Compare/Sdl.Community.PostEdit.Compare.Core/Reports/ExcelReport.cs
+++ Post Edit Compare/Sdl.Community.PostEdit.Compare.Core/Reports/ExcelReport.cs
@@ -15,7 +15,7 @@ namespace Sdl.Community.PostEdit.Compare.Core.Reports
 	{
 		public static void CreateExcelReport(List<PEMModel> analyseResults)
 		{
-
+			//save report to specific folder is not implemented yet
 			var reportPath = Path.Combine(@"C:\Users\aghisa\Desktop", "report.xlsx");
 			if (File.Exists(reportPath)) { File.Delete(reportPath); };
 			var newFile = new FileInfo(Path.Combine(@"C:\Users\aghisa\Desktop", "report.xlsx"));
@@ -28,17 +28,6 @@ namespace Sdl.Community.PostEdit.Compare.Core.Reports
 				CreateTableHeader(xlPackage, worksheet);
 				CreateFirstColumnValues(xlPackage, worksheet);
 				FillTableWithAnalyseResults(xlPackage, worksheet, analyseResults);
-				//var tableHeader = GetTableHeaderValues();
-				//var headerCellIndex = 1;
-				//for(var i = 0; i < tableHeader.Count; i++)
-				//{
-				//	worksheet.Cells[1,headerCellIndex].Value = tableHeader[i];
-				//	headerCellIndex++;
-				//}
-
-				//xlPackage.Save();
-				//var countRow = worksheet.Dimension.End.Row;
-			
 
 			}
 
@@ -64,7 +53,6 @@ namespace Sdl.Community.PostEdit.Compare.Core.Reports
 			}
 
 			xlPackage.Save();
-			//var columnIndex = analysisBandCell.Columns;
 		}
 
 		private static decimal? GetMatchingResultValue(string matchValue,string typeValue, List<PEMModel> analyseResults)
@@ -87,31 +75,32 @@ namespace Sdl.Community.PostEdit.Compare.Core.Reports
 			
 			foreach(var column in columnValues)
 			{
-				worksheet.Cells[rowIndex, 1].Value = column.Item3;
+				worksheet.Cells[rowIndex, 1].Value = column;
 				rowIndex++;
 			}
 			
 			xlPackage.Save();
 		}
 
-		private static List<Tuple<int,string,string>> GetFirstColumnValues()
+		private static List<string> GetFirstColumnValues()
 		{
-			var columnValues = new List<Tuple<int, string, string>>{
-				Tuple.Create(0,"fuzzy100","100%"),
-				Tuple.Create(1,"fuzzy99","95% - 99%"),
-				Tuple.Create(2,"fuzzy94","85% - 94%"),
-				Tuple.Create(3,"fuzzy84","75% - 84%"),
-				Tuple.Create(4,"fuzzy74","50% - 74%"),
-				Tuple.Create(5,"new","New"),
-				Tuple.Create(6,"total","Total"),
+			var columnValues = new List<string>
+			{
+				Constants.ExactMatch,
+				Constants.Fuzzy99,
+				Constants.Fuzzy94,
+				Constants.Fuzzy84,
+				Constants.Fuzzy74,
+				Constants.New,
+				Constants.Total
 			};
 			return columnValues;
 		}
 		private static void CreateTableHeader(ExcelPackage xlPackage, ExcelWorksheet worksheet)
 		{
 			var tableHeader = GetTableHeaderValues();
-			int rowNr;//= worksheet.Dimension.End.Row; // ultima linie folosita in excel
-			int columnNr;//= worksheet.Dimension.End.Column;//ultima coloana
+			int rowNr;//= worksheet.Dimension.End.Row; 
+			int columnNr;//= worksheet.Dimension.End.Column;
 			if (worksheet.Dimension == null)
 			{
 				rowNr = 1;
@@ -119,8 +108,8 @@ namespace Sdl.Community.PostEdit.Compare.Core.Reports
 			}
 			else
 			{
-				rowNr = worksheet.Dimension.End.Row; // ultima linie folosita in excel
-				columnNr = worksheet.Dimension.End.Column;//ultima coloana
+				rowNr = worksheet.Dimension.End.Row; 
+				columnNr = worksheet.Dimension.End.Column;
 			}
 			foreach (var item in tableHeader)
 			{
@@ -133,58 +122,17 @@ namespace Sdl.Community.PostEdit.Compare.Core.Reports
 
 		private static List<string> GetTableHeaderValues()
 		{
-			//var headerValues = new List<PostEditModificationsExcelTableModel>
-			//{
-			//	new PostEditModificationsExcelTableModel{
-			//		HeaderValues = Tuple.Create(0,"analysisBand","Analysis Band")
-			//	},
-			//	new PostEditModificationsExcelTableModel{
-			//		HeaderValues = Tuple.Create(1,"segments","Segments")
-			//	},
-			//	new PostEditModificationsExcelTableModel{
-			//		HeaderValues = Tuple.Create(2,"words","Words")
-			//	},
-			//	new PostEditModificationsExcelTableModel{
-			//		HeaderValues = Tuple.Create(3,"characters","Characters")
-			//	},
-			//	new PostEditModificationsExcelTableModel{
-			//		HeaderValues = Tuple.Create(4,"percent","Percent")
-			//	},
-			//	new PostEditModificationsExcelTableModel{
-			//		HeaderValues = Tuple.Create(5,"total","Total")
-			//	},
-			//};
 			var headerValues = new List<string>
-			{				
+			{
 					Constants.AnalysisBand,
 					Constants.Segments,
 					Constants.Words,
 					Constants.Characters,
 					Constants.Percent,
-					Constants.Total		};
+					Constants.Total
+			};
 			return headerValues;
 		}
 			
-		//private static List<string> GetTableHeaderValues()
-		//{
-		//	//var headerValues = new PostEditModificationsExcelTableModel
-		//	//{
-		//	//	AnalysisBand = "Analysis Band",
-		//	//	Segments = "Segments",
-		//	//	Words = "Words",
-		//	//	Characters = "Characters",
-		//	//	Percent = "Percent",
-
-		//	//};
-		//	//return headerValues;
-		//	return new List<string>
-		//	{
-		//		"Analysis Band",
-		//		"Segments",
-		//		"Words",
-		//		"Characters",
-		//		"Percent"
-		//	};
-		//} 
 	}
 }
--- Sdl.Community.PostEdit.Compare.DAL/PostEditModificationsAnalysis/PEMModel.cs
+++ Sdl.Community.PostEdit.Compare.DAL/PostEditModificationsAnalysis/PEMModel.cs
@@ -8,14 +8,6 @@ namespace Sdl.Community.PostEdit.Compare.DAL.PostEditModificationsAnalysis
 {
 	public class PEMModel
 	{
-		//public PEMResults Hundred { get; set; }
-		//public PEMResults Fuzzy99 { get; set; }
-		//public PEMResults Fuzzy94 { get; set; }
-		//public PEMResults Fuzzy84 { get; set; }
-		//public PEMResults Fuzzy74 { get; set; }
-		//public PEMResults New { get; set; }
-		//public PEMResults Total { get; set; }
-
 		public Tuple<string,string, decimal> AnalyseResult { get; set; }
 
 	}
