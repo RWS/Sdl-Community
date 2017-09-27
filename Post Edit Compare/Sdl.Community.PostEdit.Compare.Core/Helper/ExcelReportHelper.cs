84a761fd3f1b1905413201fc1612a929c26aec63
andreaGhisa <aghisa@sdl.com>
2017-09-27 12:04:18 +0300

    Added Title text and style.

--- Post Edit Compare/Sdl.Community.PostEdit.Compare.Core/Helper/ExcelReportHelper.cs
+++ Post Edit Compare/Sdl.Community.PostEdit.Compare.Core/Helper/ExcelReportHelper.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using OfficeOpenXml;
+using OfficeOpenXml.Style;
 
 namespace Sdl.Community.PostEdit.Compare.Core.Helper
 {
@@ -11,8 +12,12 @@ namespace Sdl.Community.PostEdit.Compare.Core.Helper
 	{
 		public static void CreateReportHeader(ExcelPackage xlPackage, ExcelWorksheet worksheet)
 		{
-			//worksheet.Cells["A1:T1"].Value = "Post-Edit Comparison Report";
-			worksheet.Cells["A1"].Value = "tezt";
+			worksheet.Cells["A1"].Value = "Post-Edit Comparison Report";
+			worksheet.Cells["A1:T1"].Merge = true;
+			
+			worksheet.Cells["A1"].Style.Font.Size = 18;
+			worksheet.Cells["A1"].Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+
 			xlPackage.Save();
 		}
 	}
