37b123f87bb58adfba48e2b15f8d6e9d1b2e7940
Laura Paraschivescu <lparaschivescu@sdl.com>
2017-10-16 16:37:12 +0300

    Added xmlReader button on the RibbonGroup.

---
+++ Sdl.Community.xmlReader/Sdl.Community.xmlReader/XmlReaderRibbonGroup.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+using Sdl.Desktop.IntegrationApi;
+using Sdl.Desktop.IntegrationApi.Extensions;
+using Sdl.TranslationStudioAutomation.IntegrationApi.Presentation.DefaultLocations;
+using Sdl.TranslationStudioAutomation.IntegrationApi;
+
+namespace Sdl.Community.xmlReader
+{
+    [RibbonGroup("Sdl.Community.xmlReaderRibbonButton", Name = "", ContextByType = typeof(ProjectsController))]
+    [RibbonGroupLayout(LocationByType = typeof(TranslationStudioDefaultRibbonTabs.HomeRibbonTabLocation))]
+    public class XmlReaderRibbonGroup : AbstractRibbonGroup
+    {
+    }
+
+    [Action("Sdl.Community.xmlReader.GenerateXmlReaderAction", Name = "xml Reader", Icon = "logo", Description = "Read xml files and export them as Excel...")]
+    [ActionLayout(typeof(XmlReaderRibbonGroup), 250, DisplayType.Large)]
+    class GenerateXmlReaderAction : AbstractAction
+    {
+        protected override void Execute()
+        {
+            MessageBox.Show(@"hello!", "info");
+        }
+    }
+}
