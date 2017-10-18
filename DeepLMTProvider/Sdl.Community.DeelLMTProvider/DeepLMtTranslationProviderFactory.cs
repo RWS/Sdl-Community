4ec4105766c6326525f4337d574128366e429acd
andreaGhisa <aghisa@sdl.com>
2017-10-18 09:48:07 +0300

    DeepL plugin initial checkin.

---
+++ DeepLMTProvider/Sdl.Community.DeelLMTProvider/DeepLMtTranslationProviderFactory.cs
@@ -0,0 +1,32 @@
+﻿using Sdl.LanguagePlatform.TranslationMemoryApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sdl.Community.DeepLMTProvider
+{
+    [TranslationProviderFactory(Id = "DeepLMtTranslationProviderFactory",
+                             Name = "DeepLMtTranslationProviderFactory",
+                             Description = "DeepL Mt Translation Provider")]
+    public class DeepLMtTranslationProviderFactory : ITranslationProviderFactory
+    {
+        public ITranslationProvider CreateTranslationProvider(Uri translationProviderUri, string translationProviderState, ITranslationProviderCredentialStore credentialStore)
+        {
+            var tp = new DeepLMtTranslationProvider();
+            return tp;
+        }
+
+        public TranslationProviderInfo GetTranslationProviderInfo(Uri translationProviderUri, string translationProviderState)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SupportsTranslationProviderUri(Uri translationProviderUri)
+        {
+            //here see MT enhanced for the actual  value
+            return true;
+        }
+    }
+}
---
+++ DeepLMTProvider/Sdl.Community.DeelLMTProvider/DeepLMtTranslationProviderWinFormsUI.cs
@@ -0,0 +1,53 @@
+﻿using Sdl.LanguagePlatform.TranslationMemoryApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sdl.LanguagePlatform.Core;
+using System.Windows.Forms;
+
+namespace Sdl.Community.DeepLMTProvider
+{
+    [TranslationProviderWinFormsUi(
+        Id = "DeepLMtTranslationProviderWinFormsUI",
+        Name = "DeepLMtTranslationProviderWinFormsUI",
+        Description = "DeepLMtTranslationProviderWinFormsUI")]
+    public class DeepLMtTranslationProviderWinFormsUI : ITranslationProviderWinFormsUI
+    {
+        public string TypeName => "DeepL MT Translation Provider";
+
+        public string TypeDescription => "DeepL MT Translation Provider";
+
+        public bool SupportsEditing => true;
+
+        public ITranslationProvider[] Browse(IWin32Window owner, LanguagePair[] languagePairs, ITranslationProviderCredentialStore credentialStore)
+        {
+            var dialog = new DeepLMtDialog();
+            var provider = new DeepLMtTranslationProvider();
+            return new ITranslationProvider[] { provider };
+        }
+
+        public bool Edit(IWin32Window owner, ITranslationProvider translationProvider, LanguagePair[] languagePairs, ITranslationProviderCredentialStore credentialStore)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool GetCredentialsFromUser(IWin32Window owner, Uri translationProviderUri, string translationProviderState, ITranslationProviderCredentialStore credentialStore)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TranslationProviderDisplayInfo GetDisplayInfo(Uri translationProviderUri, string translationProviderState)
+        {
+            var info = new TranslationProviderDisplayInfo();
+            info.Name = "DeepL Translation provider";
+            return info;
+        }
+
+        public bool SupportsTranslationProviderUri(Uri translationProviderUri)
+        {
+            return true;
+        }
+    }
+}
