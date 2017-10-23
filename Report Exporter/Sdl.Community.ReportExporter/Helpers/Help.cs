5d0d263a98352d9d7f56c5c7e051cf453736021a
andreaGhisa <aghisa@sdl.com>
2017-10-23 14:33:06 +0300

    Show studio projects.

---
+++ Report Exporter/Sdl.Community.ReportExporter/Helpers/Help.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sdl.Community.Toolkit.Core.Services;
+
+namespace Sdl.Community.ReportExporter.Helpers
+{
+	public static class Help
+	{
+		public static int GetInstalledStudioMajorVersion()
+		{
+			var studioService = new StudioVersionService();
+			return studioService.GetStudioVersion().ExecutableVersion.Major;
+		}
+
+		public static string GetStudioProjectsPath()
+		{
+			var installedStudioVersion = GetInstalledStudioMajorVersion();
+			var projectsPath = string.Empty;
+
+			if (installedStudioVersion.Equals(14))
+			{
+				projectsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+					@"Studio 2017\Projects\projects.xml");
+			}
+			if (installedStudioVersion.Equals(12))
+			{
+				projectsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments),
+					@"Studio 2015\Projects\projects.xml");
+			}
+
+			return projectsPath;
+		}
+	}
+}
--- Report Exporter/Sdl.Community.ReportExporter/ReportExporterControl.cs
+++ Report Exporter/Sdl.Community.ReportExporter/ReportExporterControl.cs
@@ -3,20 +3,80 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Drawing;
 using System.Data;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 using Sdl.Community.ReportExporter.Helpers;
+using Sdl.Community.ReportExporter.Model;
 using Sdl.Desktop.IntegrationApi;
+using Help = Sdl.Community.ReportExporter.Helpers.Help;
 
 namespace Sdl.Community.ReportExporter
 {
 	public partial class ReportExporterControl : UserControl, ISettingsAware<ReportExporterSettings>
 	{
+		private readonly string _projectXmlPath;
+
 		public ReportExporterControl()
 		{
 			InitializeComponent();
+			_projectXmlPath = Help.GetStudioProjectsPath();
+			LoadProjectsList();
+		}
+
+		/// <summary>
+		/// Reads studio projects from project.xml
+		/// Adds projects to listbox
+		/// </summary>
+		private void LoadProjectsList()
+		{
+			var projectXmlDocument = new XmlDocument();
+
+			projectXmlDocument.Load(_projectXmlPath);
+			
+			var projectsNodeList = projectXmlDocument.SelectNodes("//ProjectListItem");
+			if (projectsNodeList == null) return;
+			foreach (var item in projectsNodeList)
+			{
+				var projectInfo = ((XmlNode)item).SelectSingleNode("./ProjectInfo");
+				if (projectInfo?.Attributes != null && projectInfo.Attributes["IsInPlace"].Value != "true")
+				{
+					projListbox.Items.Add(CreateProjectDetails((XmlNode)item));
+				}
+			}
+		}
+
+		/// <summary>
+		/// Creates project details for given project from xml file
+		/// </summary>
+		/// <param name="projNode"></param>
+		/// <returns></returns>
+		private ProjectDetails CreateProjectDetails(XmlNode projNode)
+		{
+			var projectDetails = new ProjectDetails();
+			var projectFolderPath = string.Empty;
+
+			var selectSingleNode = projNode.SelectSingleNode("ProjectInfo");
+			if (selectSingleNode?.Attributes != null)
+			{
+				projectDetails.ProjectName = selectSingleNode.Attributes["Name"].Value;
+			}
+			if (projNode.Attributes != null)
+			{
+				projectFolderPath = projNode.Attributes["ProjectFilePath"].Value;
+			}
+			if (Path.IsPathRooted(projectFolderPath))
+			{
+				projectDetails.ProjectPath = projectFolderPath; //location outside standard project place
+			}
+			else
+			{
+				projectDetails.ProjectPath = _projectXmlPath + projectFolderPath;
+			}
+			return projectDetails;
 		}
 
 		public ReportExporterSettings Settings { get; set; }
