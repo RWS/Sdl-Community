dd4e2fc9b80d3c45efc67808999df06b48007ee9
andreaGhisa <aghisa@sdl.com>
2017-10-23 11:16:34 +0300

    Report Exporter batch task initial commit.

---
+++ Report Exporter/Sdl.Community.ReportExporter/ReportExporterControl.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Sdl.Desktop.IntegrationApi;
+
+namespace Sdl.Community.ReportExporter
+{
+	public partial class ReportExporterControl : UserControl, ISettingsAware<ReportExporterSettings>
+	{
+		public ReportExporterControl()
+		{
+			InitializeComponent();
+		}
+
+		public ReportExporterSettings Settings { get; set; }
+	}
+}
---
+++ Report Exporter/Sdl.Community.ReportExporter/ReportExporterTask.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sdl.ProjectAutomation.AutomaticTasks;
+
+namespace Sdl.Community.ReportExporter
+{
+	/// <summary>
+	/// Automatic task attribute
+	/// Parameters: task name, plugin name and plugin description
+	/// </summary>
+	[AutomaticTask("Export analysis report",
+		"Export analysis report",
+		"Exports a report  in CSV format",
+		GeneratedFileType = AutomaticTaskFileType.BilingualTarget)]
+	[AutomaticTaskSupportedFileType(AutomaticTaskFileType.BilingualTarget)]
+	[RequiresSettings(typeof(ReportExporterSettings), typeof(ReportExporterSettingsPage))]
+	public class ReportExporterTask
+	{
+	}
+}
