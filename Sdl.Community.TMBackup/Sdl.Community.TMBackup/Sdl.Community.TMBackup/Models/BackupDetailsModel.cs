1480ff591fd4c70f90729413e28a846171ac9ba9
TinaCaputa <caputa_florentina_lucia@yahoo.com>
2017-10-24 10:47:28 +0300

    TmBackup: Add models for each form

---
+++ Sdl.Community.TMBackup/Sdl.Community.TMBackup/Sdl.Community.TMBackup/Models/BackupDetailsModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Sdl.Community.TMBackup.Models
+{
+	public class BackupDetailsModel
+	{
+		public string BackupAction { get; set; }
+
+		public string BackupType { get; set; }
+
+		public string BackupPattern { get; set; }
+	}
+}
---
+++ Sdl.Community.TMBackup/Sdl.Community.TMBackup/Sdl.Community.TMBackup/Models/ChangeSettingsModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Sdl.Community.TMBackup.Models
+{
+	public class ChangeSettingsModel
+	{
+		public bool IsRealTimeOptionChecked { get; set; }
+
+		public bool IsPeriodicOptionChecked { get; set; }
+
+		public bool IsManuallyOptionChecked { get; set; }
+	}
+}
