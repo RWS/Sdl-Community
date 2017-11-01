8443a0915614735708580490cc8b8421e74822f2
andreaGhisa <aghisa@sdl.com>
2017-11-01 09:47:16 +0200

    Search comment text via regex.

--- AdvancedDisplayFilter/DisplayFilters/DisplayFilter.cs
+++ AdvancedDisplayFilter/DisplayFilters/DisplayFilter.cs
@@ -1,5 +1,6 @@
 ﻿using System.Linq;
 using Sdl.Community.Plugins.AdvancedDisplayFilter.Helpers;
+using Sdl.Community.Toolkit.FileType;
 using Sdl.Community.Toolkit.Integration;
 using Sdl.Community.Toolkit.Integration.DisplayFilter;
 using Sdl.TranslationStudioAutomation.IntegrationApi;
@@ -39,84 +40,94 @@ namespace Sdl.Community.Plugins.AdvancedDisplayFilter.DisplayFilters
         public bool EvaluateRow(DisplayFilterRowInfo rowInfo)
         {
             var success = !(!Settings.ShowAllContent && !rowInfo.IsSegment);
-                        
-            if (rowInfo.IsSegment)
-            {
-	           
-                if (success && Settings.SegmentReviewTypes != null && Settings.SegmentReviewTypes.Any())
-                    success = rowInfo.IsSegmentReviewTypes(Settings);
+
+	        if (rowInfo.IsSegment)
+	        {
+
+		        if (success && Settings.SegmentReviewTypes != null && Settings.SegmentReviewTypes.Any())
+			        success = rowInfo.IsSegmentReviewTypes(Settings);
 
 
-                if (success && Settings.ConfirmationLevels != null && Settings.ConfirmationLevels.Any())
-                    success = rowInfo.IsConfirmationLevelFound(Settings);
+		        if (success && Settings.ConfirmationLevels != null && Settings.ConfirmationLevels.Any())
+			        success = rowInfo.IsConfirmationLevelFound(Settings);
 
 
-                if (success && Settings.OriginTypes != null && Settings.OriginTypes.Any())
-                    success = rowInfo.IsOriginTypeFound(Settings);
+		        if (success && Settings.OriginTypes != null && Settings.OriginTypes.Any())
+			        success = rowInfo.IsOriginTypeFound(Settings);
 
 
-                if (success && Settings.PreviousOriginTypes != null && Settings.PreviousOriginTypes.Any())
-                    success = rowInfo.IsPreviousOriginTypeFound(Settings);
+		        if (success && Settings.PreviousOriginTypes != null && Settings.PreviousOriginTypes.Any())
+			        success = rowInfo.IsPreviousOriginTypeFound(Settings);
 
 
-                if (success && Settings.RepetitionTypes != null && Settings.RepetitionTypes.Any())
-                    success = rowInfo.IsRepetitionTypes(Settings);
+		        if (success && Settings.RepetitionTypes != null && Settings.RepetitionTypes.Any())
+			        success = rowInfo.IsRepetitionTypes(Settings);
 
 
-                if (success && Settings.SegmentLockingTypes != null && Settings.SegmentLockingTypes.Any())
-                    success = rowInfo.IsSegmentLockingTypes(Settings);
+		        if (success && Settings.SegmentLockingTypes != null && Settings.SegmentLockingTypes.Any())
+			        success = rowInfo.IsSegmentLockingTypes(Settings);
 
 
-                if (success && Settings.SegmentContentTypes != null && Settings.SegmentContentTypes.Any())
-                    success = rowInfo.IsSegmentContentTypes(Settings);
+		        if (success && Settings.SegmentContentTypes != null && Settings.SegmentContentTypes.Any())
+			        success = rowInfo.IsSegmentContentTypes(Settings);
 
 
-                if (success && Settings.SourceText.Trim() != string.Empty)
-                    success = rowInfo.IsTextFoundInSource(Settings);
+		        if (success && Settings.SourceText.Trim() != string.Empty)
+			        success = rowInfo.IsTextFoundInSource(Settings);
 
 
-                if (success && Settings.TargetText.Trim() != string.Empty)
-                    success = rowInfo.IsTextFoundInTarget(Settings);
+		        if (success && Settings.TargetText.Trim() != string.Empty)
+			        success = rowInfo.IsTextFoundInTarget(Settings);
 
 
-                if (success && Settings.CommentText.Trim() != string.Empty)
-                    success = rowInfo.IsTextFoundInComment(Settings);
+		        if (success && !CustomSettings.UseRegexCommentSearch && Settings.CommentText.Trim() != string.Empty)
+			        success = rowInfo.IsTextFoundInComment(Settings);
 
 
-                if (success && Settings.CommentAuthor.Trim() != string.Empty)
-                    success = rowInfo. IsAuthorFoundInComment(Settings);
+		        if (success && Settings.CommentAuthor.Trim() != string.Empty)
+			        success = rowInfo.IsAuthorFoundInComment(Settings);
 
 
-                if (success && Settings.CommentSeverity > 0)
-                    success = rowInfo.IsSeverityFoundInComment(Settings);
+		        if (success && Settings.CommentSeverity > 0)
+			        success = rowInfo.IsSeverityFoundInComment(Settings);
 
-                
-                if (success && Settings.ContextInfoTypes.Any())
-                    success = rowInfo.IsContextInfoTypes(Settings);
 
-				// check custom settings
-	            var rowId = rowInfo.SegmentPair.Properties.Id.Id;
-				if (success && CustomSettings.EvenNo)
-				{
-					success = SegmentNumbersHelper.IsEven(rowId);
-				}
-	            if (success && CustomSettings.OddsNo)
-	            {
-		            success = SegmentNumbersHelper.IsOdd(rowId);
-	            }
-	            if (success && CustomSettings.CommaSeparated &&
-	                !string.IsNullOrWhiteSpace(CustomSettings.CommaSeparatedVelues))
-	            {
-		            success = SegmentNumbersHelper.ContainsId(rowId,
-			            CustomSettings.CommaSeparatedVelues);
-	            }
-	            if (success && CustomSettings.Grouped && !string.IsNullOrWhiteSpace(CustomSettings.GroupedList))
-	            {
-		            success = SegmentNumbersHelper.IdInRange(rowId, CustomSettings.GroupedList);
-	            }
-			}
+		        if (success && Settings.ContextInfoTypes.Any())
+			        success = rowInfo.IsContextInfoTypes(Settings);
 
-			return success;
+		        // check custom settings
+		        var rowId = rowInfo.SegmentPair.Properties.Id.Id;
+		        if (success && CustomSettings.EvenNo)
+		        {
+			        success = SegmentNumbersHelper.IsEven(rowId);
+		        }
+		        if (success && CustomSettings.OddsNo)
+		        {
+			        success = SegmentNumbersHelper.IsOdd(rowId);
+		        }
+		        if (success && CustomSettings.CommaSeparated &&
+		            !string.IsNullOrWhiteSpace(CustomSettings.CommaSeparatedVelues))
+		        {
+			        success = SegmentNumbersHelper.ContainsId(rowId,
+				        CustomSettings.CommaSeparatedVelues);
+		        }
+		        if (success && CustomSettings.Grouped && !string.IsNullOrWhiteSpace(CustomSettings.GroupedList))
+		        {
+			        success = SegmentNumbersHelper.IdInRange(rowId, CustomSettings.GroupedList);
+		        }
+		        if (success && CustomSettings.UseRegexCommentSearch &&
+		            !string.IsNullOrWhiteSpace(CustomSettings.CommentRegex))
+		        {
+			        //create a list with source and target comments
+			        var commentsList = rowInfo.SegmentPair.Source.GetComments();
+			        commentsList.AddRange(rowInfo.SegmentPair.Target.GetComments());
+
+			        success = CommentsHelper.IsCommentTextFoundWithRegex(commentsList, CustomSettings.CommentRegex);
+		        }
+
+	        }
+
+	        return success;
         }
 
     }
