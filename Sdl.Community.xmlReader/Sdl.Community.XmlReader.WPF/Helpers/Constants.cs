fb8f28591169f6cce559ca2e9228059667f1dafd
andreaGhisa <aghisa@sdl.com>
2017-12-11 09:37:14 +0200

    Load project api dll from studio location.

---
+++ Sdl.Community.xmlReader/Sdl.Community.XmlReader.WPF/Helpers/Constants.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sdl.Community.XmlReader.WPF.Helpers
+{
+	public static class Constants
+	{
+		public static  string StudioLocation => ExecutingStudioLocation();
+		public static string ProjectApiDll = "Sdl.ProjectApi.Reporting.dll";
+
+		private static string ExecutingStudioLocation()
+		{
+			var entryAssembly = Assembly.GetEntryAssembly().Location;
+			var location = entryAssembly.Substring(0, entryAssembly.LastIndexOf(@"\", StringComparison.Ordinal));
+
+			return location;
+		}
+	}
+}
--- Sdl.Community.xmlReader/Sdl.Community.XmlReader.WPF/Helpers/Report.cs
+++ Sdl.Community.xmlReader/Sdl.Community.XmlReader.WPF/Helpers/Report.cs
@@ -14,24 +14,10 @@ namespace Sdl.Community.XmlReader.WPF.Helpers
 	public  static class Report
 	{
 		private  static Assembly _reportAssembly;
+
 		public static void GetReportDefinition()
 		{
-			var reportAssembly = Assembly.GetExecutingAssembly()
-				.GetManifestResourceNames().FirstOrDefault(s => s.EndsWith("Sdl.Community.XmlReader.WPF._3rd_party.Sdl.ProjectApi.Reporting.dll"));
-
-			using (var stream = Assembly.GetExecutingAssembly().GetManifestResourceStream(reportAssembly))
-			{
-				var block = new byte[stream.Length];
-				try
-				{
-					stream.Read(block, 0, block.Length);
-
-					_reportAssembly = Assembly.Load(block);
-				}
-				catch (Exception e)
-				{
-				}
-			}
+			_reportAssembly = Assembly.LoadFrom(Path.Combine(Constants.StudioLocation,Constants.ProjectApiDll));
 
 			var typeReport = _reportAssembly.GetType(
 				"Sdl.ProjectApi.Reporting.ReportDefinition");
@@ -43,7 +29,7 @@ namespace Sdl.Community.XmlReader.WPF.Helpers
 
 			if (reportConstructor != null)
 			{
-				object reportInstance = reportConstructor.Invoke(new object[] { });
+				var reportInstance = reportConstructor.Invoke(new object[] { });
 			}
 	
 		}
