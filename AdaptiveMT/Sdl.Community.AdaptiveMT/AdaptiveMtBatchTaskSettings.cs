16cbe6a9bda283249e84e5a4923c3c6f5f664275
andreaGhisa <aghisa@sdl.com>
2017-12-20 09:22:57 +0200

    Adaptive MT Training initial commit

---
+++ AdaptiveMT/Sdl.Community.AdaptiveMT/AdaptiveMtBatchTaskSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sdl.Core.Settings;
+
+namespace Sdl.Community.AdaptiveMT
+{
+	public class AdaptiveMtBatchTaskSettings:SettingsGroup
+	{
+	}
+}
---
+++ AdaptiveMT/Sdl.Community.AdaptiveMT/AdaptiveMtBatchTaskSettingsControl.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Sdl.Desktop.IntegrationApi;
+
+namespace Sdl.Community.AdaptiveMT
+{
+	public partial class AdaptiveMtBatchTaskSettingsControl : UserControl,ISettingsAware<AdaptiveMtBatchTaskSettings>
+	{
+		public AdaptiveMtBatchTaskSettingsControl()
+		{
+			InitializeComponent();
+		}
+
+		public AdaptiveMtBatchTaskSettings Settings { get; set; }
+	}
+}
