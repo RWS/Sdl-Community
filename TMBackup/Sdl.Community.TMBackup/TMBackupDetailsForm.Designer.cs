bb306e6ad99713cd3b6bde0fefaec999273d3248
TinaCaputa <caputa_florentina_lucia@yahoo.com>
2018-01-04 09:48:00 +0200

    Update TMBackupDetails form

--- TMBackup/Sdl.Community.TMBackup/TMBackupDetailsForm.Designer.cs
+++ TMBackup/Sdl.Community.TMBackup/TMBackupDetailsForm.Designer.cs
@@ -48,10 +48,9 @@
             // btn_Ok
             // 
             this.btn_Ok.Anchor = System.Windows.Forms.AnchorStyles.Right;
-            this.btn_Ok.Location = new System.Drawing.Point(492, 199);
-            this.btn_Ok.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_Ok.Location = new System.Drawing.Point(334, 152);
             this.btn_Ok.Name = "btn_Ok";
-            this.btn_Ok.Size = new System.Drawing.Size(100, 28);
+            this.btn_Ok.Size = new System.Drawing.Size(75, 23);
             this.btn_Ok.TabIndex = 7;
             this.btn_Ok.Text = "Ok";
             this.btn_Ok.UseVisualStyleBackColor = true;
@@ -60,10 +59,9 @@
             // btn_Cancel
             // 
             this.btn_Cancel.Anchor = System.Windows.Forms.AnchorStyles.Right;
-            this.btn_Cancel.Location = new System.Drawing.Point(612, 199);
-            this.btn_Cancel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_Cancel.Location = new System.Drawing.Point(425, 152);
             this.btn_Cancel.Name = "btn_Cancel";
-            this.btn_Cancel.Size = new System.Drawing.Size(100, 28);
+            this.btn_Cancel.Size = new System.Drawing.Size(75, 23);
             this.btn_Cancel.TabIndex = 8;
             this.btn_Cancel.Text = "Cancel";
             this.btn_Cancel.UseVisualStyleBackColor = true;
@@ -73,20 +71,18 @@
             // 
             this.lbl_RulesDescription.AutoSize = true;
             this.lbl_RulesDescription.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.lbl_RulesDescription.Location = new System.Drawing.Point(23, 149);
-            this.lbl_RulesDescription.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_RulesDescription.Location = new System.Drawing.Point(17, 121);
             this.lbl_RulesDescription.Name = "lbl_RulesDescription";
-            this.lbl_RulesDescription.Size = new System.Drawing.Size(379, 17);
+            this.lbl_RulesDescription.Size = new System.Drawing.Size(281, 13);
             this.lbl_RulesDescription.TabIndex = 10;
             this.lbl_RulesDescription.Text = "Rules are scanned from the top. First matching rule is final.";
             // 
             // btn_Add
             // 
             this.btn_Add.Anchor = System.Windows.Forms.AnchorStyles.Right;
-            this.btn_Add.Location = new System.Drawing.Point(612, 16);
-            this.btn_Add.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_Add.Location = new System.Drawing.Point(425, 13);
             this.btn_Add.Name = "btn_Add";
-            this.btn_Add.Size = new System.Drawing.Size(100, 28);
+            this.btn_Add.Size = new System.Drawing.Size(75, 23);
             this.btn_Add.TabIndex = 11;
             this.btn_Add.Text = "Add...";
             this.btn_Add.UseVisualStyleBackColor = true;
@@ -95,10 +91,9 @@
             // btn_Delete
             // 
             this.btn_Delete.Anchor = System.Windows.Forms.AnchorStyles.Right;
-            this.btn_Delete.Location = new System.Drawing.Point(612, 52);
-            this.btn_Delete.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_Delete.Location = new System.Drawing.Point(425, 42);
             this.btn_Delete.Name = "btn_Delete";
-            this.btn_Delete.Size = new System.Drawing.Size(100, 28);
+            this.btn_Delete.Size = new System.Drawing.Size(75, 23);
             this.btn_Delete.TabIndex = 12;
             this.btn_Delete.Text = "Delete";
             this.btn_Delete.UseVisualStyleBackColor = true;
@@ -109,10 +104,9 @@
             this.lbl_Line.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.lbl_Line.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.lbl_Line.Location = new System.Drawing.Point(-7, 181);
-            this.lbl_Line.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_Line.Location = new System.Drawing.Point(-5, 147);
             this.lbl_Line.Name = "lbl_Line";
-            this.lbl_Line.Size = new System.Drawing.Size(756, 2);
+            this.lbl_Line.Size = new System.Drawing.Size(535, 2);
             this.lbl_Line.TabIndex = 16;
             // 
             // backupDetailsModelBindingSource
@@ -133,10 +127,9 @@
             this.backupTypeDataGridViewTextBoxColumn,
             this.backupPatternDataGridViewTextBoxColumn});
             this.dataGridView1.DataSource = this.backupDetailsModelBindingSource;
-            this.dataGridView1.Location = new System.Drawing.Point(13, 16);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.dataGridView1.Location = new System.Drawing.Point(10, 13);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(591, 119);
+            this.dataGridView1.Size = new System.Drawing.Size(409, 84);
             this.dataGridView1.TabIndex = 17;
             this.dataGridView1.CellValidating += new System.Windows.Forms.DataGridViewCellValidatingEventHandler(this.dataGridView1_CellValidating_1);
             this.dataGridView1.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellValueChanged);
@@ -144,33 +137,32 @@
             // 
             // backupActionDataGridViewTextBoxColumn
             // 
-            this.backupActionDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.backupActionDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
             this.backupActionDataGridViewTextBoxColumn.DataPropertyName = "BackupAction";
             this.backupActionDataGridViewTextBoxColumn.HeaderText = "Action";
             this.backupActionDataGridViewTextBoxColumn.Name = "backupActionDataGridViewTextBoxColumn";
-            this.backupActionDataGridViewTextBoxColumn.Width = 210;
+            this.backupActionDataGridViewTextBoxColumn.Width = 62;
             // 
             // backupTypeDataGridViewTextBoxColumn
             // 
-            this.backupTypeDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.backupTypeDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.DisplayedCells;
             this.backupTypeDataGridViewTextBoxColumn.DataPropertyName = "BackupType";
             this.backupTypeDataGridViewTextBoxColumn.HeaderText = "Type";
             this.backupTypeDataGridViewTextBoxColumn.Name = "backupTypeDataGridViewTextBoxColumn";
-            this.backupTypeDataGridViewTextBoxColumn.Width = 190;
+            this.backupTypeDataGridViewTextBoxColumn.Width = 56;
             // 
             // backupPatternDataGridViewTextBoxColumn
             // 
-            this.backupPatternDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.None;
+            this.backupPatternDataGridViewTextBoxColumn.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
             this.backupPatternDataGridViewTextBoxColumn.DataPropertyName = "BackupPattern";
             this.backupPatternDataGridViewTextBoxColumn.HeaderText = "Pattern";
             this.backupPatternDataGridViewTextBoxColumn.Name = "backupPatternDataGridViewTextBoxColumn";
-            this.backupPatternDataGridViewTextBoxColumn.Width = 150;
             // 
             // TMBackupDetailsForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(725, 239);
+            this.ClientSize = new System.Drawing.Size(512, 182);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.lbl_Line);
             this.Controls.Add(this.lbl_RulesDescription);
@@ -179,8 +171,7 @@
             this.Controls.Add(this.btn_Cancel);
             this.Controls.Add(this.btn_Ok);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.MaximumSize = new System.Drawing.Size(743, 301);
+            this.MaximumSize = new System.Drawing.Size(561, 252);
             this.Name = "TMBackupDetailsForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "What to backup";
