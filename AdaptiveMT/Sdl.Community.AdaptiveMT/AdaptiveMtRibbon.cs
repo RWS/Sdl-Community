1ad601ff7b95c5dd7e11c1b44246dbc1ca864ccb
andreaGhisa <aghisa@sdl.com>
2018-01-15 13:43:08 +0200

    Added 4 seconds wait after confirming each segment

--- AdaptiveMT/Sdl.Community.AdaptiveMT/AdaptiveMtRibbon.cs
+++ AdaptiveMT/Sdl.Community.AdaptiveMT/AdaptiveMtRibbon.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Web;
 using Sdl.Community.AdaptiveMT.Service;
@@ -107,6 +108,8 @@ namespace Sdl.Community.AdaptiveMT
 										}
 
 									}
+
+									Thread.Sleep(4000);
 								}
 							}
 							project.Save();
