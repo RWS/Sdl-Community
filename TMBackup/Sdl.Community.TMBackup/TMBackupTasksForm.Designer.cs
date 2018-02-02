647500aa2a601e92deb37e4e8ea1af24c3f3ba4e
TinaCaputa <caputa_florentina_lucia@yahoo.com>
2018-02-02 13:50:48 +0200

    TmBackup: UI updates

--- TMBackup/Sdl.Community.TMBackup/TMBackupTasksForm.Designer.cs
+++ TMBackup/Sdl.Community.TMBackup/TMBackupTasksForm.Designer.cs
@@ -35,18 +35,18 @@
             this.createNewBackupAction = new System.Windows.Forms.ToolStripMenuItem();
             this.readMeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.btn_Refresh = new System.Windows.Forms.Button();
-            this.btn_RunTasks = new System.Windows.Forms.Button();
-            this.toolTip_RunDisableTasks = new System.Windows.Forms.ToolTip(this.components);
-            this.btn_RunManuallyTasks = new System.Windows.Forms.Button();
             this.TaskNameCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.TaskRunType = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.Status = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.LastRunCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.NextRunCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.IntervalCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.deleteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.btn_Refresh = new System.Windows.Forms.Button();
+            this.btn_RunTasks = new System.Windows.Forms.Button();
+            this.toolTip_RunDisableTasks = new System.Windows.Forms.ToolTip(this.components);
+            this.btn_RunManuallyTasks = new System.Windows.Forms.Button();
             this.menuStrip1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.contextMenuStrip1.SuspendLayout();
@@ -59,7 +59,7 @@
             this.readMeToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(819, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(887, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -100,59 +100,10 @@
             this.IntervalCol});
             this.dataGridView1.Location = new System.Drawing.Point(2, 27);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(814, 349);
+            this.dataGridView1.Size = new System.Drawing.Size(885, 372);
             this.dataGridView1.TabIndex = 1;
             this.dataGridView1.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellDoubleClick);
             this.dataGridView1.CellMouseUp += new System.Windows.Forms.DataGridViewCellMouseEventHandler(this.dataGridView1_CellMouseUp);
-            // 
-            // contextMenuStrip1
-            // 
-            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.deleteToolStripMenuItem});
-            this.contextMenuStrip1.Name = "contextMenuStrip1";
-            this.contextMenuStrip1.Size = new System.Drawing.Size(108, 26);
-            // 
-            // deleteToolStripMenuItem
-            // 
-            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
-            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
-            this.deleteToolStripMenuItem.Text = "Delete";
-            this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click);
-            // 
-            // btn_Refresh
-            // 
-            this.btn_Refresh.BackColor = System.Drawing.SystemColors.Control;
-            this.btn_Refresh.Location = new System.Drawing.Point(756, 2);
-            this.btn_Refresh.Name = "btn_Refresh";
-            this.btn_Refresh.Size = new System.Drawing.Size(60, 22);
-            this.btn_Refresh.TabIndex = 2;
-            this.btn_Refresh.Text = "Refresh";
-            this.btn_Refresh.UseVisualStyleBackColor = false;
-            this.btn_Refresh.Click += new System.EventHandler(this.btn_Refresh_Click);
-            // 
-            // btn_RunTasks
-            // 
-            this.btn_RunTasks.BackColor = System.Drawing.SystemColors.Control;
-            this.btn_RunTasks.Image = ((System.Drawing.Image)(resources.GetObject("btn_RunTasks.Image")));
-            this.btn_RunTasks.Location = new System.Drawing.Point(717, 0);
-            this.btn_RunTasks.Name = "btn_RunTasks";
-            this.btn_RunTasks.Size = new System.Drawing.Size(33, 27);
-            this.btn_RunTasks.TabIndex = 3;
-            this.toolTip_RunDisableTasks.SetToolTip(this.btn_RunTasks, "Run all tasks");
-            this.btn_RunTasks.UseVisualStyleBackColor = false;
-            this.btn_RunTasks.Click += new System.EventHandler(this.btn_RunTasks_Click);
-            // 
-            // btn_RunManuallyTasks
-            // 
-            this.btn_RunManuallyTasks.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.btn_RunManuallyTasks.Image = ((System.Drawing.Image)(resources.GetObject("btn_RunManuallyTasks.Image")));
-            this.btn_RunManuallyTasks.Location = new System.Drawing.Point(678, 0);
-            this.btn_RunManuallyTasks.Name = "btn_RunManuallyTasks";
-            this.btn_RunManuallyTasks.Size = new System.Drawing.Size(33, 27);
-            this.btn_RunManuallyTasks.TabIndex = 4;
-            this.toolTip_RunDisableTasks.SetToolTip(this.btn_RunManuallyTasks, "Start tasks manually");
-            this.btn_RunManuallyTasks.UseVisualStyleBackColor = false;
-            this.btn_RunManuallyTasks.Click += new System.EventHandler(this.btn_RunManuallyTasks_Click);
             // 
             // TaskNameCol
             // 
@@ -205,11 +156,60 @@
             this.IntervalCol.Name = "IntervalCol";
             this.IntervalCol.ReadOnly = true;
             // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.deleteToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(108, 26);
+            // 
+            // deleteToolStripMenuItem
+            // 
+            this.deleteToolStripMenuItem.Name = "deleteToolStripMenuItem";
+            this.deleteToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.deleteToolStripMenuItem.Text = "Delete";
+            this.deleteToolStripMenuItem.Click += new System.EventHandler(this.deleteToolStripMenuItem_Click);
+            // 
+            // btn_Refresh
+            // 
+            this.btn_Refresh.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_Refresh.Location = new System.Drawing.Point(815, 1);
+            this.btn_Refresh.Name = "btn_Refresh";
+            this.btn_Refresh.Size = new System.Drawing.Size(60, 25);
+            this.btn_Refresh.TabIndex = 2;
+            this.btn_Refresh.Text = "Refresh";
+            this.btn_Refresh.UseVisualStyleBackColor = false;
+            this.btn_Refresh.Click += new System.EventHandler(this.btn_Refresh_Click);
+            // 
+            // btn_RunTasks
+            // 
+            this.btn_RunTasks.BackColor = System.Drawing.SystemColors.Control;
+            this.btn_RunTasks.Image = ((System.Drawing.Image)(resources.GetObject("btn_RunTasks.Image")));
+            this.btn_RunTasks.Location = new System.Drawing.Point(772, 0);
+            this.btn_RunTasks.Name = "btn_RunTasks";
+            this.btn_RunTasks.Size = new System.Drawing.Size(30, 27);
+            this.btn_RunTasks.TabIndex = 3;
+            this.toolTip_RunDisableTasks.SetToolTip(this.btn_RunTasks, "Run all tasks");
+            this.btn_RunTasks.UseVisualStyleBackColor = false;
+            this.btn_RunTasks.Click += new System.EventHandler(this.btn_RunTasks_Click);
+            // 
+            // btn_RunManuallyTasks
+            // 
+            this.btn_RunManuallyTasks.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btn_RunManuallyTasks.Image = ((System.Drawing.Image)(resources.GetObject("btn_RunManuallyTasks.Image")));
+            this.btn_RunManuallyTasks.Location = new System.Drawing.Point(732, 0);
+            this.btn_RunManuallyTasks.Name = "btn_RunManuallyTasks";
+            this.btn_RunManuallyTasks.Size = new System.Drawing.Size(30, 27);
+            this.btn_RunManuallyTasks.TabIndex = 4;
+            this.toolTip_RunDisableTasks.SetToolTip(this.btn_RunManuallyTasks, "Start tasks manually");
+            this.btn_RunManuallyTasks.UseVisualStyleBackColor = false;
+            this.btn_RunManuallyTasks.Click += new System.EventHandler(this.btn_RunManuallyTasks_Click);
+            // 
             // TMBackupTasksForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(819, 377);
+            this.ClientSize = new System.Drawing.Size(887, 400);
             this.Controls.Add(this.btn_RunManuallyTasks);
             this.Controls.Add(this.btn_RunTasks);
             this.Controls.Add(this.btn_Refresh);
