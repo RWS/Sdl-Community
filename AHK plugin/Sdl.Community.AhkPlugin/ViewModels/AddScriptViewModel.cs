71580bedd9de4a76f94f2a00a0369d6a25313bbe
andreaGhisa <aghisa@sdl.com>
2018-02-27 14:38:52 +0200

    Add script window fields.

--- AHK plugin/Sdl.Community.AhkPlugin/ViewModels/AddScriptViewModel.cs
+++ AHK plugin/Sdl.Community.AhkPlugin/ViewModels/AddScriptViewModel.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using Sdl.Community.AhkPlugin.Helpers;
 using Sdl.Community.AhkPlugin.Ui;
 
 namespace Sdl.Community.AhkPlugin.ViewModels
@@ -10,9 +12,17 @@ namespace Sdl.Community.AhkPlugin.ViewModels
     public class AddScriptViewModel: ViewModelBase
     {
 	    private readonly MainWindowViewModel _mainWindowViewModel;
+	    private ICommand _backCommand;
 		public AddScriptViewModel(MainWindowViewModel mainWindowViewModel)
 		{
 			_mainWindowViewModel = mainWindowViewModel;
 		}
-    }
+
+	    public ICommand BackCommand => _backCommand ?? (_backCommand = new CommandHandler(BackToScriptsList, true));
+
+	    private void BackToScriptsList()
+	    {
+		    _mainWindowViewModel.LoadScriptsPage();
+	    }
+	}
 }
