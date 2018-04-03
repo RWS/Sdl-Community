6f526c02ef780f1485a675a106c29df7b7b80c31
andreaGhisa <aghisa@sdl.com>
2018-04-03 09:50:42 +0300

    Added right click actions.

--- GDPR/GDPR/GdprTask.cs
+++ GDPR/GDPR/GdprTask.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using Sdl.Desktop.IntegrationApi;
+using Sdl.Desktop.IntegrationApi.Extensions;
 using Sdl.FileTypeSupport.Framework.IntegrationApi;
 using Sdl.ProjectAutomation.AutomaticTasks;
 using Sdl.ProjectAutomation.Core;
+using Sdl.TranslationStudioAutomation.IntegrationApi.Presentation.DefaultLocations;
 
 namespace Sdl.Community.GDPR
 {
@@ -20,6 +23,35 @@ namespace Sdl.Community.GDPR
 		protected override void ConfigureConverter(ProjectFile projectFile, IMultiFileConverter multiFileConverter)
 		{
 			//In here you should add your custom bilingual processor to the file converter
+		}
+	}
+
+	[Action("GDPR Action",
+		Name = "Decrypt data",
+		Description = "Deanonymize data which was previously anonymize by the batch task",
+		Icon = "unlock"
+	)]
+	[ActionLayout(typeof(TranslationStudioDefaultContextMenus.ProjectsContextMenuLocation), 2, DisplayType.Default, "",
+		true)]
+	public class GdprDeanonymizeAction : AbstractAction
+	{
+		protected override void Execute()
+		{
+			
+		}
+	}
+	[Action("Help GDPR Action",
+		Name = "Help",
+		Description = "Help",
+		Icon = "question"
+	)]
+	[ActionLayout(typeof(TranslationStudioDefaultContextMenus.ProjectsContextMenuLocation), 2, DisplayType.Default, "",
+		true)]
+	public class GdprHelpAction : AbstractAction
+	{
+		protected override void Execute()
+		{
+			System.Diagnostics.Process.Start("https://community.sdl.com/product-groups/translationproductivity/w/customer-experience/3199.gdpr");
 		}
 	}
 }
