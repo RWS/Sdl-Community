e8229bbfc238e80033b354963e85ab8f64e3c1f7
andreaGhisa <aghisa@sdl.com>
2018-04-11 11:41:58 +0300

    Anonymize tag text content.

--- Anonymizer/Anonymizer/Process Xliff/SegmentVisitor.cs
+++ Anonymizer/Anonymizer/Process Xliff/SegmentVisitor.cs
@@ -4,7 +4,7 @@ using Sdl.FileTypeSupport.Framework.BilingualApi;
 
 namespace Sdl.Community.Anonymizer.Process_Xliff
 {
-	public class SegmentVisitor: AbstractBilingualContentProcessor,IMarkupDataVisitor
+	public class SegmentVisitor: IMarkupDataVisitor
 	{
 		private IDocumentItemFactory _factory;
 		//	private List<string> _patterns = new List<string>{ @"([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)", @"\b(?:\d[ -]*?){13,16}\b" };
@@ -15,13 +15,18 @@ namespace Sdl.Community.Anonymizer.Process_Xliff
 			VisitChildren(segment);
 		}
 
-		private void CheckForPersonalData(string text)
+		private string CheckForPersonalData(string text)
 		{
 			foreach (var pattern in _patterns)
 			{
 				var regex = new Regex(pattern,RegexOptions.IgnoreCase);
 				var result = regex.Replace(text, new MatchEvaluator(Anonymize));
+				if (!string.IsNullOrWhiteSpace(result))
+				{
+					return result;
+				}
 			}
+			return string.Empty;
 		}
 		private string Anonymize(Match m)
 		{
@@ -32,9 +37,10 @@ namespace Sdl.Community.Anonymizer.Process_Xliff
 		{
 			if (tagPair.StartTagProperties != null)
 			{
-				CheckForPersonalData(tagPair.StartTagProperties.TagContent);
-				
+				var anonymizedText=CheckForPersonalData(tagPair.StartTagProperties.TagContent);
+				tagPair.StartTagProperties.TagContent = anonymizedText;
 			}
+			VisitChildren(tagPair);
 		}
 
 		public void VisitPlaceholderTag(IPlaceholderTag tag)
@@ -45,7 +51,8 @@ namespace Sdl.Community.Anonymizer.Process_Xliff
 		public void VisitText(IText text)
 		{
 			//text.Properties.Text = "test";
-			CheckForPersonalData(text.Properties.Text);
+			var anonymizedText =CheckForPersonalData(text.Properties.Text);
+			text.Properties.Text = anonymizedText;
 		}
 
 		public void VisitSegment(ISegment segment)
