ddb7e8d1fbb321ac17752bcf79b125a4e8e65c92
andreaGhisa <aghisa@sdl.com>
2018-04-18 09:28:40 +0300

    Generate grid columns.

--- Anonymizer/Anonymizer/Ui/ExpressionsControl.cs
+++ Anonymizer/Anonymizer/Ui/ExpressionsControl.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Forms;
+﻿using System.ComponentModel;
+using System.Windows.Forms;
+using Sdl.Community.Anonymizer.Models;
 
 namespace Sdl.Community.Anonymizer.Ui
 {
@@ -7,8 +9,59 @@ namespace Sdl.Community.Anonymizer.Ui
 		public ExpressionsControl()
 		{
 			InitializeComponent();
+			expressionsDataGrid.AutoGenerateColumns = false;
+
+			//TO DO
+			//read information from JSON 
+			var expressions = new BindingList<RegexPattern>
+		{
+			new RegexPattern
+			{
+				Description = "bla bla",
+				Pattern = "daasda"
+			},
+
+			new RegexPattern
+			{
+				Description = "bla bla",
+				Pattern = "daasda"
+			},
+			new RegexPattern
+			{
+				Description = "bla bla",
+				Pattern = "daasda"
+			}
+		};
+			expressionsDataGrid.DataSource = expressions;
+
+			//genteate columns
+		
+			
+			var exportColumn = new DataGridViewCheckBoxColumn
+			{
+				HeaderText = @"Export?"
+			};
+			expressionsDataGrid.Columns.Add(exportColumn);
+			var pattern = new DataGridViewTextBoxColumn
+			{
+				HeaderText = @"Regex Pattern",
+				DataPropertyName = "Pattern"
+			};
+			expressionsDataGrid.Columns.Add(pattern);
+			var description = new DataGridViewTextBoxColumn
+			{
+				HeaderText = @"Description",
+				DataPropertyName = "Description"
+			};
+			expressionsDataGrid.Columns.Add(description);
+			var shouldEncryptColumn = new DataGridViewCheckBoxColumn
+			{
+				HeaderText = @"Encrypt?",
+				//Width = 60
+			};
+			expressionsDataGrid.Columns.Add(shouldEncryptColumn);
 		}
 
-		
+
 	}
 }
