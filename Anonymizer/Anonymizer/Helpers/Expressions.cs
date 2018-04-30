8f0a16080b6f3583de8aa89b3fa8f1306b203420
andreaGhisa <aghisa@sdl.com>
2018-04-30 15:16:14 +0300

    Import expressions from excel (see Paul's example).

--- Anonymizer/Anonymizer/Helpers/Expressions.cs
+++ Anonymizer/Anonymizer/Helpers/Expressions.cs
@@ -1,5 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Spreadsheet;
 using Newtonsoft.Json;
 using Sdl.Community.projectAnonymizer.Models;
 
@@ -21,10 +25,48 @@ namespace Sdl.Community.projectAnonymizer.Helpers
 			var patterns = new List<RegexPattern>();
 			foreach (var file in files)
 			{
-				var expressions = JsonConvert.DeserializeObject<List<RegexPattern>>(File.ReadAllText(file));
-				patterns.AddRange(expressions);
-			}
+				using (var fileStream = new FileStream(file, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+				{
+					using (var document = SpreadsheetDocument.Open(fileStream, false))
+					{
+						var workbookPart = document.WorkbookPart;
+						var tablePart = workbookPart.GetPartsOfType<SharedStringTablePart>().First();
+						var stringTable = tablePart.SharedStringTable;
+						//need to loop maybe we'll have more sheets
+						var worksheetPart = workbookPart.WorksheetParts.First();
+						var sheet = worksheetPart.Worksheet;
 
+						var cells = sheet.Descendants<Cell>().ToList();
+
+						for (var i = 0; i < cells.Count; i = i + 2)
+						{
+							var pattern = new RegexPattern
+							{
+								Id = Guid.NewGuid().ToString(),
+								ShouldEnable = true
+							};
+							var items = cells.Skip(i).Take(2);
+							foreach (var cell in items)
+							{
+								if (cell.DataType != null && cell.DataType == CellValues.SharedString)
+								{
+									var celId = int.Parse(cell.CellValue.Text);
+									var cellValue = stringTable.ChildElements[celId].InnerText;
+									if (celId % 2 == 0)
+									{
+										pattern.Pattern = cellValue;
+									}
+									else
+									{
+										pattern.Description = cellValue;
+									}
+								}
+							}
+							patterns.Add(pattern);
+						}
+					}
+				}
+			}
 			return patterns;
 		}
 	}
