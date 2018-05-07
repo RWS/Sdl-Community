3ebe2aee229b142d134b9d05002b01d3777cfa0e
andreaGhisa <aghisa@sdl.com>
2018-05-07 15:13:00 +0300

    Added commands.

--- TmAnonymizer/Sdl.Community.TmAnonymizer/ViewModel/TranslationMemoryViewModel.cs
+++ TmAnonymizer/Sdl.Community.TmAnonymizer/ViewModel/TranslationMemoryViewModel.cs
@@ -11,13 +11,26 @@ namespace Sdl.Community.TmAnonymizer.ViewModel
 	public class TranslationMemoryViewModel
 	{
 		private ICommand _selectFoldersCommand;
+		private ICommand _removeCommand;
+		private ICommand _selectTmCommand;
 
 		public ICommand SelectFoldersCommand => _selectFoldersCommand ??
 		                                        (_selectFoldersCommand = new CommandHandler(SelectFolder, true));
+		public ICommand RemoveCommand => _removeCommand ??
+		                                        (_removeCommand = new CommandHandler(Remove, true));
+		public ICommand SelectTmCommand => _selectTmCommand ??
+		                                 (_selectTmCommand = new CommandHandler(SelectTm, true));
+
+		private void SelectTm()
+		{
+		}
+
+		private void Remove()
+		{
+		}
 
 		private void SelectFolder()
 		{
-			
 		}
 	}
 }
