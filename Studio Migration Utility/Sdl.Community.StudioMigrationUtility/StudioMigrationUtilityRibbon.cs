ee122ea6ef01da0a81300a4e16be57abb98ad4e3
andreaGhisa <aghisa@sdl.com>
2018-07-03 15:47:28 +0300

    Removed Sdl.Community.StudioMigrationUtility.Integration project. Transformed  Sdl.Community.StudioMigrationUtility into an sdl plugin.

---
+++ Studio Migration Utility/Sdl.Community.StudioMigrationUtility/StudioMigrationUtilityRibbon.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sdl.Community.StudioMigrationUtility.Services;
+using Sdl.Desktop.IntegrationApi;
+using Sdl.Desktop.IntegrationApi.DefaultLocations;
+using Sdl.Desktop.IntegrationApi.Extensions;
+
+namespace Sdl.Community.StudioMigrationUtility
+{
+	[RibbonGroup("Sdl.Community.StudioMigrationUtility", Name = "Studio Migration Utility")]
+	[RibbonGroupLayout(LocationByType = typeof(StudioDefaultRibbonTabs.AddinsRibbonTabLocation))]
+	class StudioMigrationUtilityRibbon : AbstractRibbonGroup
+	{
+	}
+
+
+	[Action("Sdl.Community.StudioMigrationUtility", Name = "Studio Migration Utility", Icon = "migrate", Description = "Studio Migration Utility")]
+	[ActionLayout(typeof(StudioMigrationUtilityRibbon), 20, DisplayType.Large)]
+	class StudioMigrationUtilityViewPartAction : AbstractAction
+	{
+
+		protected override void Execute()
+		{
+
+			var migrateUtility = new MigrateUtility(new StudioVersionService());
+			migrateUtility.ShowDialog();
+
+		}
+	}
+}
