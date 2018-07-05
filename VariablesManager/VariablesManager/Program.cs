8e9767e8ff0eb6417959ce04eb3728bdb689150f
andreaGhisa <aghisa@sdl.com>
2018-07-05 14:24:54 +0300

    Transformed VariablesManager project into a plugin.

--- VariablesManager/VariablesManager/Program.cs
+++ VariablesManager/VariablesManager/Program.cs
@@ -1,21 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Windows.Forms;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Windows.Forms;
 
-namespace VariablesManager
-{
-    static class Program
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
-        {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
-        }
-    }
-}
+//namespace VariablesManager
+//{
+//    static class Program
+//    {
+//        /// <summary>
+//        /// The main entry point for the application.
+//        /// </summary>
+//        [STAThread]
+//        static void Main()
+//        {
+//            Application.EnableVisualStyles();
+//            Application.SetCompatibleTextRenderingDefault(false);
+//            Application.Run(new Form1());
+//        }
+//    }
+//}
---
+++ VariablesManager/VariablesManager/VariablesManagerRibbon.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sdl.Desktop.IntegrationApi;
+using Sdl.Desktop.IntegrationApi.DefaultLocations;
+using Sdl.Desktop.IntegrationApi.Extensions;
+
+namespace VariablesManager
+{
+	[RibbonGroup("Sdl.Community.VariablesManager", Name = "Variables Manager")]
+	[RibbonGroupLayout(LocationByType = typeof(StudioDefaultRibbonTabs.AddinsRibbonTabLocation))]
+	class VariablesManagerRibbon : AbstractRibbonGroup
+	{
+	}
+
+	[Action("Sdl.Community.VariablesManager", Name = "Variables Manager", Icon = "variables_manager___128", Description = "Variables Manager")]
+	[ActionLayout(typeof(VariablesManagerRibbon), 20, DisplayType.Large)]
+	class VariablesManagerViewPartAction : AbstractAction
+	{
+		protected override void Execute()
+		{
+			var main = new Form1();
+			main.ShowDialog();
+		}
+	}
+}
