b1f6a629a18ff9decba9ac8ae53a861ce5f82ca5
andreaGhisa <aghisa@sdl.com>
2018-07-06 10:01:12 +0300

    Transformed Sdl.Community.BetaAPIs.U into plugin project.

---
+++ Studio Beta APIs/SDL.Community.BetaAPIs/Sdl.Community.BetaAPIs.UI/BetaApisRibbon.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sdl.Desktop.IntegrationApi;
+using Sdl.Desktop.IntegrationApi.DefaultLocations;
+using  Sdl.Desktop.IntegrationApi.Extensions;
+
+namespace Sdl.Community.BetaAPIs.UI
+{
+	[RibbonGroup("Sdl.Community.StudioBetaAPIs", Name = "Studio Beta APIs")]
+	[RibbonGroupLayout(LocationByType = typeof(StudioDefaultRibbonTabs.AddinsRibbonTabLocation))]
+	public class BetaApisRibbon: AbstractRibbonGroup
+	{
+	}
+	[Action("Sdl.Community.StudioBetaAPIs", Icon = "", Name = "Available Beta APIs", Description = "Enable or disable Studio Beta APIs")]
+	[ActionLayout(typeof(BetaApisRibbon), 20, DisplayType.Normal)]
+	public class BetaApiViewPartAction : AbstractAction
+	{
+		protected override void Execute()
+		{
+			var window = new StudioBetaAPIsMainWindow();
+			window.ShowDialog();
+			
+		}
+	}
+}
