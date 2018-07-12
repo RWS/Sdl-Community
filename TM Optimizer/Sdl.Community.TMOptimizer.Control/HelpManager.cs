405680631f7142cbdf2b9640c72d9a5909c2777f
andreaGhisa <aghisa@sdl.com>
2018-07-12 16:03:36 +0300

    TM Optimizer updated plugin framework.

--- TM Optimizer/Sdl.Community.TMOptimizer.Control/HelpManager.cs
+++ TM Optimizer/Sdl.Community.TMOptimizer.Control/HelpManager.cs
@@ -6,10 +6,10 @@ namespace Sdl.Community.TMOptimizer.Control
 {
      public static class HelpManager
     {
-        public static void ShowHelp()
-        {
-            string dir = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
-            Process.Start(Path.Combine(dir, @"TMOptimizerHelp\help.htm"));
-        }
+        //public static void ShowHelp()
+        //{
+        //    string dir = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+        //    Process.Start(Path.Combine(dir, @"TMOptimizerHelp\help.htm"));
+        //}
     }
 }
