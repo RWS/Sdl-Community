e7bc0d3da9a6caecbc7fedd29cef4c31e10acbd6
TinaCaputa <caputa_florentina_lucia@yahoo.com>
2018-07-25 10:26:01 +0300

    WordCloud: migrate to Studio2019. Remove installer. Update manifest version. Add wiki help link

--- Word Cloud/Sdl.Community.WordCloud.Plugin/WordCloudRibbonGroup.cs
+++ Word Cloud/Sdl.Community.WordCloud.Plugin/WordCloudRibbonGroup.cs
@@ -7,57 +7,64 @@ using Sdl.TranslationStudioAutomation.IntegrationApi.Presentation.DefaultLocatio
 
 namespace Sdl.Community.WordCloud.Plugin
 {
-    [RibbonGroup("CodingBreeze.WordCloudRibbonGroup", Name = "Word Cloud", ContextByType = typeof(ProjectsController))]
-    [RibbonGroupLayout(LocationByType = typeof(TranslationStudioDefaultRibbonTabs.HomeRibbonTabLocation))]
-    class WordCloudRibbonGroup : AbstractRibbonGroup
-    {
-    }
+	[RibbonGroup("CodingBreeze.WordCloudRibbonGroup", Name = "Word Cloud", ContextByType = typeof(ProjectsController))]
+	[RibbonGroupLayout(LocationByType = typeof(TranslationStudioDefaultRibbonTabs.HomeRibbonTabLocation))]
+	class WordCloudRibbonGroup : AbstractRibbonGroup
+	{
+	}
 
-    [Action("CodingBreeze.WordCloud.GenerateWordCloudAction", Name = "Create Word Cloud...", Icon = "wordcloud", Description = "Generate a word cloud based on this project's content...")]
-    [ActionLayout(typeof(WordCloudRibbonGroup), 200, DisplayType.Large)]
-    class GenerateWordCloudAction : AbstractAction
-    {
+	[Action("CodingBreeze.WordCloud.GenerateWordCloudAction", Name = "Create Word Cloud...", Icon = "wordcloud", Description = "Generate a word cloud based on this project's content...")]
+	[ActionLayout(typeof(WordCloudRibbonGroup), 200, DisplayType.Large)]
+	class GenerateWordCloudAction : AbstractAction
+	{
 
-        public GenerateWordCloudAction()
-        {
-        }
+		public GenerateWordCloudAction()
+		{
+		}
 
-        public override void Initialize()
-        {
-            ProjectsController = SdlTradosStudio.Application.GetController<ProjectsController>();
-            ProjectsController.SelectedProjectsChanged += ProjectsController_SelectedProjectsChanged;
-            OnSelectedProjectsChanged();
-        }
+		public override void Initialize()
+		{
+			ProjectsController = SdlTradosStudio.Application.GetController<ProjectsController>();
+			ProjectsController.SelectedProjectsChanged += ProjectsController_SelectedProjectsChanged;
+			OnSelectedProjectsChanged();
+		}
 
-        void ProjectsController_SelectedProjectsChanged(object sender, EventArgs e)
-        {
-            OnSelectedProjectsChanged();
-        }
+		void ProjectsController_SelectedProjectsChanged(object sender, EventArgs e)
+		{
+			OnSelectedProjectsChanged();
+		}
 
-        private void OnSelectedProjectsChanged()
-        {
-            Enabled = ProjectsController.SelectedProjects.Count() == 1;
-        }
+		private void OnSelectedProjectsChanged()
+		{
+			Enabled = ProjectsController.SelectedProjects.Count() == 1;
+		}
 
-        private ProjectsController ProjectsController
-        {
-            get;
-            set;
-        }
+		private ProjectsController ProjectsController
+		{
+			get;
+			set;
+		}
 
-        private void UpdateEnabled()
-        {
+		private void UpdateEnabled()
+		{
 
-        }
+		}
 
-        protected override void Execute()
-        {
-            WordCloudViewPart wc = SdlTradosStudio.Application.GetController<WordCloudViewPart>();
-            wc.Activate();
-            wc.GenerateWordCloud();
-        }
+		protected override void Execute()
+		{
+			WordCloudViewPart wc = SdlTradosStudio.Application.GetController<WordCloudViewPart>();
+			wc.Activate();
+			wc.GenerateWordCloud();
+		}
+	}
 
-       
-    }
-
+	[Action("CodingBreeze.WordCloud.Help", Name = "Help", Icon = "help_icon", Description = "Help")]
+	[ActionLayout(typeof(WordCloudRibbonGroup), 20, DisplayType.Large)]
+	class HelpLinkAction : AbstractAction
+	{
+		protected override void Execute()
+		{
+			System.Diagnostics.Process.Start("https://community.sdl.com/product-groups/translationproductivity/w/customer-experience/3289.word-cloud");
+		}
+	}
 }
