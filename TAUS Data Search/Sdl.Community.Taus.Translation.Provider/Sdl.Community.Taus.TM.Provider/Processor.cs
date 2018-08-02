252dc773166cbc5d7b99b1a679b1f213537deafe
TinaCaputa <caputa_florentina_lucia@yahoo.com>
2018-08-02 10:28:49 +0300

    TAUS: uncomment needed code

--- TAUS Data Search/Sdl.Community.Taus.Translation.Provider/Sdl.Community.Taus.TM.Provider/Processor.cs
+++ TAUS Data Search/Sdl.Community.Taus.Translation.Provider/Sdl.Community.Taus.TM.Provider/Processor.cs
@@ -129,55 +129,54 @@ namespace Sdl.Community.Taus.Translation.Provider.Sdl.Community.Taus.TM.Provider
 
         public static string GetAuthorizationKey(string userName, string password, string appKey)
         {
-			//string authKey;
+			string authKey;
 
-			//var result = string.Empty;
+			var result = string.Empty;
 
-			//var buffer = Encoding.UTF8.GetBytes("action=login");
-			//var webrequest = (HttpWebRequest)WebRequest.Create(new Uri("https://www.taus.net/data-cloud-lp/api/auth_key.xml"));
-			//webrequest.KeepAlive = false;
-			//webrequest.Method = @"POST";
-
-
-			//webrequest.ContentLength = buffer.Length;
-			//webrequest.Credentials = new NetworkCredential(userName, password);
-
-			//webrequest.Headers.Add("X-TDA-App-Key", appKey);
+			var buffer = Encoding.UTF8.GetBytes("action=login");
+			var webrequest = (HttpWebRequest)WebRequest.Create(new Uri("https://www.taus.net/data-cloud-lp/api/auth_key.xml"));
+			webrequest.KeepAlive = false;
+			webrequest.Method = @"POST";
 
 
-			//webrequest.ContentType = "application/x-www-form-urlencoded";
+			webrequest.ContentLength = buffer.Length;
+			webrequest.Credentials = new NetworkCredential(userName, password);
 
-			//var postData = webrequest.GetRequestStream();
-			//postData.Write(buffer, 0, buffer.Length);
-			//postData.Close();
-
-			//using (var webresponse = webrequest.GetResponse() as HttpWebResponse)
-			//{
-			//	if (webresponse != null)
-			//	{
-			//		var reader = new StreamReader(webresponse.GetResponseStream());
-			//		result = reader.ReadToEnd();
-			//	}
-			//}
+			webrequest.Headers.Add("X-TDA-App-Key", appKey);
 
 
-			//var resultSegmentParser = new ResultSegmentParser();
-			//var searchSegmentResult = resultSegmentParser.ReadResult(result);
+			webrequest.ContentType = "application/x-www-form-urlencoded";
+
+			var postData = webrequest.GetRequestStream();
+			postData.Write(buffer, 0, buffer.Length);
+			postData.Close();
+
+			using (var webresponse = webrequest.GetResponse() as HttpWebResponse)
+			{
+				if (webresponse != null)
+				{
+					var reader = new StreamReader(webresponse.GetResponseStream());
+					result = reader.ReadToEnd();
+				}
+			}
 
 
-			//if (searchSegmentResult.Status == "201")
-			//{
-			//	authKey = searchSegmentResult.AuthKey.Id;
-			//}
+			var resultSegmentParser = new ResultSegmentParser();
+			var searchSegmentResult = resultSegmentParser.ReadResult(result);
 
-			//else
-			//{
-			//	throw new Exception("Status: " + searchSegmentResult.Status + "\r\nReason: " +
-			//						searchSegmentResult.Reason);
-			//}
 
-			//return authKey;
-			return "67C52A1B";
+			if (searchSegmentResult.Status == "201")
+			{
+				authKey = searchSegmentResult.AuthKey.Id;
+			}
+
+			else
+			{
+				throw new Exception("Status: " + searchSegmentResult.Status + "\r\nReason: " +
+									searchSegmentResult.Reason);
+			}
+
+			return authKey;
 		}
 
 
