7674f5847bf1a9c1b31974fcab9b61bc1f7d5915
andreaGhisa <aghisa@sdl.com>
2018-09-03 08:46:30 +0300

    Changed manigest version. Added support for missing Chinese.

--- MT Enhanced Provider/Sdl.Community.MtEnhancedProvider/MstConnect/ApiConnecter.cs
+++ MT Enhanced Provider/Sdl.Community.MtEnhancedProvider/MstConnect/ApiConnecter.cs
@@ -303,13 +303,13 @@ namespace Sdl.Community.MtEnhancedProvider.MstConnect
             if (languageCode.Contains("sr-Latn")) return "sr-Latn";
 
 	        var ci = new CultureInfo(languageCode); //construct a CultureInfo object with the language code
-            
-            //deal with chinese..MS Translator has different ones
-            if (ci.Name == "zh-TW") return "zh-Hant";
-            if (ci.Name == "zh-CN") return "zh-Hans";
-            
-            // deal with norwegian..MST needs "no" instead of nn or nb
-            if (ci.Name.Equals("nb-NO") || ci.Name.Equals("nn-NO")) return "no";
+
+			//deal with chinese..MS Translator has different ones
+			if (new[] { "zh-TW", "zh-HK", "zh-MO", "zh-Hant", "zh-CHT" }.Contains(ci.Name)) return "zh-Hant";
+	        if (new[] { "zh-CN", "zh-SG", "zh-Hans-HK", "zh-Hans-MO", "zh-Hans", "zh-CHS" }.Contains(ci.Name)) return "zh-Hans";
+
+			// deal with norwegian..MST needs "no" instead of nn or nb
+			if (ci.Name.Equals("nb-NO") || ci.Name.Equals("nn-NO")) return "no";
             //otherwise, return the two-letter code
             return ci.TwoLetterISOLanguageName;
 
