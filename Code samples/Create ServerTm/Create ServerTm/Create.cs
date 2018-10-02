ec5e47a019e5aa30874d93d8ca2927f6316895da
andreaGhisa <aghisa@sdl.com>
2018-10-02 15:59:02 +0300

    How to create server based translation memory code sample

---
+++ Code samples/Create ServerTm/Create ServerTm/Create.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sdl.Desktop.IntegrationApi;
+using Sdl.Desktop.IntegrationApi.Extensions;
+using Sdl.LanguagePlatform.TranslationMemoryApi;
+
+namespace Create_ServerTm
+{
+	[ApplicationInitializer]
+	public class Create : IApplicationInitializer
+	{
+		public void Execute()
+		{
+			var uri = new Uri(@"Add your url");
+			var translationProviderServer = new TranslationProviderServer(uri, false, "user name", "password");
+
+			var serverTm = new ServerBasedTranslationMemory(translationProviderServer)
+			{
+				Name = "Tm from API"
+			};
+			var resourceTemplate =
+				translationProviderServer.GetLanguageResourcesTemplates(LanguageResourcesTemplateProperties.All);
+			var containters = translationProviderServer.GetContainers(ContainerProperties.All);
+			var container = containters.FirstOrDefault(c => c.Name.Equals("APSIC_TM_Container"));
+			if (container != null)
+			{
+				serverTm.Container = container;
+				serverTm.ParentResourceGroupPath = container.ParentResourceGroupPath;
+				serverTm.LanguageResourcesTemplate = resourceTemplate?[0];
+				CreateLanguageDirections(serverTm.LanguageDirections);
+
+				serverTm.Save();
+			}
+		}
+
+		private void CreateLanguageDirections(ServerBasedTranslationMemoryLanguageDirectionCollection directionsCollection)
+		{
+			var direction = new ServerBasedTranslationMemoryLanguageDirection
+			{
+				SourceLanguage = CultureInfo.GetCultureInfo("en-US"),
+				TargetLanguage = CultureInfo.GetCultureInfo("de-DE")
+			};
+
+			directionsCollection.Add(direction);
+		}
+	}
+}
