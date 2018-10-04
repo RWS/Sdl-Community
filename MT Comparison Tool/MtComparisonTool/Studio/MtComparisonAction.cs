077f1faed98dbfa8755c374ed8186905eac62bce
andreaGhisa <aghisa@sdl.com>
2018-10-04 16:03:27 +0300

    Studio 2017: * MT Comparision initial commit. * Added action to Projects Controller.

---
+++ MT Comparison Tool/MtComparisonTool/Studio/MtComparisonAction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sdl.Desktop.IntegrationApi;
+using Sdl.Desktop.IntegrationApi.Extensions;
+using Sdl.TranslationStudioAutomation.IntegrationApi.Presentation.DefaultLocations;
+
+namespace MtComparisonTool.Studio
+{
+	[Action("MT Comparison",
+		Name = "MT Comparison Tool",
+		Description = "MT Comparison Tool")]
+	[ActionLayout(typeof(TranslationStudioDefaultContextMenus.ProjectsContextMenuLocation), 2, DisplayType.Default, "",true)]
+	public class MtComparisonAction : AbstractAction
+	{
+		protected override void Execute()
+		{
+			
+		}
+	}
+}
