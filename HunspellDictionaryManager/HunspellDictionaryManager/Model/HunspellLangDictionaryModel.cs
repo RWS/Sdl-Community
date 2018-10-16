eb61ed1eba1685d1eea50415518f67d054873400
TinaCaputa <caputa_florentina_lucia@yahoo.com>
2018-10-16 12:58:49 +0300

    Remove HunspellDictionaryManager plugin version from GitHub

--- HunspellDictionaryManager/HunspellDictionaryManager/Model/HunspellLangDictionaryModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Sdl.Community.HunspellDictionaryManager.Model
-{
-	public class HunspellLangDictionaryModel
-	{
-		public string DictionaryFile { get; set; }
-		public string AffFile { get; set; }
-		public string DisplayName { get; set; }
-	}
-}
