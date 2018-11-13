7482f200a7b3f6e8eeaa0f5f293e134c652a612f
TinaCaputa <caputa_florentina_lucia@yahoo.com>
2018-11-13 11:14:07 +0200

    Add initial IATE terminology provider solution

---
+++ IATETerminologyProvider/IATETerminologyProvider/IATETerminologyProvider.cs
@@ -0,0 +1,64 @@
+﻿using Sdl.Terminology.TerminologyProvider.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IATETerminologyProvider
+{
+	class IATETerminologyProvider : AbstractTerminologyProvider
+	{
+		public override IDefinition Definition
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public override string Description
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public override string Name
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public override Uri Uri
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public override IEntry GetEntry(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override IEntry GetEntry(int id, IEnumerable<ILanguage> languages)
+		{
+			throw new NotImplementedException();
+		}
+
+		public override IList<ILanguage> GetLanguages()
+		{
+			throw new NotImplementedException();
+		}
+
+		public override IList<ISearchResult> Search(string text, ILanguage source, ILanguage destination, int maxResultsCount, SearchMode mode, bool targetRequired)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ IATETerminologyProvider/IATETerminologyProvider/IATETerminologyProviderWinFormsUI.cs
@@ -0,0 +1,58 @@
+﻿using Sdl.Terminology.TerminologyProvider.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IATETerminologyProvider
+{
+	[TerminologyProviderWinFormsUI]
+	class IATETerminologyProviderWinFormsUI : ITerminologyProviderWinFormsUI
+	{
+		public bool SupportsEditing
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public string TypeDescription
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public string TypeName
+		{
+			get
+			{
+				throw new NotImplementedException();
+			}
+		}
+
+		public ITerminologyProvider[] Browse(IWin32Window owner, ITerminologyProviderCredentialStore credentialStore)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool Edit(IWin32Window owner, ITerminologyProvider terminologyProvider)
+		{
+			throw new NotImplementedException();
+		}
+
+		public TerminologyProviderDisplayInfo GetDisplayInfo(Uri terminologyProviderUri)
+		{
+			throw new NotImplementedException();
+		}
+
+		public bool SupportsTerminologyProviderUri(Uri terminologyProviderUri)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
