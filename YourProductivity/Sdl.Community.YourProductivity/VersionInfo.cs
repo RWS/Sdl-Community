ed000176980ce9e79da6f55ed0899a1f55dff03c
TinaCaputa <caputa_florentina_lucia@yahoo.com>
2018-11-14 08:55:14 +0200

    Update Assembly version

--- YourProductivity/Sdl.Community.YourProductivity/VersionInfo.cs
+++ YourProductivity/Sdl.Community.YourProductivity/VersionInfo.cs
@@ -10,5 +10,5 @@
 // [assembly: AssemblyVersion("1.0.*")]
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.1.3.1")]
-[assembly: AssemblyFileVersion("1.1.3.1")]
+[assembly: AssemblyVersion("1.1.3.2")]
+[assembly: AssemblyFileVersion("1.1.3.2")]
