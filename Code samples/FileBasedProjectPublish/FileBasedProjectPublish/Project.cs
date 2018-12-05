14a5879cb47e9e80ba66a7f6f423373d7cffb354
andreaGhisa <aghisa@sdl.com>
2018-12-05 16:13:26 +0200

    Gs Publishing issue cod sample.

--- Code samples/FileBasedProjectPublish/FileBasedProjectPublish/Project.cs
+++ Code samples/FileBasedProjectPublish/FileBasedProjectPublish/Project.cs
@@ -14,46 +14,50 @@ namespace FileBasedProjectPublish
 		// application will thorw an error when trying to publishing the project to Group Share using Windows credentials,
 		// because of an exiting bug in Studio API(the issue was raised to Studio team)
         public void Execute()
-        {
-            var groupShareServer = "GS server address";
+        {	  
+			var groupShareServer = "GS server address";
 
-            var projectFolder = Path.Combine(Path.GetTempPath(), "ProjectTemp");
-            if (!Directory.Exists(projectFolder))
-            {
-                Directory.CreateDirectory(projectFolder);
-            }
+			var projectFolder = Path.Combine(Path.GetTempPath(), "ProjectTemp");
+			if (!Directory.Exists(projectFolder))
+			{
+				Directory.CreateDirectory(projectFolder);
+			}
 
-            var projectInfo = new ProjectInfo
-            {
-                Name = "GSProjectName",
-                SourceLanguage = new Language("en-US"),
-                LocalProjectFolder = projectFolder,
-                TargetLanguages = new Language[] { new Language("fr-FR") }
-            };
+			var projectInfo = new ProjectInfo
+			{
+				Name = "GSProjectName",
+				SourceLanguage = new Language("en-US"),
+				LocalProjectFolder = projectFolder,
+				TargetLanguages = new[] { new Language("fr-FR") }
+			};
 
-            // create new project and add a translatable file to it from the local machine
-            var fileBasedProject = new FileBasedProject(projectInfo);
-            var projectFiles = fileBasedProject.AddFiles(new string[] { Path.Combine(Path.GetTempPath(), "TestFile.docx") });
-            fileBasedProject.RunAutomaticTask(projectFiles.GetIds(), AutomaticTaskTemplateIds.Scan);
-            var taskSequence = fileBasedProject.RunDefaultTaskSequence(projectFiles.GetIds());
+			// create new project and add a translatable file to it from the local machine
+			var fileBasedProject = new FileBasedProject(projectInfo);
+			var projectFiles = fileBasedProject.AddFiles(new[] { Path.Combine(Path.GetTempPath(), "TestFile.docx") });
+			fileBasedProject.RunAutomaticTask(projectFiles.GetIds(), AutomaticTaskTemplateIds.Scan);
+			var taskSequence = fileBasedProject.RunDefaultTaskSequence(projectFiles.GetIds());
 
-            if (taskSequence.Status == TaskStatus.Completed)
-            {
-                fileBasedProject.Save();
+			if (taskSequence.Status == TaskStatus.Completed)
+			{
+				//project publishing on GS will fail if GetProjectStatistics is called
+				//this bug was raised to Studio team
+				//fileBasedProject.GetProjectStatistics();
 
-                // publish project to GS using Windows credentials
-                var result = fileBasedProject.PublishProject(
-                    new Uri(groupShareServer),
-                    true,
-                    "windows userName",
-                    "windows password",
-                    "GroupShare organization",
-                    (sender, args) => { });
-            }
-            else
-            {
-                //taskSequence status can be used to see the errors in case the project was not created successfully
-            }
-        }
-    }
+				fileBasedProject.Save();
+
+				// publish project to GS using Windows credentials
+				var result = fileBasedProject.PublishProject(
+					new Uri(groupShareServer),
+					true,
+					"windows userName",
+					"windows password",
+					"GroupShare organization",
+					(sender, args) => { });
+			}
+			else
+			{
+				//taskSequence status can be used to see the errors in case the project was not created successfully
+			}
+		}	  
+	}
 }
