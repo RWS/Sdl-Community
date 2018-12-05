386d01c89baf6170ccea59c4ea1bc923eb47dfc7
Emanuel Albu <ealbu@sdl.com>
2018-12-05 16:53:35 +0200

    DeepL: Adds russian and portuguese to Suported Languages

--- DeepLMTProvider/Sdl.Community.DeelLMTProvider/Helpers.cs
+++ DeepLMTProvider/Sdl.Community.DeelLMTProvider/Helpers.cs
@@ -5,8 +5,8 @@ namespace Sdl.Community.DeepLMTProvider
 {
 	public static class Helpers
 	{
-		private static readonly List<string> SourceSupportedLanguages = new List<string> { "EN", "DE", "FR", "IT", "NL", "PL", "ES" };
-		private static readonly List<string> TargetSupportedLanguages = new List<string> { "EN", "DE", "FR", "IT", "NL", "PL", "ES" };
+		private static readonly List<string> SourceSupportedLanguages = new List<string> { "EN", "DE", "FR", "IT", "NL", "PL", "ES", "RU", "PT" };
+		private static readonly List<string> TargetSupportedLanguages = new List<string> { "EN", "DE", "FR", "IT", "NL", "PL", "ES", "RU", "PT" };
 
 		public static bool IsSuportedLanguagePair(string sourceLang, string targetLang)
 		{
