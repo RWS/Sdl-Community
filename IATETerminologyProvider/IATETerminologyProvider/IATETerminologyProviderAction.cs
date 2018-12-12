d2058caf38bab2d14086df64cfd67c286c5e571e
TinaCaputa <caputa_florentina_lucia@yahoo.com>
2018-12-12 11:47:41 +0200

    Add Action to navigate to term definition: partial implemented

---
+++ IATETerminologyProvider/IATETerminologyProvider/IATETerminologyProviderAction.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Sdl.Desktop.IntegrationApi;
+using Sdl.Desktop.IntegrationApi.Extensions;
+using Sdl.TranslationStudioAutomation.IntegrationApi;
+using Sdl.TranslationStudioAutomation.IntegrationApi.Presentation.DefaultLocations;
+
+namespace IATETerminologyProvider
+{
+	[RibbonGroup("IATETermDefinition", Name = "IATE Term Definition")]
+	[RibbonGroupLayout(LocationByType = typeof(TranslationStudioDefaultViews.TradosStudioViewsLocation))]
+	public class IATETerminologyProviderAction: AbstractRibbonGroup
+	{
+		[Action("IATETerminologyProvider", Name = "Go to IATE term definition", Icon = "Iate_logo")]
+		[ActionLayout(typeof(IATETerminologyProviderAction), 20, DisplayType.Large)]
+		[ActionLayout(typeof(TranslationStudioDefaultContextMenus.EditorDocumentContextMenuLocation), 10, DisplayType.Large)]
+		public class IATETerminologyProviderTermDefinitionAction : AbstractAction
+		{
+			public EditorController GetEditorController()
+			{
+				return SdlTradosStudio.Application.GetController<EditorController>();
+			}
+
+			protected override void Execute()
+			{
+				var editorController = GetEditorController();
+				var activeDocument = editorController != null ? editorController.ActiveDocument : null;
+				var currentSelection = activeDocument != null
+					? activeDocument.Selection != null
+					? activeDocument.Selection.Current.ToString().TrimEnd()
+					: string.Empty
+					: string.Empty;
+
+				if (!string.IsNullOrEmpty(currentSelection))
+				{
+					System.Diagnostics.Process.Start("https://iate.europa.eu/search/standard");
+				}
+			}
+		}
+	}
+}
