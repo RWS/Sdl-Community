322efe51ea5d9b5cdbab1d7dc70f366938326d7f
Emanuel Albu <ealbu@sdl.com>
2018-12-21 10:40:51 +0200

    DeepL   -> adds URI encoding for all text that is sent to DeepL   -> replaces all whiteSpaces with just one space (this can be removed when DeepL fixes its tab problem)   -> adds unitTests to the NormalizeText method within the NormalizeSourceTextHelper class

---
+++ DeepLMTProvider/DeepLMTProvider.Tests/NormalizeSourceTextHelperTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sdl.Community.DeepLMTProvider;
+using Xunit;
+
+namespace DeepLMTProvider.Tests
+{
+	public class NormalizeSourceTextHelperTests
+	{
+		[Theory]
+		[InlineData(true, "Document%20Description", "Document\tDescription")]
+		[InlineData(false, "Document%09Description", "Document\tDescription")]
+		[InlineData(null, "", "")]
+		[InlineData(null, "Document%09Description", "Document\tDescription")]
+		public void NormalizeText(bool removeTabs, string expected, string toBeNormalized)
+		{
+			// Arrange
+			var normalizeSourceTextHelper = new NormalizeSourceTextHelper();
+			var expectedText = expected;
+
+			// Act
+			var actualText = normalizeSourceTextHelper.NormalizeText(toBeNormalized, removeTabs);
+
+			// Assert
+			Assert.Equal(expectedText, actualText);
+		}
+	}
+}
