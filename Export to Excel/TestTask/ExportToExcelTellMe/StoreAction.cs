6a4afda470a64055b73419da3a9251d629851c71
Emanuel Albu <ealbu@sdl.com>
2018-12-24 16:35:52 +0200

    ExportToExcel -> adds TellMe functionality to the plugin

---
+++ Export to Excel/TestTask/ExportToExcelTellMe/StoreAction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sdl.FileTypeSupport.Framework.Core.Utilities.Properties;
+using Sdl.TellMe.ProviderApi;
+
+namespace ExportToExcel.ExportToExcelTellMe
+{
+	public class StoreAction : AbstractTellMeAction
+	{
+		public StoreAction()
+		{
+			Name = "Download ExportToExcel";
+		}
+
+		public override void Execute()
+		{
+			Process.Start("https://appstore.sdl.com/language/app/export-to-excel/532/");
+		}
+
+		public override bool IsAvailable => true;
+		public override string Category => "Export to excel results";
+		public override Icon Icon => PluginResources.Download_ExportToExcel;
+	}
+}
