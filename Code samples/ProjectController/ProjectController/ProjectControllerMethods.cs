5f3989ddf38b1b997014323598fd7003da22cd6d
andreaGhisa <aghisa@sdl.com>
2018-12-27 10:45:52 +0200

    Code sample for how to get active project from Files and Editor Controller.

---
+++ Code samples/ProjectController/ProjectController/ProjectControllerMethods.cs
@@ -0,0 +1,21 @@
+﻿using Sdl.Desktop.IntegrationApi;
+using Sdl.Desktop.IntegrationApi.Extensions;
+using Sdl.TranslationStudioAutomation.IntegrationApi;
+
+namespace ProjectController
+{
+	[ApplicationInitializer]
+	public  class ProjectControllerMethods : IApplicationInitializer
+	{
+		public void Execute()
+		{ 
+			//files controller
+			var filesController = SdlTradosStudio.Application.GetController<FilesController>();
+			var activeProjectFromFiles = filesController.CurrentProject;
+
+			//editor controler
+			var editorController = SdlTradosStudio.Application.GetController<EditorController>();
+			var activeProjectFromEditor = editorController.ActiveDocument.Project;
+		}
+	}
+}
