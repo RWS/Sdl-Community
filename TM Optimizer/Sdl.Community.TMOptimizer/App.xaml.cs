19686553a1e2204628494d6ba6205c6d7bdc6f43
TinaCaputa <caputa_florentina_lucia@yahoo.com>
2019-01-18 13:46:51 +0200

    Update TMOptimizer to Studio 2017 (solution does not includes the updates of the installer)

--- TM Optimizer/Sdl.Community.TMOptimizer/App.xaml.cs
+++ TM Optimizer/Sdl.Community.TMOptimizer/App.xaml.cs
@@ -1,18 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using Studio.AssemblyResolver;
 
 namespace Sdl.Community.TMOptimizer
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
     {
         protected override void OnStartup(StartupEventArgs e)
         {
