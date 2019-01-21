54318c61ed4880dedbf1320fd6e1e2dcd0d102d9
TinaCaputa <caputa_florentina_lucia@yahoo.com>
2019-01-21 13:48:34 +0200

    Move Help content to Sdl Community Wikipedia page. Update logic to redirect the Help action to Sdl Community Wiki page

--- TM Optimizer/Sdl.Community.TMOptimizer/HelpManager.cs
+++ TM Optimizer/Sdl.Community.TMOptimizer/HelpManager.cs
@@ -1,15 +1,10 @@
-﻿using System.Diagnostics;
-using System.IO;
-using System.Reflection;
-
-namespace Sdl.Community.TMOptimizer
+﻿namespace Sdl.Community.TMOptimizer
 {
 	public static class HelpManager
     {
         public static void ShowHelp()
         {
-            string dir = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
-            Process.Start(Path.Combine(dir, @"TMOptimizerHelp\help.htm"));
-        }
+			System.Diagnostics.Process.Start("https://community.sdl.com/product-groups/translationproductivity/w/customer-experience/3720.tm-optimizer");
+		}
     }
 }
