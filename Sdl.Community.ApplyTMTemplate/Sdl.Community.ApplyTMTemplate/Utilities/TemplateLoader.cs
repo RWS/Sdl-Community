8f90860bffa6d91d008378bd8b3f62bdca3572aa
Emanuel Albu <ealbu@sdl.com>
2019-01-24 23:57:24 +0200

    ApplyTMTemplate project -> adding project

---
+++ Sdl.Community.ApplyTMTemplate/Sdl.Community.ApplyTMTemplate/Utilities/TemplateLoader.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Sdl.Community.ApplyTMTemplate.Utilities
+{
+	public class TemplateLoader
+	{
+        public string GetTmTemplateFolderPath()
+		{
+			var data =
+				LoadDataFromFile(
+					Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+						@"SDL\SDL Trados Studio\15.0.0.0\UserSettings.xml"), "Setting");
+
+			foreach (XmlNode setting in data)
+			{
+				var id = setting?.Attributes?["Id"];
+
+				if (id.Value == "RecentLanguageResourceGroupFolder")
+				{
+					return setting.InnerText;
+				}
+			}
+
+			return null;
+		}
+
+		public XmlNodeList LoadDataFromFile(string filePath, string element)
+		{
+			var doc = new XmlDocument();
+			doc.Load(filePath);
+			var data = doc.GetElementsByTagName(element);
+
+			return data;
+		}
+	}
+}
---
+++ Sdl.Community.ApplyTMTemplate/Sdl.Community.ApplyTMTemplate/ViewModels/TranslationMemoryViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sdl.Community.ApplyTMTemplate.Models;
+using Sdl.LanguagePlatform.TranslationMemoryApi;
+
+namespace Sdl.Community.ApplyTMTemplate.ViewModels
+{
+	public class TranslationMemoryViewModel : ModelBase
+	{
+		private ObservableCollection<TranslationMemory> _tmCollection;
+
+		public TranslationMemoryViewModel(ObservableCollection<TranslationMemory> tmCollection)
+		{
+			_tmCollection = tmCollection;
+		}
+
+		public ObservableCollection<TranslationMemory> TmCollection
+		{
+			get => _tmCollection;
+			set
+			{
+				_tmCollection = value;
+				OnPropertyChanged();
+			}
+		}
+	}
+}
