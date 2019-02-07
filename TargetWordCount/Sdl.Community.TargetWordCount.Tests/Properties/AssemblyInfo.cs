7a3f3031678a80dcff36f06996ab759878b9e408
TinaCaputa <caputa_florentina_lucia@yahoo.com>
2019-02-07 11:49:37 +0200

    Update assembly name

--- TargetWordCount/Sdl.Community.TargetWordCount.Tests/Properties/AssemblyInfo.cs
+++ TargetWordCount/Sdl.Community.TargetWordCount.Tests/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
 [assembly: AssemblyProduct("TargetWordCount.Tests")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
