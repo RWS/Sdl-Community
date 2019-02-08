99fcc609957b86b2d0dcee6b1c94c41addb8e420
andreaGhisa <aghisa@sdl.com>
2019-02-08 11:13:52 +0200

    Code sample on how to update a server based translation unit.

---
+++ Code samples/Create ServerTm/UpdateServerTm/TextVisitor.cs
@@ -0,0 +1,37 @@
+﻿using Sdl.LanguagePlatform.Core;
+using Sdl.LanguagePlatform.Core.Tokenization;
+
+namespace UpdateServerTm
+{
+	public class TextVisitor	: ISegmentElementVisitor
+	{ 
+		public void VisitText(Text text)
+		{
+			text.Value = "updated from API";
+		}
+
+		public void VisitTag(Tag tag)
+		{
+		}
+
+		public void VisitDateTimeToken(DateTimeToken token)
+		{
+		}
+
+		public void VisitNumberToken(NumberToken token)
+		{
+		}
+
+		public void VisitMeasureToken(MeasureToken token)
+		{
+		}
+
+		public void VisitSimpleToken(SimpleToken token)
+		{
+		}
+
+		public void VisitTagToken(TagToken token)
+		{
+		}
+	}
+}
