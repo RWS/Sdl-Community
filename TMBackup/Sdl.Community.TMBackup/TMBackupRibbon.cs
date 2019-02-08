154cc486960a850574298c992cb170288d9ce503
TinaCaputa <caputa_florentina_lucia@yahoo.com>
2019-02-08 14:56:38 +0200

    Update app so the Sdl.Community.BackupFiles.exe file should be taken from Unpacked folder when TMBackup plugin is loaded in Studio.

--- TMBackup/Sdl.Community.TMBackup/TMBackupRibbon.cs
+++ TMBackup/Sdl.Community.TMBackup/TMBackupRibbon.cs
@@ -1,12 +1,12 @@
-﻿using Sdl.Community.BackupService.Helpers;
-using Sdl.Desktop.IntegrationApi;
-using Sdl.Desktop.IntegrationApi.Extensions;
-using Sdl.TranslationStudioAutomation.IntegrationApi;
-using Sdl.TranslationStudioAutomation.IntegrationApi.Presentation.DefaultLocations;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Reflection;
+using System.Linq;
+using Sdl.Community.BackupService.Helpers;
+using Sdl.Community.Toolkit.Core;
+using Sdl.Desktop.IntegrationApi;
+using Sdl.Desktop.IntegrationApi.Extensions;
+using Sdl.TranslationStudioAutomation.IntegrationApi.Presentation.DefaultLocations;
 
 namespace Sdl.Community.TMBackup
 {
@@ -14,6 +14,13 @@ namespace Sdl.Community.TMBackup
 	[RibbonGroupLayout(LocationByType = typeof(TranslationStudioDefaultRibbonTabs.AddinsRibbonTabLocation))]
 	public class TMBackupRibbon : AbstractRibbonGroup
 	{
+		private static readonly List<Environment.SpecialFolder> _pluginFolderLocations = new List<Environment.SpecialFolder>
+		{
+		   Environment.SpecialFolder.ApplicationData,
+		   Environment.SpecialFolder.LocalApplicationData,
+		   Environment.SpecialFolder.CommonApplicationData
+		};
+
 		[Action("Sdl.Community.TMBackup", Name = "TM Backup", Icon = "TMBackup_Icon", Description = "TM Backup")]
 		[ActionLayout(typeof(TMBackupRibbon), 20, DisplayType.Large)]
 		[ActionLayout(typeof(TranslationStudioDefaultContextMenus.ProjectsContextMenuLocation), 10, DisplayType.Large)]
@@ -21,45 +28,64 @@ namespace Sdl.Community.TMBackup
 		{
 			protected override void Execute()
 			{
-			    MoveBackupFilesAppToDirectory();
+				MoveBackupFilesAppToDirectory();
 
-				TMBackupTasksForm tmBackupTasksForm = new TMBackupTasksForm();
-				tmBackupTasksForm.ShowDialog();				
+				var tmBackupTasksForm = new TMBackupTasksForm();
+				tmBackupTasksForm.ShowDialog();
 			}
 		}
-		
+
 		private static void MoveBackupFilesAppToDirectory()
 		{
-			var path = Path.Combine(Constants.DeployPath, "Sdl.Community.BackupFiles.exe");
-
-			if(!Directory.Exists(Constants.SdlCommunityPath))
+			var pluginExePath = GetUnpackedFolder();
+			if (!string.IsNullOrEmpty(pluginExePath))
 			{
-				Directory.CreateDirectory(Constants.SdlCommunityPath);
-			}
+				var path = Path.Combine(Constants.DeployPath, "Sdl.Community.BackupFiles.exe");
 
-			if (!Directory.Exists(Constants.DeployPath))
-			{
-				Directory.CreateDirectory(Constants.DeployPath);
-			}
-
-			var directoryFiles = new List<string>(Directory.GetFiles(Constants.DeployPath));
-
-			if (!directoryFiles.Contains(path))
-			{
-				var executingAssembly = Assembly.GetExecutingAssembly();
-				var exeStream = executingAssembly.GetManifestResourceStream("Sdl.Community.TMBackup.BackupFilesExe.Sdl.Community.BackupFiles.exe");
-				using (exeStream)
+				if (!Directory.Exists(Constants.SdlCommunityPath))
 				{
-					if (exeStream != null)
+					Directory.CreateDirectory(Constants.SdlCommunityPath);
+				}
+
+				if (!Directory.Exists(Constants.DeployPath))
+				{
+					Directory.CreateDirectory(Constants.DeployPath);
+				}
+
+				var directoryFiles = new List<string>(Directory.GetFiles(Constants.DeployPath));
+
+				if (!directoryFiles.Contains(path))
+				{
+					using (var exeStream = new FileStream(pluginExePath, FileMode.Open, FileAccess.Read, FileShare.Read))
 					{
-						var fileStream = File.Create(Path.Combine(Constants.DeployPath, "Sdl.Community.BackupFiles.exe"), (int)exeStream.Length);
-						var assemblyData = new byte[(int)exeStream.Length];
-						exeStream.Read(assemblyData, 0, assemblyData.Length);
-						fileStream.Write(assemblyData, 0, assemblyData.Length);
-						fileStream.Close();
+						if (exeStream != null)
+						{
+							var fileStream = File.Create(Path.Combine(Constants.DeployPath, "Sdl.Community.BackupFiles.exe"), (int)exeStream.Length);
+							var assemblyData = new byte[(int)exeStream.Length];
+							exeStream.Read(assemblyData, 0, assemblyData.Length);
+							fileStream.Write(assemblyData, 0, assemblyData.Length);
+							fileStream.Close();
+						}
 					}
 				}
 			}
+		}
+
+		// Get the Sdl.Community.TMBackup.exe path from the Unpacked folder when plugin is loaded in Studio
+		private static string GetUnpackedFolder()
+		{
+			var executableVersion = new Studio().GetStudioVersion().ExecutableVersion.Major;
+			foreach (var pluginFolderLocation in _pluginFolderLocations)
+			{
+				var unpackedFolder = $@"{Environment.GetFolderPath(pluginFolderLocation)}\SDL\SDL Trados Studio\{executableVersion}\Plugins\Unpacked\{PluginResources.Plugin_Name}";
+
+				if (Directory.Exists(unpackedFolder))
+				{
+					var pluginExePath = Directory.GetFiles(unpackedFolder, "*.exe").FirstOrDefault().ToString();
+					return pluginExePath;
+				}
+			}
+			return string.Empty;
 		}
 	}
 }
