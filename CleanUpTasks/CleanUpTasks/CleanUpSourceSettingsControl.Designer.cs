00b6f84ae5a4de729b190247e1eb24a120bd7d27
andreaGhisa <aghisa@sdl.com>
2019-02-12 11:50:44 +0200

    2019 Cleanup task fixed UI issue.

--- CleanUpTasks/CleanUpTasks/CleanUpSourceSettingsControl.Designer.cs
+++ CleanUpTasks/CleanUpTasks/CleanUpSourceSettingsControl.Designer.cs
@@ -30,49 +30,48 @@ namespace Sdl.Community.CleanUpTasks
         /// </summary>
         private void InitializeComponent()
         {
-            this.segmentLockerControl = new SegmentLockerControl();
-            this.conversionsSettingsControl = new ConversionsSettingsControl();
-            this.tagsSettingsControl = new TagsSettingsControl();
-            this.SuspendLayout();
-            // 
-            // segmentLockerControl
-            // 
-            this.segmentLockerControl.Dock = System.Windows.Forms.DockStyle.Top;
-            this.segmentLockerControl.Location = new System.Drawing.Point(0, 0);
-            this.segmentLockerControl.Name = "segmentLockerControl";
-            this.segmentLockerControl.Size = new System.Drawing.Size(442, 149);
-            this.segmentLockerControl.TabIndex = 2;
-            // 
-            // conversionsSettingsControl
-            // 
-            this.conversionsSettingsControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+			this.segmentLockerControl = new Sdl.Community.CleanUpTasks.SegmentLockerControl();
+			this.conversionsSettingsControl = new Sdl.Community.CleanUpTasks.ConversionsSettingsControl();
+			this.tagsSettingsControl = new Sdl.Community.CleanUpTasks.TagsSettingsControl();
+			this.SuspendLayout();
+			// 
+			// segmentLockerControl
+			// 
+			this.segmentLockerControl.Dock = System.Windows.Forms.DockStyle.Top;
+			this.segmentLockerControl.Location = new System.Drawing.Point(0, 0);
+			this.segmentLockerControl.Name = "segmentLockerControl";
+			this.segmentLockerControl.Size = new System.Drawing.Size(442, 161);
+			this.segmentLockerControl.TabIndex = 2;
+			// 
+			// conversionsSettingsControl
+			// 
+			this.conversionsSettingsControl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.conversionsSettingsControl.Location = new System.Drawing.Point(0, 284);
-            this.conversionsSettingsControl.Name = "conversionsSettingsControl";
-            this.conversionsSettingsControl.Size = new System.Drawing.Size(442, 159);
-            this.conversionsSettingsControl.TabIndex = 1;
-            // 
-            // tagsSettingsControl
-            // 
-            this.tagsSettingsControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+			this.conversionsSettingsControl.Location = new System.Drawing.Point(0, 307);
+			this.conversionsSettingsControl.Margin = new System.Windows.Forms.Padding(3, 3, 3, 100);
+			this.conversionsSettingsControl.Name = "conversionsSettingsControl";
+			this.conversionsSettingsControl.Size = new System.Drawing.Size(442, 173);
+			this.conversionsSettingsControl.TabIndex = 1;
+			// 
+			// tagsSettingsControl
+			// 
+			this.tagsSettingsControl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.tagsSettingsControl.Location = new System.Drawing.Point(0, 155);
-            this.tagsSettingsControl.Name = "tagsSettingsControl";
-            this.tagsSettingsControl.Size = new System.Drawing.Size(442, 123);
-            this.tagsSettingsControl.TabIndex = 0;
-            // 
-            // CleanUpSourceSettingsControl
-            // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.segmentLockerControl);
-            this.Controls.Add(this.conversionsSettingsControl);
-            this.Controls.Add(this.tagsSettingsControl);
-            this.Name = "CleanUpSourceSettingsControl";
-            this.Size = new System.Drawing.Size(442, 443);
-            this.ResumeLayout(false);
+			this.tagsSettingsControl.Location = new System.Drawing.Point(0, 168);
+			this.tagsSettingsControl.Name = "tagsSettingsControl";
+			this.tagsSettingsControl.Size = new System.Drawing.Size(442, 133);
+			this.tagsSettingsControl.TabIndex = 0;
+			// 
+			// CleanUpSourceSettingsControl
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.segmentLockerControl);
+			this.Controls.Add(this.conversionsSettingsControl);
+			this.Controls.Add(this.tagsSettingsControl);
+			this.Name = "CleanUpSourceSettingsControl";
+			this.Size = new System.Drawing.Size(442, 480);
+			this.ResumeLayout(false);
 
         }
 
