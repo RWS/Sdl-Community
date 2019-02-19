794b67a69051c066eff9dff11fafd317cc2996a9
Emanuel Albu <ealbu@sdl.com>
2019-02-19 11:12:27 +0200

    ProjectCredentialsTest -> adding project to Code Samples

---
+++ Code samples/ProjectCredentialsTest/ProjectCredentialsTest/MyCustomTradosStudio.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Sdl.Desktop.IntegrationApi;
+using Sdl.Desktop.IntegrationApi.Extensions;
+using Sdl.TranslationStudioAutomation.IntegrationApi;
+using Sdl.TranslationStudioAutomation.IntegrationApi.Presentation.DefaultLocations;
+
+namespace ProjectCredentialsTest
+{
+	[Action("ProjectCredentialsTest")]
+	[ActionLayout(typeof(TranslationStudioDefaultContextMenus.ProjectsContextMenuLocation))]
+	public class MyCustomTradosStudio : AbstractAction
+	{
+		protected override void Execute()
+		{
+			var termbaseUri = new Uri("sdltm.http://gs2017dev.sdl.com");
+
+			var projectController = SdlTradosStudio.Application.GetController<ProjectsController>();
+			projectController.CurrentProject.Credentials.AddCredential(termbaseUri, true, "", "");
+		}
+	}
+}
