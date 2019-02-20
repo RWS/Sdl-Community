a9b582db59d32cddf379c860398ea5e5b5d46a7c
Emanuel Albu <ealbu@sdl.com>
2019-02-20 13:49:22 +0200

    MultiTermTestPlugin -> adding to 2017 branch

---
+++ Code samples/MultiTermTestPlugin/MultiTermTestPlugin/MyCustomTradosStudio.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Sdl.Desktop.IntegrationApi;
+using Sdl.Desktop.IntegrationApi.Extensions;
+using Sdl.MultiTerm.TMO.Interop;
+using Sdl.TranslationStudioAutomation.IntegrationApi.Presentation.DefaultLocations;
+
+namespace MultiTermTestPlugin
+{
+	[Action("MultiTermTestPlugin")]
+	[ActionLayout(typeof(TranslationStudioDefaultContextMenus.ProjectsContextMenuLocation))]
+	public class MyCustomTradosStudio : AbstractAction
+	{
+		protected override void Execute()
+		{
+			Application oMt = new ApplicationClass();
+
+			var oServerRep = oMt.ServerRepository;
+			oServerRep.Location = "http://gs2017dev.sdl.com";
+			oServerRep.Connect("SDLCommunity", "Commun1tyRocks");
+			Console.WriteLine("Connection successful: " + oServerRep.IsConnected);
+
+			var oTbs = oServerRep.Termbases;
+		}
+	}
+}
