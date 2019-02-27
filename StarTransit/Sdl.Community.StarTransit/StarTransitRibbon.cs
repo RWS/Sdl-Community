e424550ccf2a8e923e78c262c236075bdec0190c
Emanuel Albu <ealbu@sdl.com>
2019-02-27 14:39:41 +0200

    StarTransit -> Push test

--- StarTransit/Sdl.Community.StarTransit/StarTransitRibbon.cs
+++ StarTransit/Sdl.Community.StarTransit/StarTransitRibbon.cs
@@ -57,7 +57,7 @@ namespace Sdl.Community.StarTransit
 						PathToPrjFile = package.PathToPrjFile
 					};
 
-					// Start BackgroundWorder in InitializeMain method to have app working separately than Trados Studio processs
+					// Start BackgroundWorder in InitializeMain method to have app working separately than Trados Studio process
 					Program.InitializeMain(packageModel);
 				}
 			}
