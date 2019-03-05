6af0c4b34d4f464a0a9d3dafb5d7bb14d4622f26
Emanuel Albu <ealbu@sdl.com>
2019-03-05 14:19:18 +0200

    MultiTermStandAloneTestApp -> add to gitHub

---
+++ Code samples/MultiTermStandAloneTestApp/MultiTermStandAloneTestApp/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MultiTermStandAloneTestApp
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			var app = new MultiTermIX.ApplicationClass();
+			app.ServerRepository.Location = "";
+			app.ServerRepository.Connect("user", "password");
+
+			var termbases = app.ServerRepository.Termbases;
+			app.ServerRepository.Disconnect();
+		}
+	}
+}
