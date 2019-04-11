32f94404b27986451375156de1cdcaf163f2867b
TinaCaputa <caputa_florentina_lucia@yahoo.com>
2019-04-11 11:14:44 +0300

    Fix issue when hard return is set after translation is received, instead of soft return.

--- ETS Translation Provider/XliffConverter/Converter.cs
+++ ETS Translation Provider/XliffConverter/Converter.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -8,19 +9,22 @@ using System.Xml.Serialization;
 
 namespace XliffConverter
 {
-    public static class Converter
+	public static class Converter
     {
         private static XmlSerializer serializer;
-        static Converter()
+
+		static Converter()
         {
             serializer = new XmlSerializer(typeof(xliff));
         }
 
-
         public static xliff ParseXliffString(string text)
         {
-            if (string.IsNullOrEmpty(text))
-                return null;
+			if (string.IsNullOrEmpty(text))
+			{
+				return null;
+			}
+
             // Since XML doesn't like tags, let's temporarily replace the sourceText and targetTranslation, then put
             // them back in after the xml parser has parsed the text.
             const string sourceTextRegex = "<source>(.*?)</source>|<source />";
@@ -28,20 +32,37 @@ namespace XliffConverter
             const string emptySourceTextRegex = "<source></source>";
             const string emptyTargetTextRegex = "<target></target>";
             // Make the regex span multiple lines in case the text includes a newline
-            MatchCollection sourceText = Regex.Matches(text, sourceTextRegex, RegexOptions.Singleline);
+            var sourceText = Regex.Matches(text, sourceTextRegex, RegexOptions.Singleline);
             text = Regex.Replace(text, sourceTextRegex, emptySourceTextRegex, RegexOptions.Singleline);
-            MatchCollection targetText = Regex.Matches(text, targetTextRegex, RegexOptions.Singleline);
+            var targetText = Regex.Matches(text, targetTextRegex, RegexOptions.Singleline);
             text = Regex.Replace(text, targetTextRegex, emptyTargetTextRegex, RegexOptions.Singleline);
-            byte[] byteArray = Encoding.Unicode.GetBytes(text);
-            using (MemoryStream stream = new MemoryStream(byteArray))
+            var byteArray = Encoding.Unicode.GetBytes(text);
+            using (var stream = new MemoryStream(byteArray))
             {
-                xliff xliff = (xliff)serializer.Deserialize(stream);
+                var xliff = (xliff)serializer.Deserialize(stream);
 
-                for (int i = 0; i < sourceText.Count; i++)
+				// represents the Line Feed (new line: \n)
+				var softReturn = Convert.ToChar(10).ToString();
+				var regExPattern = new Regex("\r\n");
+
+				for (int i = 0; i < sourceText.Count; i++)
                 {
-                    if (sourceText[i].Groups.Count < 2) continue;
-                    xliff.File.Body.TranslationUnits[i].SourceText = sourceText[i].Groups[1].Value;
-                    xliff.File.Body.TranslationUnits[i].TranslationList.First().Translation.Text = targetText[i].Groups[1].Value;
+					if (sourceText[i].Groups.Count < 2)
+					{
+						continue;
+					}
+
+					// if the target result contains Carriage Return and Line Feed characters(\r\n), replace it with a soft return
+					// (otherwise a hard return is added and might crash Studio, the hard return is used for a new paragraph and not to display a new line inside of a segment)
+					if (targetText[i].Groups[1].Value.Contains("\r\n"))
+					{
+						xliff.File.Body.TranslationUnits[i].TranslationList.First().Translation.Text = regExPattern.Replace(targetText[i].Groups[1].Value, softReturn);
+					}
+					else
+					{
+						xliff.File.Body.TranslationUnits[i].TranslationList.First().Translation.Text = targetText[i].Groups[1].Value;
+					}
+					xliff.File.Body.TranslationUnits[i].SourceText = sourceText[i].Groups[1].Value;
                     xliff.File.Body.TranslationUnits[i].TranslationList.First().Translation.TargetLanguage = xliff.File.TargetLanguage;
                 }
                 return xliff;
@@ -50,9 +71,9 @@ namespace XliffConverter
 
         public static string PrintXliff(xliff xliff)
         {
-            using (StringWriter writer = new StringWriter())
+            using (var writer = new StringWriter())
             {
-                using (XmlWriter tw = XmlWriter.Create(writer, new XmlWriterSettings() { Indent = true }))
+                using (var tw = XmlWriter.Create(writer, new XmlWriterSettings() { Indent = true }))
                 {
                     serializer.Serialize(tw, xliff);
                 }
