e28b99586b30b8c5b9601359c1b600e6fe121b23
andreaGhisa <aghisa@sdl.com>
2019-05-07 09:15:58 +0300

    StarTransit: Fixed reported issue on the community regarding by replacing StatycREpource with DynamicResource.

---
+++ Code samples/GetTermbaseDetails/GetTermbaseDetails/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Sdl.Community.GroupShareKit;
+
+namespace GetTermbaseDetails
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var token =  GroupShareClient.GetRequestToken(
+				"username",
+				"pass",
+				new Uri( "url"),
+				GroupShareClient.AllScopes).Result;
+
+			var gsClient = GroupShareClient.AuthenticateClient(
+				token,
+				"username",
+				"pass",
+				new Uri("url"),
+				GroupShareClient.AllScopes).Result;
+			var termbases = gsClient.Terminology.GetTermbaseById("GermanCharacters").Result;
+		}
+	}
+}
