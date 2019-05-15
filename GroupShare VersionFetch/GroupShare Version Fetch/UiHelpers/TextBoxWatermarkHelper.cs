5a83beafb752209256c1e3db4ddb02a3540887ff
andreaGhisa <aghisa@sdl.com>
2019-05-15 09:13:45 +0300

    GSVF: Custom textbox.

---
+++ GroupShare VersionFetch/GroupShare Version Fetch/UiHelpers/TextBoxWatermarkHelper.cs
@@ -0,0 +1,54 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Sdl.Community.GSVersionFetch.UiHelpers
+{
+	public class TextBoxWatermarkHelper : DependencyObject
+	{
+		public static readonly DependencyProperty IsWatermarkVisibleProperty = DependencyProperty.RegisterAttached(
+			"IsWatermarkVisible", typeof(bool), typeof(TextBoxWatermarkHelper));
+
+		public static readonly DependencyProperty WatermarkTextProperty = DependencyProperty.RegisterAttached(
+			"WatermarkText", typeof(string), typeof(TextBoxWatermarkHelper),
+			new PropertyMetadata("Watermark", OnWatermarkTextChanged));
+
+		public static string GetWatermarkText(TextBox control)
+		{
+			return (string)control.GetValue(WatermarkTextProperty);
+		}
+
+		public static bool GetIsWatermarkVisible(TextBox control)
+		{
+			return (bool)control.GetValue(IsWatermarkVisibleProperty);
+		}
+
+		public static void SetWatermarkText(TextBox control, string text)
+		{
+			control.SetValue(WatermarkTextProperty, text);
+		}
+
+		private static void OnWatermarkTextChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+		{
+			var control = d as TextBox;
+			if (control == null) return;
+
+			control.SetValue(IsWatermarkVisibleProperty, true);
+			control.LostFocus += OnControlLostFocus;
+			control.GotFocus += OnControlGotFocus;
+		}
+
+		private static void OnControlGotFocus(object sender, RoutedEventArgs e)
+		{
+			(sender as TextBox)?.SetValue(IsWatermarkVisibleProperty, false);
+		}
+
+		private static void OnControlLostFocus(object sender, RoutedEventArgs e)
+		{
+			if (sender is TextBox control)
+			{
+				if (string.IsNullOrEmpty(control.Text))
+					control.SetValue(IsWatermarkVisibleProperty, true);
+			}
+		}
+	}
+}
--- GroupShare VersionFetch/GroupShare Version Fetch/ViewModel/LoginViewModel.cs
+++ GroupShare VersionFetch/GroupShare Version Fetch/ViewModel/LoginViewModel.cs
@@ -1,15 +1,44 @@
-﻿namespace Sdl.Community.GSVersionFetch.ViewModel
+﻿using System.Windows.Input;
+using Sdl.Community.GSVersionFetch.Commands;
+
+namespace Sdl.Community.GSVersionFetch.ViewModel
 {
 	public class LoginViewModel: ProjectWizardViewModelBase
 	{
 		private bool _isValid;
+		private string _url;
+		private ICommand _clearCommand;
 
 		public LoginViewModel(object view): base(view)
 		{
 			_isValid = true;
+			//_url = "test";
+			ClearCommand = new DelegateCommand(ExecuteClearCommand, CanExecuteClearCommand);
+
+		}
+
+		public DelegateCommand ClearCommand { get; }
+
+		private bool CanExecuteClearCommand()
+		{
+			return true;
+		}
+		//public ICommand ClearCommand => _clearCommand ?? (_clearCommand = new CommandHandler(ClearTextBox,true));
+
+		private void ClearTextBox()
+		{
+			
+		}
+
+
+		private void ExecuteClearCommand()
+		{
+			Url = string.Empty;
+			OnPropertyChanged(nameof(Url));
 		}
 
 		public override string DisplayName => "Login";
+		public bool ClearButtonVisible => _url.Length > 0;
 		public override bool IsValid
 		{
 			get => _isValid;
@@ -20,6 +49,16 @@
 
 				_isValid = value;
 				OnPropertyChanged(nameof(IsValid));
+			}
+		}
+		public string Url
+		{
+			get => _url;
+			set
+			{
+				_url = value;
+				OnPropertyChanged(nameof(Url));
+				OnPropertyChanged("ClearButtonVisible");
 			}
 		}
 	}
