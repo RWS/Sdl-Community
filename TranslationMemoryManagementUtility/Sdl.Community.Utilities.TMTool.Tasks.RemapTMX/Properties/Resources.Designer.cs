63889b28f731318cd515f805cc9a3f304a95b0fd
TinaCaputa <caputa_florentina_lucia@yahoo.com>
2019-05-15 13:58:46 +0300

    Update al the places where the naming contained 2017/2018 into 2019

--- TranslationMemoryManagementUtility/Sdl.Community.Utilities.TMTool.Tasks.RemapTMX/Properties/Resources.Designer.cs
+++ TranslationMemoryManagementUtility/Sdl.Community.Utilities.TMTool.Tasks.RemapTMX/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace Sdl.Community.Utilities.TMTool.Tasks.RemapTMX.Properties {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -115,7 +115,7 @@ namespace Sdl.Community.Utilities.TMTool.Tasks.RemapTMX.Properties {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Export for Trados 2017.
+        ///   Looks up a localized string similar to Export for Trados 2019.
         /// </summary>
         internal static string TaskName {
             get {
