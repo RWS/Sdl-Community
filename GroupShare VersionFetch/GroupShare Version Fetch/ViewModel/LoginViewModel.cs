649b043911909fd39011c798cec20c5369a138ab
andreaGhisa <aghisa@sdl.com>
2019-05-16 12:44:31 +0300

    GSVF: Authentication validations + show error messages.

--- GroupShare VersionFetch/GroupShare Version Fetch/ViewModel/LoginViewModel.cs
+++ GroupShare VersionFetch/GroupShare Version Fetch/ViewModel/LoginViewModel.cs
@@ -1,5 +1,8 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Net;
+using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
 using Sdl.Community.GSVersionFetch.Commands;
 using Sdl.Community.GSVersionFetch.Model;
 using Sdl.Community.GSVersionFetch.Service;
@@ -11,11 +14,15 @@ namespace Sdl.Community.GSVersionFetch.ViewModel
 		private bool _isValid;
 		private string _url;
 		private string _userName;
+		private string _textMessage;
+		private string _textMessageVisibility;
+		private SolidColorBrush _textMessageBrush;
 		private ICommand _loginCommand;
 
 		public LoginViewModel(object view): base(view)
 		{
-			_isValid = true;
+			_isValid = false;
+			_textMessageVisibility = "Collapsed";
 		}
 
 		public override string DisplayName => "Login";
@@ -49,9 +56,38 @@ namespace Sdl.Community.GSVersionFetch.ViewModel
 				OnPropertyChanged(nameof(UserName));
 			}
 		}
-		public ICommand LoginCommand => _loginCommand ?? (_loginCommand = new ParameterCommand(LoginUser));
+		public string TextMessage
+		{
+			get => _textMessage;
+			set
+			{
+				_textMessage = value;
+				OnPropertyChanged(nameof(TextMessage));
+			}
+		}
+		public string TextMessageVisibility
+		{
+			get => _textMessageVisibility;
+			set
+			{
+				_textMessageVisibility = value;
+				OnPropertyChanged(nameof(TextMessageVisibility));
+			}
+		}
 
-		private async void LoginUser(object parameter)
+		public SolidColorBrush TextMessageBrush
+		{
+			get => _textMessageBrush;
+			set
+			{
+				_textMessageBrush = value;
+				OnPropertyChanged(nameof(TextMessageBrush));
+			}
+		}
+
+		public ICommand LoginCommand => _loginCommand ?? (_loginCommand = new ParameterCommand(AuthenticateUser));
+
+		private async void AuthenticateUser(object parameter)
 		{
 			var passwordBox = parameter as PasswordBox;
 			var password = passwordBox?.Password;
@@ -60,11 +96,40 @@ namespace Sdl.Community.GSVersionFetch.ViewModel
 				var credentials = new Credentials
 				{
 					UserName = UserName,
-					ServiceUrl = Url,
+					ServiceUrl = Url.TrimEnd().TrimStart(),
 					Password = password
 				};
-				var token = await Authentication.Login(credentials);
+				if (Uri.IsWellFormedUriString(Url, UriKind.Absolute))
+				{
+					var statusCode = await Authentication.Login(credentials);
+					if (statusCode == HttpStatusCode.OK)
+					{
+						TextMessageVisibility = "Visible";
+						TextMessage = PluginResources.AuthenticationSuccess;
+						TextMessageBrush = (SolidColorBrush) new BrushConverter().ConvertFrom("#017701");
+						IsValid = true;
+					}
+					else
+					{
+						ShowErrorMessage(statusCode.ToString());
+					}
+				}
+				else
+				{
+					ShowErrorMessage(PluginResources.Incorrect_Url_Format);
+				}
 			}
+			else
+			{
+				ShowErrorMessage(PluginResources.Required_Fields);
+			}
+		}
+
+		private void ShowErrorMessage(string message)
+		{
+			TextMessageVisibility = "Visible";
+			TextMessage = message;
+			TextMessageBrush = new SolidColorBrush(Colors.Red);
 		}
 	}
 }
