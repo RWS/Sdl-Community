d9e54b210aa38182d801528df5bd9b32e8e7922b
andreaGhisa <aghisa@sdl.com>
2019-05-31 11:11:01 +0300

    .

--- Code samples/GetTermbaseDetails/GetTermbaseDetails/Program.cs
+++ Code samples/GetTermbaseDetails/GetTermbaseDetails/Program.cs
@@ -10,15 +10,15 @@ namespace GetTermbaseDetails
 		static void Main(string[] args)
 		{
 			var token =  GroupShareClient.GetRequestToken(
-				"SDLCommunity",
-				"Commun1tyRocks",
+				"",
+				"",
 				new Uri("http://gs2017dev.sdl.com"),
 				GroupShareClient.AllScopes).Result;
 
 			var gsClient = GroupShareClient.AuthenticateClient(
 				token,
-				"SDLCommunity",
-				"Commun1tyRocks",
+				"",
+				"",
 				new Uri("http://gs2017dev.sdl.com"),
 				GroupShareClient.AllScopes).Result;
 			var projectRequest = new ProjectsRequest("/Test API",true, 1) { Filter = { ProjectName = "a" } };
