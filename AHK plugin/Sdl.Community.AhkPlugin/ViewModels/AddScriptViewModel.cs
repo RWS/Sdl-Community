aeebbac267c8ebe0f7ffb7073adeed6f675e90e3
TinaCaputa <caputa_florentina_lucia@yahoo.com>
2019-06-03 15:04:35 +0300

    Add logging

--- AHK plugin/Sdl.Community.AhkPlugin/ViewModels/AddScriptViewModel.cs
+++ AHK plugin/Sdl.Community.AhkPlugin/ViewModels/AddScriptViewModel.cs
@@ -28,6 +28,9 @@ namespace Sdl.Community.AhkPlugin.ViewModels
 	    private readonly MasterScriptDb _masterScriptDb;
 		private string _messageColor;
 	    private bool _isDisabled;
+
+		public static readonly Log Log = Log.Instance;
+		
 		public AddScriptViewModel(MainWindowViewModel mainWindowViewModel)
 		{
 			_mainWindowViewModel = mainWindowViewModel;
@@ -46,48 +49,176 @@ namespace Sdl.Community.AhkPlugin.ViewModels
 		    _isDisabled = false;
 	    }
 
-	    public ICommand BackCommand => _backCommand ?? (_backCommand = new CommandHandler(BackToScriptsList, true));
+		public string ScriptName
+		{
+			get => _scriptName;
+
+			set
+			{
+				if (Equals(value, _scriptName))
+				{
+					return;
+				}
+				_scriptName = value;
+				OnPropertyChanged(nameof(ScriptName));
+			}
+		}
+
+		public string Message
+		{
+			get => _message;
+
+			set
+			{
+				if (Equals(value, _message))
+				{
+					return;
+				}
+				_message = value;
+				OnPropertyChanged(nameof(Message));
+			}
+		}
+
+		public string MessageColor
+		{
+			get => _messageColor;
+
+			set
+			{
+				if (Equals(value, _messageColor))
+				{
+					return;
+				}
+				_messageColor = value;
+				OnPropertyChanged(nameof(MessageColor));
+			}
+		}
+
+		public string ScriptDescription
+		{
+			get => _scriptDescription;
+
+			set
+			{
+				if (Equals(value, _scriptDescription))
+				{
+					return;
+				}
+				_scriptDescription = value;
+				OnPropertyChanged(nameof(ScriptDescription));
+			}
+		}
+
+		public string ScriptContent
+		{
+			get => _scriptContent;
+
+			set
+			{
+				if (Equals(value, _scriptContent))
+				{
+					return;
+				}
+				_scriptContent = value;
+				OnPropertyChanged(nameof(ScriptContent));
+			}
+		}
+
+		public bool FormIsValid
+		{
+			get => _formIsValid;
+
+			set
+			{
+				if (Equals(value, _formIsValid))
+				{
+					return;
+				}
+				_formIsValid = value;
+				OnPropertyChanged(nameof(FormIsValid));
+			}
+		}
+
+		public string MessageVisibility
+		{
+			get => _messageVisibility;
+
+			set
+			{
+				if (Equals(value, _messageVisibility))
+				{
+					return;
+				}
+				_messageVisibility = value;
+				OnPropertyChanged(nameof(MessageVisibility));
+			}
+		}
+
+		public bool IsDisabled
+		{
+			get => _isDisabled;
+
+			set
+			{
+				if (Equals(value, _isDisabled))
+				{
+					return;
+				}
+				_isDisabled = value;
+				OnPropertyChanged(nameof(IsDisabled));
+			}
+		}
+
+		public ICommand BackCommand => _backCommand ?? (_backCommand = new CommandHandler(BackToScriptsList, true));
 	    public ICommand InsertCommand => _insertCommand ?? (_insertCommand = new CommandHandler(InsertScript, true));
 
 	    private async void InsertScript()
 	    {
-			ValidateForm();
-		    MessageVisibility = "Visible";
-			if (FormIsValid)
-		    {
-			    var script = new Script
-			    {
-				    ScriptId = Guid.NewGuid().ToString(),
-				    Active = !IsDisabled,
-				    Description = ScriptDescription,
-				    Name = ScriptName,
-				    Text = ScriptContent
-			    };
-			    script.ScriptStateAction = script.Active ? "Disable" : "Enable";
-			    script.RowColor = script.Active ? "Black" : "DarkGray";
-
-				//add new script in data base
-				await _scriptDb.AddNewScript(script);
-
-				//add the script in master script too
-			    var masterScript = await _masterScriptDb.GetMasterScript();
-				masterScript.Scripts.Add(script);
-			    await _masterScriptDb.UpdateScript(masterScript);
-
-				//write masterscript on the disk
-				ProcessScript.ExportScript(Path.Combine(masterScript.Location,masterScript.Name),masterScript.Scripts);
-
-				Message = "Script added successfully.";
-			    MessageColor = "#3EA691";
-
-				ClearForm();
-		    }
-		    else
+			try
 			{
-				Message = "Please fill all the fields.";
-				MessageColor = Color.DarkRed.Name;
+				ValidateForm();
+				MessageVisibility = "Visible";
+				if (FormIsValid)
+				{
+					var script = new Script
+					{
+						ScriptId = Guid.NewGuid().ToString(),
+						Active = !IsDisabled,
+						Description = ScriptDescription,
+						Name = ScriptName,
+						Text = ScriptContent
+					};
+					script.ScriptStateAction = script.Active ? "Disable" : "Enable";
+					script.RowColor = script.Active ? "Black" : "DarkGray";
+
+					//add new script in data base
+					await _scriptDb.AddNewScript(script);
+
+					//add the script in master script too
+					var masterScript = await _masterScriptDb.GetMasterScript();
+					masterScript.Scripts.Add(script);
+					await _masterScriptDb.UpdateScript(masterScript);
+
+					//write masterscript on the disk
+					ProcessScript.ExportScript(Path.Combine(masterScript.Location, masterScript.Name), masterScript.Scripts);
+
+					Message = "Script added successfully.";
+					MessageColor = "#3EA691";
+
+					ClearForm();
+				}
+				else
+				{
+					Message = "Please fill all the fields.";
+					MessageColor = Color.DarkRed.Name;
+				}
 			}
-	    }
+			catch (Exception ex)
+			{
+				Log.Logger.Error($"{Constants.InsertScript}: {Constants.Message}: {ex.Message}\n " +
+					$"{Constants.StackTrace}: {ex.StackTrace}\n {Constants.InnerException}: {ex.InnerException}");
+			}
+		}
 
 	    private void ClearForm()
 	    {
@@ -95,120 +226,6 @@ namespace Sdl.Community.AhkPlugin.ViewModels
 			ScriptDescription = string.Empty;
 		    ScriptName = string.Empty;
 		    IsDisabled = false;
-	    }
-
-	    public string ScriptName
-	    {
-		    get => _scriptName;
-
-		    set
-		    {
-			    if (Equals(value, _scriptName))
-			    {
-				    return;
-			    }
-			    _scriptName = value;
-			    OnPropertyChanged(nameof(ScriptName));
-		    }
-	    }
-	    public string Message
-	    {
-		    get => _message;
-
-		    set
-		    {
-			    if (Equals(value, _message))
-			    {
-				    return;
-			    }
-			    _message = value;
-			    OnPropertyChanged(nameof(Message));
-		    }
-	    }
-	    public string MessageColor
-	    {
-		    get => _messageColor;
-
-		    set
-		    {
-			    if (Equals(value, _messageColor))
-			    {
-				    return;
-			    }
-			    _messageColor = value;
-			    OnPropertyChanged(nameof(MessageColor));
-		    }
-	    }
-		public string ScriptDescription
-	    {
-		    get => _scriptDescription;
-
-		    set
-		    {
-			    if (Equals(value, _scriptDescription))
-			    {
-				    return;
-			    }
-			    _scriptDescription = value;
-			    OnPropertyChanged(nameof(ScriptDescription));
-		    }
-	    }
-	    public string ScriptContent
-	    {
-		    get => _scriptContent;
-
-		    set
-		    {
-			    if (Equals(value, _scriptContent))
-			    {
-				    return;
-			    }
-			    _scriptContent = value;
-			    OnPropertyChanged(nameof(ScriptContent));
-		    }
-	    }
-
-	    public bool FormIsValid
-	    {
-		    get => _formIsValid;
-
-		    set
-		    {
-			    if (Equals(value, _formIsValid))
-			    {
-				    return;
-			    }
-			    _formIsValid = value;
-			    OnPropertyChanged(nameof(FormIsValid));
-		    }
-	    }
-	    public string MessageVisibility
-	    {
-		    get => _messageVisibility;
-
-		    set
-		    {
-			    if (Equals(value, _messageVisibility))
-			    {
-				    return;
-			    }
-			    _messageVisibility = value;
-			    OnPropertyChanged(nameof(MessageVisibility));
-		    }
-	    }
-	    public bool IsDisabled
-		{
-		    get => _isDisabled;
-
-		    set
-		    {
-			    if (Equals(value, _isDisabled))
-			    {
-				    return;
-			    }
-			    _isDisabled = value;
-			    OnPropertyChanged(nameof(IsDisabled));
-		    }
 	    }
 
 		private void ValidateForm()
--- AHK plugin/Sdl.Community.AhkPlugin/ViewModels/EditScriptPageViewModel.cs
+++ AHK plugin/Sdl.Community.AhkPlugin/ViewModels/EditScriptPageViewModel.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Input;
 using Sdl.Community.AhkPlugin.Helpers;
@@ -23,6 +20,9 @@ namespace Sdl.Community.AhkPlugin.ViewModels
 		private bool _isDisabled;
 		private static Script _script;
 		private static readonly MasterScriptDb MasterScriptDb = new MasterScriptDb();
+
+		public static readonly Log Log = Log.Instance;
+
 		public EditScriptPageViewModel()
 		{
 			ScriptContent = _script.Text;
@@ -30,6 +30,7 @@ namespace Sdl.Community.AhkPlugin.ViewModels
 			ScriptName = _script.Name;
 			IsDisabled = !_script.Active;
 		}
+
 		public EditScriptPageViewModel(Script script,MainWindowViewModel mainWindowViewModel)
 		{
 			_mainWindowViewModel = mainWindowViewModel;
@@ -39,30 +40,6 @@ namespace Sdl.Community.AhkPlugin.ViewModels
 		public ICommand BackCommand => _backCommand ?? (_backCommand = new CommandHandler(BackToScriptsList, true));
 		public ICommand SaveCommand => _saveCommand ?? (_saveCommand = new CommandHandler(SaveScript, true));
 
-		private async void SaveScript()
-		{
-
-			var masterScript = await MasterScriptDb.GetMasterScript();
-			var scriptToBeUpdated = masterScript.Scripts.FirstOrDefault(s => s.ScriptId.Equals(_script.ScriptId));
-			if (scriptToBeUpdated != null)
-			{
-				scriptToBeUpdated.Active = !IsDisabled;
-				scriptToBeUpdated.Name = ScriptName;
-				scriptToBeUpdated.Text = ScriptContent;
-				scriptToBeUpdated.Description = ScriptDescription;
-				scriptToBeUpdated.ScriptStateAction = !IsDisabled ? "Disable" : "Enable";
-				scriptToBeUpdated.RowColor = !IsDisabled ? "Black" : "DarkGray";
-				await MasterScriptDb.UpdateScript(masterScript);
-				//write masterscript on the disk
-				ProcessScript.ExportScript(Path.Combine(masterScript.Location, masterScript.Name), masterScript.Scripts);
-				var result =MessageBox.Show("Script edited successfully", string.Empty,
-					MessageBoxButton.OK,MessageBoxImage.Information);
-				if (result == MessageBoxResult.OK)
-				{
-					_mainWindowViewModel.LoadScriptsPage();
-				}
-			}
-		}
 		public string ScriptDescription
 		{
 			get => _scriptDescription;
@@ -77,6 +54,7 @@ namespace Sdl.Community.AhkPlugin.ViewModels
 				OnPropertyChanged(nameof(ScriptDescription));
 			}
 		}
+
 		public string ScriptContent
 		{
 			get => _scriptContent;
@@ -91,6 +69,7 @@ namespace Sdl.Community.AhkPlugin.ViewModels
 				OnPropertyChanged(nameof(ScriptContent));
 			}
 		}
+
 		public bool IsDisabled
 		{
 			get => _isDisabled;
@@ -105,10 +84,7 @@ namespace Sdl.Community.AhkPlugin.ViewModels
 				OnPropertyChanged(nameof(IsDisabled));
 			}
 		}
-		private void BackToScriptsList()
-		{
-			_mainWindowViewModel.LoadScriptsPage();
-		}
+
 		public string ScriptName
 		{
 			get => _scriptName;
@@ -121,6 +97,43 @@ namespace Sdl.Community.AhkPlugin.ViewModels
 				}
 				_scriptName = value;
 				OnPropertyChanged(nameof(ScriptName));
+			}
+		}
+
+		private void BackToScriptsList()
+		{
+			_mainWindowViewModel.LoadScriptsPage();
+		}
+
+		private async void SaveScript()
+		{
+			try
+			{
+				var masterScript = await MasterScriptDb.GetMasterScript();
+				var scriptToBeUpdated = masterScript.Scripts.FirstOrDefault(s => s.ScriptId.Equals(_script.ScriptId));
+				if (scriptToBeUpdated != null)
+				{
+					scriptToBeUpdated.Active = !IsDisabled;
+					scriptToBeUpdated.Name = ScriptName;
+					scriptToBeUpdated.Text = ScriptContent;
+					scriptToBeUpdated.Description = ScriptDescription;
+					scriptToBeUpdated.ScriptStateAction = !IsDisabled ? "Disable" : "Enable";
+					scriptToBeUpdated.RowColor = !IsDisabled ? "Black" : "DarkGray";
+					await MasterScriptDb.UpdateScript(masterScript);
+					//write masterscript on the disk
+					ProcessScript.ExportScript(Path.Combine(masterScript.Location, masterScript.Name), masterScript.Scripts);
+					var result = MessageBox.Show("Script edited successfully", string.Empty,
+						MessageBoxButton.OK, MessageBoxImage.Information);
+					if (result == MessageBoxResult.OK)
+					{
+						_mainWindowViewModel.LoadScriptsPage();
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				Log.Logger.Error($"{Constants.SaveScript}: {Constants.Message}: {ex.Message}\n " +
+					$"{Constants.StackTrace}: {ex.StackTrace}\n {Constants.InnerException}: {ex.InnerException}");
 			}
 		}
 	}
