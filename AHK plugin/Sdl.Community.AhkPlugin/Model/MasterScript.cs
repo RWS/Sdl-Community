0d6b50eb09a3ab845c62bc90fdf6de790fffc2d0
andreaGhisa <aghisa@sdl.com>
2019-06-19 15:45:21 +0300

    Ahk: refactoring.

---
+++ AHK plugin/Sdl.Community.AhkPlugin/Model/MasterScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace Sdl.Community.AhkPlugin.Model
+{
+	public class MasterScript
+	{
+		public string Location { get; set; }
+		public List<Script> Scripts { get; set; }
+		public int Id { get; set; }
+		public string Name { get; set; }
+	}
+}
---
+++ AHK plugin/Sdl.Community.AhkPlugin/Model/Script.cs
@@ -0,0 +1,73 @@
+﻿namespace Sdl.Community.AhkPlugin.Model
+{
+	public class Script : ModelBase
+	{
+		private bool _isSelected;
+		private bool _isActiveScript;
+		private string _scriptStateAction;
+		private string _rowColor;
+		private string _description;
+		private string _text;
+
+		public string ScriptId { get; set; }
+		public string Name { get; set; }
+		public string Description
+		{
+			get => _description;
+			set
+			{
+				_description = value;
+				OnPropertyChanged(nameof(Description));
+			}
+		}
+		public string Text
+		{
+			get => _text;
+			set
+			{
+				_text = value;
+				OnPropertyChanged(nameof(Text));
+			}
+		}
+
+		public string ScriptStateAction
+		{
+			get => _scriptStateAction;
+			set
+			{
+				_scriptStateAction = value;
+				OnPropertyChanged(nameof(ScriptStateAction));
+			}
+		}
+
+		public string RowColor
+		{
+			get => _rowColor;
+			set
+			{
+				_rowColor = value;
+				OnPropertyChanged(nameof(RowColor));
+			}
+		}
+
+		public bool IsSelected
+		{
+			get => _isSelected;
+			set
+			{
+				_isSelected = value;
+				OnPropertyChanged(nameof(IsSelected));
+			}
+		}
+		public bool Active
+		{
+			get => _isActiveScript;
+			set
+			{
+				_isActiveScript = value;
+				OnPropertyChanged(nameof(Active));
+			}
+		}
+
+	}
+}
