139c07319b05abade0f86ecc7ccabfe9d556fe37
Emanuel Albu <ealbu@sdl.com>
2019-06-21 15:59:37 +0300

    Adding new Code Sample -> MultiTermStandAloneTestAppNew

---
+++ Code samples/MultiTermStandAloneTestAppNew/MultiTermStandAloneTestApp/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using Sdl.MultiTerm.Client.Api;
+
+namespace MultiTermStandAloneTestApp
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			var app = new ServerRepository();
+			app.Connect("", "", "");
+
+			var oTbs = app.Termbases;
+		}
+	}
+}
