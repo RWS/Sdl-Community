57fe0da4573a3b89a0418f9cbeae4bc5f820e662
Emanuel Albu <ealbu@sdl.com>
2019-07-05 16:48:11 +0300

    RecordSourceTU 1.2.0.2 -> 1.2.0.3  - updates SDL.PluginFramework.Build to version 15.0.2  - fixes bug: path option not working correctly <- storing the fileName instead of the filePath

--- Record Source TU/Sdl.Community.RecordSourceTU/Extension.cs
+++ Record Source TU/Sdl.Community.RecordSourceTU/Extension.cs
@@ -10,128 +10,123 @@ using Sdl.TranslationStudioAutomation.IntegrationApi;
 
 namespace Sdl.Community.RecordSourceTU
 {
-    public static class Extension
-    {
-        public static Uri GetInnerProviderUri(this Uri addSourceTmProviderUri)
-        {
-            if (addSourceTmProviderUri.AbsoluteUri.StartsWith(RecordSourceTuTmTranslationProvider.ProviderUriScheme,
-                StringComparison.InvariantCultureIgnoreCase))
-            {
-                return
-                    new Uri(
-                        addSourceTmProviderUri.AbsoluteUri.Substring(
-                            RecordSourceTuTmTranslationProvider.ProviderUriScheme.Length));
-            }
+	public static class Extension
+	{
+		public static Uri GetInnerProviderUri(this Uri addSourceTmProviderUri)
+		{
+			if (addSourceTmProviderUri.AbsoluteUri.StartsWith(RecordSourceTuTmTranslationProvider.ProviderUriScheme,
+				StringComparison.InvariantCultureIgnoreCase))
+			{
+				return
+					new Uri(
+						addSourceTmProviderUri.AbsoluteUri.Substring(
+							RecordSourceTuTmTranslationProvider.ProviderUriScheme.Length));
+			}
 
-            return addSourceTmProviderUri;
-        }
+			return addSourceTmProviderUri;
+		}
 
-        public static string GetFilePath(this TranslationUnit translationUnit)
-        {
+		public static string GetFilePath(this TranslationUnit translationUnit)
+		{
+			string filePath = string.Empty;
+			if (translationUnit.FileProperties != null)
+			{
+				filePath = translationUnit.FileProperties.FileConversionProperties.OriginalFilePath;
+			}
+			else
+			{
+				var editorController = SdlTradosStudio.Application.GetController<EditorController>();
+				if (editorController == null) return filePath;
+				if (editorController.ActiveDocument == null) return filePath;
 
-            string filePath = string.Empty;
-            if (translationUnit.FileProperties != null)
-            {
-                filePath = translationUnit.FileProperties.FileConversionProperties.InputFilePath;
-            }
-            else
-            {
-                //filePath =
-                //SdlTradosStudio.Application.GetController<EditorController>()
-                //    .ActiveDocument.ActiveFileProperties.FileConversionProperties.InputFilePath;
-                var editorController = SdlTradosStudio.Application.GetController<EditorController>();
-                if (editorController == null) return filePath;
-                if (editorController.ActiveDocument == null) return filePath;
+				var activeFile = editorController.ActiveDocument.TryGetActiveFile();
 
-                var activeFile = editorController.ActiveDocument.TryGetActiveFile();
+				filePath = activeFile.LocalFilePath;
+			}
 
-                filePath = activeFile.LocalFilePath;
-            }
-            
-            return filePath;
-        }
+			return filePath;
+		}
 
-        public static string GetProjectName(this TranslationUnit translationUnit)
-        {
-            var editorController = SdlTradosStudio.Application.GetController<EditorController>();
-            if (editorController == null) return "N/A";
-            if (editorController.ActiveDocument == null)
-            {
-                var projectName =
-                    ProjectNameHelper(translationUnit.FileProperties.FileConversionProperties.InputFilePath);
-                return projectName;
-            }
+		public static string GetProjectName(this TranslationUnit translationUnit)
+		{
+			var editorController = SdlTradosStudio.Application.GetController<EditorController>();
+			if (editorController == null) return "N/A";
+			if (editorController.ActiveDocument == null)
+			{
+				var projectName =
+					ProjectNameHelper(translationUnit.FileProperties.FileConversionProperties.InputFilePath);
+				return projectName;
+			}
 
-            var projectInfo = editorController.ActiveDocument.Project.GetProjectInfo();
+			var projectInfo = editorController.ActiveDocument.Project.GetProjectInfo();
 
-            return projectInfo.Name;
-        }
+			return projectInfo.Name;
+		}
 
-        private static string ProjectNameHelper(string projectPath)
-        {
-            var projectName = projectPath.Substring(projectPath.LastIndexOf(@"\", StringComparison.Ordinal)-1);
-            return projectName;
-        }
-    public static ProjectFile TryGetActiveFile(this Document document)
-        {
+		public static ProjectFile TryGetActiveFile(this Document document)
+		{
+			if (document.ActiveFile != null) return document.ActiveFile;
+			//at the time of implementation using ActiveFile property is not working always for the virtual merged files - for some reasons in the editor
+			//there more file nodes for one file. This is a workaround to try and resolve it.
+			var segmentPair = document.ActiveSegmentPair;
+			var type = document.GetType();
+			var method = type.GetMethod("GetTargetSegmentContainerNodeById",
+				BindingFlags.NonPublic | BindingFlags.Instance);
 
-            if (document.ActiveFile != null) return document.ActiveFile;
-            //at the time of implementation using ActiveFile property is not working always for the virtual merged files - for some reasons in the editor
-            //there more file nodes for one file. This is a workaround to try and resolve it.
-            var segmentPair = document.ActiveSegmentPair;
-            var type = document.GetType();
-            var method = type.GetMethod("GetTargetSegmentContainerNodeById",
-                BindingFlags.NonPublic | BindingFlags.Instance);
+			var result = method.Invoke(document, new object[] { segmentPair.Target.Properties.Id.Id });
 
-            var result = method.Invoke(document, new object[] { segmentPair.Target.Properties.Id.Id });
+			var targetSegmentContainer = result as ISegmentContainerNode;
 
-            var targetSegmentContainer = result as ISegmentContainerNode;
+			if (targetSegmentContainer == null) return null;
 
-            if (targetSegmentContainer == null) return null;
+			IAbstractContainerNode node = targetSegmentContainer;
+			while (node != null && !(node is IFileContainerNode))
+			{
+				node = node.Parent;
+			}
+			var fileContainerNode = node as IFileContainerNode;
+			if (fileContainerNode == null) return null;
 
-            IAbstractContainerNode node = targetSegmentContainer;
-            while (node != null && !(node is IFileContainerNode))
-            {
-                node = node.Parent;
-            }
-            var fileContainerNode = node as IFileContainerNode;
-            if (fileContainerNode == null) return null;
+			var assembly = Assembly.LoadFrom("Sdl.TranslationStudio.Common.dll");
+			var filterFramweworkUtilitiesType =
+				assembly.GetType("Sdl.TranslationStudio.Common.Editing.FilterFrameworkUtilities");
+			var internalDocument =
+				type.GetProperty("InternalDocument", BindingFlags.NonPublic | BindingFlags.Instance)
+					.GetValue(document, null);
+			var nativeBillingualDocument =
+				internalDocument.GetType()
+					.GetProperty("NativeBilingualDocument")
+					.GetValue(internalDocument, null);
+			var target = nativeBillingualDocument.GetType()
+				.GetProperty("Target")
+				.GetValue(nativeBillingualDocument, null);
+			var rootContainer = target.GetType()
+				.GetProperty("RootContainer")
+				.GetValue(target, null);
+			var getFileContainerNodesMethod = filterFramweworkUtilitiesType.GetMethod("GetFileContainerNodes",
+				BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static);
+			var fileContainers = getFileContainerNodesMethod.Invoke(null, new[] { rootContainer, null });
+			var fileContainerNodes = fileContainers as List<IFileContainerNode>;
+			if (fileContainerNodes == null) return null;
 
-            var assembly = Assembly.LoadFrom("Sdl.TranslationStudio.Common.dll");
-            var filterFramweworkUtilitiesType =
-                assembly.GetType("Sdl.TranslationStudio.Common.Editing.FilterFrameworkUtilities");
-            var internalDocument =
-                type.GetProperty("InternalDocument", BindingFlags.NonPublic | BindingFlags.Instance)
-                    .GetValue(document, null);
-            var nativeBillingualDocument =
-                internalDocument.GetType()
-                    .GetProperty("NativeBilingualDocument")
-                    .GetValue(internalDocument, null);
-            var target = nativeBillingualDocument.GetType()
-                .GetProperty("Target")
-                .GetValue(nativeBillingualDocument, null);
-            var rootContainer = target.GetType()
-                .GetProperty("RootContainer")
-                .GetValue(target, null);
-            var getFileContainerNodesMethod = filterFramweworkUtilitiesType.GetMethod("GetFileContainerNodes",
-                BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Static);
-            var fileContainers = getFileContainerNodesMethod.Invoke(null, new[] { rootContainer, null });
-            var fileContainerNodes = fileContainers as List<IFileContainerNode>;
-            if (fileContainerNodes == null) return null;
+			var distinctFileContainerNodes = fileContainerNodes.DistinctBy(x => x.FileProperties.FileConversionProperties.FileId).ToList();
 
+			var index =
+				distinctFileContainerNodes.FindIndex(
+					f =>
+						f.FileProperties.FileConversionProperties.FileId.Equals(
+							fileContainerNode.FileProperties.FileConversionProperties.FileId));
+			if (0 <= index && index < document.Files.Count())
+			{
+				return document.Files.ElementAt(index);
+			}
+			return null;
+		}
 
-            var distinctFileContainerNodes = fileContainerNodes.DistinctBy(x => x.FileProperties.FileConversionProperties.FileId).ToList();
-
-            var index =
-                distinctFileContainerNodes.FindIndex(
-                    f =>
-                        f.FileProperties.FileConversionProperties.FileId.Equals(
-                            fileContainerNode.FileProperties.FileConversionProperties.FileId));
-            if (0 <= index && index < document.Files.Count())
-            {
-                return document.Files.ElementAt(index);
-            }
-            return null;
-        }
-    }
+		private static string ProjectNameHelper(string projectPath)
+		{
+			var projectName = projectPath.Substring(projectPath.LastIndexOf(@"\", StringComparison.Ordinal) - 1);
+			return projectName;
+		}
+	}
 }
