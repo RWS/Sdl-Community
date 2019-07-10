600937f2b43b3c51af28e8f6e3e30ae88bfb7946
andreaGhisa <aghisa@sdl.com>
2019-07-10 11:27:11 +0300

    Studio styles data grid, radio buttons & grouped box.

---
+++ Code samples/StudioStyles/StudioStyles/Model/Plugin.cs
@@ -0,0 +1,28 @@
+﻿namespace StudioStyles.Model
+{
+	public class Plugin:BaseModel
+	{
+		private string _pluginName;
+		private string _studioVersion;
+
+		public string PluginName
+		{
+			get => _pluginName;
+			set
+			{
+				_pluginName = value;
+				OnPropertyChanged(nameof(PluginName));
+			}
+		}
+
+		public string StudioVersion
+		{
+			get => _studioVersion;
+			set
+			{
+				_studioVersion = value;
+				OnPropertyChanged(StudioVersion);
+			}
+		}
+	}
+}
---
+++ Code samples/StudioStyles/StudioStyles/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StudioStyles.Model;
+
+namespace StudioStyles.ViewModel
+{
+	public class MainWindowViewModel:BaseModel
+	{
+		private ObservableCollection<Plugin> _pluginsCollection;
+
+		public MainWindowViewModel()
+		{
+			_pluginsCollection = new ObservableCollection<Plugin>
+			{
+				new Plugin
+				{
+					PluginName = "Subtitling",
+					StudioVersion = "Studio 2019"
+				},
+				new Plugin
+				{
+					PluginName = "IATE Real-time Terminology",
+					StudioVersion = "Studio 2017,2019"
+				},
+				new Plugin
+				{
+					PluginName = "SDL BeGlobal (NMT)",
+					StudioVersion = "Studio 2015, 2017, 2019"
+				},
+				new Plugin
+				{
+					PluginName = "MT Comparison",
+					StudioVersion = "Studio 2017, 2019"
+				},
+			};
+		}
+
+		public ObservableCollection<Plugin> PluginsCollection
+		{
+			get => _pluginsCollection;
+			set
+			{
+				_pluginsCollection = value;
+				OnPropertyChanged(nameof(PluginsCollection));
+			}
+		}
+	}
+}
