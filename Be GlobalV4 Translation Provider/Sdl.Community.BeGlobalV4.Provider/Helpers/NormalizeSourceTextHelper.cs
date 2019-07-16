3fb9a7f1a701a56e156bbfe933b050fde16a20cf
andreaGhisa <aghisa@sdl.com>
2019-07-16 10:56:37 +0300

    BeGlobal: Fixed Slovak language mapping issue.

--- Be GlobalV4 Translation Provider/Sdl.Community.BeGlobalV4.Provider/Helpers/NormalizeSourceTextHelper.cs
+++ Be GlobalV4 Translation Provider/Sdl.Community.BeGlobalV4.Provider/Helpers/NormalizeSourceTextHelper.cs
@@ -4,29 +4,37 @@ namespace Sdl.Community.BeGlobalV4.Provider.Helpers
 {
 	public class NormalizeSourceTextHelper
 	{
-
 		public string GetCorespondingLangCode(CultureInfo cultureInfo)
 		{
-			if (cultureInfo.ThreeLetterISOLanguageName.Equals("zho"))
+			if (cultureInfo != null)
 			{
-				//Chinese (Traditional, Macao S.A.R.),(Traditional, Hong Kong SAR),(Traditional, Taiwan)
-				if (cultureInfo.Name.Equals("zh-MO") || cultureInfo.Name.Equals("zh-HK") || cultureInfo.Name.Equals("zh-TW"))
+				if (cultureInfo.ThreeLetterISOLanguageName.Equals("zho"))
 				{
-					return "cht";
+					//Chinese (Traditional, Macao S.A.R.),(Traditional, Hong Kong SAR),(Traditional, Taiwan)
+					if (cultureInfo.Name.Equals("zh-MO") || cultureInfo.Name.Equals("zh-HK") || cultureInfo.Name.Equals("zh-TW"))
+					{
+						return "cht";
+					}
+					//Simplified Chinese
+					return "chi";
 				}
-				//Simplified Chinese
-				return "chi";
+				if (cultureInfo.ThreeLetterISOLanguageName.Equals("deu"))
+				{
+					return "ger";
+				}
+				//Language code for Dutch in BeGlobal is dut
+				if (cultureInfo.ThreeLetterISOLanguageName.Equals("nld"))
+				{
+					return "dut";
+				}
+				//Language code for Slovak in BeGlobal is slo
+				if (cultureInfo.ThreeLetterISOLanguageName.Equals("slk"))
+				{
+					return "slo";
+				}
+				return cultureInfo.ThreeLetterISOLanguageName;
 			}
-			if (cultureInfo.ThreeLetterISOLanguageName.Equals("deu"))
-			{
-				return "ger";
-			}
-			//Language code for Dutch in BeGlobal is dut
-			if (cultureInfo.ThreeLetterISOLanguageName.Equals("nld"))
-			{
-				return "dut";
-			}
-			return cultureInfo.ThreeLetterISOLanguageName;
+			return string.Empty;
 		}
 	}
 }
