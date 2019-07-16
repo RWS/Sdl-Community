55027068ee2a01426be935f776a3f6741051baac
Emanuel Albu <ealbu@sdl.com>
2019-07-16 16:10:23 +0300

    IATEProvider_2019 2.6.2 -> 2.6.3.0  -> changes all the calls from using RestSharp to HttpClient

--- IATETerminologyProvider/IATETerminologyProvider/Helpers/Utils.cs
+++ IATETerminologyProvider/IATETerminologyProvider/Helpers/Utils.cs
@@ -1,4 +1,6 @@
-﻿using System.Text.RegularExpressions;
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.RegularExpressions;
 
 namespace IATETerminologyProvider.Helpers
 {
@@ -19,6 +21,18 @@ namespace IATETerminologyProvider.Helpers
 			}
 			// Return char and concat substring.
 			return char.ToUpper(s[0]) + s.Substring(1);
+		}
+
+		public static void AddDefaultParameters(HttpClient httpClient)
+		{
+			httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+			httpClient.DefaultRequestHeaders.Connection.Add("Keep-Alive");
+			httpClient.DefaultRequestHeaders.Add("Connection", "Keep-Alive");
+			httpClient.DefaultRequestHeaders.Add("Pragma", "no-cache");
+			httpClient.DefaultRequestHeaders.Add("Origin", "https://iate.europa.eu");
+			httpClient.DefaultRequestHeaders.Add("Access-Control-Allow-Origin", "*");
+			httpClient.DefaultRequestHeaders.Add("Host", "iate.europa.eu");
+			httpClient.DefaultRequestHeaders.Add("Cache-Control", "no-cache");
 		}
 	}
 }
