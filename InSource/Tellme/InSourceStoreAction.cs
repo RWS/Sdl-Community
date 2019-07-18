096f3344746f08a4ff5a2316e417618e6de3a805
andreaGhisa <aghisa@sdl.com>
2019-07-18 12:45:29 +0300

    InSource SR2: Added tell me support.

---
+++ InSource/Tellme/InSourceStoreAction.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+using Sdl.TellMe.ProviderApi;
+
+namespace Sdl.Community.InSource.Tellme
+{
+	public class InSourceStoreAction : AbstractTellMeAction
+	{
+		public InSourceStoreAction()
+		{
+			Name = "Download InSource from AppStore";
+		}
+		public override void Execute()
+		{
+			Process.Start("https://appstore.sdl.com/language/app/sdl-insource/548/");
+		}
+
+		public override bool IsAvailable => true;
+		public override string Category => "InSource results";
+		public override Icon Icon => PluginResources.TellMe1;
+	}
+}
