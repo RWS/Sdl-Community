0a9ff296752d5d972f2d09c210f41ea4d2f98c21
Emanuel Albu <ealbu@sdl.com>
2019-08-08 16:03:40 +0300

    DSIViewer_2017 -> adding app to Studio2017 branch

---
+++ DSI Viewer/Sdl.Community.SdiViewer/DsiWpfControl.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Sdl.Community.DsiViewer
+{
+	/// <summary>
+	/// Interaction logic for SdiWpfControl.xaml
+	/// </summary>
+	public partial class SdiWpfControl : UserControl
+	{
+		public SdiWpfControl()
+		{
+			InitializeComponent();
+		}
+	}
+}
