10a881934ea4fbca8d12d838e7ee4db88f371e5e
Patrick Andrew Hartnett <phartnett@sdl.com>
2019-08-13 10:36:42 +0200

    ADF: minor refactoring after cherry pick from master

--- AdvancedDisplayFilter/PluginResources.Designer.cs
+++ AdvancedDisplayFilter/PluginResources.Designer.cs
@@ -19,7 +19,7 @@ namespace Sdl.Community.Plugins.AdvancedDisplayFilter {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class PluginResources {
@@ -57,16 +57,6 @@ namespace Sdl.Community.Plugins.AdvancedDisplayFilter {
             }
             set {
                 resourceCulture = value;
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
-        /// </summary>
-        internal static System.Drawing.Icon AddComment {
-            get {
-                object obj = ResourceManager.GetObject("AddComment", resourceCulture);
-                return ((System.Drawing.Icon)(obj));
             }
         }
         
