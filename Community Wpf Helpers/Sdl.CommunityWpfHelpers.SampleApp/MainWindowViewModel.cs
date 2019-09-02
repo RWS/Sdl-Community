fbfb303709067bae0a6b21f22ef90666fcf0850d
andreaGhisa <aghisa@sdl.com>
2019-09-02 14:23:25 +0300

    Wpf helpers: Window close behaviour.

--- Community Wpf Helpers/Sdl.CommunityWpfHelpers.SampleApp/MainWindowViewModel.cs
+++ Community Wpf Helpers/Sdl.CommunityWpfHelpers.SampleApp/MainWindowViewModel.cs
@@ -10,10 +10,13 @@ namespace Sdl.CommunityWpfHelpers.SampleApp
 		private ICommand _parameterCommand;
 		private ICommand _sampleCommand;
 		private ICommand _awaitableCommand;
+		private ICommand _windowClosingCommand;
+
 
 		public ICommand ParameterCommand =>_parameterCommand ?? (_parameterCommand = new RelayCommand(ParameterCommandAction));
 		public ICommand SampleCommand => _sampleCommand ?? (_sampleCommand = new CommandHandler(SampleAction, true));
 		public ICommand AwaitableCommand => _awaitableCommand ?? (_awaitableCommand = new AwaitableCommand(AwaitableAction));
+		public ICommand WindowClosingCommand => _windowClosingCommand ??(_windowClosingCommand = new CommandHandler(OnWindowClosing, true));
 
 		private async Task AwaitableAction()
 		{
@@ -28,6 +31,10 @@ namespace Sdl.CommunityWpfHelpers.SampleApp
 		private void ParameterCommandAction(object commandParameter)
 		{
 			var text = (string) commandParameter;
+		}
+
+		private void OnWindowClosing()
+		{
 		}
 	}
 }
