36a9f5ebd7c1d5fdc753ab4675aee2a93fc516e2
andreaGhisa <aghisa@sdl.com>
2019-09-06 09:00:12 +0300

    Merge file plugin example.

--- Code samples/ProjectCredentialsTest/ProjectCredentialsTest/MyCustomTradosStudio.cs
+++ Code samples/ProjectCredentialsTest/ProjectCredentialsTest/MyCustomTradosStudio.cs
@@ -1,22 +1,38 @@
-﻿using System;
-using Sdl.Desktop.IntegrationApi;
+﻿using Sdl.Desktop.IntegrationApi;
 using Sdl.Desktop.IntegrationApi.Extensions;
+using Sdl.ProjectAutomation.Core;
 using Sdl.TranslationStudioAutomation.IntegrationApi;
 using Sdl.TranslationStudioAutomation.IntegrationApi.Presentation.DefaultLocations;
 
 namespace ProjectCredentialsTest
 {
-	[Action("ProjectCredentialsTest")]
+	//[Action("ProjectCredentialsTest")]
+	//[ActionLayout(typeof(TranslationStudioDefaultContextMenus.ProjectsContextMenuLocation))]
+	//public class MyCustomTradosStudio : AbstractAction
+	//{
+	//	protected override void Execute()
+	//	{
+	//		var termbaseUri = new Uri("");
+
+
+	//		var projectController = SdlTradosStudio.Application.GetController<ProjectsController>();
+	//		projectController.CurrentProject.Credentials.AddCredential(termbaseUri, true, "", "");
+	//	}
+	//}
+	[Action("Merge Project files")]
 	[ActionLayout(typeof(TranslationStudioDefaultContextMenus.ProjectsContextMenuLocation))]
 	public class MyCustomTradosStudio : AbstractAction
 	{
 		protected override void Execute()
 		{
-			var termbaseUri = new Uri("");
-
-
 			var projectController = SdlTradosStudio.Application.GetController<ProjectsController>();
-			projectController.CurrentProject.Credentials.AddCredential(termbaseUri, true, "", "");
+			var selectedProject = projectController?.CurrentProject;
+			if (selectedProject != null)
+			{
+				var sourceFiles = selectedProject.GetSourceLanguageFiles();
+				selectedProject.SetFileRole(sourceFiles.GetIds(), FileRole.Translatable);
+				selectedProject.CreateMergedProjectFile("MergedFile.sdlxliff", string.Empty, sourceFiles.GetIds());
+			}
 		}
 	}
 }
