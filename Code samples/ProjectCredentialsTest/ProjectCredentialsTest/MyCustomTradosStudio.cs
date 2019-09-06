1862f13698cbedbbd6e768652d09298c98fc19ec
andreaGhisa <aghisa@sdl.com>
2019-09-06 15:33:56 +0300

    Added ui to sample plugin to test windows credentials.

--- Code samples/ProjectCredentialsTest/ProjectCredentialsTest/MyCustomTradosStudio.cs
+++ Code samples/ProjectCredentialsTest/ProjectCredentialsTest/MyCustomTradosStudio.cs
@@ -1,4 +1,6 @@
-﻿using Sdl.Desktop.IntegrationApi;
+﻿using System;
+using System.Windows.Forms;
+using Sdl.Desktop.IntegrationApi;
 using Sdl.Desktop.IntegrationApi.Extensions;
 using Sdl.ProjectAutomation.Core;
 using Sdl.TranslationStudioAutomation.IntegrationApi;
@@ -6,33 +8,36 @@ using Sdl.TranslationStudioAutomation.IntegrationApi.Presentation.DefaultLocatio
 
 namespace ProjectCredentialsTest
 {
-	//[Action("ProjectCredentialsTest")]
-	//[ActionLayout(typeof(TranslationStudioDefaultContextMenus.ProjectsContextMenuLocation))]
-	//public class MyCustomTradosStudio : AbstractAction
-	//{
-	//	protected override void Execute()
-	//	{
-	//		var termbaseUri = new Uri("");
-
-
-	//		var projectController = SdlTradosStudio.Application.GetController<ProjectsController>();
-	//		projectController.CurrentProject.Credentials.AddCredential(termbaseUri, true, "", "");
-	//	}
-	//}
-	[Action("Merge Project files")]
+	[Action("ProjectCredentialsTest")]
 	[ActionLayout(typeof(TranslationStudioDefaultContextMenus.ProjectsContextMenuLocation))]
 	public class MyCustomTradosStudio : AbstractAction
 	{
 		protected override void Execute()
 		{
-			var projectController = SdlTradosStudio.Application.GetController<ProjectsController>();
-			var selectedProject = projectController?.CurrentProject;
-			if (selectedProject != null)
+			var form = new Form1();
+			var dialog =form.ShowDialog();
+			if (dialog == DialogResult.OK)
 			{
-				var sourceFiles = selectedProject.GetSourceLanguageFiles();
-				selectedProject.SetFileRole(sourceFiles.GetIds(), FileRole.Translatable);
-				selectedProject.CreateMergedProjectFile("MergedFile.sdlxliff", string.Empty, sourceFiles.GetIds());
+				var tmUri = new Uri(form.Url);
+				var projectController = SdlTradosStudio.Application.GetController<ProjectsController>();
+				projectController.CurrentProject?.Credentials?.AddCredential(tmUri, true, "", "");
 			}
 		}
 	}
+	//[Action("Merge Project files")]
+	//[ActionLayout(typeof(TranslationStudioDefaultContextMenus.ProjectsContextMenuLocation))]
+	//public class MyCustomTradosStudio : AbstractAction
+	//{
+	//	protected override void Execute()
+	//	{
+	//		var projectController = SdlTradosStudio.Application.GetController<ProjectsController>();
+	//		var selectedProject = projectController?.CurrentProject;
+	//		if (selectedProject != null)
+	//		{
+	//			var sourceFiles = selectedProject.GetSourceLanguageFiles();
+	//			selectedProject.SetFileRole(sourceFiles.GetIds(), FileRole.Translatable);
+	//			selectedProject.CreateMergedProjectFile("MergedFile.sdlxliff", string.Empty, sourceFiles.GetIds());
+	//		}
+	//	}
+	//}
 }
