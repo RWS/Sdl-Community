5d1ec80deab7931bd50a26ddc10442fb95b8642e
Emanuel Albu <ealbu@sdl.com>
2019-09-17 09:34:20 +0300

    Deleting GS Version Fetch from this repository

--- GroupShare VersionFetch/GroupShare Version Fetch/Model/ProjectFilter.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Sdl.Community.GSVersionFetch.Model
-{
-    public class ProjectFilter
-    {
-	    public int Page { get; set; }
-	    public int PageSize { get; set; }
-	    public Filter Filter { get; set; }
-    }
-}
