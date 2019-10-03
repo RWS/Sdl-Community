170fcda7ab3478a36722868a463c36fc5853e3ce
Emanuel Albu <ealbu@sdl.com>
2019-10-03 16:30:40 +0300

    BeGlobalV4 TP 2.2.9.1 -> 2.2.10.0  - adds information regarding the engines and language pairs to be serialized

--- Be GlobalV4 Translation Provider/Sdl.Community.BeGlobalV4.Provider/Studio/BeGlobalTranslationOptions.cs
+++ Be GlobalV4 Translation Provider/Sdl.Community.BeGlobalV4.Provider/Studio/BeGlobalTranslationOptions.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 using Sdl.Community.BeGlobalV4.Provider.Model;
 using Sdl.Community.BeGlobalV4.Provider.Service;
 using Sdl.LanguagePlatform.TranslationMemoryApi;
@@ -7,44 +9,61 @@ namespace Sdl.Community.BeGlobalV4.Provider.Studio
 {
 	public class BeGlobalTranslationOptions
 	{
+		private readonly TranslationProviderUriBuilder _uriBuilder;
+
 		public BeGlobalTranslationOptions()
 		{
 			_uriBuilder = new TranslationProviderUriBuilder("beglobaltranslationprovider");
 		}
+
 		public BeGlobalTranslationOptions(Uri uri)
 		{
 			_uriBuilder = new TranslationProviderUriBuilder(uri);
 		}
-		public SubscriptionInfo SubscriptionInfo { get; set; }
-		public BeGlobalV4Translator BeGlobalService { get; set; }
-		
-		public Uri Uri => _uriBuilder.Uri;		 
-		private readonly TranslationProviderUriBuilder _uriBuilder;
 
+		[JsonIgnore]
+		public BeGlobalV4Translator BeGlobalService { get; set; }
+
+		public string DisplayName { get; set; }
+
+		public Dictionary<string, string> LanguagesSupported { get; set; }
+
+		[JsonIgnore]
 		public string Model
 		{
 			get => GetStringParameter("model");
 			set => SetStringParameter("model", value);
 		}
 
-		public string ResendDraftsParameter
-		{
-			get => GetStringParameter("resenddrafts");
-			set => SetStringParameter("resenddrafts", value);
-		}
+		[JsonIgnore]
 		public bool ResendDrafts
 		{
 			get => ResendDraftsParameter != null && Convert.ToBoolean(ResendDraftsParameter);
 			set => ResendDraftsParameter = value.ToString();
 		}
+
+		[JsonIgnore]
+		public string ResendDraftsParameter
+		{
+			get => GetStringParameter("resenddrafts");
+			set => SetStringParameter("resenddrafts", value);
+		}
+
+		[JsonIgnore]
+		public SubscriptionInfo SubscriptionInfo { get; set; }
+
+		[JsonIgnore]
+		public Uri Uri => _uriBuilder.Uri;
+
 		private string GetStringParameter(string p)
 		{
 			var paramString = _uriBuilder[p];
 			return paramString;
 		}
+
 		private void SetStringParameter(string p, string value)
 		{
 			_uriBuilder[p] = value;
-		} 
+		}
 	}
 }
