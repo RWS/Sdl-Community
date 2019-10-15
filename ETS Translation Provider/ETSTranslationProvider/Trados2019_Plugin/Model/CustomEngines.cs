53a04e4968219deb3b50b714809de8e5c6d96616
andreaGhisa <aghisa@sdl.com>
2019-10-15 15:15:14 +0300

    ETS: Mapped ptb source language to por engine.

--- ETS Translation Provider/ETSTranslationProvider/Trados2019_Plugin/Model/CustomEngines.cs
+++ ETS Translation Provider/ETSTranslationProvider/Trados2019_Plugin/Model/CustomEngines.cs
@@ -9,11 +9,15 @@ namespace ETSTranslationProvider.Model
 		{
 			FrenchCanadaEngineCode = "FRC";
 			SpanishLatinAmericanEngineCode = "ESL";
+			PortugueseSourceEngineCode = "POR";
 			LatinAmericanLanguageCodes = new List<string>
 			{
 				"ESS","ESV","ESB","ZZZ","ESL","ESO","ESC","ESK","ESD","ESF","ESE","ESG","ESH","ESJ","ESP","ESM","ESP","ESI","ESA","ESZ","ESR","ESU","EST","ESY"
 			};
+			
 		}
+
+		public string PortugueseSourceEngineCode { get; set; }
 
 		public string FrenchCanadaEngineCode { get; set; }
 		public string SpanishLatinAmericanEngineCode { get; set; }
