f4cbaef3c6eabccc53da3b03ee0921cc91abd1ee
Emanuel Albu <ealbu@sdl.com>
2019-11-13 14:53:49 +0200

    AnalyzeFilesTestApp -> adding project to CodeSamples

---
+++ Code samples/AnalyzeFilesTestApp/AnalyzeFilesTestApp/MyCustomTradosStudio.cs
@@ -0,0 +1,54 @@
+﻿using Sdl.Core.Globalization;
+using Sdl.Desktop.IntegrationApi;
+using Sdl.Desktop.IntegrationApi.Extensions;
+using Sdl.ProjectAutomation.Core;
+using Sdl.ProjectAutomation.FileBased;
+using Sdl.ProjectAutomation.Settings;
+using Sdl.TranslationStudioAutomation.IntegrationApi.Presentation.DefaultLocations;
+
+namespace AnalyzeFilesTestApp
+{
+	[Action("AnalyzeFilesTestApp")]
+	[ActionLayout(typeof(TranslationStudioDefaultContextMenus.ProjectsContextMenuLocation))]
+	public class MyCustomTradosStudio : AbstractAction
+	{
+		protected override void Execute()
+		{
+			var projectInfo = new ProjectInfo
+			{
+				Name = "",
+				SourceLanguage = new Language("en-US"),
+				TargetLanguages = new[] { new Language("de-DE") },
+				LocalProjectFolder = "",
+			};
+
+			var fileBasedProject = new FileBasedProject(projectInfo);
+
+			var settings = fileBasedProject.GetSettings();
+			var analysisSettings = settings.GetSettingsGroup<AnalysisTaskSettings>();
+
+			analysisSettings.ReportCrossFileRepetitions.Value = false;
+			fileBasedProject.UpdateSettings(settings);
+
+			var projectFiles = fileBasedProject.AddFiles(new[] { "" });
+
+			fileBasedProject.RunAutomaticTask(projectFiles.GetIds(), AutomaticTaskTemplateIds.Scan);
+
+			var taskSequence = fileBasedProject.RunAutomaticTasks(projectFiles.GetIds(), new[]
+			{
+				AutomaticTaskTemplateIds.ConvertToTranslatableFormat,
+				AutomaticTaskTemplateIds.CopyToTargetLanguages,
+				AutomaticTaskTemplateIds.PerfectMatch,
+				AutomaticTaskTemplateIds.PreTranslateFiles,
+				AutomaticTaskTemplateIds.AnalyzeFiles,
+				AutomaticTaskTemplateIds.UpdateMainTranslationMemories,
+			});
+
+			if (taskSequence.Status != TaskStatus.Completed)
+			{
+			}
+
+			fileBasedProject.Save();
+		}
+	}
+}
