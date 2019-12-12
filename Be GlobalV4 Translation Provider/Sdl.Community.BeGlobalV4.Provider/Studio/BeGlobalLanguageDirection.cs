427b820c97a44237c55f666ef019cbdb198e50ee
TinaCaputa <caputa.florentina.lucia@gmail.com>
2019-12-12 12:43:30 +0200

    Update translation so translation units are identified correctly based on source segment

--- Be GlobalV4 Translation Provider/Sdl.Community.BeGlobalV4.Provider/Studio/BeGlobalLanguageDirection.cs
+++ Be GlobalV4 Translation Provider/Sdl.Community.BeGlobalV4.Provider/Studio/BeGlobalLanguageDirection.cs
@@ -125,7 +125,7 @@ namespace Sdl.Community.BeGlobalV4.Provider.Studio
 			var alreadyTranslatedSegments = new List<BeGlobalSegment>();
 
 			if (!_options.ResendDrafts)
-			{				
+			{
 				// Re-send draft segment logic
 				for (var segmentIndex = 0; segmentIndex < segments.Length; segmentIndex++)
 				{
@@ -142,9 +142,10 @@ namespace Sdl.Community.BeGlobalV4.Provider.Studio
 						continue;
 					}
 
-					// get translationUnit based on the source segment from segments received from Studio
-					var correspondingTu = _translationUnits.FirstOrDefault(t => segments.Any(s => s == t.SourceSegment));
-				
+					// Set translation unit based on segment index: when the first 10 segments are translated for the first time,
+					// then the TU index is the same as segment index
+					var correspondingTu = _translationUnits[segmentIndex];
+
 					// If activeSegmentPair is not null, it means the user translates segments through Editor
 					// If activeSegmentPair is null, it means the user executes Pre-Translate Batch task, so he does not navigate through segments in editor
 					var documentLastOpenPath = _translationUnits[0]?.DocumentProperties?.LastOpenedAsPath;
@@ -363,9 +364,8 @@ namespace Sdl.Community.BeGlobalV4.Provider.Studio
 			if (mask == null || mask.Length != translationUnits.Length)
 				throw new ArgumentException("Mask in SearchSegmentsMasked");
 
-			var translationUnit = translationUnits.Where((seg, i) => mask == null || mask[i]).ToArray();
 			_translationUnits.Clear();
-			_translationUnits.AddRange(translationUnit);
+			_translationUnits.AddRange(translationUnits);
 			return SearchSegments(settings, translationUnits.Select(tu => tu?.SourceSegment).ToArray(), mask);
 		}
 
