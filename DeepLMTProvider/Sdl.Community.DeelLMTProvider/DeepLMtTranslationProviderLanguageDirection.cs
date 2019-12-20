a9e26bce813a84245132ac2612b539d48949322c
andreaGhisa <aghisa@sdl.com>
2019-12-20 14:19:46 +0200

    DeepL: Set time out, removed source already translated message.

--- DeepLMTProvider/Sdl.Community.DeelLMTProvider/DeepLMtTranslationProviderLanguageDirection.cs
+++ DeepLMTProvider/Sdl.Community.DeelLMTProvider/DeepLMtTranslationProviderLanguageDirection.cs
@@ -70,15 +70,6 @@ namespace Sdl.Community.DeepLMTProvider
 
 			try
 			{
-				// if there are match in tm the provider will not search the segment
-				#region "Confirmation Level"
-				if (!_options.ResendDrafts && _inputTu.ConfirmationLevel != ConfirmationLevel.Unspecified)
-				{
-					translation.Add(PluginResources.TranslationLookupDraftNotResentMessage);
-					//later get these strings from resource file
-					results.Add(CreateSearchResult(segment, translation));
-					return results;
-				}
 				var newseg = segment.Duplicate();
 				if (newseg.HasTags)
 				{
@@ -105,7 +96,6 @@ namespace Sdl.Community.DeepLMTProvider
 					}
 
 				}
-				#endregion
 			}
 			catch (Exception e)
 			{
