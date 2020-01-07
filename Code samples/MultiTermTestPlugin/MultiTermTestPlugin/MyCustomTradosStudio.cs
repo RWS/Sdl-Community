1b09bbf2b0395a8493dd8b29d92d0be062d3ebb8
andreaGhisa <aghisa@sdl.com>
2020-01-07 11:24:25 +0200

    Multiterm Investigation: Add a new concept with English and German term for a termbase.

--- Code samples/MultiTermTestPlugin/MultiTermTestPlugin/MyCustomTradosStudio.cs
+++ Code samples/MultiTermTestPlugin/MultiTermTestPlugin/MyCustomTradosStudio.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using MultiTermIX;
 using Sdl.Desktop.IntegrationApi;
 using Sdl.Desktop.IntegrationApi.Extensions;
@@ -13,13 +16,25 @@ namespace MultiTermTestPlugin
 		protected override void Execute()
 		{
 			Application oMt = new ApplicationClass();
+			var localRep = oMt.LocalRepository;
+			localRep.Connect("", "");
+			var termbases = localRep.Termbases;
+			var path = "";// termbase local path
+			termbases.Add(path,"","");
+			var termbase = termbases[path];
+			var entries = termbase.Entries;
+			//Concept with terms for English and German
+			var entryText =
+				"<conceptGrp><languageGrp><language type=\"English\" lang=\"en-US\"></language><termGrp><term>for</term></termGrp></languageGrp><languageGrp><language type=\"German\" lang=\"de-DE\"></language><termGrp><term>für</term></termGrp></languageGrp></conceptGrp>";
+			entries.New(entryText, true);
 
-			var oServerRep = oMt.ServerRepository;
-			oServerRep.Location = "";
-			oServerRep.Connect("", "");
-			Console.WriteLine("Connection successful: " + oServerRep.IsConnected);
+			//var oServerRep = oMt.ServerRepository;
+			//oServerRep.Location = "";
+			//oServerRep.Connect("", "");
+			//Console.WriteLine("Connection successful: " + oServerRep.IsConnected);
 
-			var oTbs = oServerRep.Termbases;
+			//var oTbs = oServerRep.Termbases[0];
+			//oTbs.Entries.New()
 		}
 	}
 }
