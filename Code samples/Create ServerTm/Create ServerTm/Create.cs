d174470fbd232b3b3c157015741d95d66e81b64b
andreaGhisa <aghisa@sdl.com>
2020-02-03 15:17:22 +0200

    CodeSamples: Import xliff to existing ServerTm.

--- Code samples/Create ServerTm/Create ServerTm/Create.cs
+++ Code samples/Create ServerTm/Create ServerTm/Create.cs
@@ -1,9 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Sdl.Desktop.IntegrationApi;
 using Sdl.Desktop.IntegrationApi.Extensions;
 using Sdl.LanguagePlatform.TranslationMemoryApi;
@@ -46,6 +44,24 @@ namespace Create_ServerTm
 			};
 
 			directionsCollection.Add(direction);
+		}
+
+		private void ImportXliffToExistingTm()
+		{
+			var uri = new Uri(@"uri");
+
+			var translationProviderServer = new TranslationProviderServer(uri, false, "user name", "password");
+
+			//Import to existing tm
+			var guid = new Guid("Existing TM Guid");
+			var tm = translationProviderServer.GetTranslationMemory(guid, TranslationMemoryProperties.None);
+			var langDirection = tm.LanguageDirections;
+
+			var import = new ScheduledServerTranslationMemoryImport(langDirection[0])
+			{
+				Source = new FileInfo(@"path to xliff")
+			};
+			import.Queue();
 		}
 	}
 }
