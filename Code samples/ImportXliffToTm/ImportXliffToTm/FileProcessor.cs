5a68bad70a576778d50bf597a0db8f89d057ac70
andreaGhisa <aghisa@sdl.com>
2020-02-05 15:13:45 +0200

    Code sample - Import xliff into tm.

---
+++ Code samples/ImportXliffToTm/ImportXliffToTm/FileProcessor.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sdl.FileTypeSupport.Framework.Bilingual;
+using Sdl.FileTypeSupport.Framework.BilingualApi;
+using Sdl.FileTypeSupport.Framework.NativeApi;
+using Sdl.LanguagePlatform.TranslationMemory;
+using Sdl.LanguagePlatform.TranslationMemoryApi;
+
+namespace ImportXliffToTm
+{
+	public class FileProcessor: AbstractBilingualContentProcessor
+	{
+		private readonly FileBasedTranslationMemory _tm;
+		private readonly  string _fileName;
+
+		public FileProcessor(FileBasedTranslationMemory tm,string fileName)
+		{
+			_tm = tm;
+			_fileName = fileName;
+		}
+
+		public override void ProcessParagraphUnit(IParagraphUnit paragraphUnit)
+		{
+			base.ProcessParagraphUnit(paragraphUnit);
+			if (paragraphUnit.IsStructure) { return; }
+
+			foreach (var segmentPair in paragraphUnit.SegmentPairs.ToList())
+			{
+				var tu = new TranslationUnit
+				{
+					SourceSegment = new Sdl.LanguagePlatform.Core.Segment(_tm.LanguageDirection.SourceLanguage),
+					TargetSegment = new Sdl.LanguagePlatform.Core.Segment(_tm.LanguageDirection.TargetLanguage),
+				};
+				tu.SourceSegment.Add(segmentPair.Source.ToString());
+				tu.TargetSegment.Add(segmentPair.Target.ToString());
+				var fields = _tm.FieldDefinitions;
+
+				foreach (var field in fields)
+				{
+					if (field.Name.Equals("FileName"))
+					{
+						var value = field.CreateValue();
+						value.Add(_fileName);
+						tu.FieldValues.Add(value);
+					}
+				}
+
+				//var fileNameField = fields.FirstOrDefault(f => f.Name.Equals("FileName"));
+				//if (fileNameField != null)
+				//{
+				//	var value = fileNameField.CreateValue();
+				//	value.Add(_fileName);
+				//	tu.FieldValues.Add(value);
+				//}
+
+				//var containsSegIdField = fields.FirstOrDefault(f => f.Name.Equals("SegmentId"));
+				//if (containsSegIdField == null)
+				//{
+				//	var segmentIdFieldDefinition = new FieldDefinition("SegmentId", FieldValueType.MultipleString);
+				//	var segmentIdValue = segmentIdFieldDefinition.CreateValue();
+				//	segmentIdValue.Add(segmentPair.Properties.Id.Id);
+
+				//	tu.FieldValues.Add(segmentIdValue);
+				//}
+				_tm.LanguageDirection.AddTranslationUnit(tu, GetImportSettings());
+			}
+			_tm.Save();
+		}
+
+		private ImportSettings GetImportSettings()
+		{
+			var settings = new ImportSettings
+			{
+				CheckMatchingSublanguages = true,
+				ExistingFieldsUpdateMode = ImportSettings.FieldUpdateMode.Merge
+			};
+
+			return settings;
+		}
+	}
+}
---
+++ Code samples/ImportXliffToTm/ImportXliffToTm/MyCustomTradosStudio.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Sdl.Desktop.IntegrationApi;
+using Sdl.Desktop.IntegrationApi.Extensions;
+using Sdl.FileTypeSupport.Framework.Core.Utilities.BilingualApi;
+using Sdl.FileTypeSupport.Framework.Core.Utilities.IntegrationApi;
+using Sdl.FileTypeSupport.Framework.Integration;
+using Sdl.LanguagePlatform.TranslationMemory;
+using Sdl.LanguagePlatform.TranslationMemoryApi;
+using Sdl.TranslationStudioAutomation.IntegrationApi;
+using Sdl.TranslationStudioAutomation.IntegrationApi.Extensions;
+
+namespace ImportXliffToTm
+{
+	[ApplicationInitializer]
+	public class MyCustomTradosStudio : IApplicationInitializer
+	{
+		public void Execute()
+		{
+			//ImportInTmWithoutFields(
+			//	@"C:\Users\aghisa\Documents\Studio 2019\Translation Memories\SDLTMPortTestWithoutFields.sdltm");
+			ImportInTmWithNameField(@"C:\Users\aghisa\Documents\Studio 2019\Translation Memories\SDLImportFields.sdltm");
+		}
+
+		private void ImportInTmWithoutFields(string tmPath)
+		{
+			var tm = new FileBasedTranslationMemory(tmPath);
+			var tmImporter = new TranslationMemoryImporter(tm.LanguageDirection);
+			tmImporter.BatchImported += TmImporter_BatchImported;
+			tmImporter.ChunkSize = 20;
+			var importSettings = tmImporter.ImportSettings;
+			importSettings.ExistingFieldsUpdateMode = ImportSettings.FieldUpdateMode.Merge;
+			ImportFiles(tmImporter);
+		}
+
+		private void ImportInTmWithNameField(string tmPath)
+		{
+			var tm = new FileBasedTranslationMemory(tmPath);
+			var files = new List<string>
+			{
+				@"C:\Users\aghisa\Documents\Studio 2019\Projects\SDLTMImportEn-DE\de-de\first.txt.sdlxliff",
+				@"C:\Users\aghisa\Documents\Studio 2019\Projects\SDLTMImportEn-DE\de-de\second.txt.sdlxliff"
+			};
+
+			foreach (var file in files)
+			{
+				var converter = DefaultFileTypeManager.CreateInstance(true).GetConverterToDefaultBilingual(file,file, null);
+				var fileInfo = new FileInfo(file);
+				var contentProcessor = new FileProcessor(tm,fileInfo.Name);
+				converter.AddBilingualProcessor(new BilingualContentHandlerAdapter(contentProcessor));
+				converter.Parse();
+			}
+		
+
+			//var tmImporter = new TranslationMemoryImporter(tm.LanguageDirection);
+			//tmImporter.BatchImported += TmImporter_BatchImported;
+			//tmImporter.ChunkSize = 20;
+			//var importSettings = tmImporter.ImportSettings;
+			//importSettings.ExistingFieldsUpdateMode = ImportSettings.FieldUpdateMode.Merge;
+			//var fields = tm.FieldDefinitions;
+
+
+
+		
+			//tm.Save();
+			//ImportFiles(tmImporter);
+		}
+
+		private void ImportFiles(TranslationMemoryImporter tmImporter)
+		{
+			tmImporter.Import(@"C:\Users\aghisa\Documents\Studio 2019\Projects\SDLTMImportEn-DE\de-de\first.txt.sdlxliff");
+			tmImporter.Import(@"C:\Users\aghisa\Documents\Studio 2019\Projects\SDLTMImportEn-DE\de-de\second.txt.sdlxliff");
+		}
+
+		private void TmImporter_BatchImported(object sender, BatchImportedEventArgs e)
+		{
+			string info;
+			var stats = e.Statistics;
+
+			info = "Total read: " + stats.TotalRead + "\n";
+			info += "Total imported: " + stats.TotalImported + "\n";
+			info += "TUs added: " + stats.AddedTranslationUnits + "\n";
+			info += "TUs discarded: " + stats.DiscardedTranslationUnits + "\n";
+			info += "TUs merged: " + stats.MergedTranslationUnits + "\n";
+			info += "Errors: " + stats.Errors + "\n";
+		}
+	}
+}
