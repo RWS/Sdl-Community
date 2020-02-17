e5595945f971d06f24ba53ba12da0f075e7352c7
Emanuel Albu <ealbu@sdl.com>
2020-02-17 11:32:08 +0200

    Adding code sample for creating a project using a template

--- Code samples/FileBasedProjectPublish/FileBasedProjectPublish/Project.cs
+++ Code samples/FileBasedProjectPublish/FileBasedProjectPublish/Project.cs
@@ -5,6 +5,7 @@ using Sdl.ProjectAutomation.Core;
 using Sdl.ProjectAutomation.FileBased;
 using System;
 using System.IO;
+using Sdl.TranslationStudioAutomation.IntegrationApi.Presentation.DefaultLocations;
 
 namespace FileBasedProjectPublish
 {
@@ -16,7 +17,6 @@ namespace FileBasedProjectPublish
         public void Execute()
         {	  
 			var groupShareServer = "GS server address";
-
 			var projectFolder = Path.Combine(Path.GetTempPath(), "ProjectTemp");
 			if (!Directory.Exists(projectFolder))
 			{
@@ -60,4 +60,57 @@ namespace FileBasedProjectPublish
 			}
 		}	  
 	}
+
+	// same thing done as a button in project context menu; and with a template used for the project creation
+	// code comented as the two do almost the same thing and should be used separately
+	//[Action("CreateAndPublishGSProject")]
+ //   [ActionLayout(typeof(TranslationStudioDefaultContextMenus.ProjectsContextMenuLocation))]
+ //   public class CreateGsProjectUsingTemplate : AbstractAction
+ //   {
+ //       protected override void Execute()
+ //       {
+ //           //plugin which uses project template to create a new server
+ //           var groupShareServer = "[serverUri]";
+
+ //           var projectFolder = Path.Combine(Path.GetTempPath(), "ProjectTemp");
+ //           if (!Directory.Exists(projectFolder))
+ //           {
+ //               Directory.CreateDirectory(projectFolder);
+ //           }
+
+ //           var projectInfo = new ProjectInfo
+ //           {
+ //               Name = "",
+ //               LocalProjectFolder = projectFolder,
+ //           };
+
+ //           // create new project and add a translatable file to it from the local machine
+ //           //using following template
+ //           var projectTemplate = new ProjectTemplateReference(
+ //               @"[pathToTemplate]");
+ //           var fileBasedProject = new FileBasedProject(projectInfo, projectTemplate);
+ //           var projectFiles = fileBasedProject.AddFiles(new[] {@"[pathToATestFile]"});
+
+ //           fileBasedProject.RunAutomaticTask(projectFiles.GetIds(), AutomaticTaskTemplateIds.Scan);
+ //           var taskSequence = fileBasedProject.RunDefaultTaskSequence(projectFiles.GetIds());
+
+ //           if (taskSequence.Status == TaskStatus.Completed)
+ //           {
+ //               fileBasedProject.Save();
+
+ //               // publish project to GS using Windows credentials
+ //               var result = fileBasedProject.PublishProject(
+ //                   new Uri(groupShareServer),
+ //                   false,
+ //                   "",
+ //                   "",
+ //                   "",
+ //                   null);
+ //           }
+ //           else
+ //           {
+ //               //taskSequence status can be used to see the errors in case the project was not created successfully
+ //           }
+ //       }
+ //   }
 }
