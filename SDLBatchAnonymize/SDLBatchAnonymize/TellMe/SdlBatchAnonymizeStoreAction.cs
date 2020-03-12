bc05f2882172cb78995b7c27468a25e426a505f7
andreaGhisa <aghisa@sdl.com>
2020-03-12 13:31:49 +0200

    SDL Batch Anonymize: Added Tell me support.

---
+++ SDLBatchAnonymize/SDLBatchAnonymize/TellMe/SdlBatchAnonymizeStoreAction.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+using Sdl.TellMe.ProviderApi;
+
+namespace Sdl.Community.SDLBatchAnonymize.TellMe
+{
+	public class SdlBatchAnonymizeStoreAction : AbstractTellMeAction
+	{
+		public override bool IsAvailable => true;
+		public override string Category => "SDL Batch Anonymize results";
+		public override Icon Icon => PluginResources.Download;
+
+		public SdlBatchAnonymizeStoreAction()
+		{
+			Name = "Download SDL Batch Anonymize from AppStore";
+		}
+
+		public override void Execute()
+		{
+			Process.Start("");
+		}
+	}
+}
---
+++ SDLBatchAnonymize/SDLBatchAnonymize/TellMe/SdlBatchAnonymizeTellMeProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sdl.TellMe.ProviderApi;
+
+namespace Sdl.Community.SDLBatchAnonymize.TellMe
+{
+	[TellMeProvider]
+	public class SdlBatchAnonymizeTellMeProvider: ITellMeProvider
+	{
+		public string Name => "SDL BatchAnonymize tell me provider";
+		public AbstractTellMeAction[] ProviderActions => new AbstractTellMeAction[]
+		{
+			new SdlBatchAnonymizeCommunitySupportAction
+			{
+				Keywords = new []{ "sdl batch anonymize", "sdlbatchanonymize", "sdl batch anonymize community", "sdl batch anonymize support" }
+			},
+			new SdlBatchAnonymizeStoreAction
+			{
+				Keywords = new []{ "sdl batch anonymize", "sdl batch anonymize store", "sdlbatchanonymize store", "sdl batch anonymize store download" }
+			}
+		};
+	}
+}
