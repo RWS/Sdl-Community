7d58f525bf58678c6505ffe1cd9c4e48d71c2ffb
andreaGhisa <aghisa@sdl.com>
2020-03-12 11:19:32 +0200

    SDL Batch Anonymizer initial commit.

---
+++ SDLBatchAnonymize/SDLBatchAnonymize/AnonymizerProcessor.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using Sdl.FileTypeSupport.Framework.BilingualApi;
+using Sdl.FileTypeSupport.Framework.Core.Utilities.NativeApi;
+using Sdl.FileTypeSupport.Framework.NativeApi;
+
+namespace Sdl.Community.SDLBatchAnonymize
+{
+	public class AnonymizerProcessor : AbstractBilingualContentProcessor
+	{
+		public override void ProcessParagraphUnit(IParagraphUnit paragraphUnit)
+		{
+			base.ProcessParagraphUnit(paragraphUnit);
+			if (paragraphUnit.IsStructure)
+			{
+				return;
+			}
+			foreach (var segmentPair in paragraphUnit.SegmentPairs.ToList())
+			{
+				Anonymize(segmentPair?.Properties?.TranslationOrigin);
+				Anonymize(segmentPair?.Properties?.TranslationOrigin?.OriginBeforeAdaptation);
+			}
+		}
+
+		private void Anonymize(ITranslationOrigin translationOrigin)
+		{
+			var originType = translationOrigin?.OriginType;
+			if (!string.IsNullOrEmpty(originType) && (originType.Equals(DefaultTranslationOrigin.MachineTranslation) || originType.Equals(DefaultTranslationOrigin.NeuralMachineTranslation)))
+			{
+				translationOrigin.OriginType = DefaultTranslationOrigin.Interactive;
+				translationOrigin.OriginSystem = string.Empty;
+			}
+		}
+	}
+}
