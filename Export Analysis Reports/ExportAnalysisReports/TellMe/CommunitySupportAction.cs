6e44732a8411d3d751b7d8ef65f44c50d9cd3f41
TinaCaputa <caputa.florentina.lucia@gmail.com>
2020-03-26 14:52:44 +0200

    Remove SDl.Community from the solution's name Add new feature to persist the Export output path. Upate plugin's version

---
+++ Export Analysis Reports/ExportAnalysisReports/TellMe/CommunitySupportAction.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+using Sdl.TellMe.ProviderApi;
+
+namespace ExportAnalysisReports.TellMe
+{
+	public class CommunitySupportAction : AbstractTellMeAction
+	{
+		public CommunitySupportAction()
+		{
+			Name = "SDL Community AppStore forum";
+		}
+		public override void Execute()
+		{
+			Process.Start("https://community.sdl.com/product-groups/translationproductivity/f/160");
+		}
+
+		public override bool IsAvailable => true;
+		public override string Category => "ExportAnalysisReports results";
+
+		public override Icon Icon => PluginResources.ForumIcon;
+	}
+}
