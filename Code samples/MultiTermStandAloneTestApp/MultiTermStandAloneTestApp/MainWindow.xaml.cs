0f2c9fa946fea82b3c2d2bd15d57f20da0cb71de
TinaCaputa <caputa.florentina.lucia@gmail.com>
2020-04-01 10:41:11 +0300

    Multiterm standalone code sample: Add code sample to get the number of term entries for each language using the standalone application

--- Code samples/MultiTermStandAloneTestApp/MultiTermStandAloneTestApp/MainWindow.xaml.cs
+++ Code samples/MultiTermStandAloneTestApp/MultiTermStandAloneTestApp/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System.Collections.Generic;
+using System.Windows;
+using MultiTermIX;
 
 namespace MultiTermStandAloneTestApp
 {
@@ -14,17 +16,41 @@ namespace MultiTermStandAloneTestApp
 
 		private void Button_Click(object sender, RoutedEventArgs e)
 		{
-			var app = new MultiTermIX.ApplicationClass();
-			app.ServerRepository.Location = "";
-			app.ServerRepository.Connect("user", "password");
+			var app = new ApplicationClass();
+			
+			// Termbase Server connection
+			//app.ServerRepository.Location = "";
+			//app.ServerRepository.Connect("user", "password");
+			//var termbases = app.ServerRepository.Termbases;
+			//app.ServerRepository.Disconnect();
 
-			var termbases = app.ServerRepository.Termbases;
-			app.ServerRepository.Disconnect();
-
-			//create local termbase
+			// Use local termbase
 			app.LocalRepository.Connect("", "");
 			var localTermbases = app.LocalRepository.Termbases;
-			localTermbases.New("testT", "ss", "", @"C:\Users\[username]\Desktop\test.sdltb");
+
+			//set the path to an existing termbase
+			var path = "";
+			localTermbases.Add(path, "", "");
+			var termbase = localTermbases[path];			
+			var numberOfEntriesForLanguage = GetNumberOfEntriesForEachLanguage(termbase);
+
+			// Add local termbase
+			//localTermbases.New("testT", "ss", "", @"C:\Users\{UserName}\Desktop\test.sdltb");
+		}
+
+		// Get number of entries for each language, using the Language Index (eg: "English")
+		private Dictionary<string,int> GetNumberOfEntriesForEachLanguage(Termbase localTermbase)
+		{
+			var numberOfEntries = new Dictionary<string, int>();
+			var termbaseInformation = localTermbase.Information;
+
+			var languageIndexes = localTermbase.Definition.Indexes;
+			foreach (Index languageIndex in languageIndexes)
+			{
+				var numberOfTermEntries = termbaseInformation.NumberOfEntriesInIndex[languageIndex.Language];
+				numberOfEntries.Add(languageIndex.Language, numberOfTermEntries);
+			}
+			return numberOfEntries;
 		}
 	}
 }
