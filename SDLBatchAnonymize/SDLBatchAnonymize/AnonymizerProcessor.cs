8a44a056a06da3fac0a2c8543bb3099498a21805
andreaGhisa <aghisa@sdl.com>
2020-04-08 15:07:18 +0300

    SDL Batch Anonymizer: Back up projects in a zip before running the batchtask.

--- SDLBatchAnonymize/SDLBatchAnonymize/AnonymizerProcessor.cs
+++ SDLBatchAnonymize/SDLBatchAnonymize/AnonymizerProcessor.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Globalization;
-using System.IO;
 using System.Linq;
 using Sdl.Community.SDLBatchAnonymize.Interface;
 using Sdl.FileTypeSupport.Framework.BilingualApi;
-using Sdl.FileTypeSupport.Framework.Core.Utilities.NativeApi;
-using Sdl.FileTypeSupport.Framework.NativeApi;
 
 namespace Sdl.Community.SDLBatchAnonymize
 {
@@ -14,7 +10,7 @@ namespace Sdl.Community.SDLBatchAnonymize
 		public static readonly Log Log = Log.Instance;
 		private readonly IBatchAnonymizerSettings _settings;
 		private readonly IUserNameService _usernameService;
-		private IResourceOriginsService _resourceOriginsService;
+		private readonly IResourceOriginsService _resourceOriginsService;
 
 		public AnonymizerProcessor(IBatchAnonymizerSettings settings, IUserNameService usernameService,IResourceOriginsService resourceOriginsService)
 		{
@@ -54,7 +50,6 @@ namespace Sdl.Community.SDLBatchAnonymize
 					{
 						_resourceOriginsService.RemoveTm(segmentPair, _settings);
 					}
-
 				}
 			}
 			catch (Exception exception)
@@ -62,7 +57,6 @@ namespace Sdl.Community.SDLBatchAnonymize
 				Log.Logger.Error($"{exception.Message}\n {exception.StackTrace}");
 			}
 		}
-		
 
 		public override void SetFileProperties(IFileProperties fileInfo)
 		{
@@ -70,34 +64,6 @@ namespace Sdl.Community.SDLBatchAnonymize
 			{
 				_usernameService.AnonymizeCommentAuthor(fileInfo, _settings.CommentAuthorName);
 			}
-		}
-
-		private bool IsAutomatedTranslated(ITranslationOrigin translationOrigin)
-		{
-			var originType = translationOrigin?.OriginType;
-
-			return !string.IsNullOrEmpty(originType) &&
-			       (originType.Equals(DefaultTranslationOrigin.MachineTranslation) ||
-			        originType.Equals(DefaultTranslationOrigin.NeuralMachineTranslation) ||
-			        originType.Equals(DefaultTranslationOrigin.AdaptiveMachineTranslation));
-		}
-
-		private void AnonymizeComplete(ITranslationOrigin translationOrigin)
-		{
-			translationOrigin.OriginType = DefaultTranslationOrigin.Interactive;
-			translationOrigin.OriginSystem = string.Empty;
-			translationOrigin.MatchPercent = byte.Parse("0");
-		}
-
-		private void AnonymizeTmMatch(ITranslationOrigin translationOrigin)
-		{
-			translationOrigin.OriginType = DefaultTranslationOrigin.TranslationMemory;
-			if (!string.IsNullOrEmpty(_settings.TmName))
-			{
-				translationOrigin.OriginSystem = Path.GetFileNameWithoutExtension(_settings.TmName);
-			}
-			var fuzzy = _settings.FuzzyScore.ToString(CultureInfo.InvariantCulture);
-			translationOrigin.MatchPercent= byte.Parse(fuzzy);
 		}
 	}
 }
