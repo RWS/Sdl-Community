0b95df1b0bd1af77a6a774ce21d5b019a37e93e7
andreaGhisa <aghisa@sdl.com>
2020-04-13 15:45:32 +0300

    IATE Refactoring: Removed GetEditorController methods.

--- IATETerminologyProvider/IATETerminologyProvider/IATETerminologyProviderAction.cs
+++ IATETerminologyProvider/IATETerminologyProvider/IATETerminologyProviderAction.cs
@@ -12,11 +12,6 @@ namespace IATETerminologyProvider
 	[RibbonGroupLayout(LocationByType = typeof(TranslationStudioDefaultViews.TradosStudioViewsLocation))]
 	public class IATETerminologyProviderAction: AbstractRibbonGroup
 	{
-        private static EditorController GetEditorController()
-		{
-			return SdlTradosStudio.Application.GetController<EditorController>();
-		}
-
 		private static SearchResultsViewerController GetSearchResultsController()
 		{
 			return SdlTradosStudio.Application.GetController<SearchResultsViewerController>();
@@ -28,7 +23,7 @@ namespace IATETerminologyProvider
 		/// <param name="isSearchAll"></param>
 		public static void NavigateToIATE(bool isSearchAll)
 		{
-			var editorController = GetEditorController();
+			var editorController = SdlTradosStudio.Application.GetController<EditorController>();
 			var activeDocument = editorController?.ActiveDocument;
 			if (activeDocument != null)
 			{
