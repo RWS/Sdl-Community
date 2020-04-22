92b3c9745de970dd6625af79058cf6e113d66af9
andreaGhisa <aghisa@sdl.com>
2020-04-22 17:32:19 +0300

    SDL Batch Anonymize: Remove File version and template id from sdl proj.

---
+++ SDLBatchAnonymize/Sdl.Community.SDLBatchAnonymizeUT/AnonymizeSdlProjServiceUnitTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sdl.Community.SDLBatchAnonymize.Interface;
+using Sdl.Community.SDLBatchAnonymize.Service;
+using Sdl.Core.Globalization;
+using Xunit;
+
+namespace Sdl.Community.SDLBatchAnonymizeUT
+{
+	public class AnonymizeSdlProjServiceUnitTest
+	{
+		private readonly IAnonymizeSdlProj _anonymizeSdlProjService;
+		public AnonymizeSdlProjServiceUnitTest()
+		{
+			_anonymizeSdlProjService = new AnonymizeSdlProjService();
+		}
+
+		[Theory]
+		[InlineData(@"C:\Users\aghisa\Documents\Studio 2019\Projects\AnonDel\AnonDel.sdlproj")]
+		public void AnonymizeFileVersions(string projectPath)
+		{
+			_anonymizeSdlProjService.RemoveFileVersionComment(projectPath);
+			_anonymizeSdlProjService.RemoveTemplateId(projectPath);
+		}
+	}
+}
