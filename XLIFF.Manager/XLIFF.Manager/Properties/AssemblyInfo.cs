1bb72b140e444eb6ed2e3b9cbc7fdd39e504a918
Patrick Andrew Hartnett <phartnett@sdl.com>
2020-04-27 17:53:34 +0200

    Add default datagrid to Project Files view

--- XLIFF.Manager/XLIFF.Manager/Properties/AssemblyInfo.cs
+++ XLIFF.Manager/XLIFF.Manager/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("XLIFF.Manager")]
+[assembly: AssemblyTitle("XLIFF Manager")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
