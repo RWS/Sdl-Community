7d74f14b6f4cb80427293ea65b0ec01abd8ead9b
TinaCaputa <caputa.florentina.lucia@gmail.com>
2020-05-14 13:27:41 +0300

    Migrate solution to Studio 2021

---
+++ InvoiceAndQuotes/Sdl.Community.InvoiceAndQuotes/Helpers/Utils.cs
@@ -0,0 +1,14 @@
+﻿using Sdl.Community.Toolkit.Core.Services;
+
+namespace Sdl.Community.InvoiceAndQuotes.Helpers
+{
+	public class Utils
+	{
+		public string GetStudioVersion()
+		{
+			var studioService = new StudioVersionService();
+			var shortVersion = studioService.GetStudioVersion()?.ShortVersion;
+			return $"Studio {shortVersion}";
+		}
+	}
+}
