b7f563ddbee3c7e05e7e701edb383703025b2245
TinaCaputa <caputa.florentina.lucia@gmail.com>
2020-05-25 08:46:41 +0300

    Migrate the single file project changes to Studio 2021

--- Export Analysis Reports/Sdl.Community.ExportAnalysisReports.UnitTests/ProjectServiceUnitTests.cs
+++ Export Analysis Reports/Sdl.Community.ExportAnalysisReports.UnitTests/ProjectServiceUnitTests.cs
@@ -9,7 +9,7 @@ using Xunit;
 namespace Sdl.Community.ExportAnalysisReports.UnitTests
 {
 	public class ProjectServiceUnitTests
-    {
+	{
 		private ExportAnalysisReportsConfiguration _exportReportsConfiguration;
 		private readonly ProjectService _projectService;
 		private readonly string _testingFilesPath = Path.Combine($"{AppDomain.CurrentDomain.BaseDirectory}", "TestFiles");
@@ -20,14 +20,15 @@ namespace Sdl.Community.ExportAnalysisReports.UnitTests
 			_exportReportsConfiguration = new ExportAnalysisReportsConfiguration();
 		}
 
-		[Fact]
-		private void AddFiles_IsSuccessfullyAdded_UnitTest()
+		[Theory]
+		[InlineData(@"Project 2\Project2.sdlproj")]
+		private void AddFiles_IsSuccessfullyAdded_UnitTest(string filePath)
 		{
 			// Arrange
-			var projectPath = Path.Combine(_testingFilesPath, @"Project 2\Project2.sdlproj");
+			var projectPath = Path.Combine(_testingFilesPath, filePath);
 			var studioProjectsPath = new List<string>();
 			studioProjectsPath.Add(projectPath);
-			
+
 			// Act
 			var files = _projectService.AddFilePaths(studioProjectsPath);
 
@@ -43,17 +44,18 @@ namespace Sdl.Community.ExportAnalysisReports.UnitTests
 
 			// Act
 			var files = _projectService.AddFilePaths(studioProjectsPath);
-			
+
 			// Assert
 			Assert.Empty(files);
 		}
 
-		[Fact]
-		private void RemoveSingleFileLanguage_IsSuccessfullyRemoved_UnitTest()
+		[Theory]
+		[InlineData("English (United Kingdom)")]
+		private void RemoveSingleFileLanguage_IsSuccessfullyRemoved_UnitTest(string languageName)
 		{
 			// Arrange
-			var languagesToRemove = _exportReportsConfiguration.AddMultipleLanguages();
-			var projectLanguages = _exportReportsConfiguration.AddLanguageDetails("English (United Kingdom)");
+			var languagesToRemove = _exportReportsConfiguration.GetMultipleLanguages();
+			var projectLanguages = _exportReportsConfiguration.AddLanguageDetails(languageName);
 
 			// Act
 			_projectService.RemoveSingleFileProjectLanguages(languagesToRemove, projectLanguages);
@@ -62,12 +64,13 @@ namespace Sdl.Community.ExportAnalysisReports.UnitTests
 			Assert.True(languagesToRemove.Count.Equals(1));
 		}
 
-		[Fact]
-		private void RemoveSingleFileLanguage_IsNotSuccessfullyRemoved_UnitTest()
+		[Theory]
+		[InlineData("English (United Kingdom)")]
+		private void RemoveSingleFileLanguage_IsNotSuccessfullyRemoved_UnitTest(string languageName)
 		{
 			// Arrange
-			var languagesToRemove = _exportReportsConfiguration.AddProjectLanguage();
-			var projectLanguages = _exportReportsConfiguration.AddLanguageDetails("English (United Kingdom)");
+			var languagesToRemove = _exportReportsConfiguration.GetProjectLanguage();
+			var projectLanguages = _exportReportsConfiguration.AddLanguageDetails(languageName);
 
 			// Act
 			_projectService.RemoveSingleFileProjectLanguages(languagesToRemove, projectLanguages);
@@ -76,34 +79,97 @@ namespace Sdl.Community.ExportAnalysisReports.UnitTests
 			Assert.NotEmpty(languagesToRemove);
 		}
 
-		[Fact]
-		private void SetProjectLanguages_IsSuccessfullySet_UnitTest()
+		[Theory]
+		[InlineData(false, "Project 2", true, "English (United Kingdom)")]
+		private void SetProjectLanguages_IsSuccessfullySet_UnitTest(bool isSingleFileProject, string projectName, bool isChecked, string languageName)
 		{
 			// Arrange
 			var projectDetails = new List<ProjectDetails>();
-			projectDetails.Add(_exportReportsConfiguration.GetProjectDetails());
-			
+			projectDetails.Add(_exportReportsConfiguration.GetProjectDetails(isSingleFileProject, projectName));
+
 			// Act
-			_projectService.SetProjectLanguages(projectDetails, true, "English (United Kingdom)");
-			var isLanguageChecked = projectDetails[0]?.ProjectLanguages["English (United Kingdom)"];
+			_projectService.SetProjectLanguages(projectDetails, isChecked, languageName);
+			var isLanguageChecked = projectDetails[0]?.ProjectLanguages[languageName];
 
 			// Assert
 			Assert.True(isLanguageChecked);
 		}
 
-		[Fact]
-		private void SetProjectLanguages_IsNotSuccessfullySet_UnitTest()
+		[Theory]
+		[InlineData(true, "Project 2", false, "English (United Kingdom)")]
+		private void SetProjectLanguages_IsNotSuccessfullySet_UnitTest(bool isSingleFileProject, string projectName, bool isChecked, string languageName)
 		{
 			// Arrange
 			var projectDetails = new List<ProjectDetails>();
-			projectDetails.Add(_exportReportsConfiguration.GetProjectDetails());
+			projectDetails.Add(_exportReportsConfiguration.GetProjectDetails(isSingleFileProject, projectName));
 
 			// Act
-			_projectService.SetProjectLanguages(projectDetails, false, "English (United Kingdom)");
-			var isLanguageChecked = projectDetails[0]?.ProjectLanguages["English (United Kingdom)"];
+			_projectService.SetProjectLanguages(projectDetails, isChecked, languageName);
+			var isLanguageChecked = projectDetails[0]?.ProjectLanguages[languageName];
 
 			// Assert
 			Assert.False(isLanguageChecked);
+		}
+
+		[Theory]
+		[InlineData(true, "Project 2")]
+		private void RemoveAllSingleProjects_AreSuccessfullyRemoved_UnitTest(bool isSingleFileProject, string projectName)
+		{
+			// Arrange
+			var projectDetails = new List<ProjectDetails>();
+			projectDetails.Add(_exportReportsConfiguration.GetProjectDetails(isSingleFileProject, projectName));
+
+			// Act
+			_projectService.RemoveAllSingleProjects(projectDetails);
+
+			// Assert
+			Assert.True(projectDetails.Count == 0);
+		}
+
+		[Theory]
+		[InlineData(false, "Project 2")]
+		private void RemoveAllSingleProjects_AreNotSuccessfullyRemoved_UnitTest(bool isSingleFileProject, string projectName)
+		{
+			// Arrange
+			var projectDetails = new List<ProjectDetails>();
+			projectDetails.Add(_exportReportsConfiguration.GetProjectDetails(isSingleFileProject, projectName));
+
+			// Act
+			_projectService.RemoveAllSingleProjects(projectDetails);
+
+			// Assert
+			Assert.False(projectDetails.Count == 0);
+		}
+
+		[Fact]
+		private void SetAllProjectDetails_AreSuccessfullySet_UnitTest()
+		{
+			// Arrange
+			var projectsDetails = new List<ProjectDetails>();
+			projectsDetails.Add(_exportReportsConfiguration.GetProjectDetails(false, "Project 2"));
+			var projectDetails = _exportReportsConfiguration.GetProjectDetails(false, "Project 3");
+
+			// Act
+			_projectService.SetAllProjectDetails(projectsDetails, projectDetails);
+
+			// Assert
+			Assert.True(projectsDetails.Count > 1);
+		}
+
+		[Theory]
+		[InlineData(false, "Project 2")]
+		private void SetAllProjectDetails_AreNotSuccessfullySet_UnitTest(bool isSingleFileProject, string projectName)
+		{
+			// Arrange
+			var projectsDetails = new List<ProjectDetails>();
+			projectsDetails.Add(_exportReportsConfiguration.GetProjectDetails(isSingleFileProject, projectName));
+			var projectDetails = _exportReportsConfiguration.GetProjectDetails(isSingleFileProject, projectName);
+
+			// Act
+			_projectService.SetAllProjectDetails(projectsDetails, projectDetails);
+
+			// Assert
+			Assert.False(projectsDetails.Count > 1);
 		}
 	}
 }
