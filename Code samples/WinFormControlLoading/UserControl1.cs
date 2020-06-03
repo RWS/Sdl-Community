edb4b06296487d128733f70a3d9bb5e5955eee85
TinaCaputa <caputa.florentina.lucia@gmail.com>
2020-06-03 12:15:54 +0300

    Add sample code for win forms control loading in Studio 2021

---
+++ Code samples/WinFormControlLoading/UserControl1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Sdl.Desktop.IntegrationApi.Interfaces;
+
+namespace WinFormControlLoading
+{
+    public partial class UserControl1 : UserControl, IUIControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Code samples/WinFormControlLoading/ViewController.cs
@@ -0,0 +1,33 @@
+﻿using Sdl.Desktop.IntegrationApi;
+using Sdl.Desktop.IntegrationApi.Extensions;
+using Sdl.Desktop.IntegrationApi.Interfaces;
+using Sdl.TranslationStudioAutomation.IntegrationApi.Presentation.DefaultLocations;
+using System;
+
+namespace WinFormControlLoading
+{
+	[View(
+		Id = "WinFormControl",
+		Name = "WinFormControl",
+		Description = "Sample code for loading the control in Studio 2021",
+		LocationByType = typeof(TranslationStudioDefaultViews.TradosStudioViewsLocation))]
+	public class ViewController : AbstractViewController
+	{
+		private static readonly UserControl1 UserControl1 = new UserControl1();
+		private static readonly UserControl2 UserControl2 = new UserControl2();
+
+		protected override void Initialize(IViewContext context)
+		{
+		}
+
+		protected override IUIControl GetExplorerBarControl()
+		{
+			return UserControl2;
+		}
+
+		protected override IUIControl GetContentControl()
+		{
+			return UserControl1;
+		}
+	}
+}
