49d0431a0b8d546121fac3b933a3e908e472c012
TinaCaputa <caputa.florentina.lucia@gmail.com>
2020-07-17 13:04:37 +0300

    Fix file not found issue : backup the native mxliff file and use it when user creates a new project based on mxliff file and exports the translated file (which is used further in Memsource web). Apply code cleaning. Update plugin version.

---
+++ FileTypeSupport.MXLIFF/Utils/Helper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace Sdl.Community.FileTypeSupport.MXLIFF.Utils
+{
+	public class Helper
+	{
+		private readonly string _communityFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "SDL Community", "MXLIFF File Type Support", "Native MXLIFF Files");
+
+		// Backup the native .mxliff file locally
+		// When user creates a project using the new project wizard, the native file is used when user saves the translated file using "Save Target As" option
+		public void BackupNativeFile(string nativeFilePath)
+		{
+			Directory.CreateDirectory(_communityFolder);
+
+			var backupFile = Path.Combine(_communityFolder, Path.GetFileName(nativeFilePath));
+			File.Copy(nativeFilePath, backupFile, true);
+		}
+
+		// Get the corresponding mxliff file from the backup  native files folder
+		public string GetBackupFile(string fileNamePath)
+		{
+			var fileName = Path.GetFileNameWithoutExtension(fileNamePath);
+			var mxliffFiles = Directory.GetFiles(_communityFolder);
+
+			foreach (var file in mxliffFiles)
+			{
+				if (Path.GetFileNameWithoutExtension(file).Equals(fileName))
+				{
+					return file;
+				}
+			}
+
+			return string.Empty;
+		}
+	}
+}
