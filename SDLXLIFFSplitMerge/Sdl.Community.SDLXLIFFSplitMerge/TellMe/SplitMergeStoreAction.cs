ddb968b7ca9e22c404f78623630e2a9d9e7cd118
TinaCaputa <caputa.florentina.lucia@gmail.com>
2020-07-22 12:47:39 +0300

    Migrate solution to Studio 2021. Use Sdl.Versioning instead of Sdl.Community.Toolkit.Core nuget Remove morelinq nuget which is not used.

---
+++ SDLXLIFFSplitMerge/Sdl.Community.SDLXLIFFSplitMerge/TellMe/SplitMergeStoreAction.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+using Sdl.TellMe.ProviderApi;
+
+namespace Sdl.Community.SDLXLIFFSplitMerge.TellMe
+{
+	public class SplitMergeStoreAction : AbstractTellMeAction
+	{
+		public override bool IsAvailable => true;
+		public override string Category => "SDLXLIFF Split/Merge results";
+		public override Icon Icon => PluginResources.Download;
+
+		public SplitMergeStoreAction()
+		{
+			Name = "Download SDLXLIFF Split/Merge from the AppStore";
+		}
+
+		public override void Execute()
+		{
+			Process.Start("https://appstore.sdl.com/language/app/sdlxliff-split-merge/20/");
+		}
+	}
+}
