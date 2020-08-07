435b33bfe7faa2b8bee2489d21757a7aef71dc57
andreaGhisa <aghisa@sdl.com>
2020-08-07 10:45:29 +0300

    DeleteAllCommentsCode Sample.

---
+++ Code samples/DeleteAllCommentsCodeSample/DeleteAllCommentsCodeSample/MyCustomTradosStudio.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Sdl.Desktop.IntegrationApi;
+using Sdl.Desktop.IntegrationApi.Extensions;
+using Sdl.TranslationStudioAutomation.IntegrationApi;
+using Sdl.TranslationStudioAutomation.IntegrationApi.Extensions;
+using Sdl.TranslationStudioAutomation.IntegrationApi.Presentation.DefaultLocations;
+
+namespace DeleteAllCommentsCodeSample
+{
+
+	public class MyCustomTradosStudio
+	{
+		
+	}
+}
