fbdfe30fd956e180168fbdff45aa33affaaa0135
Patrick Andrew Hartnett <phartnett@sdl.com>
2020-08-24 14:19:58 +0200

    Removed: Reports.Viewer.API project

--- Reports.Viewer.API/Reports.Viewer.API/Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Sdl.Reports.Viewer.API
-{
-	public class Constants
-	{
-		public const string ReportsViewerFolderName = "Reports.Viewer";
-		public const string ReportsFolderName = "Reports";
-	}
-}
