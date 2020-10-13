18e9c5a7838d435c1419c83e36f87a7ace9a2dcc
TinaCaputa <caputa.florentina.lucia@gmail.com>
2020-10-13 13:46:31 +0300

    Apply changes done in Studio 2021: -Normal verification, regex updates -Alphanumerics verification improvements -Update the NLog configuration to use the lastest implementation which avoids the information to be written in a wrong file -Fix "Report modified alphanumerics" option is not taken into consideration when the "Alphanumerics custom separator"
    Besides the above changes, also the following updates were done:
    - Update PluginFramework and PluginFramework.Build Nuget packages to the latest stable versions, to avoid possible issues, like:maxversion is not written within the plugin manifest file.
    -Use "Migrate packages.config to Nuget Reference" option as it is also on Studio 2021 (Studio nugets and other solutions will be using this option too)
    -Update plugin's  version

--- Number Verifier/Helpers/Log.cs
+++ Number Verifier/Helpers/Log.cs
@@ -1,45 +1,38 @@
 ﻿using System;
 using System.IO;
-using System.Reflection;
 using NLog;
 using NLog.Config;
 using NLog.Targets;
 
 namespace Sdl.Community.NumberVerifier.Helpers
 {
-	public sealed class Log
+	public static class Log
 	{
-		public static Logger Logger;
-		private static readonly Lazy<Log> _instance = new Lazy<Log>(() => new Log());
-		public static Log Instance { get { return _instance.Value; } }
-
-		private Log()
+		public static void Setup()
 		{
-			var config = new LoggingConfiguration();
-			var assembly = Assembly.GetExecutingAssembly();
-			var logDirectoryPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), Constants.SDLCommunity, Constants.NumberVerifier);
-			if (!Directory.Exists(logDirectoryPath))
+			if (LogManager.Configuration == null)
 			{
-				Directory.CreateDirectory(logDirectoryPath);
+				LogManager.Configuration = new LoggingConfiguration();
 			}
+
+			var config = LogManager.Configuration;
+
+			var logDirectoryPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+				"SDL Community",
+				"Number Verifier");
+			Directory.CreateDirectory(logDirectoryPath);
+
 			var target = new FileTarget
 			{
+				Name = "Number Verifier",
 				FileName = Path.Combine(logDirectoryPath, "NumberVerifierLogs.txt"),
-				// Roll over the log every 10 MB
-				ArchiveAboveSize = 10000000,
-				ArchiveNumbering = ArchiveNumberingMode.Date,
-
-				// Path.combine nor string.format like the {#####}, which is used to replace the date, therefore
-				// we need to do basic string concatenation.
-				ArchiveFileName = logDirectoryPath + "/" + assembly.GetName().Name + ".log.{#####}.txt"
+				Layout = "${logger}: ${longdate} ${level} ${message}  ${exception}"
 			};
 
-			config.AddTarget("file", target);
-			var rule = new LoggingRule("*", LogLevel.Debug, target);
-			config.LoggingRules.Add(rule);
-			LogManager.Configuration = config;
-			//NLog object
-			Logger = LogManager.GetCurrentClassLogger();
+			config.AddTarget(target);
+			config.AddRuleForAllLevels(target, "*Sdl.Community.NumberVerifier*");
+
+			LogManager.ReconfigExistingLoggers();
 		}
 	}
 }
--- Number Verifier/NumberVerifierMain.cs
+++ Number Verifier/NumberVerifierMain.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Text.RegularExpressions;
+using NLog;
 using Sdl.Community.Extended.MessageUI;
 using Sdl.Community.NumberVerifier.Composers;
 using Sdl.Community.NumberVerifier.Helpers;
@@ -41,10 +42,9 @@ namespace Sdl.Community.NumberVerifier
 		private string _sourceText;
 		private string _thousandWithoutDecimal;
 		private string _decimalAfterThousand;
+		private readonly Logger _logger = LogManager.GetCurrentClassLogger();
 
 		#endregion
-
-		public static readonly Log Log = Log.Instance;
 
 		public NumberVerifierMain() : this(null)
 		{
@@ -414,7 +414,7 @@ namespace Sdl.Community.NumberVerifier
 			}
 			catch (Exception ex)
 			{
-				Log.Logger.Error($"{MethodBase.GetCurrentMethod().Name} {ex.Message}\n {ex.StackTrace}");
+				_logger.Error($"{MethodBase.GetCurrentMethod().Name} \n {ex}");
 			}
 		}
 
@@ -456,7 +456,7 @@ namespace Sdl.Community.NumberVerifier
 			}
 			catch (Exception ex)
 			{
-				Log.Logger.Error($"{MethodBase.GetCurrentMethod().Name} {ex.Message}\n {ex.StackTrace}");
+				_logger.Error($"{MethodBase.GetCurrentMethod().Name} \n {ex}");
 				return new List<ErrorReporting>();
 			}
 		}
@@ -543,16 +543,13 @@ namespace Sdl.Community.NumberVerifier
 			var targetNormalizedNumberList = targetNumbersTuple?.Item2;
 
 			// remove identical numbers found both in source and target from respective list
-			RemoveIdenticalNumbers(sourceNumberList, targetNumberList, targetNormalizedNumberList,
-				sourceNormalizedNumberList);
+			RemoveIdenticalNumbers(sourceNumberList, targetNumberList, targetNormalizedNumberList, sourceNormalizedNumberList);
 
 			// remove numbers found both in source and target from respective list disregarding difference in thousands and decimal separators
-			RemoveNumbersIgnoreThousandsAndDecimalSeparators(sourceNumberList, targetNormalizedNumberList,
-				sourceNormalizedNumberList, targetNumberList);
+			RemoveNumbersIgnoreThousandsAndDecimalSeparators(sourceNumberList, targetNormalizedNumberList, sourceNormalizedNumberList, targetNumberList);
 
 			// remove numbers found both in source and target from respective list disregarding difference when thousands and decimal separators are undefined due to ambiguity 
-			RemoveNumbersUndefinedThousandsAndDecimalSeparator(targetNumberList, sourceNumberList,
-				sourceNormalizedNumberList, targetNormalizedNumberList);
+			RemoveNumbersUndefinedThousandsAndDecimalSeparator(targetNumberList, sourceNumberList, sourceNormalizedNumberList, targetNormalizedNumberList);
 
 			var sourceHindiList = initialSourceHindiText.Equals(numberModel.SourceText) ? new List<string> { numberModel.SourceText } : new List<string> { initialSourceHindiText };
 			var targetHindiList = initialTargetHindiText.Equals(numberModel.TargetText) ? new List<string> { numberModel.TargetText } : new List<string> { initialTargetHindiText };
@@ -592,7 +589,7 @@ namespace Sdl.Community.NumberVerifier
 				TargetText = targetText
 			};
 
-			if (_verificationSettings.CustomsSeparatorsAlphanumerics)
+			if (_verificationSettings.ReportModifiedAlphanumerics || _verificationSettings.CustomsSeparatorsAlphanumerics)
 			{
 				var errorsListFromAlphanumerics = CheckAlphanumerics(sourceText, targetText);
 				errorList.AddRange(errorsListFromAlphanumerics);
@@ -703,7 +700,7 @@ namespace Sdl.Community.NumberVerifier
 			}
 			catch (Exception ex)
 			{
-				Log.Logger.Error($"{MethodBase.GetCurrentMethod().Name} {ex.Message}\n {ex.StackTrace}");
+				_logger.Error($"{MethodBase.GetCurrentMethod().Name} \n {ex}");
 			}
 		}
 
@@ -738,7 +735,7 @@ namespace Sdl.Community.NumberVerifier
 			}
 			catch (Exception ex)
 			{
-				Log.Logger.Error($"{MethodBase.GetCurrentMethod().Name} {ex.Message}\n {ex.StackTrace}");
+				_logger.Error($"{MethodBase.GetCurrentMethod().Name} \n {ex}");
 			}
 		}
 
@@ -762,7 +759,7 @@ namespace Sdl.Community.NumberVerifier
 			}
 			catch (Exception ex)
 			{
-				Log.Logger.Error($"{MethodBase.GetCurrentMethod().Name} {ex.Message}\n {ex.StackTrace}");
+				_logger.Error($"{MethodBase.GetCurrentMethod().Name} \n {ex}");
 			}
 		}
 
@@ -822,7 +819,7 @@ namespace Sdl.Community.NumberVerifier
 			}
 			catch (Exception ex)
 			{
-				Log.Logger.Error($"{MethodBase.GetCurrentMethod().Name} {ex.Message}\n {ex.StackTrace}");
+				_logger.Error($"{MethodBase.GetCurrentMethod().Name} \n {ex}");
 			}
 			return separators.ToString();
 		}
@@ -833,52 +830,61 @@ namespace Sdl.Community.NumberVerifier
 			{
 				var customDecimalSeparators = GetCustomSeparators(VerificationSettings.GetSourceDecimalCustomSeparator, VerificationSettings.GetTargetDecimalCustomSeparator, VerificationSettings.SourceDecimalCustomSeparator, VerificationSettings.TargetDecimalCustomSeparator);
 				var customThousandSeparators = GetCustomSeparators(VerificationSettings.GetSourceThousandsCustomSeparator, VerificationSettings.GetTargetThousandsCustomSeparator, VerificationSettings.SourceThousandsCustomSeparator, VerificationSettings.TargetThousandsCustomSeparator);
-				var sepChars = _textFormatter.GetSeparatorsChars(customDecimalSeparators, customThousandSeparators);
+				var separatorChars = _textFormatter.GetSeparatorsChars(customDecimalSeparators, customThousandSeparators);
+				var separators = $".,{customDecimalSeparators}{customThousandSeparators}{Constants.SpaceSeparators}";
+				var pattern = $"[+-−]?\\d+[{separators}\\s]{{0,1}}\\d{{0,3}}(?<![{separators}\\s])[{separators}\\s]{{0,1}}\\d{{0,3}}" +
+				              $"(?<![{separators}\\s])[{separators}\\s]{{0,1}}\\d{{0,3}}(?<![{separators}\\s])[{separators}\\s]{{0,1}}\\d*";
 
-				var numbers = Regex.Split(normalizedNumber.Text, $@"[^−\-\0-9 ０-９\[.,{customDecimalSeparators}{customThousandSeparators}{Constants.SpaceSeparators}]+").Where(c => c != "." && c.Trim() != "");
-				
-				if (numbers.Any())
+				var numbers = Regex.Matches(normalizedNumber.Text, pattern);
+
+				foreach (Match item in numbers)
 				{
-					foreach (var item in numbers)
+					if (!string.IsNullOrEmpty(item.Value))
 					{
-						var text = item.Trim();
-						text = !string.IsNullOrWhiteSpace(text) ? _textFormatter.RemovePunctuationChar(text, sepChars, _omitLeadingZero) : string.Empty;
-						
+						var text = item.Value.Trim();
+						text = !string.IsNullOrWhiteSpace(text)
+							? _textFormatter.RemovePunctuationChar(text, separatorChars, _omitLeadingZero)
+							: string.Empty;
+
 						if (string.IsNullOrWhiteSpace(text)) continue;
-
-						var separatorModel = GetSeparatorModel(normalizedNumber, text, customDecimalSeparators, customThousandSeparators);
-
-						if (IsNumberDecimal(text, separatorModel))
-						{
-							_isNoSeparator = false; // the 'NoSeparator' option can be checked only for thousand or thousand-decimal numbers 
-							normalizedNumber.Separators = $"{normalizedNumber.DecimalSeparators}{_textFormatter.GetSeparators(separatorModel.DecimalCustomSeparators)}";
-							ConfigureNormalizedNumber(normalizedNumber, text);
-						}
-						else if (IsNumberThousandDecimal(text, separatorModel))
-						{
-							var separators = $"{normalizedNumber.ThousandSeparators}{normalizedNumber.DecimalSeparators}" +
-											 $"{_textFormatter.GetSeparators(separatorModel.DecimalCustomSeparators)}{_textFormatter.GetSeparators(separatorModel.ThousandCustomSeparators)}";
-
-							_isNoSeparator = VerificationSettings.SourceNoSeparator || VerificationSettings.TargetNoSeparator;
-							var customSeparators = $"{separatorModel.ThousandCustomSeparators}{separatorModel.DecimalCustomSeparators}";
-							SetSeparatorInformation(separatorModel, customSeparators, true);
-							normalizedNumber.Separators = separators;
-							ConfigureNormalizedNumber(normalizedNumber, text);
-						}
-						else
-						{
-							// number is only thousand
-							_isNoSeparator = VerificationSettings.SourceNoSeparator || VerificationSettings.TargetNoSeparator;
-							SetSeparatorInformation(separatorModel, separatorModel.ThousandCustomSeparators, false);
-							normalizedNumber.Separators = normalizedNumber.ThousandSeparators;
-							ConfigureNormalizedNumber(normalizedNumber, text);
-						}
+						ProcessText(normalizedNumber, text, customDecimalSeparators, customThousandSeparators);
 					}
 				}
 			}
 			catch (Exception ex)
 			{
-				Log.Logger.Error($"{MethodBase.GetCurrentMethod().Name} {ex.Message}\n {ex.StackTrace}");
+				_logger.Error($"{MethodBase.GetCurrentMethod().Name} \n {ex}");
+			}
+		}
+		
+		private void ProcessText(NormalizedNumber normalizedNumber, string text, string customDecimalSeparators, string customThousandSeparators)
+		{
+			var separatorModel = GetSeparatorModel(normalizedNumber, text, customDecimalSeparators, customThousandSeparators);
+
+			if (IsNumberDecimal(text, separatorModel))
+			{
+				_isNoSeparator = false; // the 'NoSeparator' option can be checked only for thousand or thousand-decimal numbers 
+				normalizedNumber.Separators = $"{normalizedNumber.DecimalSeparators}{_textFormatter.GetSeparators(separatorModel.DecimalCustomSeparators)}";
+				ConfigureNormalizedNumber(normalizedNumber, text);
+			}
+			else if (IsNumberThousandDecimal(text, separatorModel))
+			{
+				var separators = $"{normalizedNumber.ThousandSeparators}{normalizedNumber.DecimalSeparators}" +
+				                 $"{_textFormatter.GetSeparators(separatorModel.DecimalCustomSeparators)}{_textFormatter.GetSeparators(separatorModel.ThousandCustomSeparators)}";
+
+				_isNoSeparator = VerificationSettings.SourceNoSeparator || VerificationSettings.TargetNoSeparator;
+				var customSeparators = $"{separatorModel.ThousandCustomSeparators}{separatorModel.DecimalCustomSeparators}";
+				SetSeparatorInformation(separatorModel, customSeparators, true);
+				normalizedNumber.Separators = separators;
+				ConfigureNormalizedNumber(normalizedNumber, text);
+			}
+			else
+			{
+				// number is only thousand
+				_isNoSeparator = VerificationSettings.SourceNoSeparator || VerificationSettings.TargetNoSeparator;
+				SetSeparatorInformation(separatorModel, separatorModel.ThousandCustomSeparators, false);
+				normalizedNumber.Separators = normalizedNumber.ThousandSeparators;
+				ConfigureNormalizedNumber(normalizedNumber, text);
 			}
 		}
 
@@ -996,7 +1002,7 @@ namespace Sdl.Community.NumberVerifier
 			}
 			catch (Exception ex)
 			{
-				Log.Logger.Error($"{MethodBase.GetCurrentMethod().Name} {ex.Message}\n {ex.StackTrace}");
+				_logger.Error($"{MethodBase.GetCurrentMethod().Name} \n {ex}");
 			}
 			return number;
 		}
@@ -1032,7 +1038,7 @@ namespace Sdl.Community.NumberVerifier
 			}
 			catch (Exception ex)
 			{
-				Log.Logger.Error($"{MethodBase.GetCurrentMethod().Name} {ex.Message}\n {ex.StackTrace}");
+				_logger.Error($"{MethodBase.GetCurrentMethod().Name} \n {ex}");
 				return string.Empty;
 			}
 		}
@@ -1069,7 +1075,7 @@ namespace Sdl.Community.NumberVerifier
 			}
 			catch (Exception ex)
 			{
-				Log.Logger.Error($"{MethodBase.GetCurrentMethod().Name} {ex.Message}\n {ex.StackTrace}");
+				_logger.Error($"{MethodBase.GetCurrentMethod().Name} \n {ex}");
 				return string.Empty;
 			}
 		}
@@ -1094,7 +1100,7 @@ namespace Sdl.Community.NumberVerifier
 			}
 			catch (Exception ex)
 			{
-				Log.Logger.Error($"{MethodBase.GetCurrentMethod().Name} {ex.Message}\n {ex.StackTrace}");
+				_logger.Error($"{MethodBase.GetCurrentMethod().Name} \n {ex}");
 			}
 
 			return normalizedNumber.Normalize(NormalizationForm.FormKC);
@@ -1247,15 +1253,33 @@ namespace Sdl.Community.NumberVerifier
 			return regExExpression;
 		}
 
+		// Remove the matching items which are the same in both sourceAlphanumeric and targetAlphanumeric lists
+		// or in case sourceAlphanumericsList contains items which are not alphanumerics (ex: ABCD) 
 		private void RemoveMatchingAlphanumerics(IList<string> sourceAlphanumericsList, ICollection<string> targetAlphanumericsList)
 		{
-
 			for (var nJ = sourceAlphanumericsList.Count - 1; nJ >= 0; nJ--)
 			{
-				if (!targetAlphanumericsList.Contains(sourceAlphanumericsList[nJ])) continue;
+				if (targetAlphanumericsList.Contains(sourceAlphanumericsList[nJ]) || !sourceAlphanumericsList[nJ].Any(c => char.IsDigit(c)))
+				{
+					targetAlphanumericsList.Remove(sourceAlphanumericsList[nJ]);
+					sourceAlphanumericsList.RemoveAt(nJ);
+				}
+			}
 
-				targetAlphanumericsList.Remove(sourceAlphanumericsList[nJ]);
-				sourceAlphanumericsList.RemoveAt(nJ);
+			// Remove the items which are not alphanumerics and where not identified within the sourceAlphanumericsList
+			RemoveNoAlphanumerics(targetAlphanumericsList);
+		}
+
+		// Remove the items which are not alphanumerics
+		private void RemoveNoAlphanumerics(ICollection<string> targetAlphanumericsList)
+		{
+			var itemsToRemove = targetAlphanumericsList.Where(item => !item.Any(char.IsDigit) || item.All(char.IsDigit)).ToList();
+			if (itemsToRemove.Any())
+			{
+				foreach (var item in itemsToRemove.Where(targetAlphanumericsList.Contains))
+				{
+					targetAlphanumericsList.Remove(item);
+				}
 			}
 		}
 
@@ -1265,7 +1289,10 @@ namespace Sdl.Community.NumberVerifier
 			{
 				var normalizedAlphaList = new List<string>();
 				var words = Regex.Split(text, @"\s");
-
+				var customsSeparators = !string.IsNullOrEmpty(_verificationSettings.GetAlphanumericsCustomSeparator)
+					? _verificationSettings.GetAlphanumericsCustomSeparator.Split(',')
+					: new string[0];
+				
 				// The below foreach is used when checking those tags like Source: "<color=70236>Word" and Target:<color=70236>OtherWord
 				// and no empty space is between the '>' and 'Word' or between the '>' and 'OtherWord'.
 				// Because of the missing of empty space, the functionality recognize as beeing alphanumeric and when source and target were not that same('Word' different than 'OtherWord'
@@ -1275,7 +1302,7 @@ namespace Sdl.Community.NumberVerifier
 				{
 					if (w.Contains('<') || w.Contains('>'))
 					{
-						string[] wRes = new string[] { };
+						var wRes = new string[] { };
 						if (w.Contains('<'))
 						{
 							var charIndex = w.IndexOf('<');
@@ -1301,45 +1328,26 @@ namespace Sdl.Community.NumberVerifier
 					}
 				}
 
-				if (_verificationSettings.CustomsSeparatorsAlphanumerics)
-				{
-					string[] customsSeparators = !string.IsNullOrEmpty(_verificationSettings.GetAlphanumericsCustomSeparator)
-					? _verificationSettings.GetAlphanumericsCustomSeparator.Split(',')
-					: new string[0];
+				var separators = _verificationSettings.CustomsSeparatorsAlphanumerics 
+					? _textFormatter.GetAlphanumericsCustomSeparators(customsSeparators)
+					: string.Empty;
+				var regex = $"(?i)(?=.*[0-9])(?=.*[a-z])([a-z0-9{separators}]+)";
 
-					var res = string.Join(string.Empty, customsSeparators);
-
-					// replace \ with \\ in order to recognize the regex expression
-					if (res.Contains(@"\"))
-					{
-						res = res.Replace(@"\", @"\\");
-					}
-					var regex = string.Format(@"^-?\u2212?(^(?=.*[a-zA-Z{0}])(?=.*[0-9]).+$)", res);
-
-					normalizedAlphaList.AddRange(
-						from word in wordsRes
-						from Match match in Regex.Matches(word.Normalize(NormalizationForm.FormKC), regex)
-						select Regex.Replace(match.Value, "\u2212|-", "m"));
-				}
-				else
-				{
-					normalizedAlphaList.AddRange(
-						from word in wordsRes
-						from Match match in Regex.Matches(word.Normalize(NormalizationForm.FormKC), @"^-?\u2212?(^(?=.*[a-zA-Z-])(?=.*[0-9]).+$)")
-						select Regex.Replace(match.Value, "\u2212|-", "m"));
-				}
-
-				//get all the words which are normalized and put them in source tuple item
+				normalizedAlphaList.AddRange(
+					from word in wordsRes
+					from Match match in Regex.Matches(word.Normalize(NormalizationForm.FormKC), regex)
+					select Regex.Replace(match.Value, "\u2212|-", "m"));
+				
 				var unNormalizedAlphanumerics = new List<string>();
 				unNormalizedAlphanumerics.AddRange(from word in wordsRes
-												   from Match match in Regex.Matches(word.Normalize(NormalizationForm.FormKC), @"^-?\u2212?(^(?=.*[a-zA-Z-])(?=.*[0-9]).+$)")
+												   from Match match in Regex.Matches(word.Normalize(NormalizationForm.FormKC), regex)
 												   select word);
-
+				
 				return GetAlphnumericsTuple(unNormalizedAlphanumerics, normalizedAlphaList);
 			}
 			catch (Exception ex)
 			{
-				Log.Logger.Error($"{MethodBase.GetCurrentMethod().Name} {ex.Message}\n {ex.StackTrace}");
+				_logger.Error($"{MethodBase.GetCurrentMethod().Name} \n {ex}");
 				return new Tuple<List<string>, List<string>>(new List<string>(), new List<string>());
 			}
 		}
@@ -1352,7 +1360,7 @@ namespace Sdl.Community.NumberVerifier
 			}
 			catch (Exception ex)
 			{
-				Log.Logger.Error($"{MethodBase.GetCurrentMethod().Name} {ex.Message}\n {ex.StackTrace}");
+				_logger.Error($"{MethodBase.GetCurrentMethod().Name} \n {ex}");
 				return string.Empty;
 			}
 		}
@@ -1439,7 +1447,7 @@ namespace Sdl.Community.NumberVerifier
 			}
 			catch (Exception ex)
 			{
-				Log.Logger.Error($"{MethodBase.GetCurrentMethod().Name} {ex.Message}\n {ex.StackTrace}");
+				_logger.Error($"{MethodBase.GetCurrentMethod().Name} \n {ex}");
 			}
 			return result;
 		}
@@ -1541,7 +1549,7 @@ namespace Sdl.Community.NumberVerifier
 			}
 			catch (Exception ex)
 			{
-				Log.Logger.Error($"{MethodBase.GetCurrentMethod().Name} {ex.Message}\n {ex.StackTrace}");
+				_logger.Error($"{MethodBase.GetCurrentMethod().Name} \n {ex}");
 			}
 			return result;
 		}
