2f736f7dc8ccdbad16e75473a5956a0d2fcf7446
andreaGhisa <aghisa@sdl.com>
2020-11-24 11:08:41 +0200

    TQA: Added new quality level option on Main window.

--- TQA/TQA/MainWindow.Designer.cs
+++ TQA/TQA/MainWindow.Designer.cs
@@ -1,39 +1,41 @@
 ﻿namespace Sdl.Community.TQA
 {
-    partial class MainWindow
-    {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
-        private System.ComponentModel.IContainer components = null;
+	partial class MainWindow
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-        protected override void Dispose(bool disposing)
-        {
-            if( disposing && ( components != null ) )
-            {
-                components.Dispose();
-            }
-            base.Dispose( disposing );
-        }
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
 
-        #region Windows Form Designer generated code
+		#region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
 			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainWindow));
 			this.ProjectNameLabel = new System.Windows.Forms.Label();
 			this.LanguageSelector = new System.Windows.Forms.ComboBox();
 			this.LanguageInfoLabel = new System.Windows.Forms.Label();
 			this.StartButton = new System.Windows.Forms.Button();
 			this.outputSaveDialog = new System.Windows.Forms.SaveFileDialog();
+			this.QualityLevelLbl = new System.Windows.Forms.Label();
+			this.QualityCombo = new System.Windows.Forms.ComboBox();
 			this.SuspendLayout();
 			// 
 			// ProjectNameLabel
@@ -47,14 +49,12 @@
 			// 
 			// LanguageSelector
 			// 
-			this.LanguageSelector.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
 			this.LanguageSelector.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
 			this.LanguageSelector.FormattingEnabled = true;
 			this.LanguageSelector.Location = new System.Drawing.Point(15, 42);
 			this.LanguageSelector.MaxDropDownItems = 16;
 			this.LanguageSelector.Name = "LanguageSelector";
-			this.LanguageSelector.Size = new System.Drawing.Size(351, 21);
+			this.LanguageSelector.Size = new System.Drawing.Size(240, 21);
 			this.LanguageSelector.TabIndex = 1;
 			// 
 			// LanguageInfoLabel
@@ -69,7 +69,7 @@
 			// StartButton
 			// 
 			this.StartButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-			this.StartButton.Location = new System.Drawing.Point(372, 41);
+			this.StartButton.Location = new System.Drawing.Point(271, 86);
 			this.StartButton.Name = "StartButton";
 			this.StartButton.Size = new System.Drawing.Size(75, 23);
 			this.StartButton.TabIndex = 3;
@@ -82,11 +82,35 @@
 			this.outputSaveDialog.DefaultExt = "*.xlsm";
 			this.outputSaveDialog.Filter = "Excel Macro-Enabled Workbook|*.xlsm";
 			// 
+			// QualityLevelLbl
+			// 
+			this.QualityLevelLbl.AutoSize = true;
+			this.QualityLevelLbl.Location = new System.Drawing.Point(12, 70);
+			this.QualityLevelLbl.Name = "QualityLevelLbl";
+			this.QualityLevelLbl.Size = new System.Drawing.Size(131, 13);
+			this.QualityLevelLbl.TabIndex = 4;
+			this.QualityLevelLbl.Text = "Please select quality level:";
+			// 
+			// QualityCombo
+			// 
+			this.QualityCombo.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.QualityCombo.FormattingEnabled = true;
+			this.QualityCombo.Items.AddRange(new object[] {
+			"Premium",
+			"Value",
+			"Economy"});
+			this.QualityCombo.Location = new System.Drawing.Point(15, 86);
+			this.QualityCombo.Name = "QualityCombo";
+			this.QualityCombo.Size = new System.Drawing.Size(240, 21);
+			this.QualityCombo.TabIndex = 5;
+			// 
 			// MainWindow
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(456, 74);
+			this.ClientSize = new System.Drawing.Size(358, 128);
+			this.Controls.Add(this.QualityCombo);
+			this.Controls.Add(this.QualityLevelLbl);
 			this.Controls.Add(this.StartButton);
 			this.Controls.Add(this.LanguageInfoLabel);
 			this.Controls.Add(this.LanguageSelector);
@@ -98,14 +122,16 @@
 			this.ResumeLayout(false);
 			this.PerformLayout();
 
-        }
+		}
 
-        #endregion
+		#endregion
 
-        private System.Windows.Forms.Label ProjectNameLabel;
-        private System.Windows.Forms.ComboBox LanguageSelector;
-        private System.Windows.Forms.Label LanguageInfoLabel;
-        private System.Windows.Forms.Button StartButton;
-        private System.Windows.Forms.SaveFileDialog outputSaveDialog;
-    }
+		private System.Windows.Forms.Label ProjectNameLabel;
+		private System.Windows.Forms.ComboBox LanguageSelector;
+		private System.Windows.Forms.Label LanguageInfoLabel;
+		private System.Windows.Forms.Button StartButton;
+		private System.Windows.Forms.SaveFileDialog outputSaveDialog;
+		private System.Windows.Forms.Label QualityLevelLbl;
+		private System.Windows.Forms.ComboBox QualityCombo;
+	}
 }
