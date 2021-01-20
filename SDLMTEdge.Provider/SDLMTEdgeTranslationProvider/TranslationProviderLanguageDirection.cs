9503b853f41ba079b8d8a501c9e83cda308f2365
Emanuel Albu <ealbu@sdl.com>
2021-01-20 16:20:30 +0200

    SDL MT Edge 2.3.7.0 -> 2.3.8.0  - fixes invalid xliff issue

--- SDLMTEdge.Provider/SDLMTEdgeTranslationProvider/TranslationProviderLanguageDirection.cs
+++ SDLMTEdge.Provider/SDLMTEdgeTranslationProvider/TranslationProviderLanguageDirection.cs
@@ -2,42 +2,44 @@ using System;
 using System.Linq;
 using NLog;
 using Sdl.Community.MTEdge.Provider.SDLMTEdgeApi;
-using Sdl.Community.Toolkit.LanguagePlatform.XliffConverter;
+using Sdl.Community.MTEdge.Provider.XliffConverter.Converter;
+using Sdl.Community.MTEdge.Provider.XliffConverter.Models;
 using Sdl.Core.Globalization;
 using Sdl.LanguagePlatform.Core;
 using Sdl.LanguagePlatform.TranslationMemory;
 using Sdl.LanguagePlatform.TranslationMemoryApi;
+using TranslationUnit = Sdl.LanguagePlatform.TranslationMemory.TranslationUnit;
 
 namespace Sdl.Community.MTEdge.Provider
 {
 	public class TranslationProviderLanguageDirection : ITranslationProviderLanguageDirection
-    {
-        private readonly TranslationProvider provider;
-        private readonly LanguagePair languageDirection;
+	{
+		private readonly TranslationProvider provider;
+		private readonly LanguagePair languageDirection;
 
-        private static readonly Logger _logger = NLog.LogManager.GetCurrentClassLogger();
+		private static readonly Logger _logger = NLog.LogManager.GetCurrentClassLogger();
 
 		public TranslationProviderLanguageDirection(TranslationProvider provider, LanguagePair languages)
-        {
-            _logger.Trace("");
-            this.provider = provider;
-            languageDirection = languages;
-        }
+		{
+			_logger.Trace("");
+			this.provider = provider;
+			languageDirection = languages;
+		}
 
-        public System.Globalization.CultureInfo SourceLanguage
-        {
-            get { return languageDirection.SourceCulture; }
-        }
+		public System.Globalization.CultureInfo SourceLanguage
+		{
+			get { return languageDirection.SourceCulture; }
+		}
 
-        public System.Globalization.CultureInfo TargetLanguage
-        {
-            get { return languageDirection.TargetCulture; }
-        }
+		public System.Globalization.CultureInfo TargetLanguage
+		{
+			get { return languageDirection.TargetCulture; }
+		}
 
-        public ITranslationProvider TranslationProvider
-        {
-            get { return provider; }
-        }
+		public ITranslationProvider TranslationProvider
+		{
+			get { return provider; }
+		}
 
 		public bool CanReverseLanguageDirection
 		{
@@ -82,7 +84,7 @@ namespace Sdl.Community.MTEdge.Provider
 			var translatedXliff = Converter.ParseXliffString(translatedXliffText);
 			if (translatedXliff != null)
 			{
-				return translatedXliff.GetTargetSegments();
+				return translatedXliff.GetTargetSegments(out _);
 			}
 			return new Segment[sourceSegments.Length];
 		}
@@ -105,22 +107,22 @@ namespace Sdl.Community.MTEdge.Provider
 				Origin = TranslationUnitOrigin.Nmt
 			};
 			unit.ResourceId = new PersistentObjectToken(unit.GetHashCode(), Guid.Empty);
-            var searchResult = new SearchResult(unit);
+			var searchResult = new SearchResult(unit);
 
-            // We do not currently support scoring, so always say that we're 25% sure on this translation.
-            searchResult.ScoringResult = new ScoringResult() { BaseScore = 25 };
+			// We do not currently support scoring, so always say that we're 25% sure on this translation.
+			searchResult.ScoringResult = new ScoringResult() { BaseScore = 25 };
 
-            return searchResult;
-        }
-		
-        public SearchResults[] SearchSegments(SearchSettings settings, Segment[] segments)
-        {
-            // Need this vs having mask parameter default to null as inheritence doesn't allow default values to
-            // count as the same thing as having no parameter at all. IE, you can't have
-            // public string foo(string s = null) override public string foo().
+			return searchResult;
+		}
 
-            return SearchSegments(settings, segments, null);
-        }
+		public SearchResults[] SearchSegments(SearchSettings settings, Segment[] segments)
+		{
+			// Need this vs having mask parameter default to null as inheritence doesn't allow default values to
+			// count as the same thing as having no parameter at all. IE, you can't have
+			// public string foo(string s = null) override public string foo().
+
+			return SearchSegments(settings, segments, null);
+		}
 
 		/// <summary>
 		/// Translate an array of segments.
@@ -163,65 +165,65 @@ namespace Sdl.Community.MTEdge.Provider
 			return results;
 		}
 
-        /// <summary>
-        /// Translate an array of segments depending on the partner mask array's values.
-        /// </summary>
-        /// <param name="settings"></param>
-        /// <param name="segments"></param>
-        /// <param name="mask"></param>
-        /// <returns></returns>
-        public SearchResults[] SearchSegmentsMasked(SearchSettings settings, Segment[] segments, bool[] mask)
-        {
-            _logger.Trace("");
-            if (segments == null)
-            {
-                throw new ArgumentNullException("segments", "Segments in SearchSegmentsMasked");
-            }
-            if (mask == null || mask.Length != segments.Length)
-            {
-                throw new ArgumentException("Mask in SearchSegmentsMasked");
-            }
+		/// <summary>
+		/// Translate an array of segments depending on the partner mask array's values.
+		/// </summary>
+		/// <param name="settings"></param>
+		/// <param name="segments"></param>
+		/// <param name="mask"></param>
+		/// <returns></returns>
+		public SearchResults[] SearchSegmentsMasked(SearchSettings settings, Segment[] segments, bool[] mask)
+		{
+			_logger.Trace("");
+			if (segments == null)
+			{
+				throw new ArgumentNullException("segments", "Segments in SearchSegmentsMasked");
+			}
+			if (mask == null || mask.Length != segments.Length)
+			{
+				throw new ArgumentException("Mask in SearchSegmentsMasked");
+			}
 
-            return SearchSegments(settings, segments, mask);
-        }
+			return SearchSegments(settings, segments, mask);
+		}
 
-        /// <summary>
-        /// Translate a string of text.
-        /// </summary>
-        /// <param name="settings"></param>
-        /// <param name="segment"></param>
-        /// <returns></returns>
-        public SearchResults SearchText(SearchSettings settings, string segment)
-        {
-            _logger.Trace("");
-            var seg = new Segment(languageDirection.SourceCulture);
-            seg.Add(segment);
-            return SearchSegment(settings, seg);
-        }
+		/// <summary>
+		/// Translate a string of text.
+		/// </summary>
+		/// <param name="settings"></param>
+		/// <param name="segment"></param>
+		/// <returns></returns>
+		public SearchResults SearchText(SearchSettings settings, string segment)
+		{
+			_logger.Trace("");
+			var seg = new Segment(languageDirection.SourceCulture);
+			seg.Add(segment);
+			return SearchSegment(settings, seg);
+		}
 
-        /// <summary>
-        /// Translate a translation unit using the source segment.
-        /// </summary>
-        /// <param name="settings"></param>
-        /// <param name="translationUnit"></param>
-        /// <returns></returns>
-        public SearchResults SearchTranslationUnit(SearchSettings settings, TranslationUnit translationUnit)
-        {
-            _logger.Trace("");
-            return SearchSegment(settings, translationUnit.SourceSegment);
-        }
+		/// <summary>
+		/// Translate a translation unit using the source segment.
+		/// </summary>
+		/// <param name="settings"></param>
+		/// <param name="translationUnit"></param>
+		/// <returns></returns>
+		public SearchResults SearchTranslationUnit(SearchSettings settings, TranslationUnit translationUnit)
+		{
+			_logger.Trace("");
+			return SearchSegment(settings, translationUnit.SourceSegment);
+		}
 
-        /// <summary>
-        /// Translate an array of translation units using their source segments.
-        /// </summary>
-        /// <param name="settings"></param>
-        /// <param name="translationUnits"></param>
-        /// <returns></returns>
-        public SearchResults[] SearchTranslationUnits(SearchSettings settings, TranslationUnit[] translationUnits)
-        {
-            _logger.Trace("");
-            return SearchSegments(settings, translationUnits.Select(tu => tu.SourceSegment).ToArray());
-        }
+		/// <summary>
+		/// Translate an array of translation units using their source segments.
+		/// </summary>
+		/// <param name="settings"></param>
+		/// <param name="translationUnits"></param>
+		/// <returns></returns>
+		public SearchResults[] SearchTranslationUnits(SearchSettings settings, TranslationUnit[] translationUnits)
+		{
+			_logger.Trace("");
+			return SearchSegments(settings, translationUnits.Select(tu => tu.SourceSegment).ToArray());
+		}
 
 		/// <summary>
 		/// Translate an array of translation units using their source segments.
@@ -258,8 +260,16 @@ namespace Sdl.Community.MTEdge.Provider
 		/// <returns>Xliff</returns>
 		public Xliff CreateXliffFile(Segment[] segments)
 		{
-			_logger.Trace("");
-			var xliffDocument = new Xliff(languageDirection.SourceCulture, languageDirection.TargetCulture);
+			var file = new File
+			{
+				SourceCulture = languageDirection.SourceCulture,
+				TargetCulture = languageDirection.TargetCulture
+			};
+
+			var xliffDocument = new Xliff
+			{
+				File = file
+			};
 
 			foreach (var seg in segments)
 			{
@@ -268,111 +278,112 @@ namespace Sdl.Community.MTEdge.Provider
 					xliffDocument.AddSourceSegment(seg);
 				}
 			}
+
 			return xliffDocument;
 		}
 
-        #region Unnecessary Training Methods
-        /// <summary>
-        /// Not required for this implementation. This is used to "train" the source, which if it's readonly
-        /// doesn't work.
-        /// </summary>
-        /// <param name="translationUnits"></param>
-        /// <param name="settings"></param>
-        /// <param name="mask"></param>
-        /// <returns></returns>
-        public ImportResult[] AddTranslationUnitsMasked(
-            TranslationUnit[] translationUnits,
-            ImportSettings settings,
-            bool[] mask)
-        {
-            _logger.Trace("");
-            throw new NotImplementedException();
-        }
+		#region Unnecessary Training Methods
+		/// <summary>
+		/// Not required for this implementation. This is used to "train" the source, which if it's readonly
+		/// doesn't work.
+		/// </summary>
+		/// <param name="translationUnits"></param>
+		/// <param name="settings"></param>
+		/// <param name="mask"></param>
+		/// <returns></returns>
+		public ImportResult[] AddTranslationUnitsMasked(
+			TranslationUnit[] translationUnits,
+			ImportSettings settings,
+			bool[] mask)
+		{
+			_logger.Trace("");
+			throw new NotImplementedException();
+		}
 
-        /// <summary>
-        /// Not required for this implementation. This is used to "train" the source, which if it's readonly
-        /// doesn't work.
-        /// </summary>
-        /// <param name="translationUnit"></param>
-        /// <returns></returns>
-        public ImportResult UpdateTranslationUnit(TranslationUnit translationUnit)
-        {
-            _logger.Trace("");
-            throw new NotImplementedException();
-        }
+		/// <summary>
+		/// Not required for this implementation. This is used to "train" the source, which if it's readonly
+		/// doesn't work.
+		/// </summary>
+		/// <param name="translationUnit"></param>
+		/// <returns></returns>
+		public ImportResult UpdateTranslationUnit(TranslationUnit translationUnit)
+		{
+			_logger.Trace("");
+			throw new NotImplementedException();
+		}
 
-        /// <summary>
-        /// Not required for this implementation. This is used to "train" the source, which if it's readonly
-        /// doesn't work.
-        /// </summary>
-        /// <param name="translationUnits"></param>
-        /// <returns></returns>
-        public ImportResult[] UpdateTranslationUnits(TranslationUnit[] translationUnits)
-        {
-            _logger.Trace("");
-            throw new NotImplementedException();
-        }
-        /// <summary>
-        /// Not required for this implementation. This is used to "train" the source, which if it's readonly
-        /// doesn't work.
-        /// </summary>
-        /// <param name="translationUnits"></param>
-        /// <param name="previousTranslationHashes"></param>
-        /// <param name="settings"></param>
-        /// <param name="mask"></param>
-        /// <returns></returns>
-        public ImportResult[] AddOrUpdateTranslationUnitsMasked(
-            TranslationUnit[] translationUnits,
-            int[] previousTranslationHashes,
-            ImportSettings settings,
-            bool[] mask)
-        {
-            _logger.Trace("");
-            throw new NotImplementedException();
-        }
+		/// <summary>
+		/// Not required for this implementation. This is used to "train" the source, which if it's readonly
+		/// doesn't work.
+		/// </summary>
+		/// <param name="translationUnits"></param>
+		/// <returns></returns>
+		public ImportResult[] UpdateTranslationUnits(TranslationUnit[] translationUnits)
+		{
+			_logger.Trace("");
+			throw new NotImplementedException();
+		}
+		/// <summary>
+		/// Not required for this implementation. This is used to "train" the source, which if it's readonly
+		/// doesn't work.
+		/// </summary>
+		/// <param name="translationUnits"></param>
+		/// <param name="previousTranslationHashes"></param>
+		/// <param name="settings"></param>
+		/// <param name="mask"></param>
+		/// <returns></returns>
+		public ImportResult[] AddOrUpdateTranslationUnitsMasked(
+			TranslationUnit[] translationUnits,
+			int[] previousTranslationHashes,
+			ImportSettings settings,
+			bool[] mask)
+		{
+			_logger.Trace("");
+			throw new NotImplementedException();
+		}
 
-        /// <summary>
-        /// Not required for this implementation. This is used to "train" the source, which if it's readonly
-        ///  doesn't work.
-        /// </summary>
-        /// <param name="translationUnit"></param>
-        /// <param name="settings"></param>
-        /// <returns></returns>
-        public ImportResult AddTranslationUnit(TranslationUnit translationUnit, ImportSettings settings)
-        {
-            _logger.Trace("");
-            throw new NotImplementedException();
-        }
+		/// <summary>
+		/// Not required for this implementation. This is used to "train" the source, which if it's readonly
+		///  doesn't work.
+		/// </summary>
+		/// <param name="translationUnit"></param>
+		/// <param name="settings"></param>
+		/// <returns></returns>
+		public ImportResult AddTranslationUnit(TranslationUnit translationUnit, ImportSettings settings)
+		{
+			_logger.Trace("");
+			throw new NotImplementedException();
+		}
 
-        /// <summary>
-        /// Not required for this implementation. This is used to "train" the source, which if it's readonly
-        /// doesn't work.
-        /// </summary>
-        /// <param name="translationUnits"></param>
-        /// <param name="settings"></param>
-        /// <returns></returns>
-        public ImportResult[] AddTranslationUnits(TranslationUnit[] translationUnits, ImportSettings settings)
-        {
-            _logger.Trace("");
-            throw new NotImplementedException();
-        }
+		/// <summary>
+		/// Not required for this implementation. This is used to "train" the source, which if it's readonly
+		/// doesn't work.
+		/// </summary>
+		/// <param name="translationUnits"></param>
+		/// <param name="settings"></param>
+		/// <returns></returns>
+		public ImportResult[] AddTranslationUnits(TranslationUnit[] translationUnits, ImportSettings settings)
+		{
+			_logger.Trace("");
+			throw new NotImplementedException();
+		}
 
-        /// <summary>
-        /// Not required for this implementation. This is used to "train" the source, which if it's readonly
-        /// doesn't work.
-        /// </summary>
-        /// <param name="translationUnits"></param>
-        /// <param name="previousTranslationHashes"></param>
-        /// <param name="settings"></param>
-        /// <returns></returns>
-        public ImportResult[] AddOrUpdateTranslationUnits(
-            TranslationUnit[] translationUnits,
-            int[] previousTranslationHashes,
-            ImportSettings settings)
-        {
-            _logger.Trace("");
-            throw new NotImplementedException();
-        }
-        #endregion
-    }
+		/// <summary>
+		/// Not required for this implementation. This is used to "train" the source, which if it's readonly
+		/// doesn't work.
+		/// </summary>
+		/// <param name="translationUnits"></param>
+		/// <param name="previousTranslationHashes"></param>
+		/// <param name="settings"></param>
+		/// <returns></returns>
+		public ImportResult[] AddOrUpdateTranslationUnits(
+			TranslationUnit[] translationUnits,
+			int[] previousTranslationHashes,
+			ImportSettings settings)
+		{
+			_logger.Trace("");
+			throw new NotImplementedException();
+		}
+		#endregion
+	}
 }
