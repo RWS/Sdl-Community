914e36256b0998e459d7e133b65bd148b0c846a8
Emanuel Albu <ealbu@sdl.com>
2021-01-20 16:20:30 +0200

    SDL MT Edge 3.0.1.1 -> 3.0.2.0  - fixes invalid xliff issue

---
+++ SDLMTEdge.Provider/SDLMTEdgeTranslationProvider/XliffConverter/Models/Header.cs
@@ -0,0 +1,10 @@
+﻿using System.Xml.Serialization;
+
+namespace Sdl.Community.MTEdge.Provider.XliffConverter.Models
+{
+	public class Header
+	{
+		[XmlElement("tool")]
+		public Tool[] Tools { get; set; }		
+	}
+}
