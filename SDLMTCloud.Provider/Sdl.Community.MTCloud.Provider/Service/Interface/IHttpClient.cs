0a0681d2aa325cd61dbc5b86f730b1223e2ae0ff
Emanuel Albu <ealbu@sdl.com>
2021-01-26 14:51:05 +0200

    - use only one instance of httpClient throughout the entire lifecycle of the app

---
+++ SDLMTCloud.Provider/Sdl.Community.MTCloud.Provider/Service/Interface/IHttpClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace Sdl.Community.MTCloud.Provider.Service.Interface
+{
+	public interface IHttpClient
+	{
+		HttpRequestHeaders DefaultRequestHeaders { get; }
+
+		TimeSpan Timeout { get; set; }
+
+		Task<HttpResponseMessage> SendAsync(HttpRequestMessage httpRequestMessage);
+
+		Task<HttpResponseMessage> SendAsync(HttpRequestMessage httpRequestMessage, HttpCompletionOption httpCompletionOption);
+	}
+}
