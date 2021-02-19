d257542e18ae5849302adbdc0a5a1a00592ec1ff
Emanuel Albu <42834028+ealbu@users.noreply.github.com>
2021-02-19 08:56:38 +0200

    DSI Viewer 3.0.3.0 (#706)
    * DSI Viewer 3.0.3.0
     - accomodate the new "qualityEstimation" parameter from SDL MT Cloud
    
    * - UI/UX changes for quality estimation part of the app
    
    * - UI/UX changes for filter options
    
    * - add copy comments capabilities
    
    * - CR changes
    
    * - clean code and delete unused usings
    
    * ...

--- DSI Viewer/Sdl.Community.SdiViewer/ViewModel/DsiViewerViewModel.cs
+++ DSI Viewer/Sdl.Community.SdiViewer/ViewModel/DsiViewerViewModel.cs
@@ -1,8 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Windows.Input;
+using Sdl.Community.DsiViewer.Commands;
 using Sdl.Community.DsiViewer.Model;
+using Sdl.Community.DsiViewer.Service;
 using Sdl.Community.DsiViewer.Services;
 using Sdl.FileTypeSupport.Framework.BilingualApi;
 using Sdl.FileTypeSupport.Framework.Core.Utilities.NativeApi;
@@ -13,11 +17,14 @@ namespace Sdl.Community.DsiViewer.ViewModel
 {
 	public class DsiViewerViewModel : ModelBase, IDisposable
 	{
-		private List<DsiModel> _documentStructureInformation;
-		private List<IComment> _comments;
-		private IStudioDocument _activeDocument;
 		private readonly EditorController _editorController;
 		private readonly SegmentVisitor _segmentVisitor;
+		private IStudioDocument _activeDocument;
+		private ICommand _applySdlMtCloudFilter;
+		private ICommand _clearSdlMtCloudFilter;
+		private List<IComment> _comments;
+		private List<DsiModel> _documentStructureInformation;
+		private TranslationOriginData _translationOriginData;
 
 		public DsiViewerViewModel()
 		{
@@ -25,10 +32,27 @@ namespace Sdl.Community.DsiViewer.ViewModel
 			_comments = new List<IComment>();
 			_documentStructureInformation = new List<DsiModel>();
 
-			_editorController = GetEditorController();
+			_editorController = DsiViewerInitializer.EditorController;
 			_editorController.ActiveDocumentChanged += EditorController_ActiveDocumentChanged;
 
 			SetActiveDocument(_editorController.ActiveDocument);
+		}
+
+		public ICommand ApplySdlMtCloudFilter => _applySdlMtCloudFilter ??= new CommandHandler(() => ApplyFilter(), true);
+		public ICommand ClearSdlMtCloudFilter => _clearSdlMtCloudFilter ??= new CommandHandler(() => ApplyFilter(true), true);
+
+		public IOrderedEnumerable<IComment> Comments
+		{
+			get
+			{
+				_comments ??= new List<IComment>();
+				return _comments.OrderByDescending(a => (int)a.Severity).ThenByDescending(a => a.Date);
+			}
+			set
+			{
+				_comments = value?.ToList();
+				OnPropertyChanged(nameof(Comments));
+			}
 		}
 
 		public IOrderedEnumerable<DsiModel> DocumentStructureInformation
@@ -49,27 +73,183 @@ namespace Sdl.Community.DsiViewer.ViewModel
 			}
 		}
 
-		public IOrderedEnumerable<IComment> Comments
-		{
-			get
-			{
-				if (_comments == null)
-				{
-					_comments = new List<IComment>();
-				}
+		public FilterApplier FilterApplier => DsiViewerInitializer.FilterApplier;
 
-				return _comments.OrderByDescending(a => (int)a.Severity).ThenByDescending(a => a.Date);
-			}
-			set
-			{
-				_comments = value?.ToList();
-				OnPropertyChanged(nameof(Comments));
-			}
-		}
+		public bool HasComments => Comments.Any();
 
 		public bool HasDocumentStructureInformation => DocumentStructureInformation.Any();
 
-		public bool HasComments => Comments.Any();
+		public bool HasSdlMtCloudRelatedInfo
+													=>
+				_editorController?.ActiveDocument?.SegmentPairs.Any(
+					sp => sp.Properties.TranslationOrigin.MetaDataContainsKey("quality_estimation")) ?? false;
+
+		public bool HasTranslationOriginMetadata => TranslationOriginData != null;
+
+		public object SelectedItem { get; set; }
+
+		public TranslationOriginData TranslationOriginData
+		{
+			get => _translationOriginData;
+			set
+			{
+				_translationOriginData = value;
+				OnPropertyChanged(nameof(TranslationOriginData));
+				OnPropertyChanged(nameof(HasTranslationOriginMetadata));
+			}
+		}
+
+		public void Dispose()
+		{
+			if (_editorController != null)
+			{
+				_editorController.ActiveDocumentChanged -= EditorController_ActiveDocumentChanged;
+			}
+
+			if (_activeDocument != null)
+			{
+				_activeDocument.ActiveSegmentChanged -= ActiveDocument_ActiveSegmentChanged;
+				_activeDocument.SegmentsTranslationOriginChanged -= ActiveDocument_SegmentsTranslationOriginChanged;
+			}
+		}
+
+		private static string GetMetaValue(string metaValue)
+		{
+			var containsNumber = int.TryParse(metaValue, out _);
+
+			return containsNumber ? metaValue : PluginResources.ResourceManager.GetString("StructureContextInfo_MetaValue_" + metaValue);
+		}
+
+		private void ActiveDocument_ActiveSegmentChanged(object sender, EventArgs e)
+		{
+			UpdateDocumentStructureInformation();
+			UpdateComments();
+			UpdateTranslationOriginInformation();
+		}
+
+		private void ActiveDocument_SegmentsTranslationOriginChanged(object sender, EventArgs e)
+		{
+			UpdateTranslationOriginInformation();
+			OnPropertyChanged(nameof(HasSdlMtCloudRelatedInfo));
+		}
+
+		private void AddComments(ISegment segment)
+		{
+			_segmentVisitor.VisitSegment(segment);
+			foreach (var comment in _segmentVisitor.Comments)
+			{
+				_comments.Add(comment);
+			}
+		}
+
+		private void AddComments(ISegmentPair segmentPair)
+		{
+			var paragraphInfo = segmentPair?.GetParagraphUnitProperties();
+			if (paragraphInfo?.Comments?.Comments == null)
+			{
+				return;
+			}
+
+			foreach (var comment in paragraphInfo.Comments.Comments)
+			{
+				_comments.Add(comment);
+			}
+		}
+
+		private void ApplyFilter(bool isClearing = false)
+		{
+			if (isClearing)
+			{
+				FilterApplier.ClearFilter();
+			}
+
+			FilterApplier.ApplyFilter();
+		}
+
+		private void EditorController_ActiveDocumentChanged(object sender, DocumentEventArgs e)
+		{
+			SetActiveDocument(e.Document);
+			OnPropertyChanged(nameof(HasSdlMtCloudRelatedInfo));
+		}
+
+		private string GetAdditionalInformation(IContextInfo context)
+		{
+			var additionalInfo = new StringBuilder();
+			foreach (var metaPair in context.MetaData)
+			{
+				var metaDataInfoDescriptionKey = PluginResources.ResourceManager.GetString("StructureContextInfo_MetaKey_" + metaPair.Key);
+				if (!string.IsNullOrEmpty(metaDataInfoDescriptionKey))
+				{
+					additionalInfo.Append($"{metaDataInfoDescriptionKey}: {GetMetaValue(metaPair.Value)}; ");
+				}
+			}
+			return additionalInfo.ToString();
+		}
+
+		private string GetEstimationColorLabel(string estimation)
+		{
+			Color color;
+			switch (estimation)
+			{
+				case "Good":
+					color = Color.FromArgb(0, 128, 64);
+					break;
+
+				case "Adequate":
+					color = Color.FromArgb(0, 128, 255);
+					break;
+
+				case "Poor":
+					color = Color.FromArgb(255, 72, 72);
+					break;
+
+				default:
+					color = Color.FromArgb(183, 183, 219);
+					break;
+			}
+
+			return "#" + color.R.ToString("X2") + color.G.ToString("X2") + color.B.ToString("X2");
+		}
+
+		private void SetActiveDocument(IStudioDocument document)
+		{
+			if (_activeDocument != null)
+			{
+				_activeDocument.ActiveSegmentChanged -= ActiveDocument_ActiveSegmentChanged;
+			}
+
+			_activeDocument = document;
+
+			if (_activeDocument != null)
+			{
+				_activeDocument.ActiveSegmentChanged += ActiveDocument_ActiveSegmentChanged;
+				_activeDocument.SegmentsTranslationOriginChanged += ActiveDocument_SegmentsTranslationOriginChanged;
+
+				UpdateDocumentStructureInformation();
+				UpdateComments();
+				UpdateTranslationOriginInformation();
+			}
+		}
+
+		private void UpdateComments()
+		{
+			_comments.Clear();
+
+			var segmentPair = _activeDocument?.ActiveSegmentPair;
+			if (segmentPair == null)
+			{
+				OnPropertyChanged(nameof(Comments));
+				OnPropertyChanged(nameof(HasComments));
+				return;
+			}
+
+			AddComments(segmentPair);
+			AddComments(segmentPair.Source);
+			AddComments(segmentPair.Target);
+
+			OnPropertyChanged(nameof(Comments));
+			OnPropertyChanged(nameof(HasComments));
+		}
 
 		private void UpdateDocumentStructureInformation()
 		{
@@ -118,115 +298,24 @@ namespace Sdl.Community.DsiViewer.ViewModel
 			OnPropertyChanged(nameof(HasDocumentStructureInformation));
 		}
 
-		private void UpdateComments()
+		private void UpdateTranslationOriginInformation()
 		{
-			_comments.Clear();
-
-			var segmentPair = _activeDocument?.ActiveSegmentPair;
-			if (segmentPair == null)
+			TranslationOriginData = null;
+			var translationOrigin = _activeDocument.ActiveSegmentPair?.Properties.TranslationOrigin;
+			if (translationOrigin is null || string.IsNullOrWhiteSpace(translationOrigin.GetMetaData("quality_estimation")))
 			{
-				OnPropertyChanged(nameof(Comments));
-				OnPropertyChanged(nameof(HasComments));
+				OnPropertyChanged(nameof(TranslationOriginData));
+				OnPropertyChanged(nameof(HasTranslationOriginMetadata));
 				return;
 			}
 
-			AddComments(segmentPair);
-			AddComments(segmentPair.Source);
-			AddComments(segmentPair.Target);
-
-			OnPropertyChanged(nameof(Comments));
-			OnPropertyChanged(nameof(HasComments));
-		}
-
-		private void AddComments(ISegment segment)
-		{
-			_segmentVisitor.VisitSegment(segment);
-			foreach (var comment in _segmentVisitor.Comments)
+			var qualityEstimation = translationOrigin.GetMetaData("quality_estimation");
+			TranslationOriginData = new TranslationOriginData
 			{
-				_comments.Add(comment);
-			}
-		}
-
-		private void AddComments(ISegmentPair segmentPair)
-		{
-			var paragraphInfo = segmentPair?.GetParagraphUnitProperties();
-			if (paragraphInfo?.Comments?.Comments == null)
-			{
-				return;
-			}
-
-			foreach (var comment in paragraphInfo.Comments.Comments)
-			{
-				_comments.Add(comment);
-			}
-		}
-
-		private void EditorController_ActiveDocumentChanged(object sender, DocumentEventArgs e)
-		{
-			SetActiveDocument(e.Document);
-		}
-
-		private void SetActiveDocument(IStudioDocument document)
-		{
-			if (_activeDocument != null)
-			{
-				_activeDocument.ActiveSegmentChanged -= ActiveDocument_ActiveSegmentChanged;
-			}
-
-			_activeDocument = document;
-
-			if (_activeDocument != null)
-			{
-				_activeDocument.ActiveSegmentChanged += ActiveDocument_ActiveSegmentChanged;
-
-				UpdateDocumentStructureInformation();
-				UpdateComments();
-			}
-		}
-
-		private void ActiveDocument_ActiveSegmentChanged(object sender, EventArgs e)
-		{
-			UpdateDocumentStructureInformation();
-			UpdateComments();
-		}
-
-		private static EditorController GetEditorController()
-		{
-			return SdlTradosStudio.Application.GetController<EditorController>();
-		}
-
-		public string GetAdditionalInformation(IContextInfo context)
-		{
-			var additionalInfo = new StringBuilder();
-			foreach (var metaPair in context.MetaData)
-			{
-				var metaDataInfoDescriptionKey = PluginResources.ResourceManager.GetString("StructureContextInfo_MetaKey_" + metaPair.Key);
-				if (!string.IsNullOrEmpty(metaDataInfoDescriptionKey))
-				{
-					additionalInfo.Append($"{metaDataInfoDescriptionKey}: {GetMetaValue(metaPair.Value)}; ");
-				}
-			}
-			return additionalInfo.ToString();
-		}
-		private static string GetMetaValue(string metaValue)
-		{
-			var containsNumber = int.TryParse(metaValue, out _);
-
-			return containsNumber ? metaValue : PluginResources.ResourceManager.GetString("StructureContextInfo_MetaValue_" + metaValue);
-		}
-
-		public void Dispose()
-		{
-			if (_editorController != null)
-			{
-				_editorController.ActiveDocumentChanged -= EditorController_ActiveDocumentChanged;
-			}
-
-			if (_activeDocument != null)
-			{
-				_activeDocument.ActiveSegmentChanged -= ActiveDocument_ActiveSegmentChanged;
-			}
+				QualityEstimation = qualityEstimation,
+				Model = translationOrigin.GetMetaData("model"),
+				ColorCode = GetEstimationColorLabel(qualityEstimation)
+			};
 		}
 	}
 }
-
