3538bfa192e67cd5252b44b7df0bdb1f43404087
Emanuel Albu <42834028+ealbu@users.noreply.github.com>
2021-03-05 15:27:30 +0200

    DSI Viewer 2.0.3.0 -> migrate changes to 2019 (#714)
    * DSI Viewer 2.0.3.0 -> migrate changes to 2019:
     - Quality Estimation and Model parameters accomodation
    
    * Update DSI Viewer.yml for Azure Pipelines
    
    (cherry picked from commit c8e47b545e7043712636ecb7b551d32ce3447f2c)
    
    Co-authored-by: Ionel-Cristian Moldovan <66057010+icmoldovan@users.noreply.github.com>

---
+++ DSI Viewer/Sdl.Community.SdiViewer/DsiViewerInitializer.cs
@@ -0,0 +1,25 @@
+﻿using Sdl.Community.DsiViewer.Service;
+using Sdl.Desktop.IntegrationApi;
+using Sdl.Desktop.IntegrationApi.Extensions;
+using Sdl.TranslationStudioAutomation.IntegrationApi;
+
+namespace Sdl.Community.DsiViewer
+{
+	[ApplicationInitializer]
+	public class DsiViewerInitializer : IApplicationInitializer
+	{
+		public const string AdequateQuality = "ADEQUATE";
+		public const string GoodQuality = "GOOD";
+		public const string NoneAvailable = "N/A";
+		public const string PoorQuality = "POOR";
+
+		public static EditorController EditorController { get; private set; }
+
+		public static FilterApplier FilterApplier { get; } = new FilterApplier();
+
+		public void Execute()
+		{
+			EditorController = SdlTradosStudio.Application.GetController<EditorController>();
+		}
+	}
+}
---
+++ DSI Viewer/Sdl.Community.SdiViewer/Studio/DisplayFilters/SdlMtCloudDisplayFilter.cs
@@ -0,0 +1,58 @@
+﻿using Sdl.Community.DsiViewer.Model;
+using Sdl.FileTypeSupport.Framework.NativeApi;
+using Sdl.TranslationStudioAutomation.IntegrationApi.DisplayFilters;
+
+namespace Sdl.Community.DsiViewer.Studio.DisplayFilters
+{
+	public class SdlMtCloudDisplayFilter : IDisplayFilter
+	{
+		private const string Adequate = "Adequate";
+		private const string Good = "Good";
+		private const string NoneAvailable = "N/A";
+		private const string Poor = "Poor";
+		public SdlMtCloudFilterSettings Settings { get; } = new SdlMtCloudFilterSettings();
+
+		public bool EvaluateRow(DisplayFilterRowInfo rowInfo)
+		{
+			if (!rowInfo.IsSegment) return false;
+
+			var shouldDisplayRow = true;
+			var translationOrigin = rowInfo?.SegmentPair?.Properties?.TranslationOrigin;
+
+			if (Settings.ByModel)
+			{
+				shouldDisplayRow = translationOrigin?.GetMetaData("model") == Settings.Model;
+			}
+
+			if (Settings.ByQualityEstimation)
+			{
+				var anyQualityEstimation = false;
+				if (Settings.QeNoneAvailable)
+				{
+					anyQualityEstimation = GetQualityEstimation(translationOrigin) == DsiViewerInitializer.NoneAvailable;
+				}
+				if (Settings.QePoor)
+				{
+					anyQualityEstimation |= GetQualityEstimation(translationOrigin) == DsiViewerInitializer.PoorQuality;
+				}
+				if (Settings.QeGood)
+				{
+					anyQualityEstimation |= GetQualityEstimation(translationOrigin) == DsiViewerInitializer.GoodQuality;
+				}
+				if (Settings.QeAdequate)
+				{
+					anyQualityEstimation |= GetQualityEstimation(translationOrigin) == DsiViewerInitializer.AdequateQuality;
+				}
+
+				shouldDisplayRow &= anyQualityEstimation;
+			}
+
+			return shouldDisplayRow;
+		}
+
+		private static string GetQualityEstimation(ITranslationOrigin translationOrigin)
+		{
+			return translationOrigin?.GetMetaData("quality_estimation");
+		}
+	}
+}
