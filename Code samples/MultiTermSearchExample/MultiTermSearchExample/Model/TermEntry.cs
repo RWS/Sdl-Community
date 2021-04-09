8eec11adc8b5d72a015b316f25962c2b1da6ca5d
Patrick Andrew Hartnett <phartnett@sdl.com>
2021-04-09 17:35:32 +0200

    Sample project that demonstrates how to:  - Load a multiterm database  - Perform a search  - Reading the results

---
+++ Code samples/MultiTermSearchExample/MultiTermSearchExample/Model/TermEntry.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace MultiTermSearchExample.Model
+{
+	public class TermEntry
+	{
+		public string Term { get; set; }
+		
+		public string Language { get; set; }
+		
+		public List<EntryField> IndexFields { get; set; }
+		
+		public List<EntryField> TermFields { get; set; }
+		
+		public bool ReadOnly { get; set; }
+	}
+}
