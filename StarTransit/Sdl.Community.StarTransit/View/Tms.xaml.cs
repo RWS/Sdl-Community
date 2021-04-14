21a628ae69689bdcf7b2f7261bf27fb3d64f9585
andreaGhisa <aghisa@sdl.com>
2021-04-14 13:41:10 +0300

    Star Transit TM Page: Fixed scroll viewer.

--- StarTransit/Sdl.Community.StarTransit/View/Tms.xaml.cs
+++ StarTransit/Sdl.Community.StarTransit/View/Tms.xaml.cs
@@ -1,28 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
+﻿using System.Windows.Controls;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Sdl.Community.StarTransit.View
 {
 	/// <summary>
 	/// Interaction logic for Tms.xaml
 	/// </summary>
-	public partial class Tms : UserControl
+	public partial class Tms
 	{
 		public Tms()
 		{
 			InitializeComponent();
+		}
+
+		private void OnPreviewMouseWheel(object sender, MouseWheelEventArgs e)
+		{
+			if (!(sender is ScrollViewer scv)) return;
+			scv.ScrollToVerticalOffset(scv.VerticalOffset - e.Delta);
+			e.Handled = true;
 		}
 	}
 }
