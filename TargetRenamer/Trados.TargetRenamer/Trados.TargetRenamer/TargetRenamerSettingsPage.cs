2da9348a515a6738a0b6b6684f3d07937655a702
Andrei Verciuc <averciuc@sdl.com>
2021-04-21 23:54:46 +0300

    Target Renamer: Adds the working MVVM working skeleton of the application

---
+++ TargetRenamer/Trados.TargetRenamer/Trados.TargetRenamer/TargetRenamerSettingsPage.cs
@@ -0,0 +1,37 @@
+﻿using Sdl.Desktop.IntegrationApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sdl.Core.Settings;
+using Trados.TargetRenamer.Control;
+using Trados.TargetRenamer.ViewModel;
+
+namespace Trados.TargetRenamer
+{
+    public class TargetRenamerSettingsPage : DefaultSettingsPage<TargetRenamerSettingsControl, TargetRenamerSettings>
+    {
+	    private TargetRenamerSettingsControl _control;
+	    private TargetRenamerSettings _settings;
+
+	    public override object GetControl()
+	    {
+		    _settings = ((ISettingsBundle) DataSource).GetSettingsGroup<TargetRenamerSettings>();
+			_control = base.GetControl() as TargetRenamerSettingsControl;
+			if (!(_control is null))
+			{
+				_control.TargetRenamerSettingsViewModel.Settings = _settings;
+			}
+
+			return _control;
+	    }
+
+	    public override void Save()
+		{
+			base.Save();
+			if(_settings is null) return;
+			
+		}
+    }
+}
