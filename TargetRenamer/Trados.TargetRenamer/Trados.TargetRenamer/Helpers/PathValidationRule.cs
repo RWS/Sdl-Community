bb594fe43d72487827d2fc303247e0d60f71ef3a
Andrei Verciuc <averciuc@sdl.com>
2021-05-14 00:22:07 +0300

    Target Renamer: Adds Validation Rules for path and filename

---
+++ TargetRenamer/Trados.TargetRenamer/Trados.TargetRenamer/Helpers/PathValidationRule.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using System.Windows.Controls;
+
+namespace Trados.TargetRenamer.Helpers
+{
+	public class PathValidationRule : ValidationRule
+	{
+		public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+		{
+			var path = string.Empty;
+			try
+			{
+				if (((string) value).Length > 0)
+				{
+					path = value.ToString();
+				}
+			}
+			catch (Exception e)
+			{
+				return new ValidationResult(false, "Path is empty.");
+			}
+
+			try
+			{
+				Path.GetDirectoryName(path);
+			}
+			catch (Exception e)
+			{
+				return new ValidationResult(false, "Path is not valid.");
+			}
+
+			if (path.IndexOfAny(Path.GetInvalidPathChars()) >= 0)
+				return new ValidationResult(false, "Path contains invalid characters.");
+
+			return ValidationResult.ValidResult;
+		}
+	}
+}
