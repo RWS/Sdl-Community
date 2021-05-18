35e85f2bb41dc6013e89795bf4c7ba88fdee657f
Emanuel Albu <42834028+ealbu@users.noreply.github.com>
2021-05-18 11:21:42 +0300

    SDLCOM-2667: MT Cloud: Enabling/Disabling AddDictionaryTermAction not working properly (#752)
    * AddDictionaryTermAction Disable/Enable issue fixed
    
    * Code cleaning and reorganizing

--- SDLMTCloud.Provider/Sdl.Community.MTCloud.Provider/Studio/ContextMenuAction/AddDictionaryTermAction.cs
+++ SDLMTCloud.Provider/Sdl.Community.MTCloud.Provider/Studio/ContextMenuAction/AddDictionaryTermAction.cs
@@ -4,16 +4,12 @@ using Sdl.Community.MTCloud.Provider.Events;
 using Sdl.Community.MTCloud.Provider.Model;
 using Sdl.Desktop.IntegrationApi;
 using Sdl.Desktop.IntegrationApi.Extensions;
-using Sdl.Desktop.IntegrationApi.Interfaces;
 using Sdl.Desktop.IntegrationApi.Notifications.Events;
 using Sdl.ProjectAutomation.Settings.Events;
-using Sdl.TranslationStudioAutomation.IntegrationApi;
 using Sdl.TranslationStudioAutomation.IntegrationApi.Presentation.DefaultLocations;
 
 namespace Sdl.Community.MTCloud.Provider.Studio.ContextMenuAction
 {
-
-
 	[Action("Add Dictionary Term",
 		Name = "Add Term to MT Cloud Dictionary",
 		Icon = "add_dictionary",
@@ -25,7 +21,6 @@ namespace Sdl.Community.MTCloud.Provider.Studio.ContextMenuAction
 		public AddDictionaryTermAction()
 		{
 			Enabled = false;
-			//TODO: move this functionality to its own class, sharing it with other features
 			//Because this object gets constructed before the EditorController has been initialized, we need to make sure that the subscription to the event happens only after the EditorController has had a chance to be initialized
 			//We cannot do this in the MTAppInitializer because this object gets constructed even before MtAppInit.Execute() is called
 			MtCloudApplicationInitializer.Subscribe<StudioWindowCreatedNotificationEvent>(
@@ -37,30 +32,6 @@ namespace Sdl.Community.MTCloud.Provider.Studio.ContextMenuAction
 			MtCloudApplicationInitializer.Subscribe<TranslationProviderStatusChanged>(EnableAction);
 		}
 
-		private void EnableAction(TranslationProviderStatusChanged tpStatus = null)
-		{
-			if (MtCloudApplicationInitializer.GetProjectInProcessing().GetProjectInfo().Id !=
-			    MtCloudApplicationInitializer.EditorController.ActiveDocument?.Project.GetProjectInfo().Id) return;
-
-			bool? hasSdlMtAdded;
-			if (tpStatus == null)
-			{
-				hasSdlMtAdded = MtCloudApplicationInitializer.EditorController.ActiveDocument?.Project
-					.GetTranslationProviderConfiguration().
-					Entries?.Any(
-						entry =>
-							entry.MainTranslationProvider
-								.Uri
-								.ToString().Contains("sdlmtcloud"));
-			}
-			else
-			{
-				hasSdlMtAdded = tpStatus.NewStatus;
-			}
-
-			Enabled = hasSdlMtAdded ?? false;
-		}
-
 		protected override async void Execute()
 		{
 			var selection = MtCloudApplicationInitializer.EditorController.ActiveDocument.Selection;
@@ -70,9 +41,32 @@ namespace Sdl.Community.MTCloud.Provider.Studio.ContextMenuAction
 				Target = selection.Target.ToString()
 			};
 
-
-
 			await MtCloudApplicationInitializer.TranslationService.AddTermToDictionary(term);
+		}
+
+		private void EnableAction(TranslationProviderStatusChanged tpStatus = null)
+		{
+			if (MtCloudApplicationInitializer.GetProjectInProcessing().GetProjectInfo().Id !=
+				MtCloudApplicationInitializer.EditorController.ActiveDocument?.Project.GetProjectInfo().Id) return;
+
+			bool? hasSdlMtAdded;
+			if (tpStatus == null)
+			{
+				hasSdlMtAdded = MtCloudApplicationInitializer.EditorController.ActiveDocument?.Project
+					.GetTranslationProviderConfiguration().
+					Entries?.FirstOrDefault(
+						entry =>
+							entry.MainTranslationProvider
+								.Uri
+								.ToString().Contains(PluginResources.SDLMTCloudUri))?.MainTranslationProvider.Enabled;
+			}
+			else
+			{
+				if (!tpStatus.TpUri?.ToString().Contains(PluginResources.SDLMTCloudUri) ?? false) return;
+				hasSdlMtAdded = tpStatus.NewStatus;
+			}
+
+			Enabled = hasSdlMtAdded ?? false;
 		}
 	}
 }
