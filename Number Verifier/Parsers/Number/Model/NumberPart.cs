f0d1b688faaaf61affcc707e318a0ebe4a769d5e
Patrick Andrew Hartnett <phartnett@sdl.com>
2021-06-24 16:20:17 +0200

    Number Verifier: Implemented a new NumberParser + Unit-tests

---
+++ Number Verifier/Parsers/Number/Model/NumberPart.cs
@@ -0,0 +1,24 @@
+﻿namespace Sdl.Community.NumberVerifier.Parsers.Number.Model
+{
+	public class NumberPart
+	{
+		public enum NumberType
+		{
+			GroupSeparator,
+			DecimalSeparator,
+			Number,
+			Separator,
+			Sign,
+			Currency,
+			WhiteSpace,
+			Exponent,
+			Invalid
+		}
+
+		public NumberType Type { get; set; }
+
+		public string Value { get; set; }
+		
+		public string Message { get; set; }
+	}
+}
---
+++ Number Verifier/Parsers/Number/Model/NumberToken.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Sdl.Community.NumberVerifier.Parsers.Number.Model
+{
+	public class NumberToken
+	{
+		public NumberToken(string text, List<NumberPart> numberParts)
+		{
+			Text = text;
+			NumberParts = numberParts;
+			Valid = numberParts?.Count > 0 && numberParts.All(a => a.Type != NumberPart.NumberType.Invalid);
+
+			if (numberParts != null)
+			{
+				HasDecimalSeparator = numberParts.Any(a => a.Type == NumberPart.NumberType.DecimalSeparator);
+				DecimalSeparator = numberParts.FirstOrDefault(a => a.Type == NumberPart.NumberType.DecimalSeparator)?.Value;
+				HasGroupSeparator = numberParts.Any(a => a.Type == NumberPart.NumberType.GroupSeparator);
+				GroupSeparator = numberParts.FirstOrDefault(a => a.Type == NumberPart.NumberType.GroupSeparator)?.Value;
+				HasCurrency = numberParts.Any(a => a.Type == NumberPart.NumberType.Currency);
+				Currency = numberParts.FirstOrDefault(a => a.Type == NumberPart.NumberType.Currency)?.Value;
+				HasSign = numberParts.Any(a => a.Type == NumberPart.NumberType.Sign);
+				Sign = numberParts.FirstOrDefault(a => a.Type == NumberPart.NumberType.Sign)?.Value;
+			}
+		}
+
+		public string Text { get; }
+
+		public List<NumberPart> NumberParts { get; }
+
+		public bool Valid { get; }
+
+		public bool HasDecimalSeparator { get; }
+		
+		public bool HasGroupSeparator { get; }
+		
+		public bool HasCurrency { get; }
+		
+		public bool HasSign { get; }
+
+		public string DecimalSeparator { get; }
+
+		public string GroupSeparator { get; }
+
+		public string Currency { get; }
+		
+		public string Sign { get; }
+	}
+}
