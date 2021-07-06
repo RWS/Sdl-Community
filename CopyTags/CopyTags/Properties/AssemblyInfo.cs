aca2a87cad93c270bc8a2aef68f750686d684843
andreaGhisa <aghisa@sdl.com>
2021-07-06 17:14:55 +0300

    Rebranded SDL Copy Tags 2021 SDLCOM-2804 (#771)

--- CopyTags/CopyTags/Properties/AssemblyInfo.cs
+++ CopyTags/CopyTags/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SDLCopyTags")]
+[assembly: AssemblyTitle("TradosCopyTags")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SDLCopyTags")]
+[assembly: AssemblyProduct("TradosCopyTags")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
