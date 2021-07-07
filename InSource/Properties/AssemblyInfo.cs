d11d7dcf9b7f570220f126f3dc9842e4a50c21ef
Andrea-Melinda Ghisa <aghisa@sdl.com>
2021-07-07 15:26:28 +0300

    InSource2021 rebranding.

--- InSource/Properties/AssemblyInfo.cs
+++ InSource/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("InSource!")]
+[assembly: AssemblyTitle("Trados InSource!")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("InSource!")]
-[assembly: AssemblyCopyright("Copyright SDL ©  2016")]
+[assembly: AssemblyProduct("Trados InSource!")]
+[assembly: AssemblyCopyright("Copyright RWS©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
--- InSource/Tellme/InSourceStoreAction.cs
+++ InSource/Tellme/InSourceStoreAction.cs
@@ -8,15 +8,15 @@ namespace Sdl.Community.InSource.Tellme
 	{
 		public InSourceStoreAction()
 		{
-			Name = "Download InSource from AppStore";
+			Name = "Download Trados InSource! from AppStore";
 		}
 		public override void Execute()
 		{
-			Process.Start("https://appstore.sdl.com/language/app/sdl-insource/548/");
+			Process.Start("https://appstore.sdl.com/language/app/trados-insource/548/");
 		}
 
 		public override bool IsAvailable => true;
-		public override string Category => "InSource results";
+		public override string Category => "Trados InSource! results";
 		public override Icon Icon => PluginResources.TellMe1;
 	}
 }
