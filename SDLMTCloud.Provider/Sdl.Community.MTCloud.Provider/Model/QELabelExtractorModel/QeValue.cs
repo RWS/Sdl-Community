a740ab31532e75149d58abd40d9c0bf1740e95d4
Emanuel Albu <ealbu@sdl.com>
2021-07-12 14:56:32 +0300

    SDLCOM-2790: MT Cloud Report: Number of words in the report

--- SDLMTCloud.Provider/Sdl.Community.MTCloud.Provider/Model/QELabelExtractorModel/QeValue.cs
+++ SDLMTCloud.Provider/Sdl.Community.MTCloud.Provider/Model/QELabelExtractorModel/QeValue.cs
@@ -5,7 +5,10 @@ namespace Sdl.Community.MTCloud.Provider.Model.QELabelExtractorModel
 	public class QeValue
 	{
 		[XmlAttribute]
-		public int Total { get; set; }
+		public int SegmentsTotal { get; set; }
+
+		[XmlAttribute]
+		public int WordsTotal { get; set; }
 
 		[XmlText]
 		public string QualityEstimation{ get; set; }
