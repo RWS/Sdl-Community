366b07b06043fc54344b48c49ba7d05549319ddc
Emanuel Albu <42834028+ealbu@users.noreply.github.com>
2021-07-12 13:58:58 +0300

    SDLCOM-2792: MT Cloud Report: Categories with 0 segments (#781)
    SDLCOM-2791: MT Cloud Report: Category order in the report

--- SDLMTCloud.Provider/Sdl.Community.MTCloud.Provider/Model/QELabelExtractorModel/QeFileReport.cs
+++ SDLMTCloud.Provider/Sdl.Community.MTCloud.Provider/Model/QELabelExtractorModel/QeFileReport.cs
@@ -8,6 +8,11 @@ namespace Sdl.Community.MTCloud.Provider.Model.QELabelExtractorModel
 	{
 		public LanguageDirection LanguageDirection { get; set; }
 		public string FileName { get; set; }
-		public Dictionary<string, List<ISegmentPair>> SegmentsPerCategory { get; set; } = new();
+		public Dictionary<string, List<ISegmentPair>> SegmentsPerCategory { get; set; } = new Dictionary<string, List<ISegmentPair>>
+		{
+			[PluginResources.GoodQuality] = new List<ISegmentPair>(),
+			[PluginResources.AdequateQuality] = new List<ISegmentPair>(),
+			[PluginResources.PoorQuality] = new List<ISegmentPair>()
+		};
 	}
 }
