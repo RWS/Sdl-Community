84a2ddb5b3971eb36c32133b062798a94105f15f
Andrea-Melinda Ghisa <aghisa@sdl.com>
2021-07-22 16:20:05 +0300

    2019 Legit rebranding.

--- Legit/Sdl.Community.Legit/Properties/AssemblyInfo.cs
+++ Legit/Sdl.Community.Legit/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Sdl.Community.Legit")]
+[assembly: AssemblyTitle("Trados Legit!")]
 [assembly: AssemblyDescription("Tool for converting files.")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
