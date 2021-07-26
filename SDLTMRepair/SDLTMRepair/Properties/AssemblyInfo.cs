2ac5d38f71abcc2f7a72f779a72e852e1c4820fe
andreaGhisa <aghisa@sdl.com>
2021-07-26 12:13:53 +0300

    Rebranding. (#811)

--- SDLTMRepair/SDLTMRepair/Properties/AssemblyInfo.cs
+++ SDLTMRepair/SDLTMRepair/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SDLTMRepair")]
+[assembly: AssemblyTitle("TMRepair")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SDLTMRepair")]
+[assembly: AssemblyProduct("TMRepair")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
