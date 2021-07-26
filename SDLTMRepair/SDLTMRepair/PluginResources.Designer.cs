bee609a28278193786959206cdd93c5c08d70ecc
Andrea-Melinda Ghisa <aghisa@sdl.com>
2021-07-26 11:55:45 +0300

    TM Repair rebranding.

--- SDLTMRepair/SDLTMRepair/PluginResources.Designer.cs
+++ SDLTMRepair/SDLTMRepair/PluginResources.Designer.cs
@@ -19,7 +19,7 @@ namespace Sdl.Community.TMRepair {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "15.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class PluginResources {
@@ -61,7 +61,7 @@ namespace Sdl.Community.TMRepair {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to Sdl.Community.TMRepair.
+        ///   Looks up a localized string similar to Trados Translation Memory Management Utility.
         /// </summary>
         internal static string Plugin_Name {
             get {
