59a6a8ad0c196cde97e6b3490002e2b5570d3560
Emanuel Albu <ealbu@sdl.com>
2021-07-28 16:55:13 +0300

    Add more input validation

--- TargetRenamer/Trados.TargetRenamer/Trados.TargetRenamer/View/TargetRenamerSettingsView.xaml.cs
+++ TargetRenamer/Trados.TargetRenamer/Trados.TargetRenamer/View/TargetRenamerSettingsView.xaml.cs
@@ -9,46 +9,62 @@ namespace Trados.TargetRenamer.View
 	/// Interaction logic for TargetRenamerSettingsView.xaml
 	/// </summary>
 	public partial class TargetRenamerSettingsView : ISettingsAware<TargetRenamerSettings>
-    {
-        public TargetRenamerSettingsView()
-        {
-            InitializeComponent();
-            AddErrorHandlers();
-        }
+	{
+		public TargetRenamerSettingsView()
+		{
+			InitializeComponent();
+			AddErrorHandlers();
+		}
 
-        public TargetRenamerSettings Settings { get; set; }
-        public TargetRenamerSettingsViewModel TargetRenamerSettingsViewModel => (TargetRenamerSettingsViewModel)DataContext;
-        private bool CustomLocationHasErrors { get; set; }
-        private bool DelimiterHasErrors { get; set; }
+		public TargetRenamerSettings Settings { get; set; }
+		public TargetRenamerSettingsViewModel TargetRenamerSettingsViewModel => (TargetRenamerSettingsViewModel)DataContext;
 
-        public void AddErrorHandlers()
-        {
-            Validation.AddErrorHandler(CustomLocation, ErrorHandler);
-            Validation.AddErrorHandler(Delimiter, ErrorHandler);
-        }
+		private bool CustomLocationHasErrors { get; set; }
+		private bool CustomStringHasErrors { get; set; }
+		private bool DelimiterHasErrors { get; set; }
+		private bool RegexReplaceWithHasErrors { get; set; }
 
-        public void Dispose()
-        {
-        }
+		public void AddErrorHandlers()
+		{
+			Validation.AddErrorHandler(CustomLocation, ErrorHandler);
+			Validation.AddErrorHandler(Delimiter, ErrorHandler);
+			Validation.AddErrorHandler(RegexReplaceWith, ErrorHandler);
+			Validation.AddErrorHandler(CustomString, ErrorHandler);
+		}
 
-        private void ErrorHandler(object sender, ValidationErrorEventArgs e)
-        {
-            var errorSource = e.OriginalSource as TextBox;
-            if (errorSource.Name == nameof(CustomLocation))
-            {
-                CustomLocationHasErrors = e.Action == ValidationErrorEventAction.Added;
-            }
-            if (errorSource.Name == nameof(Delimiter))
-            {
-                DelimiterHasErrors = e.Action == ValidationErrorEventAction.Added;
-            }
+		public void Dispose()
+		{
+		}
 
-            SetHasErrorOnViewModel();
-        }
+		private void ErrorHandler(object sender, ValidationErrorEventArgs e)
+		{
+			var errorSource = e.OriginalSource as TextBox;
 
-        private void SetHasErrorOnViewModel()
-        {
-            TargetRenamerSettingsViewModel.HasErrors = CustomLocationHasErrors || DelimiterHasErrors;
-        }
-    }
+			switch (errorSource?.Name)
+			{
+				case nameof(CustomLocation):
+					CustomLocationHasErrors = Validation.GetErrors(CustomLocation).Count > 0;
+					break;
+
+				case nameof(Delimiter):
+					DelimiterHasErrors = Validation.GetErrors(Delimiter).Count > 0;
+					break;
+
+				case nameof(RegexReplaceWith):
+					RegexReplaceWithHasErrors = Validation.GetErrors(RegexReplaceWith).Count > 0;
+					break;
+
+				case nameof(CustomString):
+					CustomStringHasErrors = Validation.GetErrors(CustomString).Count > 0;
+					break;
+			}
+
+			SetHasErrorOnViewModel();
+		}
+
+		private void SetHasErrorOnViewModel()
+		{
+			TargetRenamerSettingsViewModel.HasErrors = CustomLocationHasErrors || DelimiterHasErrors || RegexReplaceWithHasErrors || CustomStringHasErrors;
+		}
+	}
 }
