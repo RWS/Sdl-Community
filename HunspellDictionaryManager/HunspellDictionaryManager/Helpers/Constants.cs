be47e66cb5f2ce1f1fb7231b00d35c0455e8eaa5
Emanuel Albu <42834028+ealbu@users.noreply.github.com>
2021-09-20 14:33:12 +0300

    SDLCOM-3006: Remove mahApps dependency (#858)
    * Remove mahApps dependency
    
    * Change paths of files and var names needed for installer
    
    * ...
    
    * Correct paths
    
    * ...
    
    * ...
    
    * Rebranding part 2
    
    * Correct folder structure
    
    * ...

--- HunspellDictionaryManager/HunspellDictionaryManager/Helpers/Constants.cs
+++ HunspellDictionaryManager/HunspellDictionaryManager/Helpers/Constants.cs
@@ -4,7 +4,7 @@ namespace Sdl.Community.HunspellDictionaryManager.Helpers
 {
 	public static class Constants
 	{
-		public static readonly string BackupFolderPath = string.Format(@"C:\Users\{0}\AppData\Roaming\SDL Community", Environment.UserName);
+		public static readonly string BackupFolderPath = string.Format(@"C:\Users\{0}\AppData\Roaming\RWS AppStore", Environment.UserName);
 		public static readonly string Backup2017HunspellDicFolderPath = @"HunspellDictionaryManager\Studio2017\HunspellDictionaries";
 		public static readonly string Backup2019HunspellDicFolderPath = @"HunspellDictionaryManager\Studio2019\HunspellDictionaries";
 		public static readonly string Restore2017HunspellDicFolderPath = @"HunspellDictionaryManager\Studio2017\RestoreHunspellDictionaries";
@@ -33,13 +33,6 @@ namespace Sdl.Community.HunspellDictionaryManager.Helpers
 
 		public static readonly string LanguageAlreadyExists = "Hunspell language dictionary already exists with specified configuration!";
 		public static readonly string NoLanguageDictionaryFound = "Please select a language dictionary for deletion!";
-
-		public static readonly string ControlsStylePath = "pack://application:,,,/MahApps.Metro;component/Styles/Controls.xaml";
-		public static readonly string ColorsStylePath = "pack://application:,,,/MahApps.Metro;component/Styles/Colors.xaml";
-		public static readonly string FontsStylePath = "pack://application:,,,/MahApps.Metro;component/Styles/Fonts.xaml";
-		public static readonly string GreenAccentStylePath = "pack://application:,,,/MahApps.Metro;component/Styles/Accents/Green.xaml";
-		public static readonly string BaseLightAccentStylePath = "pack://application:,,,/MahApps.Metro;component/Styles/Accents/BaseLight.xaml";
-		public static readonly string FlatButtonStylePath = "pack://application:,,,/MahApps.Metro;component/Styles/FlatButton.xaml";
 
 		// Logging messages
 		public static readonly string GetInstalledStudioPath = "GetInstalledStudioPath method";
--- HunspellDictionaryManager/HunspellDictionaryManager/Helpers/Utils.cs
+++ HunspellDictionaryManager/HunspellDictionaryManager/Helpers/Utils.cs
@@ -18,36 +18,6 @@ namespace Sdl.Community.HunspellDictionaryManager.Helpers
 			if (Application.Current != null)
 			{
 				Application.Current.ShutdownMode = ShutdownMode.OnExplicitShutdown;
-				var controlsResources = new ResourceDictionary
-				{
-					Source = new Uri(Constants.ControlsStylePath, UriKind.Absolute)
-				};
-				var colorsResources = new ResourceDictionary
-				{
-					Source = new Uri(Constants.ColorsStylePath, UriKind.Absolute)
-				};
-				var fontsResources = new ResourceDictionary
-				{
-					Source = new Uri(Constants.FontsStylePath, UriKind.Absolute)
-				};
-				var greenResources = new ResourceDictionary
-				{
-					Source = new Uri(Constants.GreenAccentStylePath, UriKind.Absolute)
-				};
-				var baseLightResources = new ResourceDictionary
-				{
-					Source = new Uri(Constants.BaseLightAccentStylePath, UriKind.Absolute)
-				};
-				var flatButtonsResources = new ResourceDictionary
-				{
-					Source = new Uri(Constants.FlatButtonStylePath, UriKind.Absolute)
-				};
-
-				Application.Current.Resources.MergedDictionaries.Add(colorsResources);
-				Application.Current.Resources.MergedDictionaries.Add(greenResources);
-				Application.Current.Resources.MergedDictionaries.Add(baseLightResources);
-				Application.Current.Resources.MergedDictionaries.Add(flatButtonsResources);
-				Application.Current.Resources.MergedDictionaries.Add(controlsResources);
 			}
 		}
 
