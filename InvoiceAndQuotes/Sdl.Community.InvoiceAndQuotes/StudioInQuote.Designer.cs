a6fa6c07c869d2e4e015b8298678c8a35757bf82
andreaGhisa <aghisa@sdl.com>
2021-10-04 09:15:28 +0300

    In quote UI options (#872)
    * Fixed action name
    
    * Fixed  'Customers' box appears to be misaligned as per the screenshot.

--- InvoiceAndQuotes/Sdl.Community.InvoiceAndQuotes/StudioInQuote.Designer.cs
+++ InvoiceAndQuotes/Sdl.Community.InvoiceAndQuotes/StudioInQuote.Designer.cs
@@ -445,6 +445,7 @@
 			this.Controls.Add(this.tabControl1);
 			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
 			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MaximizeBox = false;
 			this.MinimizeBox = false;
 			this.Name = "StudioInQuote";
 			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
