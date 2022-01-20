2a3bcc8b0665c36e3ef9b8be55a182dc57ae3811
Emanuel Albu <ealbu@sdl.com>
2022-01-20 16:40:36 +0200

    LW Cloud 4.2.15.11: NA QE Report count fixed

--- SDLMTCloud.Provider/Sdl.Community.MTCloud.Provider/Model/QELabelExtractorModel/QeFileReport.cs
+++ SDLMTCloud.Provider/Sdl.Community.MTCloud.Provider/Model/QELabelExtractorModel/QeFileReport.cs
@@ -10,11 +10,12 @@ namespace Sdl.Community.MTCloud.Provider.Model.QELabelExtractorModel
 	{
 		public LanguageDirection LanguageDirection { get; set; }
 		public string FileName { get; set; }
-		public Dictionary<string, (List<ISegmentPair>, CountData)> SegmentsPerCategory { get; set; } = new Dictionary<string, (List<ISegmentPair>, CountData)>
+		public Dictionary<string, (List<ISegmentPair>, CountData)> SegmentsPerCategory { get; set; } = new()
 		{
 			[PluginResources.GoodQuality] = Tuple.Create(new List<ISegmentPair>(), new CountData()).ToValueTuple(),
 			[PluginResources.AdequateQuality] = Tuple.Create(new List<ISegmentPair>(), new CountData()).ToValueTuple(),
 			[PluginResources.PoorQuality] = Tuple.Create(new List<ISegmentPair>(), new CountData()).ToValueTuple(),
+			[PluginResources.UnknownQuality] = Tuple.Create(new List<ISegmentPair>(), new CountData()).ToValueTuple()
 		};
 	}
 }
