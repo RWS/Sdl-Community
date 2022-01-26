002f3f056a928a3f810dda64fb57cc1440d81861
Emanuel Albu <ealbu@sdl.com>
2022-01-26 11:57:50 +0200

    Add "Unknown" status

--- DSI Viewer/Sdl.Community.SdiViewer/DsiViewerInitializer.cs
+++ DSI Viewer/Sdl.Community.SdiViewer/DsiViewerInitializer.cs
@@ -10,7 +10,7 @@ namespace Sdl.Community.DsiViewer
 	{
 		public const string AdequateQuality = "Adequate";
 		public const string GoodQuality = "Good";
-		public const string NoneAvailable = "N/a";
+		public const string NoneAvailable = "Unknown";
 		public const string PoorQuality = "Poor";
 
 		public static EditorController EditorController { get; private set; }
