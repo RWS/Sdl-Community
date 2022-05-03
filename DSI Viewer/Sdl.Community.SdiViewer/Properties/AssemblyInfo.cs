d4987b48772627730fc883dc8de8f086bc400d30
Emanuel Albu <ealbu@sdl.com>
2022-05-03 09:48:03 +0200

    DSI Viewer  - update for Studio 2021 CU9

--- DSI Viewer/Sdl.Community.SdiViewer/Properties/AssemblyInfo.cs
+++ DSI Viewer/Sdl.Community.SdiViewer/Properties/AssemblyInfo.cs
@@ -5,13 +5,8 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Sdl.Community.DsiViewer")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Sdl.Community.DsiViewer")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -32,4 +27,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.4.0")]
+[assembly: AssemblyFileVersion("3.0.8.0")]
