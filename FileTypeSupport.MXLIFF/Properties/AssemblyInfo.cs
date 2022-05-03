e80e09cdf01e097e40bda7f8bb4b1e9098cd05cb
Emanuel Albu <ealbu@sdl.com>
2022-05-03 11:10:38 +0200

    MXLIFF Filetype 3.6.4.0 / 2021 CU9

--- FileTypeSupport.MXLIFF/Properties/AssemblyInfo.cs
+++ FileTypeSupport.MXLIFF/Properties/AssemblyInfo.cs
@@ -1,19 +1,12 @@
 ﻿using System.Resources;
 using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Memsource MXLIFF")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MXLIFF File type")]
-[assembly: AssemblyCopyright("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -33,8 +26,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.6.4.0")]
 [assembly: Sdl.Core.PluginFramework.Plugin("PluginName")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
