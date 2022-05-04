f5aedf1310de4198ff692016b60779c173d64d10
Cristian Florian <cflorian@sdl.com>
2022-05-04 10:12:03 +0300

    Updated warning message when changing termbases from "SDL Trados Studio API" to "Trados Studio API"

--- ApplyStudioProjectTemplate/Sdl.Community.ApplyStudioProjectTemplate/TermbaseWarningForm.Designer.cs
+++ ApplyStudioProjectTemplate/Sdl.Community.ApplyStudioProjectTemplate/TermbaseWarningForm.Designer.cs
@@ -41,7 +41,8 @@
             // WarningPicture
             // 
             this.WarningPicture.Image = ((System.Drawing.Image)(resources.GetObject("WarningPicture.Image")));
-            this.WarningPicture.Location = new System.Drawing.Point(12, 12);
+            this.WarningPicture.Location = new System.Drawing.Point(18, 18);
+            this.WarningPicture.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.WarningPicture.Name = "WarningPicture";
             this.WarningPicture.Size = new System.Drawing.Size(32, 32);
             this.WarningPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
@@ -52,28 +53,30 @@
             // 
             this.WarningHeading.BackColor = System.Drawing.Color.Yellow;
             this.WarningHeading.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.WarningHeading.Location = new System.Drawing.Point(50, 12);
+            this.WarningHeading.Location = new System.Drawing.Point(75, 18);
+            this.WarningHeading.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.WarningHeading.Name = "WarningHeading";
-            this.WarningHeading.Size = new System.Drawing.Size(367, 32);
+            this.WarningHeading.Size = new System.Drawing.Size(550, 49);
             this.WarningHeading.TabIndex = 0;
             this.WarningHeading.Text = "Warning";
             this.WarningHeading.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // WarningText
             // 
-            this.WarningText.Location = new System.Drawing.Point(12, 56);
+            this.WarningText.Location = new System.Drawing.Point(18, 86);
+            this.WarningText.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.WarningText.Name = "WarningText";
-            this.WarningText.Size = new System.Drawing.Size(405, 49);
+            this.WarningText.Size = new System.Drawing.Size(608, 75);
             this.WarningText.TabIndex = 1;
             this.WarningText.Text = resources.GetString("WarningText.Text");
             // 
             // DismissButton
             // 
             this.DismissButton.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.DismissButton.Location = new System.Drawing.Point(335, 116);
-            this.DismissButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.DismissButton.Location = new System.Drawing.Point(502, 178);
+            this.DismissButton.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
             this.DismissButton.Name = "DismissButton";
-            this.DismissButton.Size = new System.Drawing.Size(82, 23);
+            this.DismissButton.Size = new System.Drawing.Size(123, 35);
             this.DismissButton.TabIndex = 4;
             this.DismissButton.Text = "Cancel";
             this.DismissButton.UseVisualStyleBackColor = true;
@@ -81,19 +84,20 @@
             // OkButton
             // 
             this.OkButton.DialogResult = System.Windows.Forms.DialogResult.OK;
-            this.OkButton.Location = new System.Drawing.Point(247, 116);
-            this.OkButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.OkButton.Location = new System.Drawing.Point(370, 178);
+            this.OkButton.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
             this.OkButton.Name = "OkButton";
-            this.OkButton.Size = new System.Drawing.Size(82, 23);
+            this.OkButton.Size = new System.Drawing.Size(123, 35);
             this.OkButton.TabIndex = 3;
             this.OkButton.Text = "OK";
             this.OkButton.UseVisualStyleBackColor = true;
             // 
             // DontShow
             // 
-            this.DontShow.Location = new System.Drawing.Point(12, 117);
+            this.DontShow.Location = new System.Drawing.Point(18, 180);
+            this.DontShow.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.DontShow.Name = "DontShow";
-            this.DontShow.Size = new System.Drawing.Size(229, 23);
+            this.DontShow.Size = new System.Drawing.Size(344, 35);
             this.DontShow.TabIndex = 2;
             this.DontShow.Text = "&Don\'t show this warning again";
             this.DontShow.UseVisualStyleBackColor = true;
@@ -101,10 +105,10 @@
             // TermbaseWarningForm
             // 
             this.AcceptButton = this.OkButton;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.DismissButton;
-            this.ClientSize = new System.Drawing.Size(429, 151);
+            this.ClientSize = new System.Drawing.Size(644, 232);
             this.Controls.Add(this.DontShow);
             this.Controls.Add(this.DismissButton);
             this.Controls.Add(this.OkButton);
@@ -113,6 +117,7 @@
             this.Controls.Add(this.WarningPicture);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "TermbaseWarningForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Changing Termbases";
