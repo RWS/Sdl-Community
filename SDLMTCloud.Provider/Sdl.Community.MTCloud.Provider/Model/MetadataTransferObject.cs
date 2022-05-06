7c188c6359d17308df0a9f153cc7e119f27a4e8f
Emanuel Albu <ealbu@sdl.com>
2022-05-06 08:54:14 +0200

    Fix null reference exception

--- SDLMTCloud.Provider/Sdl.Community.MTCloud.Provider/Model/MetadataTransferObject.cs
+++ SDLMTCloud.Provider/Sdl.Community.MTCloud.Provider/Model/MetadataTransferObject.cs
@@ -6,6 +6,7 @@ namespace Sdl.Community.MTCloud.Provider.Model
 {
 	public class MetadataTransferObject
 	{
+		public string TargetLanguage { get; set; }
 		public string FilePath { get; set; }
 		public List<SegmentId> SegmentIds { get; set; }
 		public TranslationOriginData TranslationOriginData{ get; set; }
