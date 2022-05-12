8864bdae1f1f4431349d1e27f2c49b940a254b2d
Emanuel Albu <ealbu@sdl.com>
2022-05-12 09:47:15 +0200

    Filetype Support: Wordfast TXML 3.0.3.0 / 2021 CU9

--- Wordfast TXML/Properties/AssemblyInfo.cs
+++ Wordfast TXML/Properties/AssemblyInfo.cs
@@ -1,18 +1,12 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Sdl.Sdk.FilterFramework.Samples.Bil")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Sdl.Sdk.FilterFramework.Samples.Bil")]
 [assembly: AssemblyCopyright("Copyright ©  2010")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -32,6 +26,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.0")]
-[assembly: AssemblyFileVersion("1.1.0.0")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.0.3.0")]
 [assembly: Sdl.Core.PluginFramework.Plugin("Plugin_Name")]
