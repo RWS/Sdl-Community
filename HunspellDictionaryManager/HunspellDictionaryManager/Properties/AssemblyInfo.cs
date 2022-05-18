9550444ac5c3f41a980fcfddad89029cbaefce20
vnechita <96413558+vnechita@users.noreply.github.com>
2022-05-19 00:44:34 +0300

    Revert "Align the assembly version (app version) with the installer version. "

--- HunspellDictionaryManager/HunspellDictionaryManager/Properties/AssemblyInfo.cs
+++ HunspellDictionaryManager/HunspellDictionaryManager/Properties/AssemblyInfo.cs
@@ -45,4 +45,4 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.2.11.1")]
+[assembly: AssemblyFileVersion("3.0.2.0")]
