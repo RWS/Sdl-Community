068c3ba608472f826791cd8939c9c3548b4b1350
Emanuel Albu <ealbu@sdl.com>
2022-05-23 10:17:30 +0200

    MXLIFF 4.6.4.0 / 2022

--- FileTypeSupport.MXLIFF/Properties/AssemblyInfo.cs
+++ FileTypeSupport.MXLIFF/Properties/AssemblyInfo.cs
@@ -26,8 +26,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.6.4.0")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.6.4.0")]
 [assembly: Sdl.Core.PluginFramework.Plugin("PluginName")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
