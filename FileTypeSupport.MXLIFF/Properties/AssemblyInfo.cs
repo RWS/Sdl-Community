5904384fa2df2c51df9023bb318bd8c5130e73cb
Emanuel Albu <ealbu@sdl.com>
2022-05-23 16:54:42 +0200

    MXLIFF: Add copyright info

--- FileTypeSupport.MXLIFF/Properties/AssemblyInfo.cs
+++ FileTypeSupport.MXLIFF/Properties/AssemblyInfo.cs
@@ -7,6 +7,8 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("Memsource MXLIFF")]
 [assembly: AssemblyProduct("MXLIFF File type")]
+[assembly: AssemblyCompany("SDL Limited as part of the RWS Holdings Plc group of companies")]
+[assembly: AssemblyCopyright("Copyright © 2011 - 2022 SDL Limited as part of the RWS Holdings Plc group of companies (\"RWS Group\").")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
