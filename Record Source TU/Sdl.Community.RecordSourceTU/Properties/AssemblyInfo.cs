6da02c86915086349cdf9c6a4966bbd52460ca16
Emanuel Albu <ealbu@sdl.com>
2022-05-24 15:01:07 +0200

    ...

--- Record Source TU/Sdl.Community.RecordSourceTU/Properties/AssemblyInfo.cs
+++ Record Source TU/Sdl.Community.RecordSourceTU/Properties/AssemblyInfo.cs
@@ -6,7 +6,8 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("Sdl.Community.AddSourceTM")]
 [assembly: AssemblyProduct("Sdl.Community.AddSourceTM")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("SDL Limited as part of the RWS Holdings Plc group of companies")]
+[assembly: AssemblyCopyright("Copyright © 2011 - 2022 SDL Limited as part of the RWS Holdings Plc group of companies (\"RWS Group\").")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
