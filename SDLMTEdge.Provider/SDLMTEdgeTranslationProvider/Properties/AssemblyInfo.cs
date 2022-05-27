2909f562db1c7fb4bf3ab3c622c134885608136e
Emanuel Albu <ealbu@sdl.com>
2022-05-27 15:40:56 +0200

    SDLMT Edge 4.0.0.0 / 2022

--- SDLMTEdge.Provider/SDLMTEdgeTranslationProvider/Properties/AssemblyInfo.cs
+++ SDLMTEdge.Provider/SDLMTEdgeTranslationProvider/Properties/AssemblyInfo.cs
@@ -6,12 +6,9 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("SDLMTEdgePlugin")]
 [assembly: AssemblyDescription("RWS Plugin for enabling Trados Studio to use Language Weaver Edge.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("SDL")]
 [assembly: AssemblyProduct("SDLMTEdgePlugin")]
-[assembly: AssemblyCopyright("Copyright © SDL 2017")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCompany("SDL Limited as part of the RWS Holdings Plc group of companies")]
+[assembly: AssemblyCopyright("Copyright © 2011 - 2022 SDL Limited as part of the RWS Holdings Plc group of companies (\"RWS Group\").")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -31,5 +28,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.8.1")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
