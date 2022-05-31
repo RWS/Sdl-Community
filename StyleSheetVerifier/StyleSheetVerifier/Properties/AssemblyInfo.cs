af746c5589317bbe8ac2d68113bfeddb2e8d6b91
vnechita <vnechita@sdl.com>
2022-05-31 14:52:04 +0300

    Changes for Deploy Studio 2022 Plugins: Stylesheet Verifier SDLCOM-3456

--- StyleSheetVerifier/StyleSheetVerifier/Properties/AssemblyInfo.cs
+++ StyleSheetVerifier/StyleSheetVerifier/Properties/AssemblyInfo.cs
@@ -4,9 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("StyleSheetVerifier")]
-[assembly: AssemblyProduct("StyleSheetVerifier")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTitle("Stylesheet Verifier")]
+[assembly: AssemblyProduct("Stylesheet Verifier")]
+[assembly: AssemblyCompany("SDL Limited as part of the RWS Holdings Plc group of companies")]
+[assembly: AssemblyCopyright("Copyright © 2011 - 2022 SDL Limited as part of the RWS Holdings Plc group of companies (\"RWS Group\").")]
+[assembly: AssemblyDescription("Provides a way to generate a preview of an XML file against a stylesheet")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -26,5 +28,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.4.0")]
+[assembly: AssemblyVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.0")]
