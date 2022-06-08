c0d3d9511aa1f9028c80b049d3ae8f1c89c0b2ba
vnechita <vnechita@sdl.com>
2022-06-08 22:09:44 +0300

    Changes for Deploy Studio 2022 Plugins: TermInjector SDLCOM-3466

--- TermInjector/Sdl.Community.TermInjector/Properties/AssemblyInfo.cs
+++ TermInjector/Sdl.Community.TermInjector/Properties/AssemblyInfo.cs
@@ -6,11 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("TermInjector")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("TermInjector")]
-[assembly: AssemblyCopyright("")]
+[assembly: AssemblyCompany("SDL Limited as part of the RWS Holdings Plc group of companies")]
+[assembly: AssemblyCopyright("Copyright © 2011 - 2022 SDL Limited as part of the RWS Holdings Plc group of companies (\"RWS Group\").")]
+[assembly: AssemblyDescription("TermInjector is a plugin that modifies the output of a translation memory in the editor during translation.")]
+
+[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +33,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("5.0.0.0")]
+[assembly: AssemblyFileVersion("5.0.0.0")]
