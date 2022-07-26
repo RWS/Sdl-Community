d82fa4bc749548075f88d3d5185995cc7d66b37f
Emanuel Albu <albuemanuel@live.com>
2022-07-26 17:47:01 +0300

    Number Verifier 3.1.2.1 / 2021 - separate reporting out of NumberVerifierMain - use settings to determine what to report to the user

---
+++ Number Verifier/Reporter/MessageFilter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Sdl.Community.NumberVerifier.Reporter
+{
+	public class MessageFilter : IMessageFilter
+	{
+		public MessageFilter(Dictionary<string, bool> resultTable) => ResultTable = resultTable;
+
+		private Dictionary<string, bool> ResultTable { get; }
+
+		public bool IsAllowed(string message) => !ResultTable.ContainsKey(message) || ResultTable[message];
+	}
+}
