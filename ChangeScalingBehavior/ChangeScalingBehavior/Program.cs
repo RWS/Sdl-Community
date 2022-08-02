e2d7d0af5fc59933655dc5e310026b2f6ce6fa0f
Patrick Andrew Hartnett <phartnett@sdl.com>
2022-08-02 12:50:36 +0200

    ChangeScalingBehavior: Add dependencies as embedded resources

--- ChangeScalingBehavior/ChangeScalingBehavior/Program.cs
+++ ChangeScalingBehavior/ChangeScalingBehavior/Program.cs
@@ -9,62 +9,62 @@ using System.Diagnostics;
 namespace ChangeScalingBehavior
 {
 	internal static class Program
-    {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        private static void Main()
-        {
-            LoadAssemblies();
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		private static void Main()
+		{
+			LoadAssemblies();
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
 
-	        if (Process.GetProcessesByName(Path.GetFileNameWithoutExtension(Assembly.GetEntryAssembly().Location)).Length > 1)
-	        {
-		        Process.GetCurrentProcess().Kill();
-	        }
+			if (Process.GetProcessesByName(Path.GetFileNameWithoutExtension(Assembly.GetEntryAssembly().Location)).Length > 1)
+			{
+				Process.GetCurrentProcess().Kill();
+			}
 
-            InitializeLog.InitializeLoggingConfiguration();
-            
-            Application.Run(new HighDPIChange());
-        }
+			InitializeLog.InitializeLoggingConfiguration();
+
+			Application.Run(new HighDPIChange());
+		}
 
 
-        [MethodImpl(MethodImplOptions.NoInlining)]
-        private static void LoadAssemblies()
-        {
-            var assemblies = new Dictionary<string, Assembly>();
-            Assembly OnAssemblyResolve(object sender, ResolveEventArgs args)
-            {
-                var shortName = new AssemblyName(args.Name).Name;
-                if (assemblies.TryGetValue(shortName, out var assembly))
-                {
-                    return assembly;
-                }
+		[MethodImpl(MethodImplOptions.NoInlining)]
+		private static void LoadAssemblies()
+		{
+			var assemblies = new Dictionary<string, Assembly>();
+			Assembly OnAssemblyResolve(object sender, ResolveEventArgs args)
+			{
+				var shortName = new AssemblyName(args.Name).Name;
+				if (assemblies.TryGetValue(shortName, out var assembly))
+				{
+					return assembly;
+				}
 
-                return null;
-            }
+				return null;
+			}
 
-            var appAssembly = typeof(HighDPIChange).Assembly;
-            foreach (var resourceName in appAssembly.GetManifestResourceNames())
-            {
-                if (resourceName.EndsWith(".dll", StringComparison.InvariantCultureIgnoreCase))
-                {
-                    using (var stream = appAssembly.GetManifestResourceStream(resourceName))
-                    {
-	                    if (stream != null)
-	                    {
-		                    var assemblyData = new byte[(int) stream.Length];
-		                    stream.Read(assemblyData, 0, assemblyData.Length);
-		                    var assembly = Assembly.Load(assemblyData);
-		                    assemblies.Add(assembly.GetName().Name, assembly);
-	                    }
-                    }
-                }
-            }
+			var appAssembly = typeof(HighDPIChange).Assembly;
+			foreach (var resourceName in appAssembly.GetManifestResourceNames())
+			{
+				if (resourceName.EndsWith(".dll", StringComparison.InvariantCultureIgnoreCase))
+				{
+					using (var stream = appAssembly.GetManifestResourceStream(resourceName))
+					{
+						if (stream != null)
+						{
+							var assemblyData = new byte[(int)stream.Length];
+							stream.Read(assemblyData, 0, assemblyData.Length);
+							var assembly = Assembly.Load(assemblyData);
+							assemblies.Add(assembly.GetName().Name, assembly);
+						}
+					}
+				}
+			}
 
-            AppDomain.CurrentDomain.AssemblyResolve += OnAssemblyResolve;
-        }
-    }
+			AppDomain.CurrentDomain.AssemblyResolve += OnAssemblyResolve;
+		}
+	}
 }
--- ChangeScalingBehavior/ChangeScalingBehavior/ResourceFilesManager.cs
+++ ChangeScalingBehavior/ChangeScalingBehavior/ResourceFilesManager.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using Sdl.Versioning;
 using Trados.Community.Toolkit.Core;
 
 namespace ChangeScalingBehavior
