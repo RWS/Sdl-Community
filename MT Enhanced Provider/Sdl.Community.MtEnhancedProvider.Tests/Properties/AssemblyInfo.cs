720af509cf3080e283d57330223d9e12865da4a2
Emanuel Albu <ealbu@sdl.com>
2022-09-09 14:36:18 +0200

    Increase version to 5.4.2.0

--- MT Enhanced Provider/Sdl.Community.MtEnhancedProvider.Tests/Properties/AssemblyInfo.cs
+++ MT Enhanced Provider/Sdl.Community.MtEnhancedProvider.Tests/Properties/AssemblyInfo.cs
@@ -12,4 +12,4 @@ using System.Runtime.InteropServices;
 
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("5.0.0.0")]
-[assembly: AssemblyFileVersion("5.4.1.1")]
+[assembly: AssemblyFileVersion("5.4.2.0")]
