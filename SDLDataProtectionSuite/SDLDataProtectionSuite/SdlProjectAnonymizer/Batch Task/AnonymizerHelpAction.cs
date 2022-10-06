34d48aaaff74382049b110fa465ccf71ea5c2540
John Torjo <jtorjo@users.noreply.github.com>
2022-10-06 22:30:50 +0300

    small changes + version bump

--- SDLDataProtectionSuite/SDLDataProtectionSuite/SdlProjectAnonymizer/Batch Task/AnonymizerHelpAction.cs
+++ SDLDataProtectionSuite/SDLDataProtectionSuite/SdlProjectAnonymizer/Batch Task/AnonymizerHelpAction.cs
@@ -2,6 +2,10 @@
 using Sdl.Desktop.IntegrationApi.Extensions;
 using Sdl.TranslationStudioAutomation.IntegrationApi.Presentation.DefaultLocations;
 
+/* if you run into a build error during building the plugin (the dll gets built, you get an msbuild error pfe103)
+ * remove Sdl.TranslationStudioAutomation.IntegrationApi.dll and \Sdl.TranslationStudioAutomation.IntegrationApi.Extensions.dll
+ * from the project's references, and then manually add them (via Visual Studio's UI).
+ */
 namespace Sdl.Community.SdlDataProtectionSuite.SdlProjectAnonymizer.Batch_Task
 {
 	[Action("SDPSProjectAnonymizerHelpAction",
