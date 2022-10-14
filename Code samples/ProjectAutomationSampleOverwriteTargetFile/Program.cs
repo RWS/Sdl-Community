b4768f81ca1b10906f2ebe9d43f332810b93a930
John Torjo <jtorjo@users.noreply.github.com>
2022-10-14 10:44:59 +0300

    added new sample -- allows you to overwrite a target file in an existing project

---
+++ Code samples/ProjectAutomationSampleOverwriteTargetFile/Program.cs
@@ -0,0 +1,16 @@
+﻿
+namespace ProjectAutomationSample
+{
+    internal class Program
+    {
+        // https://jira.sdl.com/browse/SDLCOM-3667
+        // AutomaticTaskTemplateIds.TranslationCount does not change the ConfirmationStatistics.CombinedConfirmationLevel
+        static void Main(string[] args)
+        {
+            // this just gets us the path to a project - in your specific case, it's the actual path to your project
+            var projectPath = OverwriteTargetFileSample.CreateTestProject();
+            var targetTranslatedFile = "C:\\john\\OneDrive - SDL\\test3667\\constitution en short.txt.sdlxliff";
+            OverwriteTargetFileSample.CopyTargetLanguageFileToProject(projectPath, targetTranslatedFile);
+        }
+    }
+}
