7da24aec0299b826f9d9fa5675832b97fd7a6f3b
Andrei Birleanu <abirleanu@sdl.com>
2022-11-07 15:55:47 +0200

    Fixed exported file names and incremented version number to 4.0.1.0

--- TranslationMemoryManagementUtility/Sdl.Community.TranslationMemoryManagementUtility/Properties/AssemblyInfo.cs
+++ TranslationMemoryManagementUtility/Sdl.Community.TranslationMemoryManagementUtility/Properties/AssemblyInfo.cs
@@ -1,13 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Sdl.Community.TranslationMemoryManagementUtility")]
-[assembly: AssemblyProduct("Sdl.Community.TranslationMemoryManagementUtility")]
-[assembly: AssemblyCopyright("Copyright ©  2019")]
-
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
@@ -15,16 +8,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("df51a3c1-4a82-4e08-9f78-7448ed33c271")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.0.2.0")]
