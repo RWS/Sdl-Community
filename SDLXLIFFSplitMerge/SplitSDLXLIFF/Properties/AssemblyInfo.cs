15e33cb1c66e8eba65b5a1713cac3430564223cb
Andrei Birleanu <abirleanu@sdl.com>
2022-11-08 16:25:48 +0200

    edited AssemblyInfo

--- SDLXLIFFSplitMerge/SplitSDLXLIFF/Properties/AssemblyInfo.cs
+++ SDLXLIFFSplitMerge/SplitSDLXLIFF/Properties/AssemblyInfo.cs
@@ -4,8 +4,8 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SDLXLIFF Split/Merge")]
-[assembly: AssemblyProduct("SDLXLIFF Split/Merge")]
+[assembly: AssemblyTitle("Sdl.Utilities.SplitSDLXLIFF")]
+[assembly: AssemblyProduct("Sdl.Utilities.SplitSDLXLIFF")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
