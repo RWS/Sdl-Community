2ec3340c9cc601ff059f825ce3978511d707ce50
Andrei Birleanu <abirleanu@sdl.com>
2022-11-08 13:43:37 +0200

    added shared assemblyinfo (SolutionInfo) across projects

--- SDLXLIFFSplitMerge/Lib/Properties/AssemblyInfo.cs
+++ SDLXLIFFSplitMerge/Lib/Properties/AssemblyInfo.cs
@@ -6,8 +6,6 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("SDLXLIFF Split / Merge")]
 [assembly: AssemblyProduct("SDLXLIFF Split / Merge")]
-[assembly: AssemblyCompany("SDL Limited as part of the RWS Holdings Plc group of companies")]
-[assembly: AssemblyCopyright("Copyright © 2011 - 2022 SDL Limited as part of the RWS Holdings Plc group of companies (\"RWS Group\").")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -16,17 +14,4 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("9c4cc3e4-f585-4c33-8afb-ecfc1c3ae8eb")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.0.0.0")]
 
