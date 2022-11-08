7d65d23aa6146c6a281aad29053073b650f923af
Andrei Birleanu <abirleanu@sdl.com>
2022-11-08 13:52:04 +0200

    added shared assemblyinfo (SolutionInfo) across projects

--- SDLXLIFFSplitMerge/Sdl.Community.SplitMergeUIWix/Properties/AssemblyInfo.cs
+++ SDLXLIFFSplitMerge/Sdl.Community.SplitMergeUIWix/Properties/AssemblyInfo.cs
@@ -6,13 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Sdl.Community.SplitMergeUIWix")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Sdl.Community.SplitMergeUIWix")]
-[assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
