999b2a2082d289b3ac1081720ba3309c88d4ff23
Andrei Birleanu <abirleanu@sdl.com>
2022-11-09 18:26:27 +0200

    refactored v3connector

--- GoogleTranslatorProvider/GoogleTranslatorProvider/GoogleAPI/GoogleV3Connector.cs
+++ GoogleTranslatorProvider/GoogleTranslatorProvider/GoogleAPI/GoogleV3Connector.cs
@@ -1,51 +1,37 @@
-﻿using Google.Api.Gax.ResourceNames;
-using Google.Cloud.Translate.V3;
-using GoogleTranslatorProvider.Interfaces;
-using GoogleTranslatorProvider.Models;
-using NLog;
-using Sdl.LanguagePlatform.Core;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using Google.Api.Gax.ResourceNames;
+using Google.Cloud.Translate.V3;
+using GoogleTranslatorProvider.Interfaces;
+using GoogleTranslatorProvider.Models;
+using NLog;
+using Sdl.LanguagePlatform.Core;
 
 namespace GoogleTranslatorProvider.GoogleAPI
 {
-	public class GoogleV3Connecter : IGoogleV3Connecter
+	public class GoogleV3Connecter
 	{
-		private readonly Logger _logger = NLog.LogManager.GetCurrentClassLogger();
-		private readonly TranslationServiceClient _translationServiceClient;
-		private readonly string _modelPath;
-		private readonly string _glossaryResourceLocation;
+		private readonly Logger _logger = LogManager.GetCurrentClassLogger();
+
 		private readonly ITranslationOptions _options;
-		public static List<GoogleV3LanguageModel> SupportedLanguages { get; set; }
-		public string GlossaryId { get; set; }
+		private readonly TranslationServiceClient _translationServiceClient;
+		private readonly string _glossaryResourceLocation;
+		private readonly string _modelPath;
 
 		public GoogleV3Connecter(ITranslationOptions options)
 		{
-			_options = options;
 			SupportedLanguages = new List<GoogleV3LanguageModel>();
-
-			//We put by default NMT model if the nmt model is not supported for the language pair 
-			//Google knows to use basic model
-			var model = "general/nmt";
-			if (!string.IsNullOrEmpty(_options.GoogleEngineModel))
-			{
-				model = _options.GoogleEngineModel;
-			}
-
-			_modelPath = $"projects/{_options.ProjectName}/locations/{_options.ProjectLocation}/models/{model}";
-			if (!string.IsNullOrEmpty(_options.GlossaryPath))
-			{
-				GlossaryId = Path.GetFileNameWithoutExtension(_options.GlossaryPath).Replace(" ", string.Empty); //Google doesn't allow spaces in glossary id
-				_glossaryResourceLocation = $"projects/{_options.ProjectName}/locations/{_options.ProjectLocation}/glossaries/{GlossaryId}";
-			}
+			_options = options;
+			_modelPath = SetModelPath();
+			_glossaryResourceLocation = SetGlossary();
 
 			try
 			{
-				//Environment.SetEnvironmentVariable(PluginResources.GoogleApiEnvironmentVariableName, _options.JsonFilePath);
+				Environment.SetEnvironmentVariable(PluginResources.GoogleApiEnvironmentVariableName, _options.JsonFilePath);
 				_translationServiceClient = TranslationServiceClient.Create();
 			}
 			catch (Exception e)
@@ -54,79 +40,140 @@ namespace GoogleTranslatorProvider.GoogleAPI
 			}
 		}
 
+		public string GlossaryId { get; set; }
+
+		public List<GoogleV3LanguageModel> SupportedLanguages { get; set; }
+
+		public void TryToAuthenticateUser()
+		{
+			var request = new TranslateTextRequest
+			{
+				Contents =
+				{
+					"test"
+				},
+				TargetLanguageCode = "fr-FR",
+				Parent = new ProjectName(_options.ProjectName).ToString()
+			};
+
+			_translationServiceClient.TranslateText(request);
+		}
+
 		public string TranslateText(CultureInfo sourceLanguage, CultureInfo targetLanguage, string sourceText, string format)
 		{
 			try
 			{
-				// V2 uses "html" and "text" but for V3 its "text/html" and "text/plain"
-				var mimeType = format == "text" ? "text/plain" : "text/html";
-				var request = new TranslateTextRequest
-				{
-					Contents =
-					{
-						sourceText
-					},
-					Model = _modelPath,
-					TargetLanguageCode = targetLanguage.TwoLetterISOLanguageName,
-					SourceLanguageCode = sourceLanguage.TwoLetterISOLanguageName,
-					Parent = new ProjectName(_options.ProjectName).ToString(),
-					MimeType = mimeType
-				};
-				if (!string.IsNullOrEmpty(_glossaryResourceLocation))
-				{
-					request.GlossaryConfig = new TranslateTextGlossaryConfig
-					{
-						Glossary = _glossaryResourceLocation,
-						IgnoreCase = true
-					};
-				}
-				var translationResponse = _translationServiceClient.TranslateText(request);
-				//There are multiple translations only when we send a list of strings to be translated
-				if (translationResponse != null)
-				{
-					if (!string.IsNullOrEmpty(_glossaryResourceLocation))
-					{
-						return translationResponse.GlossaryTranslations[0].TranslatedText;
-					}
-					return translationResponse.Translations?[0].TranslatedText;
-				}
+				return TryTranslateText(sourceLanguage, targetLanguage, sourceText, format);
 			}
 			catch (Exception e)
 			{
 				_logger.Error($"{MethodBase.GetCurrentMethod().Name}: {e}");
 				throw;
 			}
-
-			return string.Empty;
 		}
 
+		public bool IsSupportedLanguage(CultureInfo sourceLanguage, CultureInfo targetLanguage)
+		{
+			if (!SupportedLanguages.Any())
+			{
+				SetGoogleAvailableLanguages();
+			}
+
+			var searchedSource = SupportedLanguages.FirstOrDefault(x => x.CultureInfo.Name.Equals(sourceLanguage.TwoLetterISOLanguageName));
+			var searchedTarget = SupportedLanguages.FirstOrDefault(x => x.CultureInfo.Name.Equals(targetLanguage.TwoLetterISOLanguageName));
+
+			return searchedSource.SupportSource && searchedTarget.SupportTarget;
+		}
+		
 		public void CreateGoogleGlossary(LanguagePair[] languagePairs)
 		{
+			if (string.IsNullOrEmpty(_options.GlossaryPath))
+			{
+				return;
+			}
+
 			try
 			{
-				if (!string.IsNullOrEmpty(_options.GlossaryPath))
-				{
-					Glossary glossary;
-					if (_options.BasicCsv)
-					{
-						var sourceLang = languagePairs[0].SourceCultureName;
-						var targetLang = languagePairs[0].TargetCultureName;
-						glossary = CreateUnidirectionalCsvGlossary(sourceLang, targetLang);
-					}
-					else
-					{
-						var glossaryLanguages = GetGlossaryLanguages(languagePairs);
-						glossary = CreatetCsvGlossary(glossaryLanguages);
-					}
-
-					var glossaryResponse = _translationServiceClient.CreateGlossary(
-						new LocationName(_options.ProjectName, _options.ProjectLocation).ToString(), glossary);
-				}
+				TryCreateGoogleGlossary(languagePairs);
 			}
 			catch (Exception e)
 			{
 				_logger.Error($"{MethodBase.GetCurrentMethod().Name}: {e}");
 			}
+		}
+
+		private string TryTranslateText(CultureInfo sourceLanguage, CultureInfo targetLanguage, string sourceText, string format)
+		{
+			var request = new TranslateTextRequest
+			{
+				Contents = { sourceText },
+				Model = _modelPath,
+				TargetLanguageCode = targetLanguage.TwoLetterISOLanguageName,
+				SourceLanguageCode = sourceLanguage.TwoLetterISOLanguageName,
+				Parent = new ProjectName(_options.ProjectName).ToString(),
+				MimeType = format == "text" ? "text/plain" : "text/html"
+			};
+
+			if (!string.IsNullOrEmpty(_glossaryResourceLocation))
+			{
+				request.GlossaryConfig = new TranslateTextGlossaryConfig
+				{
+					Glossary = _glossaryResourceLocation,
+					IgnoreCase = true
+				};
+			}
+
+			var translationResponse = _translationServiceClient.TranslateText(request);
+			return translationResponse is null
+				? string.Empty
+				: string.IsNullOrEmpty(_glossaryResourceLocation) ? translationResponse.Translations?[0].TranslatedText
+																  : translationResponse.GlossaryTranslations[0].TranslatedText;
+		}
+
+		private void SetGoogleAvailableLanguages()
+		{
+			try
+			{
+				TrySetGoogleAvailableLanguages();
+			}
+			catch (Exception e)
+			{
+				_logger.Error($"{MethodBase.GetCurrentMethod().Name}: {e}");
+			}
+		}
+
+		private void TrySetGoogleAvailableLanguages()
+		{
+			var locationName = new LocationName(_options.ProjectName, "global");
+			var request = new GetSupportedLanguagesRequest { ParentAsLocationName = locationName };
+			var response = _translationServiceClient.GetSupportedLanguages(request);
+
+			SupportedLanguages.AddRange(response.Languages.Select(language => new GoogleV3LanguageModel
+			{
+				GoogleLanguageCode = language.LanguageCode,
+				SupportSource = language.SupportSource,
+				SupportTarget = language.SupportTarget,
+				CultureInfo = new CultureInfo(language.LanguageCode)
+			}));
+		}
+
+		private void TryCreateGoogleGlossary(LanguagePair[] languagePairs)
+		{
+			Glossary glossary;
+			if (_options.BasicCsv)
+			{
+				var sourceLang = languagePairs[0].SourceCultureName;
+				var targetLang = languagePairs[0].TargetCultureName;
+				glossary = CreateUnidirectionalCsvGlossary(sourceLang, targetLang);
+			}
+			else
+			{
+				var glossaryLanguages = GetGlossaryLanguages(languagePairs);
+				glossary = CreatetCsvGlossary(glossaryLanguages);
+			}
+
+			_ = _translationServiceClient.CreateGlossary(new LocationName(_options.ProjectName, _options.ProjectLocation).ToString(),
+														 glossary);
 		}
 
 		private Glossary CreatetCsvGlossary(List<string> glossaryLanguages)
@@ -143,6 +190,7 @@ namespace GoogleTranslatorProvider.GoogleAPI
 					}
 				}
 			};
+
 			glossary.LanguageCodesSet.LanguageCodes.AddRange(glossaryLanguages);
 			return glossary;
 		}
@@ -153,16 +201,18 @@ namespace GoogleTranslatorProvider.GoogleAPI
 			{
 				languagePairs[0].SourceCulture.TwoLetterISOLanguageName
 			};
+
 			foreach (var languagePair in languagePairs)
 			{
 				glossaryLanguages.Add(languagePair.TargetCulture.TwoLetterISOLanguageName);
 			}
+
 			return glossaryLanguages;
 		}
 
 		private Glossary CreateUnidirectionalCsvGlossary(string sourceLanguage, string targetLanguage)
 		{
-			var glossary = new Glossary
+			return new Glossary
 			{
 				Name = new GlossaryName(_options.ProjectName, _options.ProjectLocation, GlossaryId).ToString(),
 				LanguagePair = new Glossary.Types.LanguageCodePair
@@ -178,76 +228,31 @@ namespace GoogleTranslatorProvider.GoogleAPI
 					}
 				}
 			};
-
-			return glossary;
 		}
 
-		public void SetGoogleAvailableLanguages()
+		private string SetModelPath()
 		{
-			try
-			{
-				var request = new GetSupportedLanguagesRequest
-				{
-					ParentAsLocationName = new LocationName(_options.ProjectName, "global"),
-				};
-				var response = _translationServiceClient.GetSupportedLanguages(request);
-
-				foreach (var language in response.Languages)
-				{
-					var languageModel = new GoogleV3LanguageModel
-					{
-						GoogleLanguageCode = language.LanguageCode,
-						SupportSource = language.SupportSource,
-						SupportTarget = language.SupportTarget,
-						CultureInfo = new CultureInfo(language.LanguageCode)
-					};
-					SupportedLanguages.Add(languageModel);
-				}
-			}
-			catch (Exception e)
-			{
-				_logger.Error($"{MethodBase.GetCurrentMethod().Name}: {e}");
-			}
+			const string defaultModel = "general/nmt";
+			return string.Format(
+				"projects/{0}/locations/{1}/models/{2}",
+				_options.ProjectName,
+				_options.ProjectLocation,
+				string.IsNullOrEmpty(_options.GoogleEngineModel) ? defaultModel : _options.GoogleEngineModel);
 		}
 
-		public void TryToAuthenticateUser()
+		private string SetGlossary()
 		{
-			var request = new TranslateTextRequest
+			if (string.IsNullOrEmpty(_options.GlossaryPath))
 			{
-				Contents =
-				{
-					"test"
-				},
-				TargetLanguageCode = "fr-FR",
-				Parent = new ProjectName(_options.ProjectName).ToString()
-			};
-			_translationServiceClient.TranslateText(request);
-		}
-
-		public bool IsSupportedLanguage(CultureInfo sourceLanguage, CultureInfo targetLanguage)
-		{
-			if (!SupportedLanguages.Any())
-			{
-				SetGoogleAvailableLanguages();
+				return null;
 			}
 
-			var suportsSource = false;
-			var supportsTarget = false;
-
-			var searchedSource =
-				SupportedLanguages?.FirstOrDefault(l => l.CultureInfo.Name.Equals(sourceLanguage.TwoLetterISOLanguageName));
-			var searchedTarget =
-				SupportedLanguages?.FirstOrDefault(l => l.CultureInfo.Name.Equals(targetLanguage.TwoLetterISOLanguageName));
-
-			if (searchedSource != null)
-			{
-				suportsSource = searchedSource.SupportSource;
-			}
-			if (searchedTarget != null)
-			{
-				supportsTarget = searchedTarget.SupportTarget;
-			}
-			return suportsSource && supportsTarget;
+			GlossaryId = Path.GetFileNameWithoutExtension(_options.GlossaryPath).Replace(" ", string.Empty);
+			return string.Format(
+				"projects/{0}/locations/{1}/glossaries/{2}",
+				_options.ProjectName,
+				_options.ProjectLocation,
+				GlossaryId);
 		}
 	}
 }
