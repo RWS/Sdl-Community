a914a1a2de3c2199efe7ed4875cec006e8914cdf
Andrei Birleanu <abirleanu@sdl.com>
2022-11-15 13:52:23 +0200

    added shared assemblyinfo (SolutionInfo) across projects

--- SDLMTCloud.Provider/Sdl.Community.MTCloud.Provider/Properties/AssemblyInfo.cs
+++ SDLMTCloud.Provider/Sdl.Community.MTCloud.Provider/Properties/AssemblyInfo.cs
@@ -8,8 +8,6 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("SDL Machine Translation Cloud provider")]
 [assembly: AssemblyProduct("SDL Machine Translation Cloud provider")]
-[assembly: AssemblyCompany("SDL Limited as part of the RWS Holdings Plc group of companies")]
-[assembly: AssemblyCopyright("Copyright © 2011 - 2022 SDL Limited as part of the RWS Holdings Plc group of companies (\"RWS Group\").")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -19,15 +17,3 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("c5c7e441-7416-47f3-a4b0-c20aeec12abf")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.2.22.1")]
--- SDLMTCloud.Provider/Tests/UnitTests/Sdl.Community.MTCloud.Provider.UnitTests/Properties/AssemblyInfo.cs
+++ SDLMTCloud.Provider/Tests/UnitTests/Sdl.Community.MTCloud.Provider.UnitTests/Properties/AssemblyInfo.cs
@@ -6,8 +6,6 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("Sdl.Community.MTCloud.Provider.UnitTests")]
 [assembly: AssemblyProduct("Sdl.Community.MTCloud.Provider.UnitTests")]
-[assembly: AssemblyCompany("SDL Limited as part of the RWS Holdings Plc group of companies")]
-[assembly: AssemblyCopyright("Copyright © 2011 - 2022 SDL Limited as part of the RWS Holdings Plc group of companies (\"RWS Group\").")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -16,16 +14,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("1d6b3478-0b2d-429b-b7d7-1f45ce7e4b05")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.2.22.1")]
