9694204a00f3775981f2d08b750d89d20882de50
Andrei Birleanu <abirleanu@sdl.com>
2022-11-15 13:52:23 +0200

    added shared assemblyinfo (SolutionInfo) across projects

--- SDLMTCloud.Provider/Tests/UnitTests/Sdl.Community.MTCloud.Provider.UnitTests/Properties/AssemblyInfo.cs
+++ SDLMTCloud.Provider/Tests/UnitTests/Sdl.Community.MTCloud.Provider.UnitTests/Properties/AssemblyInfo.cs
@@ -6,9 +6,6 @@ using System.Runtime.InteropServices;
 // associated with an assembly.
 [assembly: AssemblyTitle("Language Weaver provider tests")]
 [assembly: AssemblyProduct("Language Weaver provider tests")]
-[assembly: AssemblyDescription("Unit test project for Language Weaver provider")]
-[assembly: AssemblyCompany("SDL Limited as part of the RWS Holdings Plc group of companies")]
-[assembly: AssemblyCopyright("Copyright © 2011 - 2022 SDL Limited as part of the RWS Holdings Plc group of companies (\"RWS Group\").")]
 
 
 // Setting ComVisible to false makes the types in this assembly not visible
@@ -18,16 +15,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("1d6b3478-0b2d-429b-b7d7-1f45ce7e4b05")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.0.0.0")]
-[assembly: AssemblyFileVersion("5.0.6.0")]
