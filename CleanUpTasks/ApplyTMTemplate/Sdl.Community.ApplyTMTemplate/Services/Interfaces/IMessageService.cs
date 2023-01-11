1e9c5382e96b871649c85a56a5066d07ac093345
Andrei Birleanu <abirleanu@sdl.com>
2023-01-11 09:42:22 +0200

    reverted unwanted changes on StudioViews and CleanupTasks

--- CleanUpTasks/ApplyTMTemplate/Sdl.Community.ApplyTMTemplate/Services/Interfaces/IMessageService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Sdl.Community.ApplyTMTemplate.Services.Interfaces
-{
-	public interface IMessageService
-	{
-		void ShowWarningMessage(string title, string warningMessage);
-		void ShowMessage(string title, string message);
-		void ShowErrorMessage(string title, string errorMessage);
-	}
-}
