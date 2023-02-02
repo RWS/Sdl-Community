24f8ae6995d75579b03c6cb49c8f7fd46cd798b0
Patrick Andrew Hartnett <phartnett@sdl.com>
2023-02-02 16:03:14 +0100

    Enable user to override the default system paths for MyDocument & ApplicationData from a configuration file.
    - Removed 'Sdl.Community.' from the assembly output names.
    - Bumped release version to 4.1.6.0 / Studio 2021 SR2 CU12
    
    Details:
    Starting with this latest release, a new configuration file 'Qualitivity.config' is now unpacked in plugins folder (e.g. ...\SDL\SDL Trados Studio\16\Plugins\Unpacked\Qualitivity\Qualitivity.dll.config).  You will find two new properties MyDocumentsPath & ApplicationDataPath that enable the user to override these default paths from the system.
    Note: It is possible to specify the same folder for both.  If the folder/s specified are empty or not valid, then the default system path/s will be used.
    
    Example: (Qualitivity.dll.config)
    <?xml version="1.0" encoding="utf-8" ?>
    <configuration>
     <appSettings>
      <add key="MyDocumentsPath" value="C:\Temp\" />
      <add key="ApplicationDataPath" value="C:\Temp\" />
     </appSettings>
    </configuration>

--- Qualitivity/Build/SolutionInfo.cs
+++ Qualitivity/Build/SolutionInfo.cs
@@ -3,7 +3,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyProductAttribute("Qualitivity")]
 [assembly: AssemblyVersionAttribute("4.0.0.0")]
-[assembly: AssemblyFileVersionAttribute("4.1.5.0")]
+[assembly: AssemblyFileVersionAttribute("4.1.6.0")]
 [assembly: ComVisibleAttribute(false)]
 namespace System
 {
@@ -11,7 +11,7 @@ namespace System
 	{
 		internal const System.String AssemblyProduct = "Qualitivity";
 		internal const System.String AssemblyVersion = "4.0.0.0";
-		internal const System.String AssemblyFileVersion = "4.1.5.0";
+		internal const System.String AssemblyFileVersion = "4.1.6.0";
 		internal const System.Boolean ComVisible = false;
 	}
 }
--- Qualitivity/Sdl.Community.Qualitivity.Models/Configuration/ApplicationPaths.cs
+++ Qualitivity/Sdl.Community.Qualitivity.Models/Configuration/ApplicationPaths.cs
@@ -1,74 +1,110 @@
 ﻿using System;
+using System.Configuration;
 using System.IO;
+using System.Linq;
+using System.Reflection;
 
 namespace Sdl.Community.Structures.Configuration
 {
-    [Serializable]
-    public class ApplicationPaths : ICloneable
-    {
-        public string ApplicationSettingsPath { get; set; }
-       
-        public string ApplicationTrackChangesPath { get; set; }
-        public string ApplicationTrackChangesReportPath { get; set; }
+	[Serializable]
+	public class ApplicationPaths : ICloneable
+	{
+		public string ApplicationSettingsPath { get; set; }
 
-        public string ApplicationMyDocumentsPath { get; set; }
-        public string ApplicationMyDocumentsDatabasePath { get; set; }
-        public string ApplicationMyDocumentsDatabaseSettingsPath { get; set; }
-        public string ApplicationMyDocumentsDatabaseProjectsPath { get; set; }
+		public string ApplicationTrackChangesPath { get; set; }
+		public string ApplicationTrackChangesReportPath { get; set; }
 
-        public string ApplicationBackupDatabasePath { get; set; }
+		public string ApplicationMyDocumentsPath { get; set; }
+		public string ApplicationMyDocumentsDatabasePath { get; set; }
+		public string ApplicationMyDocumentsDatabaseSettingsPath { get; set; }
+		public string ApplicationMyDocumentsDatabaseProjectsPath { get; set; }
 
-        public ApplicationPaths()
-        {
-            ApplicationSettingsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Qualitivity");
-            if (!Directory.Exists(ApplicationSettingsPath))
-                Directory.CreateDirectory(ApplicationSettingsPath);
-           
+		public string ApplicationBackupDatabasePath { get; set; }
 
-            ApplicationTrackChangesPath = Path.Combine(ApplicationSettingsPath, "Track.Changes");
-            if (!Directory.Exists(ApplicationTrackChangesPath))
-                Directory.CreateDirectory(ApplicationTrackChangesPath);
+		const string MyDocumentsPath = "MyDocumentsPath";
+		const string ApplicationDataPath = "ApplicationDataPath";
 
-            ApplicationTrackChangesReportPath = Path.Combine(ApplicationTrackChangesPath, "Reports");
-            if (!Directory.Exists(ApplicationTrackChangesReportPath))
-                Directory.CreateDirectory(ApplicationTrackChangesReportPath);
+		public ApplicationPaths()
+		{
+			var location = Assembly.GetExecutingAssembly().Location;
+			var unpackedFolder = location.Substring(0, location.LastIndexOf("\\", StringComparison.Ordinal));
 
+			var assembly = Path.Combine(unpackedFolder, "Qualitivity.dll");
+			var config = ConfigurationManager.OpenExeConfiguration(assembly);
 
-            ApplicationMyDocumentsPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments), "Qualitivity");
-            if (!Directory.Exists(ApplicationMyDocumentsPath))
-                Directory.CreateDirectory(ApplicationMyDocumentsPath);
-            ApplicationMyDocumentsDatabasePath = Path.Combine(ApplicationMyDocumentsPath, "Database");
-            if (!Directory.Exists(ApplicationMyDocumentsDatabasePath))
-                Directory.CreateDirectory(ApplicationMyDocumentsDatabasePath);
-            ApplicationMyDocumentsDatabaseSettingsPath = Path.Combine(ApplicationMyDocumentsDatabasePath, "Settings.sqlite");
-            ApplicationMyDocumentsDatabaseProjectsPath = Path.Combine(ApplicationMyDocumentsDatabasePath, "Projects.sqlite");
+			var myDocumentsPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+			var applicationDataPath = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
 
+			if (config.AppSettings?.Settings != null)
+			{
+				var myDocumentsPathCustom = GetConfigValue(config, MyDocumentsPath);
+				if (myDocumentsPathCustom != null)
+				{
+					myDocumentsPath = myDocumentsPathCustom;
+				}
 
-      
+				var applicationDataPathCustom = GetConfigValue(config, ApplicationDataPath);
+				if (applicationDataPathCustom != null)
+				{
+					applicationDataPath = applicationDataPathCustom;
+				}
+			}
 
-            ApplicationBackupDatabasePath = Path.Combine(ApplicationMyDocumentsDatabasePath, "Backups");
-            if (!Directory.Exists(ApplicationBackupDatabasePath))
-                Directory.CreateDirectory(ApplicationBackupDatabasePath);
-        }
+			ApplicationMyDocumentsPath = Path.Combine(myDocumentsPath, "Qualitivity");
+			if (!Directory.Exists(ApplicationMyDocumentsPath))
+				Directory.CreateDirectory(ApplicationMyDocumentsPath);
 
-        public object Clone()
-        {
-            var applicationPaths = new ApplicationPaths
-            {
-                ApplicationSettingsPath = ApplicationSettingsPath,
-                ApplicationTrackChangesPath = ApplicationTrackChangesPath,
-                ApplicationTrackChangesReportPath = ApplicationTrackChangesReportPath,
-                ApplicationMyDocumentsPath = ApplicationMyDocumentsPath,
-                ApplicationMyDocumentsDatabasePath = ApplicationMyDocumentsDatabasePath,
-                ApplicationBackupDatabasePath = ApplicationBackupDatabasePath
-            };
+			ApplicationMyDocumentsDatabasePath = Path.Combine(ApplicationMyDocumentsPath, "Database");
+			if (!Directory.Exists(ApplicationMyDocumentsDatabasePath))
+				Directory.CreateDirectory(ApplicationMyDocumentsDatabasePath);
 
+			ApplicationMyDocumentsDatabaseSettingsPath = Path.Combine(ApplicationMyDocumentsDatabasePath, "Settings.sqlite");
+			ApplicationMyDocumentsDatabaseProjectsPath = Path.Combine(ApplicationMyDocumentsDatabasePath, "Projects.sqlite");
 
+			ApplicationBackupDatabasePath = Path.Combine(ApplicationMyDocumentsDatabasePath, "Backups");
+			if (!Directory.Exists(ApplicationBackupDatabasePath))
+				Directory.CreateDirectory(ApplicationBackupDatabasePath);
 
+			ApplicationSettingsPath = Path.Combine(applicationDataPath, "Qualitivity");
+			if (!Directory.Exists(ApplicationSettingsPath))
+				Directory.CreateDirectory(ApplicationSettingsPath);
 
+			ApplicationTrackChangesPath = Path.Combine(ApplicationSettingsPath, "Track.Changes");
+			if (!Directory.Exists(ApplicationTrackChangesPath))
+				Directory.CreateDirectory(ApplicationTrackChangesPath);
 
+			ApplicationTrackChangesReportPath = Path.Combine(ApplicationTrackChangesPath, "Reports");
+			if (!Directory.Exists(ApplicationTrackChangesReportPath))
+				Directory.CreateDirectory(ApplicationTrackChangesReportPath);
+		}
 
-            return applicationPaths;
-        }
-    }
+		private static string GetConfigValue(System.Configuration.Configuration config, string propertyName)
+		{
+			if (config.AppSettings.Settings.AllKeys.Any(a => a == propertyName))
+			{
+				var value = config.AppSettings.Settings[propertyName].Value;
+				if (value != null && Directory.Exists(value))
+				{
+					return value;
+				}
+			}
+
+			return null;
+		}
+
+		public object Clone()
+		{
+			var applicationPaths = new ApplicationPaths
+			{
+				ApplicationSettingsPath = ApplicationSettingsPath,
+				ApplicationTrackChangesPath = ApplicationTrackChangesPath,
+				ApplicationTrackChangesReportPath = ApplicationTrackChangesReportPath,
+				ApplicationMyDocumentsPath = ApplicationMyDocumentsPath,
+				ApplicationMyDocumentsDatabasePath = ApplicationMyDocumentsDatabasePath,
+				ApplicationBackupDatabasePath = ApplicationBackupDatabasePath
+			};
+
+			return applicationPaths;
+		}
+	}
 }
