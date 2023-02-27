134bce15a82f1b002845ae61cff4e31b9d547599
Andrei Birleanu <abirleanu@sdl.com>
2023-02-27 19:04:45 +0200

    incremented AmazonTP version number to 3.1.5.0

--- AmazonTranslateTradosPlugin/Properties/AssemblyInfo.cs
+++ AmazonTranslateTradosPlugin/Properties/AssemblyInfo.cs
@@ -6,13 +6,8 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Amazon Translate Provider")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Sdl Community")]
 [assembly: AssemblyProduct("Amazon Translate Provider")]
-[assembly: AssemblyCopyright("")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -33,6 +28,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.6")]
-[assembly: AssemblyFileVersion("1.6")]
+[assembly: AssemblyVersion("3.0.0.0")]
+[assembly: AssemblyFileVersion("3.1.5.0")]
 
