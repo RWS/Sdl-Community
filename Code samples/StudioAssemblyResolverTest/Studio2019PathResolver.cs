47c1e1d8ac35d1a6c7e63db2df8e56641b580bc5
Emanuel Albu <ealbu@sdl.com>
2023-03-13 11:13:27 +0200

    Add StudioAssemblyResolverTest sample

---
+++ Code samples/StudioAssemblyResolverTest/Studio2019PathResolver.cs
@@ -0,0 +1,9 @@
+﻿using Rws.StudioAssemblyResolver.PathResolver;
+
+namespace StudioAssemblyResolverTest
+{
+    public class Studio2021PathResolver : IPathResolver
+    {
+        public string Resolve() => "C:\\Program Files (x86)\\SDL\\SDL Trados Studio\\Studio16";
+    }
+}
