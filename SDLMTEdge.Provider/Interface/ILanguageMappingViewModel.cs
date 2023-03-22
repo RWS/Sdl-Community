11d6312a18cf28567c81fa03e236ca25c0e68756
Andrei Birleanu <107615876+andreibirleanu10@users.noreply.github.com>
2023-03-22 16:28:05 +0200

    migrate LW:Edge from WinForms to WPF (#1149)
    * migrate LW:Edge from WinForms to WPF
    
    * added login command
    
    * SDLCOM-4408
    
    * backup
    
    * SDLCOM-4409 #1
    
    * SDLCOM-4451
    
    * SDLCOM-4405

---
+++ SDLMTEdge.Provider/Interface/ILanguageMappingViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Sdl.Community.MTEdge.Provider.Model;
+
+namespace Sdl.Community.MTEdge.Provider.Interface
+{
+	public interface ILanguageMappingViewModel
+	{
+		BaseModel ViewModel { get; set; }
+
+		ObservableCollection<TradosToMTEdgeLanguagePair> LanguageMapping { get; set; }
+	}
+}
