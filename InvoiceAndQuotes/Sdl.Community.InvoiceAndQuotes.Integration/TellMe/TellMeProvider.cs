76d3225f61e857e0d089d2d99e689d5af5e39d94
Flaviu Tritean <ftritean@sdl.com>
2023-04-05 12:48:33 +0300

    Resolve issues reported (Missing TellMe, Rebranding Remnants) Jira: SDLCOM-4552

---
+++ InvoiceAndQuotes/Sdl.Community.InvoiceAndQuotes.Integration/TellMe/TellMeProvider.cs
@@ -0,0 +1,25 @@
+﻿using Sdl.TellMe.ProviderApi;
+
+namespace Sdl.Community.InvoiceAndQuotes.Integration.TellMe
+{
+	[TellMeProvider]
+	public class TellMeProvider: ITellMeProvider
+	{
+		public string Name => string.Format(PluginResources.TellMe_Provider, PluginResources.Plugin_Name);
+
+		public AbstractTellMeAction[] ProviderActions => new AbstractTellMeAction[]
+		{
+			new CommunityWikiAction
+			{
+				Keywords = new[] {"invoice", "quote", "inquote", "invoiceandquote", "community", "support", "wiki" }
+			},
+			new AppStoreForumAction
+			{
+				Keywords = new[] { "invoice", "quote", "inquote", "invoiceandquote", "support", "forum" }
+			},
+			new AppStoreDownloadAction
+			{
+				Keywords = new[] { "invoice", "quote", "inquote", "invoiceandquote", "store", "download", "appstore" }}
+		};
+	}
+}
