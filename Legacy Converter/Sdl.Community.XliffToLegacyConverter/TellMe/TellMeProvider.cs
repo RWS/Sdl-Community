e7cd2dd2ab8010b9b6604a9dc2d8b0340a6aff27
FlaviuTritean <107054734+FlaviuTritean@users.noreply.github.com>
2023-04-06 09:18:21 +0300

    Resolve issues reported (Missing TellMe) (#1174)
    Jira: SDLCOM-4492

---
+++ Legacy Converter/Sdl.Community.XliffToLegacyConverter/TellMe/TellMeProvider.cs
@@ -0,0 +1,25 @@
+﻿using Sdl.TellMe.ProviderApi;
+
+namespace Sdl.Community.XliffToLegacyConverter.TellMe
+{
+	[TellMeProvider]
+	public class TellMeProvider: ITellMeProvider
+	{
+		public string Name => string.Format(PluginResources.TellMe_Provider, PluginResources.Plugin_Name);
+
+		public AbstractTellMeAction[] ProviderActions => new AbstractTellMeAction[]
+		{
+			new CommunityWikiAction
+			{
+				Keywords = new[] { "sdlxliff", "legacy", "converter", "legacy converter", "community", "support", "wiki" }
+			},
+			new AppStoreForumAction
+			{
+				Keywords = new[] { "sdlxliff", "legacy", "converter", "legacy converter", "support", "forum" }
+			},
+			new AppStoreDownloadAction
+			{
+				Keywords = new[] {"sdlxliff", "legacy", "converter", "legacy converter", "store", "download", "appstore" }}
+		};
+	}
+}
