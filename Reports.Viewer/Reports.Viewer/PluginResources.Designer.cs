b459b0cadf8e2818e8ff14cbbf4ac382539cc72e
FlaviuTritean <107054734+FlaviuTritean@users.noreply.github.com>
2023-04-07 16:06:52 +0300

    Resolve issues reported ( Missing TellMe ) (#1181)
    Jira: SDLCOM-4503

--- Reports.Viewer/Reports.Viewer/PluginResources.Designer.cs
+++ Reports.Viewer/Reports.Viewer/PluginResources.Designer.cs
@@ -19,7 +19,7 @@ namespace Reports.Viewer.Plus {
     // class via a tool like ResGen or Visual Studio.
     // To add or remove a member, edit your .ResX file then rerun ResGen
     // with the /str option, or rebuild your VS project.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class PluginResources {
@@ -83,6 +83,16 @@ namespace Reports.Viewer.Plus {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
         /// </summary>
+        internal static System.Drawing.Icon Download {
+            get {
+                object obj = ResourceManager.GetObject("Download", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
         internal static System.Drawing.Icon Edit {
             get {
                 object obj = ResourceManager.GetObject("Edit", resourceCulture);
@@ -96,6 +106,16 @@ namespace Reports.Viewer.Plus {
         internal static string ErrorMessage_UnableToLocateXmlFile {
             get {
                 return ResourceManager.GetString("ErrorMessage_UnableToLocateXmlFile", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon ForumIcon {
+            get {
+                object obj = ResourceManager.GetObject("ForumIcon", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
             }
         }
         
@@ -251,6 +271,16 @@ namespace Reports.Viewer.Plus {
         internal static System.Drawing.Icon PrintPreview {
             get {
                 object obj = ResourceManager.GetObject("PrintPreview", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        internal static System.Drawing.Icon Question {
+            get {
+                object obj = ResourceManager.GetObject("Question", resourceCulture);
                 return ((System.Drawing.Icon)(obj));
             }
         }
@@ -536,6 +566,24 @@ namespace Reports.Viewer.Plus {
         internal static string StatusLabel_ReportsSelected {
             get {
                 return ResourceManager.GetString("StatusLabel_ReportsSelected", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to {0} TellMe provider.
+        /// </summary>
+        internal static string TellMe_Provider {
+            get {
+                return ResourceManager.GetString("TellMe_Provider", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to {0} results.
+        /// </summary>
+        internal static string TellMe_Provider_Results {
+            get {
+                return ResourceManager.GetString("TellMe_Provider_Results", resourceCulture);
             }
         }
         
---
+++ Reports.Viewer/Reports.Viewer/TellMe/TellMeProvider.cs
@@ -0,0 +1,25 @@
+﻿using Sdl.TellMe.ProviderApi;
+
+namespace Reports.Viewer.Plus.TellMe
+{
+	[TellMeProvider]
+	public class TellMeProvider: ITellMeProvider
+	{
+		public string Name => string.Format(PluginResources.TellMe_Provider, PluginResources.Plugin_Name);
+
+		public AbstractTellMeAction[] ProviderActions => new AbstractTellMeAction[]
+		{
+			new CommunityWikiAction
+			{
+				Keywords = new[] { "reports", "viewer", "plus", "community", "support", "wiki" }
+			},
+			new AppStoreForumAction
+			{
+				Keywords = new[] { "reports", "viewer", "plus", "support", "forum" }
+			},
+			new AppStoreDownloadAction
+			{
+				Keywords = new[] { "reports", "viewer", "plus", "store", "download", "appstore" }}
+		};
+	}
+}
