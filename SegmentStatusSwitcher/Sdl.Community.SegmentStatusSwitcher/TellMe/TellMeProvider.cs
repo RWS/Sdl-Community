d8711046aa43d215beab28935358960b0619d2f9
Flaviu Tritean <ftritean@sdl.com>
2023-04-11 09:34:42 +0300

    Resolve issues reported ( Missing Tell Me ) Jira: SDLCOM-4513

---
+++ SegmentStatusSwitcher/Sdl.Community.SegmentStatusSwitcher/TellMe/TellMeProvider.cs
@@ -0,0 +1,25 @@
+﻿using Sdl.TellMe.ProviderApi;
+
+namespace Sdl.Community.SegmentStatusSwitcher.TellMe
+{
+	[TellMeProvider]
+	public class TellMeProvider: ITellMeProvider
+	{
+		public string Name => string.Format(PluginResources.TellMe_Provider, PluginResources.Plugin_Name);
+
+		public AbstractTellMeAction[] ProviderActions => new AbstractTellMeAction[]
+		{
+			new CommunityWikiAction
+			{
+				Keywords = new[] { "segment", "status", "switcher", "community", "support", "wiki" }
+			},
+			new AppStoreForumAction
+			{
+				Keywords = new[] { "segment", "status", "switcher", "support", "forum" }
+			},
+			new AppStoreDownloadAction
+			{
+				Keywords = new[] { "segment", "status", "switcher", "store", "download", "appstore" }}
+		};
+	}
+}
