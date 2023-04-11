890feecf1b08fad4c1caadf6e654ddf071757471
Flaviu Tritean <ftritean@sdl.com>
2023-04-11 17:10:03 +0300

    Resolve issues reported ( Broken URL, Rebranding Remnants ) Jira: SDLCOM-4521

--- TermExcelerator/Sdl.Community.TermExcelerator/TellMe/ExcelTermCommunityWikiAction.cs
+++ TermExcelerator/Sdl.Community.TermExcelerator/TellMe/ExcelTermCommunityWikiAction.cs
@@ -17,7 +17,7 @@ namespace Sdl.Community.TermExcelerator.TellMe
 
 		public override void Execute()
 		{
-			Process.Start("https://community.sdl.com/product-groups/translationproductivity/w/customer-experience/5067/excel-terminology-provider-termexcelerator");
+			Process.Start("https://community.rws.com/product-groups/trados-portfolio/rws-appstore/w/wiki/5067/excel-terminology-provider-termexcelerator");
 		}
 	}
 }
