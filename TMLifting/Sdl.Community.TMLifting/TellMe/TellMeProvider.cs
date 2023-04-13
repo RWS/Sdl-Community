1a9feea1e61f80ed36e0da612a6998f4e7e749c4
FlaviuTritean <107054734+FlaviuTritean@users.noreply.github.com>
2023-04-13 09:26:21 +0300

    Resolve issues reported (Broken URL, Missing TellMe, Rebranding Remnants) (#1193)
    Jira: SDLCOM-4527

---
+++ TMLifting/Sdl.Community.TMLifting/TellMe/TellMeProvider.cs
@@ -0,0 +1,25 @@
+﻿using Sdl.TellMe.ProviderApi;
+
+namespace Sdl.Community.TMLifting.TranslationMemory.TellMe
+{
+	[TellMeProvider]
+	public class TellMeProvider: ITellMeProvider
+	{
+		public string Name => string.Format(PluginResources.TellMe_Provider, PluginResources.Plugin_Name);
+
+		public AbstractTellMeAction[] ProviderActions => new AbstractTellMeAction[]
+		{
+			new CommunityWikiAction
+			{
+				Keywords = new[] { "tm lifting", "tmlifting", "lifting", "community", "support", "wiki" }
+			},
+			new AppStoreForumAction
+			{
+				Keywords = new[] { "tm lifting", "tmlifting", "lifting", "support", "forum" }
+			},
+			new AppStoreDownloadAction
+			{
+				Keywords = new[] { "tm lifting", "tmlifting", "lifting", "store", "download", "appstore" }}
+		};
+	}
+}
