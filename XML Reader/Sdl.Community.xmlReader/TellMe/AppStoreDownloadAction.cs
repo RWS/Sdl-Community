b27c1d3a26ab56f8023b99783e053f62af3b9926
Flaviu Tritean <ftritean@sdl.com>
2023-04-18 14:58:48 +0300

    Resolve issues reported ( Missing TellMe ) Jira: SDLCOM-4536

---
+++ XML Reader/Sdl.Community.xmlReader/TellMe/AppStoreDownloadAction.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+using Sdl.TellMe.ProviderApi;
+
+namespace Sdl.Community.XmlReader.TellMe
+{
+	public class AppStoreDownloadAction : AbstractTellMeAction
+	{
+		public override bool IsAvailable => true;
+
+		public override string Category => string.Format(PluginResources.TellMe_Provider_Results, PluginResources.Plugin_Name);
+
+		public override Icon Icon => PluginResources.Download;
+
+		public AppStoreDownloadAction()
+        {
+            Name = string.Format("Download {0} from the AppStore", PluginResources.Plugin_Name);
+        }
+
+		public override void Execute()
+		{
+			Process.Start("https://appstore.rws.com/Plugin/81");
+		}
+	}
+}
