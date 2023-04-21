fcabb6c601e36ce5e4e210b8ad743d03f4813bf7
Rajendra Dogne <RaDogne@moravia.com>
2023-04-21 15:49:02 +0530

    • Resolve issues reported (Broken URL, Missing TellMe, Rebranding Remnants) as needed. Jira: SDLCOM-4495

--- Multilingual.Excel.FileType/Multilingual.Excel.FileType/TellMe/CommunityAppStoreForumAction.cs
+++ Multilingual.Excel.FileType/Multilingual.Excel.FileType/TellMe/CommunityAppStoreForumAction.cs
@@ -19,7 +19,7 @@ namespace Multilingual.Excel.FileType.TellMe
 
 		public override void Execute()
 		{
-			Process.Start("https://community.rws.com/appsupport");
+			Process.Start("https://community.rws.com/product-groups/trados-portfolio/rws-appstore/");
 		}
 	}
 }
