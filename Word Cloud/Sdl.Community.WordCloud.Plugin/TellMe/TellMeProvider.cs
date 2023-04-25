a1997b283e11c0e092a9944641cc49936c2a3507
Rajendra Dogne <RaDogne@moravia.com>
2023-04-25 16:38:10 +0530

    • Resolve issues reported (Broken URL, Missing TellMe, Rebranding Remnants) as needed. Jira: SDLCOM-4533

---
+++ Word Cloud/Sdl.Community.WordCloud.Plugin/TellMe/TellMeProvider.cs
@@ -0,0 +1,26 @@
+﻿using Sdl.TellMe.ProviderApi;
+
+namespace Sdl.Community.WordCloud.Plugin
+{
+	[TellMeProvider]
+	public class TellMeProvider: ITellMeProvider
+	{
+		public string Name => string.Format(PluginResources.TellMe_String_TellMe_Provider, PluginResources.Plugin_Name);
+
+		public AbstractTellMeAction[] ProviderActions => new AbstractTellMeAction[]
+		{
+			new CommunityWikiAction
+			{
+				Keywords = new[] { "trados","studio" ,"word", "cloud", "community", "support", "wiki" }
+			},
+			new CommunityAppStoreForumAction
+			{
+				Keywords = new[] { "trados", "studio", "word", "cloud", "support", "forum" }
+			},
+			new CommunityAppStoreAction
+			{
+				Keywords = new[] { "trados", "studio", "word", "cloud", "store", "download", "appstore" }
+			}
+		};
+	}
+}
