29d5261405e561748f9b7865791f8567877d6990
Rajendra Dogne <RaDogne@moravia.com>
2023-04-28 14:16:04 +0530

    • Resolve issues reported (Broken URL, Missing TellMe, Rebranding Remnants) as needed. JIRA: SDLCOM-4534

--- Wordfast TXML/Properties/AssemblyInfo.cs
+++ Wordfast TXML/Properties/AssemblyInfo.cs
@@ -30,5 +30,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.1.0")]
 [assembly: Sdl.Core.PluginFramework.Plugin("Wordfast TXML")]
