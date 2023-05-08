1d0cc8f45968329d1a7c0071cf2f5d87d30af518
Flaviu Tritean <ftritean@sdl.com>
2023-05-08 08:58:14 +0300

    Resolve issues reported ( Broken URL, Rebranding ) Jira: SDLCOM-4512

--- SDLXLIFFSplitMerge/SplitSDLXLIFF/Helpers/Constants.cs
+++ SDLXLIFFSplitMerge/SplitSDLXLIFF/Helpers/Constants.cs
@@ -3,7 +3,7 @@
 	public static class Constants
 	{
 		public static readonly  string SplitInFilterText = "SDLXLIFF Files (*.sdlxliff)|*.sdlxliff";
-		public static readonly string SplitAddFilterText = "SDL Trados Studio Project Files (*.sdlproj)|*.sdlproj";
+		public static readonly string SplitAddFilterText = "Trados Studio Project Files (*.sdlproj)|*.sdlproj";
 		public static readonly string MergeInfoText  = "SDL Split Info Files (*.splitinfo)|*.splitinfo";
 		public static readonly string MergeOrigText = "SDLXLIFF Files (*.sdlxliff)|*.sdlxliff";
 		public static readonly string SDLXLIFFName = "SDLXLIFF Split/Merge";
