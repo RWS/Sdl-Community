8c2c016a9b6993d0e12be9fd78cd5ead909367ef
Flaviu Tritean <ftritean@sdl.com>
2023-05-08 09:08:36 +0300

    Minor changes

--- SDLXLIFFSplitMerge/SplitSDLXLIFF/Helpers/Constants.cs
+++ SDLXLIFFSplitMerge/SplitSDLXLIFF/Helpers/Constants.cs
@@ -4,7 +4,7 @@
 	{
 		public static readonly  string SplitInFilterText = "SDLXLIFF Files (*.sdlxliff)|*.sdlxliff";
 		public static readonly string SplitAddFilterText = "Trados Studio Project Files (*.sdlproj)|*.sdlproj";
-		public static readonly string MergeInfoText  = "SDL Split Info Files (*.splitinfo)|*.splitinfo";
+		public static readonly string MergeInfoText  = "Trados Split Info Files (*.splitinfo)|*.splitinfo";
 		public static readonly string MergeOrigText = "SDLXLIFF Files (*.sdlxliff)|*.sdlxliff";
 		public static readonly string SDLXLIFFName = "SDLXLIFF Split/Merge";
 	}
