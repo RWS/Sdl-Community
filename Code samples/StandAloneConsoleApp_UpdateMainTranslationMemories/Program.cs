53c8ca7c69934a356298e02f22270bb74a1efa38
Emanuel Albu <ealbu@sdl.com>
2023-05-31 19:29:33 +0300

    Add sample for updating main translation memories

---
+++ Code samples/StandAloneConsoleApp_UpdateMainTranslationMemories/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Sdl.ProjectAutomation.Core;
+using Sdl.ProjectAutomation.FileBased;
+using Sdl.ProjectAutomation.Settings;
+
+namespace StandAloneConsoleApp_UpdateMainTranslationMemories
+{
+	public class Program
+	{
+		private static void Main(string[] args)
+		{
+			var project = new FileBasedProject(@"<projectFilePath>");
+
+			UpdateProjectProviderSettings(project);
+
+			var tpConfig = project.GetTranslationProviderConfiguration();
+
+			var tpUriString = @"sdltm.file:///<TMPath>";
+			var tpReference = new TranslationProviderReference(new Uri(tpUriString), null, true);
+			var tpCascadeEntry = new TranslationProviderCascadeEntry(tpReference, true, true, false);
+			tpConfig.Entries.Add(tpCascadeEntry);
+			project.UpdateTranslationProviderConfiguration(tpConfig);
+			project.Save();
+
+			project.RunAutomaticTasks(project.GetTargetLanguageFiles().GetIds(), new[]
+			{
+				AutomaticTaskTemplateIds.UpdateMainTranslationMemories
+			});
+
+			project.Save();
+		}
+
+		private static void UpdateProjectProviderSettings(FileBasedProject project)
+		{
+			var settings = project.GetSettings();
+			var preTranslateSettings = settings.GetSettingsGroup<TranslateTaskSettings>();
+			preTranslateSettings.NoTranslationMemoryMatchFoundAction.Value = NoTranslationMemoryMatchFoundAction.ApplyAutomatedTranslation;
+			preTranslateSettings.MinimumMatchScore.Value = 75;
+
+			project.UpdateSettings(settings);
+			project.Save();
+		}
+	}
+}
