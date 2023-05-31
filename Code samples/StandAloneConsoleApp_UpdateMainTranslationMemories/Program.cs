e533acf1f565073aa652ce7aa23b289c0c1d348f
Emanuel Albu <ealbu@sdl.com>
2023-05-31 19:49:05 +0300

    ...

--- Code samples/StandAloneConsoleApp_UpdateMainTranslationMemories/Program.cs
+++ Code samples/StandAloneConsoleApp_UpdateMainTranslationMemories/Program.cs
@@ -1,14 +1,36 @@
 ﻿using System;
 using Sdl.ProjectAutomation.Core;
 using Sdl.ProjectAutomation.FileBased;
+using Sdl.ProjectAutomation.Settings;
 
 namespace StandAloneConsoleApp_UpdateMainTranslationMemories
 {
 	public class Program
 	{
+		public static void GetUpdateTmTaskSettings(FileBasedProject project)
+		{
+			var settings = project.GetSettings();
+			var updateTmSettings = settings.GetSettingsGroup<TranslationMemoryUpdateTaskSettings>();
+
+			updateTmSettings.TmImportOptions.Value = TmImportOption.MergeTranslation;
+
+			updateTmSettings.UpdateWithApprovedSignOffSegments.Value = true;
+			updateTmSettings.UpdateWithApprovedTranslationSegments.Value = true;
+			updateTmSettings.UpdateWithTranslatedSegments.Value = true;
+
+			updateTmSettings.UpdateWithDraftSegments.Value = false;
+			updateTmSettings.UpdateWithRejectedSignOffSegments.Value = false;
+			updateTmSettings.UpdateWithRejectedTranslationSegments.Value = false;
+			updateTmSettings.UpdateWithUnspecifiedSegments.Value = false;
+
+			project.UpdateSettings(settings);
+		}
+
 		private static void Main(string[] args)
 		{
 			var project = new FileBasedProject(@"<projectFilePath>");
+			GetUpdateTmTaskSettings(project);
+
 			var tpConfig = project.GetTranslationProviderConfiguration();
 
 			var tpUriString = @"sdltm.file:///<TMPath>";
