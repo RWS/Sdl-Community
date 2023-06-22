cd6fc6f9ef8ee6cd05e0ef98d80f32a793414d6a
RajendraDogne-RWS <83598481+RajendraDogne-RWS@users.noreply.github.com>
2023-06-22 18:06:21 +0530

    Reorganise the naming in Tell Me provider (#1311)
    Jira: • SDLCOM-4687
    
    Co-authored-by: Rajendra Dogne <RaDogne@moravia.com>

--- ApplyTMTemplate/Sdl.Community.ApplyTMTemplate/TellMe/ApplyTMTemplateTellMeProvider.cs
+++ ApplyTMTemplate/Sdl.Community.ApplyTMTemplate/TellMe/ApplyTMTemplateTellMeProvider.cs
@@ -11,19 +11,19 @@ namespace Sdl.Community.ApplyTMTemplate.TellMe
 		{
 			new ApplyTMTemplateStoreAction
 			{
-				Keywords = new[] {"applytm", "apply tm store", "apply tm download"}
+				Keywords = new[] {  "ApplyTm", "applytm","apply", "tm", "template", "apply tm template", "apply tm store", "apply tm download"}
 			},
 			new ApplyTMCommunitySupportAction
 			{
-				Keywords = new[] {"apply tm template","forum", "community", "support"}
+				Keywords = new[] { "ApplyTm", "applytm", "apply", "tm", "template", "apply tm template","forum", "community", "support"}
 			},
 			new ApplyTmTemplateAction
 			{
-				Keywords = new[] {"apply tm template", "start"}
+				Keywords = new[] { "ApplyTm", "applytm", "apply", "tm", "template", "apply tm template", "start"}
 			},
 			new ApplyTMTemplateHelpAction()
 			{
-				Keywords = new []{"apply tm template", "wiki", "help"}
+				Keywords = new []{ "ApplyTm", "applytm", "apply", "tm", "template", "apply tm template", "wiki", "help"}
 			}
 		};
 	}
