dbd11a14e30f9fa348603f41a1a38d69936ae978
FlaviuTritean <107054734+FlaviuTritean@users.noreply.github.com>
2023-06-30 16:04:33 +0300

    Rezolved - Settings are not preserved in the fields. (#1319)
    * Rezolved - Settings are not preserved in the fields.
    Jira: SDLCOM-4521
    
    * minor changes
    
    * Remove unnecessary lines of code

---
+++ TermExcelerator/Sdl.Community.TermExcelerator/Allow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sdl.Community.TermExcelerator
+{
+	public class Allow
+	{
+		private static bool Access;
+
+		public Allow() { }
+
+		public static bool GetAccess()
+		{
+			return Access;
+		}
+
+		public static void SetAccess(bool x)
+		{
+		      Access = x;
+		}
+	}
+}
