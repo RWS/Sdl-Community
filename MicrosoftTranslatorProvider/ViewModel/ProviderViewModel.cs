6c6f44771b9bbfe2537ab2fc6d7c0b54178a5404
Andrei Birleanu <107615876+andreibirleanu10@users.noreply.github.com>
2023-07-26 10:52:20 +0300

    Added auth label (#1346)

--- MicrosoftTranslatorProvider/ViewModel/ProviderViewModel.cs
+++ MicrosoftTranslatorProvider/ViewModel/ProviderViewModel.cs
@@ -21,20 +21,33 @@ namespace MicrosoftTranslatorProvider.ViewModel
 		private List<RegionSubscription> _regions;
 		private RegionSubscription _selectedRegion;
 
+		private bool _editProvider;
 		private bool _persistMicrosoftKey;
 		private string _apiKey;
 
 		private ICommand _learnMoreCommand;
 
-		public ProviderViewModel(ITranslationOptions options, LanguagePair[] languagePairs)
+		public ProviderViewModel(ITranslationOptions options, LanguagePair[] languagePairs, bool editProvider)
 		{
 			_options = options;
 			_languagePairs = languagePairs;
+			EditProvider = editProvider;
 			InitializeComponent();
 			CreateMapping();
 		}
 
 		public BaseModel ViewModel => this;
+
+		public bool EditProvider
+		{
+			get => _editProvider;
+			set
+			{
+				if (_editProvider == value) return;
+				_editProvider = value;
+				OnPropertyChanged();
+			}
+		}
 
 		public string ApiKey
 		{
