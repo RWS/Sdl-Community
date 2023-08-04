38b0cb057d6c40391560e1f988a44bb1725500e3
Emanuel Albu <ealbu@sdl.com>
2023-08-04 18:29:34 +0200

    Refactoring

--- InterpretBank/InterpretBank/TermbaseViewer/UI/TermbaseViewer.xaml.cs
+++ InterpretBank/InterpretBank/TermbaseViewer/UI/TermbaseViewer.xaml.cs
@@ -51,15 +51,10 @@ namespace InterpretBank.TermbaseViewer.UI
 		}
 
 		/// <summary>
-		/// In this section, we are invoking the command directly from code-behind instead of using
-		/// command binding for a specific reason. Due to the timing requirements between the command
-		/// execution and subsequent UI actions, using command binding would not guarantee the correct
-		/// order of operations in this particular scenario.
-		///
-		/// The invoked command (TermbaseViewerViewModel.AddNewTermCommand) adds a new item to a ListBox,
-		/// and the UI event handler needs to focus an EditableTextBlock bound to properties of the newly added item.
-		/// This requires the command
-		/// to be executed before focusing the EditableTextBlock.
+		/// Here, we are invoking a command from code-behind for two reasons: firstly, the same reason mentioned above
+		/// but in regards to another command and secondly, because multiple keyboard modifiers
+		/// cannot be specified in code-behind, so PreviewKeyDown cannot be used in all cases.
+		/// In order to not use PreviewKeyDown event for some of the keys and commands for the others, we'll use just one.
 		/// </summary>
 		private void SetEditingFromKeyboard(object parameter)
 		{
