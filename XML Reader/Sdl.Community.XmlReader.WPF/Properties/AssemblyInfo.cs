0c65b228a12751bcd4d452658e3f37b5c53c7990
Rajendra Dogne <RaDogne@moravia.com>
2023-08-17 14:45:10 +0530

    Resolved XML Reader : error message in SR1 Jira: SDLCOM-4915

--- XML Reader/Sdl.Community.XmlReader.WPF/Properties/AssemblyInfo.cs
+++ XML Reader/Sdl.Community.XmlReader.WPF/Properties/AssemblyInfo.cs
@@ -50,4 +50,4 @@ using System.Windows;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.0.0.1")]
+[assembly: AssemblyFileVersion("4.0.2.0")]
