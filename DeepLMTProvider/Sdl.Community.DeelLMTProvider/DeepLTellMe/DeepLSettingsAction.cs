77353d9c0447878cc25f0f2f36f090fcceca8492
Emanuel Albu <ealbu@sdl.com>
2023-08-28 11:29:24 +0300

    Fix TellMe action

--- DeepLMTProvider/Sdl.Community.DeelLMTProvider/DeepLTellMe/DeepLSettingsAction.cs
+++ DeepLMTProvider/Sdl.Community.DeelLMTProvider/DeepLTellMe/DeepLSettingsAction.cs
@@ -1,18 +1,22 @@
-﻿using System.Drawing;
-using System.Linq;
-using System.Windows.Forms;
-using Newtonsoft.Json;
+﻿using Newtonsoft.Json;
 using Sdl.Community.DeepLMTProvider.Client;
+using Sdl.Community.DeepLMTProvider.Helpers;
+using Sdl.Community.DeepLMTProvider.Helpers.GlossaryReadersWriters;
 using Sdl.Community.DeepLMTProvider.Model;
 using Sdl.Community.DeepLMTProvider.Service;
 using Sdl.Community.DeepLMTProvider.UI;
 using Sdl.Community.DeepLMTProvider.ViewModel;
 using Sdl.TellMe.ProviderApi;
 using Sdl.TranslationStudioAutomation.IntegrationApi;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
 
 namespace Sdl.Community.DeepLMTProvider.DeepLTellMe
 {
-	public class DeepLSettingsAction : AbstractTellMeAction
+    public class DeepLSettingsAction : AbstractTellMeAction
     {
         public DeepLSettingsAction()
         {
@@ -25,6 +29,8 @@ namespace Sdl.Community.DeepLMTProvider.DeepLTellMe
 
         public override bool IsAvailable => true;
 
+        private DeepLWindowViewModel ViewModel { get; set; }
+
         public override void Execute()
         {
             var currentProject = SdlTradosStudio.Application.GetController<ProjectsController>().CurrentProject;
@@ -35,8 +41,8 @@ namespace Sdl.Community.DeepLMTProvider.DeepLTellMe
             }
             else
             {
-				//TODO: Don't forget to fix this
-				var settings = currentProject.GetTranslationProviderConfiguration();
+                //TODO: Don't forget to fix this
+                var settings = currentProject.GetTranslationProviderConfiguration();
 
                 if (!settings.Entries.Any(entry =>
                     entry.MainTranslationProvider.Uri.OriginalString.Contains("deepltranslationprovider")))
@@ -54,20 +60,86 @@ namespace Sdl.Community.DeepLMTProvider.DeepLTellMe
                         var state = translationProvider.MainTranslationProvider.State;
                         var options = new DeepLTranslationOptions(uri, state);
 
-                        var viewModel = new DeepLWindowViewModel(options, new DeepLGlossaryClient(), new MessageService());
-                        var dialog = new DeepLWindow(viewModel);
+                        ViewModel = new DeepLWindowViewModel(options, new DeepLGlossaryClient(), new MessageService());
+                        ViewModel.ManageGlossaries += ViewModel_ManageGlossaries;
+
+                        var dialog = new DeepLWindow(ViewModel);
 
                         dialog.ShowDialog();
 
                         if (dialog.DialogResult.HasValue && dialog.DialogResult.Value)
                         {
                             translationProvider.MainTranslationProvider.Uri = options.Uri;
-							translationProvider.MainTranslationProvider.State = JsonConvert.SerializeObject(options);
-							currentProject.UpdateTranslationProviderConfiguration(settings);
+                            translationProvider.MainTranslationProvider.State = JsonConvert.SerializeObject(options);
+                            currentProject.UpdateTranslationProviderConfiguration(settings);
                         }
                     }
                 }
             }
+        }
+
+        private static string GetDateTimeToString(DateTime dateTime)
+        {
+            var value = dateTime.Year +
+                        dateTime.Month.ToString().PadLeft(2, '0') +
+                        dateTime.Day.ToString().PadLeft(2, '0') +
+                        "-" +
+                        dateTime.Hour.ToString().PadLeft(2, '0') +
+                        dateTime.Minute.ToString().PadLeft(2, '0') +
+                        dateTime.Second.ToString().PadLeft(2, '0');
+            return value;
+        }
+
+        private static bool UserHasWritePermission(DirectoryInfo directoryInfo)
+        {
+            try
+            {
+                // Attempt to create a temporary file in the directory
+                string tempFilePath = Path.Combine(directoryInfo.FullName, Guid.NewGuid().ToString("N") + ".tmp");
+                using (FileStream fs = File.Create(tempFilePath)) { }
+
+                // Delete the temporary file
+                File.Delete(tempFilePath);
+
+                return true; // User has write permission
+            }
+            catch (UnauthorizedAccessException)
+            {
+                return false; // User does not have write permission
+            }
+        }
+
+        private void ViewModel_ManageGlossaries()
+        {
+            var glossaryReaderWriterFactory = new GlossaryReaderWriterFactory();
+            var messageService = new MessageService();
+
+            var glossariesWindowViewModel = new GlossariesWindowViewModel(new DeepLGlossaryClient(),
+                messageService, new GlossaryImportExportService(new DialogWrapper()),
+                new GlossaryReaderWriterService(glossaryReaderWriterFactory), new ProcessStarter(), new EditGlossaryService());
+
+            var (success, glossaryWriter, _) = glossaryReaderWriterFactory.CreateGlossaryWriter(GlossaryReaderWriterService.Format.CSV);
+            if (!success) messageService.ShowWarning("Backup service could not be initialized.\nUse glossary manager carefully!", "Manage glossaries");
+
+            var backupPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                "Trados AppStore",
+                $@"DeepLLogs\Backup");
+
+            if (!UserHasWritePermission(new DirectoryInfo(backupPath))) messageService.ShowWarning($"User not authorized to write to backup location {backupPath}");
+
+            glossariesWindowViewModel.ShouldBackUp += glossary =>
+            {
+                var backupDirectory = $@"{backupPath}\{GetDateTimeToString(DateTime.Now)}";
+                Directory.CreateDirectory(backupDirectory);
+
+                glossaryWriter.WriteGlossary(glossary,
+                    $@"{backupPath}\{GetDateTimeToString(DateTime.Now)}\{glossary.Name}_{glossary.SourceLanguage}-{glossary.TargetLanguage}.csv");
+            };
+
+            var glossariesWindow = new GlossariesWindow { DataContext = glossariesWindowViewModel };
+            glossariesWindow.ShowDialog();
+
+            ViewModel.LoadLanguagePairSettings();
         }
     }
 }
