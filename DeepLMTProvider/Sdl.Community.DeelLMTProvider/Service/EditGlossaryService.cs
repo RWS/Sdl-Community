c5152803e25e307797abde8f0a4b25815b836ad0
Emanuel Albu <ealbu@sdl.com>
2023-09-25 14:35:12 +0300

    Trim entries and glossary name before uploading to DeepL

--- DeepLMTProvider/Sdl.Community.DeelLMTProvider/Service/EditGlossaryService.cs
+++ DeepLMTProvider/Sdl.Community.DeelLMTProvider/Service/EditGlossaryService.cs
@@ -1,4 +1,5 @@
-﻿using Sdl.Community.DeepLMTProvider.Interface;
+﻿using Sdl.Community.DeepLMTProvider.Extensions;
+using Sdl.Community.DeepLMTProvider.Interface;
 using Sdl.Community.DeepLMTProvider.Model;
 using Sdl.Community.DeepLMTProvider.UI;
 using System.Collections.Generic;
@@ -7,9 +8,22 @@ namespace Sdl.Community.DeepLMTProvider.Service
 {
     public class EditGlossaryService : IEditGlossaryService
     {
-        public List<GlossaryEntry> GlossaryEntries => new(EditGlossaryWindow.GlossaryEntries);
+        public List<GlossaryEntry> GlossaryEntries
+        {
+            get
+            {
+                var entries = EditGlossaryWindow.GlossaryEntries;
+                entries.ForEach(e =>
+                {
+                    e.SourceTerm = e.SourceTerm.Trim();
+                    e.TargetTerm = e.TargetTerm.Trim();
+                });
 
-        public string GlossaryName => EditGlossaryWindow.GlossaryName;
+                return new(entries);
+            }
+        }
+
+        public string GlossaryName => EditGlossaryWindow.GlossaryName.Trim();
         private EditGlossaryWindow EditGlossaryWindow { get; set; }
 
         public bool EditGlossary(List<GlossaryEntry> glossaryEntries, string glossaryName)
