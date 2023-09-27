6000969e47c928b8514dd7cfd5d775b342ac7338
Emanuel Albu <ealbu@sdl.com>
2023-09-27 14:49:52 +0300

    User Feedback: Persist glossary selection

--- DeepLMTProvider/DeepLMTProvider.Tests/Properties/AssemblyInfo.cs
+++ DeepLMTProvider/DeepLMTProvider.Tests/Properties/AssemblyInfo.cs
@@ -28,4 +28,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("6.0.0.0")]
-[assembly: AssemblyFileVersion("6.1.2.14")]
+[assembly: AssemblyFileVersion("6.1.2.15")]
