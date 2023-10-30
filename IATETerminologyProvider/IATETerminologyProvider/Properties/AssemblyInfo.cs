6d4457440689e4c439ff7f26a3b1e1e38ef61c64
Rajendra Dogne <RaDogne@moravia.com>
2023-10-30 15:35:41 +0530

    Integrating the Microsoft Chromium browser webview2 to replace CefSharp Chromium browser. Jira- SDLCOM-3883

--- IATETerminologyProvider/IATETerminologyProvider/Properties/AssemblyInfo.cs
+++ IATETerminologyProvider/IATETerminologyProvider/Properties/AssemblyInfo.cs
@@ -27,8 +27,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or youcan default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.1.3.0")]
+[assembly: AssemblyFileVersion("4.1.4.0")]
