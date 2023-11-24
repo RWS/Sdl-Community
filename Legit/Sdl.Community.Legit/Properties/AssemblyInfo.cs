516c02b5a2c0be4a448258f827768dea705541f1
FlaviuTritean <107054734+FlaviuTritean@users.noreply.github.com>
2023-11-24 15:06:16 +0200

    Legit: now references point to $(MSBuildProgramFiles32) (#1425)
    * Legit: now references point to $(MSBuildProgramFiles32)
    
    * minor change

--- Legit/Sdl.Community.Legit/Properties/AssemblyInfo.cs
+++ Legit/Sdl.Community.Legit/Properties/AssemblyInfo.cs
@@ -20,4 +20,4 @@ using System.Runtime.InteropServices;
 [assembly: Guid("80880bc9-fa76-4d3c-8739-d5615e947d9c")]
 
 [assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.0.0.1")]
+[assembly: AssemblyFileVersion("4.0.1.0")]
