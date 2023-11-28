ac9ea278475a5506261d82d803a21379861c7fd1
Flaviu Tritean <ftritean@sdl.com>
2023-11-28 15:05:40 +0200

    Language Weaver: Update toolkit in unit tests

--- SDLMTCloud.Provider/SolutionInfo.cs
+++ SDLMTCloud.Provider/SolutionInfo.cs
@@ -17,4 +17,4 @@
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("5.0.0.0")]
-[assembly: AssemblyFileVersion("5.1.3.0")]
+[assembly: AssemblyFileVersion("5.1.4.0")]
