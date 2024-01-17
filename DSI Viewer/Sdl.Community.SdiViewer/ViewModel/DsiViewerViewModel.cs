0fc03a58640ff8298832f9ecadfe48d4c45c4f86
Andrei Birleanu <abirleanu@sdl.com>
2024-01-17 09:24:42 +0200

    DSI Viewer 4.2.0.1: SDLCOM-3513 & SDLCOM-5309

--- DSI Viewer/Sdl.Community.SdiViewer/ViewModel/DsiViewerViewModel.cs
+++ DSI Viewer/Sdl.Community.SdiViewer/ViewModel/DsiViewerViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Windows.Input;
 using Sdl.Community.DsiViewer.Commands;
 using Sdl.Community.DsiViewer.Model;
@@ -10,6 +11,7 @@ using Sdl.Community.DsiViewer.Services;
 using Sdl.FileTypeSupport.Framework.BilingualApi;
 using Sdl.FileTypeSupport.Framework.Core.Utilities.NativeApi;
 using Sdl.FileTypeSupport.Framework.NativeApi;
+using Sdl.LanguagePlatform.Core;
 using Sdl.TranslationStudioAutomation.IntegrationApi;
 
 namespace Sdl.Community.DsiViewer.ViewModel
@@ -23,7 +25,23 @@ namespace Sdl.Community.DsiViewer.ViewModel
 		private ICommand _clearSdlMtCloudFilter;
 		private List<IComment> _comments;
 		private List<DsiModel> _documentStructureInformation;
+		private List<DSITagModel> _segmentTags;
 		private TranslationOriginData _translationOriginData;
+
+		private string _collapseDsiButtonName;
+		private string _collapseCommentsButtonName;
+		private string _collapseTagsButtonName;
+		private string _collapseLwButtonName;
+
+		private bool _isDsiVisible;
+		private bool _isCommentsVisible;
+		private bool _isTagsVisible;
+		private bool _isLwVisible;
+
+		private ICommand _collapseCommentsCommand;
+		private ICommand _collapseTagsCommand;
+		private ICommand _collapseDsiCommand;
+		private ICommand _collapseLwCommand;
 
 		public DsiViewerViewModel()
 		{
@@ -34,11 +52,25 @@ namespace Sdl.Community.DsiViewer.ViewModel
 			_editorController = DsiViewerInitializer.EditorController;
 			_editorController.ActiveDocumentChanged += EditorController_ActiveDocumentChanged;
 
+			CollapseDsiButtonName = "Collapse";
+			CollapseCommentsButtonName = "Collapse";
+			CollapseTagsButtonName = "Collapse";
+			CollapseLwButtonName = "Collapse";
+
+			IsDsiVisible = true;
+			IsCommentsVisible = true;
+			IsTagsVisible = true;
+			IsLwVisible = true;
+
 			SetActiveDocument(_editorController.ActiveDocument);
 		}
 
 		public ICommand ApplySdlMtCloudFilter => _applySdlMtCloudFilter ??= new CommandHandler(() => ApplyFilter(), true);
 		public ICommand ClearSdlMtCloudFilter => _clearSdlMtCloudFilter ??= new CommandHandler(() => ApplyFilter(true), true);
+		public ICommand CollapseDsiCommand => _collapseDsiCommand ??= new CommandHandler(CollapseDsi, true);
+		public ICommand CollapseCommentsCommand => _collapseCommentsCommand ??= new CommandHandler(CollapseComments, true);
+		public ICommand CollapseTagsCommand => _collapseTagsCommand ??= new CommandHandler(CollapseTags, true);
+		public ICommand CollapseLwCommand => _collapseLwCommand ??= new CommandHandler(CollapseLw, true);
 
 		public IOrderedEnumerable<IComment> Comments
 		{
@@ -51,37 +83,33 @@ namespace Sdl.Community.DsiViewer.ViewModel
 			{
 				_comments = value?.ToList();
 				OnPropertyChanged(nameof(Comments));
+				OnPropertyChanged(nameof(HasComments));
 			}
 		}
+
+		public bool HasComments => Comments?.Any() ?? false;
 
 		public IOrderedEnumerable<DsiModel> DocumentStructureInformation
 		{
 			get
 			{
-				if (_documentStructureInformation == null)
-				{
-					_documentStructureInformation = new List<DsiModel>();
-				}
-
+				_documentStructureInformation ??= new List<DsiModel>();
 				return _documentStructureInformation.OrderBy(a => a.DisplayName).ThenBy(a => a.Code);
 			}
 			set
 			{
 				_documentStructureInformation = value?.ToList();
 				OnPropertyChanged(nameof(DocumentStructureInformation));
+				OnPropertyChanged(nameof(HasDocumentStructureInformation));
 			}
 		}
 
+		public bool HasDocumentStructureInformation => DocumentStructureInformation?.Any() ?? false;
+
 		public FilterApplier FilterApplier { get; set; } = new();
 
-		public bool HasComments => Comments.Any();
-
-		public bool HasDocumentStructureInformation => DocumentStructureInformation.Any();
-
-		public bool HasSdlMtCloudRelatedInfo
-													=>
-				_editorController?.ActiveDocument?.SegmentPairs.Any(
-					sp => sp.Properties.TranslationOrigin.MetaDataContainsKey("quality_estimation")) ?? false;
+		public bool HasSdlMtCloudRelatedInfo =>
+				_editorController?.ActiveDocument?.ActiveSegmentPair?.Properties?.TranslationOrigin?.MetaDataContainsKey("quality_estimation") ?? false;
 
 		public bool HasTranslationOriginMetadata => TranslationOriginData != null;
 
@@ -98,7 +126,122 @@ namespace Sdl.Community.DsiViewer.ViewModel
 			}
 		}
 
-		
+		public List<DSITagModel> SegmentTags
+		{
+			get => _segmentTags;
+			set
+			{
+				_segmentTags = value;
+				OnPropertyChanged();
+				OnPropertyChanged(nameof(HasTags));
+			}
+		}
+
+		public bool HasTags => SegmentTags?.Any() ?? false;
+
+		public string CollapseDsiButtonName
+		{
+			get { return _collapseDsiButtonName; }
+			set
+			{
+				if (_collapseDsiButtonName != value)
+				{
+					_collapseDsiButtonName = value;
+					OnPropertyChanged();
+				}
+			}
+		}
+
+		public string CollapseCommentsButtonName
+		{
+			get { return _collapseCommentsButtonName; }
+			set
+			{
+				if (_collapseCommentsButtonName != value)
+				{
+					_collapseCommentsButtonName = value;
+					OnPropertyChanged();
+				}
+			}
+		}
+
+		public string CollapseTagsButtonName
+		{
+			get { return _collapseTagsButtonName; }
+			set
+			{
+				if (_collapseTagsButtonName != value)
+				{
+					_collapseTagsButtonName = value;
+					OnPropertyChanged();
+				}
+			}
+		}
+
+		public string CollapseLwButtonName
+		{
+			get { return _collapseLwButtonName; }
+			set
+			{
+				if (_collapseLwButtonName != value)
+				{
+					_collapseLwButtonName = value;
+					OnPropertyChanged();
+				}
+			}
+		}
+
+		public bool IsDsiVisible
+		{
+			get { return _isDsiVisible; }
+			set
+			{
+				if (_isDsiVisible != value)
+				{
+					_isDsiVisible = value;
+					OnPropertyChanged();
+				}
+			}
+		}
+
+		public bool IsCommentsVisible
+		{
+			get { return _isCommentsVisible; }
+			set
+			{
+				if (_isCommentsVisible != value)
+				{
+					_isCommentsVisible = value;
+					OnPropertyChanged();
+				}
+			}
+		}
+
+		public bool IsTagsVisible
+		{
+			get { return _isTagsVisible; }
+			set
+			{
+				if (_isTagsVisible != value)
+				{
+					_isTagsVisible = value;
+					OnPropertyChanged();
+				}
+			}
+		}
+
+		public bool IsLwVisible
+		{
+			get { return _isLwVisible; }
+			set
+			{
+				if (_isLwVisible != value)
+				{
+					_isLwVisible = value;
+					OnPropertyChanged();
+				}
+			}
+		}
 
 		public void Dispose()
 		{
@@ -127,6 +270,8 @@ namespace Sdl.Community.DsiViewer.ViewModel
 			UpdateDocumentStructureInformation();
 			UpdateComments();
 			UpdateTranslationOriginInformation();
+			UpdateTags();
+			OnPropertyChanged(nameof(HasSdlMtCloudRelatedInfo));
 		}
 
 		private void ActiveDocument_ContentChanged(object sender, DocumentContentEventArgs e)
@@ -137,6 +282,7 @@ namespace Sdl.Community.DsiViewer.ViewModel
 		private void ActiveDocument_SegmentsTranslationOriginChanged(object sender, EventArgs e)
 		{
 			UpdateTranslationOriginInformation();
+			UpdateTags();
 			OnPropertyChanged(nameof(HasSdlMtCloudRelatedInfo));
 		}
 
@@ -211,6 +357,7 @@ namespace Sdl.Community.DsiViewer.ViewModel
 				UpdateDocumentStructureInformation();
 				UpdateComments();
 				UpdateTranslationOriginInformation();
+				UpdateTags();
 			}
 		}
 
@@ -292,23 +439,136 @@ namespace Sdl.Community.DsiViewer.ViewModel
 				return;
 			}
 
-			switch (translationOrigin.OriginSystem)
+			if (translationOrigin.OriginSystem is null)
 			{
-				case not null when translationOrigin.OriginSystem.ToLower().Contains(PluginResources.ProviderId):
-					{
-						var qualityEstimation = translationOrigin.GetMetaData("quality_estimation");
-						TranslationOriginData = new TranslationOriginData
-						{
-							QualityEstimation = qualityEstimation,
-							Model = translationOrigin.GetMetaData("model"),
-						};
-						break;
-					}
-
-				default:
-					TranslationOriginData = null;
-					break;
+				TranslationOriginData = null;
+				return;
 			}
+
+			var qualityEstimation = translationOrigin.GetMetaData("quality_estimation");
+			TranslationOriginData = new TranslationOriginData
+			{
+				QualityEstimation = qualityEstimation,
+				Model = translationOrigin.GetMetaData("model"),
+			};
+		}
+
+		private void UpdateTags()
+		{
+			SegmentTags = new();
+			var activeSegmentSource = _activeDocument?.ActiveSegmentPair?.Source;
+			if (activeSegmentSource is null)
+			{
+				return;
+			}
+
+			var tags = new List<DSITagModel>();
+			ExtractTags(activeSegmentSource.AllSubItems, tags);
+			SegmentTags = tags.Distinct().ToList();
+		}
+
+		private void ExtractTags(IEnumerable<IAbstractMarkupData> allSubItems, List<DSITagModel> tags)
+		{
+			if (allSubItems is null)
+			{
+				return;
+			}
+
+			foreach (var item in allSubItems)
+			{
+				if (item is ILockedContent lockedContent)
+				{
+					ExtractTags(lockedContent.Content.AllSubItems, tags);
+					continue;
+				}
+
+				if (item is Text || item is not IAbstractTag)
+				{
+					continue;
+				}
+
+				var tagPair = item as ITagPair;
+				var placeholderTag = item as IPlaceholderTag;
+				var structureTag = item as IStructureTag;
+
+				ExtractTagPairProperties(tagPair, tags);
+				ExtractPlaceholderTagProperties(placeholderTag, tags);
+				ExtractStructureTagProperties(structureTag, tags);
+
+				if (tagPair is not null)
+				{
+					ExtractTags(tagPair, tags);
+				}
+			}
+		}
+
+		private void ExtractTagPairProperties(ITagPair tag, List<DSITagModel> tags)
+		{
+			if (tag is null)
+			{
+				return;
+			}
+
+			var tagId = tag.TagProperties.TagId.Id;
+			if (tags.Any(x => x.Id == tagId))
+			{
+				return;
+			}
+
+			var pattern = @"<(\w+)([^>]*)>(.*?)<\/\1>";
+			var example = tag.ToString();
+			var match = Regex.Match(example, pattern, RegexOptions.Singleline);
+
+			var newTagModel = new DSITagModel()
+			{
+				Id = tagId,
+				StartTag = $"<{match.Groups[1].Value}>",
+				EndTag = $"</{match.Groups[1].Value}>",
+				Attributes = match.Groups[2].Value,
+				Content = match.Groups[3].Value,
+			};
+
+			tags.Add(newTagModel);
+		}
+
+		private void ExtractPlaceholderTagProperties(IPlaceholderTag tag, List<DSITagModel> tags)
+		{
+			if (tag is null)
+			{
+				return;
+			}
+		}
+
+		private void ExtractStructureTagProperties(IStructureTag tag, List<DSITagModel> tags)
+		{
+			if (tag is null)
+			{
+				return;
+			}
+		}
+
+		private void CollapseDsi()
+		{
+			IsDsiVisible = !IsDsiVisible;
+			CollapseDsiButtonName = IsDsiVisible ? "Collapse" : "Expand";
+		}
+
+		private void CollapseComments()
+		{
+			IsCommentsVisible = !IsCommentsVisible;
+			CollapseCommentsButtonName = IsCommentsVisible ? "Collapse" : "Expand";
+		}
+
+		private void CollapseTags()
+		{
+			IsTagsVisible = !IsTagsVisible;
+			CollapseTagsButtonName = IsTagsVisible ? "Collapse" : "Expand";
+		}
+
+		private void CollapseLw()
+		{
+			IsLwVisible = !IsLwVisible;
+			CollapseLwButtonName = IsLwVisible ? "Collapse" : "Expand";
 		}
 	}
 }
