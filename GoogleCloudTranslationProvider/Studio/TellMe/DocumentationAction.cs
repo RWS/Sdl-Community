498d0c55d5359ffe7be978a43b1d883058463f41
Andrei Birleanu <abirleanu@sdl.com>
2024-02-13 11:43:29 +0200

    updated documentation url

--- GoogleCloudTranslationProvider/Studio/TellMe/DocumentationAction.cs
+++ GoogleCloudTranslationProvider/Studio/TellMe/DocumentationAction.cs
@@ -6,7 +6,7 @@ namespace GoogleCloudTranslationProvider.Studio.TellMe
 	{
 		private static readonly string[] _helpKeywords = { "help", "guide" };
 		private static readonly string _actionName = $"{PluginResources.Plugin_Name} Documentation";
-		private static readonly string _url = "https://appstore.rws.com/Plugin/39?tab=documentation";
+		private static readonly string _url = "https://appstore.rws.com/Plugin/174?tab=documentation";
 		private static readonly bool _isAvailable = true;
 		private static readonly Icon _icon = PluginResources.documentation;
 
