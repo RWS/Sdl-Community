7bfb98716886be0b824b529f43527bb3fcc64578
Emanuel Albu <ealbu@sdl.com>
2024-03-14 17:32:52 +0100

    Open drop down of multiselect combobox directly when clicked on

--- InterpretBank/InterpretBank/Controls/ChooseFilepathControl.xaml.cs
+++ InterpretBank/InterpretBank/Controls/ChooseFilepathControl.xaml.cs
@@ -3,10 +3,8 @@ using InterpretBank.Interface;
 using InterpretBank.Model;
 using Newtonsoft.Json;
 using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -25,19 +23,15 @@ namespace InterpretBank.Controls
                             DependencyProperty.Register(nameof(Filepath), typeof(string), typeof(ChooseFilepathControl),
                 new PropertyMetadata(string.Empty));
 
-        private static readonly string DbListPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
-            @"Trados AppStore\InterpretBank\DatabaseList.json");
-        
         private static readonly string DbListFolderPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
             @"Trados AppStore\InterpretBank");
 
-
+        private static readonly string DbListPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                    @"Trados AppStore\InterpretBank\DatabaseList.json");
 
         public ChooseFilepathControl()
         {
             InitializeComponent();
-
-            
         }
 
         public DatabaseList DatabaseList
@@ -75,109 +69,13 @@ namespace InterpretBank.Controls
             if (!DatabaseList.List.Contains(filepath))
                 DatabaseList.List.Add(filepath);
 
-            DatabaseList.LastUsed = filepath;
             File.WriteAllText(DbListPath, JsonConvert.SerializeObject(DatabaseList));
         }
-
-        //private void AutoCompleteList_OnPreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        //{
-        //    var autoCompleteOption = (string)AutoCompleteList.SelectedItem;
-
-        //    if (!File.Exists(autoCompleteOption))
-        //    {
-        //        var confirmation =
-        //            UserInteractionService.Confirm("This DB no longer exists. Do you wish to remove it from this list?");
-        //        if (confirmation)
-        //        {
-        //            DatabaseList.Remove(autoCompleteOption);
-        //            DatabaseList.Remove(autoCompleteOption);
-        //        }
-        //    }
-        //    else
-        //    {
-        //        Filepath = autoCompleteOption;
-        //    }
-        //    AutoCompleteList.IsDropDownOpen = false;
-        //}
 
         private void BrowseButton_OnClick(object sender, RoutedEventArgs e)
         {
             if (!UserInteractionService.GetFilePath(out var filepath, "Interpret Bank Databases (*.db)|*.db")) return;
             Filepath = filepath;
-        }
-
-        //private void ClearFilepathButton(object sender, RoutedEventArgs e)
-        //{
-        //    FilepathTextBox.Clear();
-        //    Filepath = null;
-        //}
-
-        //private void FilepathTextBox_OnPreviewMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
-        //{
-        //    OpenMenu();
-        //}
-
-        //private void FilepathTextBox_PreviewKeyDown(object sender, KeyEventArgs e)
-        //{
-        //    if (e.Key == Key.Escape)
-        //    {
-        //        e.Handled = true;
-
-        //        if (AutoCompleteList.IsDropDownOpen)
-        //            AutoCompleteList.IsDropDownOpen = false;
-        //        else if (!string.IsNullOrWhiteSpace(Filepath))
-        //            FilepathTextBox.Clear();
-        //        else
-        //        {
-        //            e.Handled = false;
-        //        }
-        //    }
-
-        //    if (e.Key == Key.Enter)
-        //    {
-        //        if (AutoCompleteList.IsDropDownOpen)
-        //        {
-        //            Filepath = DatabaseList[AutoCompleteList.SelectedIndex];
-        //            AutoCompleteList.IsDropDownOpen = false;
-        //        }
-        //        else OpenMenu();
-
-        //        e.Handled = true;
-        //    }
-
-        //    if (e.Key == Key.Down)
-        //    {
-        //        AutoCompleteList.IsDropDownOpen = true;
-        //        if (AutoCompleteList.SelectedIndex < AutoCompleteList.Items.Count - 1)
-        //        {
-        //            AutoCompleteList.SelectedIndex++;
-        //            //AutoCompleteList.ScrollIntoView(AutoCompleteList.SelectedItem);
-        //        }
-        //        e.Handled = true;
-        //    }
-        //    else if (e.Key == Key.Up)
-        //    {
-        //        if (AutoCompleteList.SelectedIndex > 0)
-        //        {
-        //            AutoCompleteList.SelectedIndex--;
-        //            //AutoCompleteList.ScrollIntoView(AutoCompleteList.SelectedItem);
-        //        }
-        //        e.Handled = true;
-        //    }
-        //}
-
-        //private void OpenMenu()
-        //{
-        //    var isDropDownOpen = AutoCompleteList.IsDropDownOpen;
-        //    AutoCompleteList.IsDropDownOpen = !isDropDownOpen;
-
-        //    //AutoCompleteList.Visibility = AutoCompleteList.Visibility == Visibility.Visible
-        //    //    ? Visibility.Collapsed
-        //    //    : Visibility.Visible;
-        //}
-        private void DocumentationButton_OnClick(object sender, RoutedEventArgs e)
-        {
-            Process.Start("https://appstore.rws.com/Plugin/243?tab=documentation");
         }
 
         private void ChooseFilepathControl_OnLoaded(object sender, RoutedEventArgs e)
@@ -194,6 +92,17 @@ namespace InterpretBank.Controls
             {
                 FilepathCombobox.SelectedIndex = DatabaseList.List.IndexOf(DatabaseList.LastUsed);
             }
+        }
+
+        private void DocumentationButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://appstore.rws.com/Plugin/243?tab=documentation");
+        }
+
+        private void FilepathCombobox_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            DatabaseList.LastUsed = Filepath;
+            File.WriteAllText(DbListPath, JsonConvert.SerializeObject(DatabaseList));
         }
     }
 }
