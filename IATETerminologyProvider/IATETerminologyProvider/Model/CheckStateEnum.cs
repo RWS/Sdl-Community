cf046d903ee5f0c470f57c3a979a5d7530003863
Emanuel Albu <ealbu@sdl.com>
2024-04-15 20:11:34 +0200

    IATE 5.0.0.2: Accessibility features

---
+++ IATETerminologyProvider/IATETerminologyProvider/Model/CheckStateEnum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sdl.Community.IATETerminologyProvider.Model
+{
+    [Flags]
+    public enum CheckState
+    {
+        AllNotChecked = 0,
+        SomeChecked = 1,
+        AllChecked = 2
+    }
+}
