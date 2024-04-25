aa2012083bb330dff6c39710644e79ea1d284684
FlaviuTritean <107054734+FlaviuTritean@users.noreply.github.com>
2024-04-25 09:24:56 +0300

    Update to OMEGA and TellMe (#1493)

---
+++ Number Verifier/TellMe/NumVerifierCommunityDocumentation.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+using Sdl.TellMe.ProviderApi;
+
+namespace Sdl.Community.NumberVerifier.TellMe
+{
+	public class NumVerifierCommunityDocumentation : AbstractTellMeAction
+	{
+		public override bool IsAvailable => true;
+		public override string Category => "Number Verifier results";
+		public override Icon Icon => PluginResources.TellmeDocumentation;
+
+		public NumVerifierCommunityDocumentation()
+		{
+			Name = "Trados Number Verifier Documentation";
+		}
+
+		public override void Execute()
+		{
+			Process.Start("https://appstore.rws.com/Plugin/33?tab=documentation");
+		}
+	}
+}
