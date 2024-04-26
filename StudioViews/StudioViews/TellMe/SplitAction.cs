3b6f753504b082e2738d792ee3401c3d9db00dd7
Emanuel Albu <ealbu@sdl.com>
2024-04-26 13:47:23 +0200

    StudioViews 3.0.0.2 - Add TellMe actions: Import and Split

---
+++ StudioViews/StudioViews/TellMe/SplitAction.cs
@@ -0,0 +1,25 @@
+﻿using Sdl.Community.StudioViews.Actions;
+using Sdl.TellMe.ProviderApi;
+using Sdl.TranslationStudioAutomation.IntegrationApi;
+using System.Drawing;
+
+namespace Sdl.Community.StudioViews.TellMe
+{
+    public class SplitAction : AbstractTellMeAction
+    {
+        public SplitAction()
+        {
+            Name = $"{PluginResources.Plugin_Name} Split";
+        }
+
+        public override string Category => string.Format(PluginResources.TellMe_String_Results, PluginResources.Plugin_Name);
+        public override Icon Icon => PluginResources.ForumIcon;
+        public override bool IsAvailable => true;
+
+        public override void Execute()
+        {
+            var splitAction = SdlTradosStudio.Application.GetAction<SpitSelectedFilesAction>();
+            splitAction.Run();
+        }
+    }
+}
