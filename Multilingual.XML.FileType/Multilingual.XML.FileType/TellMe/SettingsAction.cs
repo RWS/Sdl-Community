459ed4673ed7942462ccd4b1ca81f9124542402a
Emanuel Albu <42834028+ealbu@users.noreply.github.com>
2024-04-29 14:31:24 +0200

    Multilingual XML FileType 3.0.0.0 / 2024
    Fix TellMe

---
+++ Multilingual.XML.FileType/Multilingual.XML.FileType/TellMe/SettingsAction.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+using Multilingual.XML.FileType.TellMe.WarningWindow;
+using Sdl.TellMe.ProviderApi;
+
+namespace Multilingual.XML.FileType.TellMe
+{
+	public class SettingsAction : AbstractTellMeAction
+	{
+		public SettingsAction()
+		{
+			Name = $"{PluginResources.Plugin_Name} Settings";
+		}
+
+		public override string Category => string.Format(PluginResources.TellMe_String_Results, PluginResources.Plugin_Name);
+		public override Icon Icon => PluginResources.Settings;
+		public override bool IsAvailable => true;
+
+		public override void Execute()
+		{
+			var settingsWindow = new SettingsActionWarning("https://appstore.rws.com/Plugin/13?tab=documentation");
+			settingsWindow.Show();
+		}
+	}
+}
