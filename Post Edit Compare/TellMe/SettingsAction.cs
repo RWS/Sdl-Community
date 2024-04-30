d96add5c4a8c59dafdeb62fb66a96552c5a8b76f
Emanuel Albu <42834028+ealbu@users.noreply.github.com>
2024-04-30 08:16:18 +0200

    Post Edit & Compare 8.0.0.0 / 2024: Fix TellMe (#1503)
    * Post Edit & Compare 8.0.0.0 / 2024: Fix TellMe
    
    * ...

---
+++ Post Edit Compare/TellMe/SettingsAction.cs
@@ -0,0 +1,18 @@
+﻿using System.Drawing;
+using Sdl.TellMe.ProviderApi;
+using Sdl.TranslationStudioAutomation.IntegrationApi;
+
+namespace Sdl.Community.PostEdit.Versions.TellMe
+{
+	public class SettingsAction : AbstractTellMeAction
+	{
+		public SettingsAction() => Name = $"{PluginResources.Plugin_Name} Settings";
+
+		public override string Category => string.Format(PluginResources.TellMe_Provider_Results, PluginResources.Plugin_Name);
+		public override Icon Icon => PluginResources.Settings2;
+		public override bool IsAvailable => true;
+
+		public override void Execute() =>
+			SdlTradosStudio.Application.GetAction<PostEditCompareActionsConfiguration>().Run();
+	}
+}
