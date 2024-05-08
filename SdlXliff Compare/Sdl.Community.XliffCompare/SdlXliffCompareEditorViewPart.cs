a26c6647a09959c080c91ff85140da7a9007736d
Emanuel Albu <ealbu@sdl.com>
2024-05-08 16:07:54 +0200

    XLIFF Compare 4.0.0.0 / 2024 - TellMe update

---
+++ SdlXliff Compare/Sdl.Community.XliffCompare/SdlXliffCompareEditorViewPart.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Sdl.Desktop.IntegrationApi;
+using Sdl.Desktop.IntegrationApi.Extensions;
+using Sdl.Desktop.IntegrationApi.Interfaces;
+using Sdl.TranslationStudioAutomation.IntegrationApi;
+
+namespace Sdl.Community.XliffCompare
+{
+	public abstract class SDLXLIFFViewPartController : AbstractViewPartController
+	{
+		public FormMain Control => (FormMain)GetContentControl();
+	}
+
+	[ViewPart(Id = "SdlXliffCompare.Files", Name = "SdlXliffCompare", Description = "SdlXliffCompare", Icon = "SDLXLIFFCompare")]
+	[ViewPartLayout(typeof(FilesController), Dock = DockType.Bottom)]
+	public class SdlXliffCompareFilesViewPart : SDLXLIFFViewPartController
+	{
+		private static readonly Lazy<FormMain> _control = new Lazy<FormMain>(() => new FormMain());
+
+		protected override IUIControl GetContentControl()
+		{
+			return _control.Value;
+		}
+
+		protected override void Initialize()
+		{
+		}
+	}
+
+	[ViewPart(Id = "SdlXliffCompare.Projects", Name = "SdlXliffCompare", Description = "SdlXliffCompare", Icon = "SDLXLIFFCompare")]
+	[ViewPartLayout(typeof(ProjectsController), Dock = DockType.Bottom)]
+	public class SdlXliffCompareProjectsViewPart : SDLXLIFFViewPartController
+	{
+		private static readonly Lazy<FormMain> _control = new Lazy<FormMain>(() => new FormMain());
+
+		protected override IUIControl GetContentControl()
+		{
+			return _control.Value;
+		}
+
+		protected override void Initialize()
+		{
+		}
+	}
+
+	[ViewPart(Id = "SdlXliffCompare.Editor", Name = "SdlXliffCompare", Description = "SdlXliffCompare", Icon = "SDLXLIFFCompare")]
+	[ViewPartLayout(typeof(EditorController), Dock = DockType.Bottom)]
+	public class SdlXliffCompareEditorViewPart : SDLXLIFFViewPartController
+	{
+		private static readonly Lazy<FormMain> _control = new Lazy<FormMain>(() => new FormMain());
+
+		protected override IUIControl GetContentControl()
+		{
+			return _control.Value;
+		}
+
+		protected override void Initialize()
+		{
+		}
+	}
+}
--- SdlXliff Compare/Sdl.Community.XliffCompare/TellMe/TellMeProvider.cs
+++ SdlXliff Compare/Sdl.Community.XliffCompare/TellMe/TellMeProvider.cs
@@ -1,15 +1,16 @@
-﻿using Sdl.TellMe.ProviderApi;
+﻿using Sdl.Community.XliffCompare.TellMe.Actions;
+using Sdl.TellMe.ProviderApi;
 
 namespace Sdl.Community.XliffCompare.TellMe
 {
 	[TellMeProvider]
-	public class TellMeProvider: ITellMeProvider
+	public class TellMeProvider : ITellMeProvider
 	{
 		public string Name => string.Format(PluginResources.TellMe_Provider, PluginResources.Plugin_Name);
 
 		public AbstractTellMeAction[] ProviderActions => new AbstractTellMeAction[]
 		{
-			new CommunityWikiAction
+			new DocumentationAction
 			{
 				Keywords = new[] { "sdlxliff compare", "community", "support", "wiki" }
 			},
@@ -17,9 +18,18 @@ namespace Sdl.Community.XliffCompare.TellMe
 			{
 				Keywords = new[] { "sdlxliff compare", "support", "forum" }
 			},
-			new AppStoreDownloadAction
+			new SourceCodeAction
 			{
-				Keywords = new[] { "sdlxliff compare", "store", "download", "appstore" }}
+				Keywords = new[] { "sdlxliff compare source code" }
+			},
+			new SdlXliffCompareAction
+			{
+				Keywords = new[] { "sdlxliff compare" }
+			},
+			new SettingsAction
+			{
+				Keywords = new[] { "sdlxliff compare settings" }
+			}
 		};
 	}
 }
