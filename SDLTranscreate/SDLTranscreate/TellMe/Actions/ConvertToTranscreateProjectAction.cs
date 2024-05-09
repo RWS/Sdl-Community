ee6341960b7717e313df064badf9ac5e6c4de0cd
Emanuel Albu <42834028+ealbu@users.noreply.github.com>
2024-05-09 15:49:45 +0200

    Transcreate 4.0.0.1: Fix TellMe and add more actions (#1520)
    * Transcreate 4.0.0.1: Fix TellMe and add more actions

---
+++ SDLTranscreate/SDLTranscreate/TellMe/Actions/ConvertToTranscreateProjectAction.cs
@@ -0,0 +1,68 @@
+﻿using Sdl.ProjectAutomation.Core;
+using Sdl.ProjectAutomation.FileBased;
+using Sdl.TellMe.ProviderApi;
+using Sdl.TranslationStudioAutomation.IntegrationApi;
+using System.Drawing;
+using System.Linq;
+using Trados.Transcreate.Actions;
+using Trados.Transcreate.Common;
+using Trados.Transcreate.TellMe.WarningWindow;
+
+namespace Trados.Transcreate.TellMe.Actions
+{
+    public class ConvertToTranscreateProjectAction : AbstractTellMeAction
+    {
+        private ProjectsController _projectsController;
+
+        public ConvertToTranscreateProjectAction()
+        {
+            Name = $"{PluginResources.Plugin_Name} Convert Project";
+        }
+
+        public override string Category => $"{PluginResources.Plugin_Name} results";
+        public override Icon Icon => PluginResources.Icon;
+
+        public override bool IsAvailable
+        {
+            get
+            {
+                var selectedProject = GetSelectedProject(out _);
+                return selectedProject.GetProjectInfo().ProjectType != ProjectType.InLanguageCloud &&
+                       (selectedProject == null || selectedProject.GetProjectInfo().ProjectOrigin !=
+                           Constants.ProjectOrigin_TranscreateProject);
+            }
+        }
+
+        private ProjectsController ProjectsController =>
+            _projectsController ??= SdlTradosStudio.Application.GetController<ProjectsController>();
+
+        public override void Execute()
+        {
+            GetSelectedProject(out var projectsTotal);
+
+            switch (projectsTotal)
+            {
+                case > 1:
+                    new SettingsActionWarning(PluginResources.SettingsAction_ConvertToTranscreate_MoreThanOneFileSelected,
+                        1).ShowDialog();
+                    return;
+
+                case 0:
+                    new SettingsActionWarning(PluginResources.SettingsAction_ConvertToTranscreate_NoFileSelected, 0).ShowDialog();
+                    return;
+            }
+
+            SdlTradosStudio.Application.ExecuteAction<ConvertProjectAction>();
+        }
+
+        public FileBasedProject GetSelectedProject(out int projectsTotal)
+        {
+            var currentProject = ProjectsController.CurrentProject;
+
+            projectsTotal = ProjectsController.SelectedProjects.Count();
+            if (projectsTotal == 0 && currentProject != null) projectsTotal = 1;
+
+            return projectsTotal is > 1 or 0 ? null : ProjectsController.SelectedProjects.FirstOrDefault() ?? currentProject;
+        }
+    }
+}
---
+++ SDLTranscreate/SDLTranscreate/TellMe/Actions/ImportAction.cs
@@ -0,0 +1,23 @@
+﻿using Sdl.TellMe.ProviderApi;
+using Sdl.TranslationStudioAutomation.IntegrationApi;
+using System.Drawing;
+
+namespace Trados.Transcreate.TellMe.Actions
+{
+    public class ImportAction : AbstractTellMeAction
+    {
+        public ImportAction()
+        {
+            Name = $"{PluginResources.Plugin_Name} Import";
+        }
+
+        public override string Category => $"{PluginResources.Plugin_Name} results";
+        public override Icon Icon => PluginResources.Import;
+
+        public override bool IsAvailable =>
+            SdlTradosStudio.Application.GetAction<Transcreate.Actions.ImportAction>().Enabled &&
+            SdlTradosStudio.Application.GetController<TranscreateViewController>().IsActive;
+
+        public override void Execute() => SdlTradosStudio.Application.ExecuteAction<Transcreate.Actions.ImportAction>();
+    }
+}
