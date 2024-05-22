4a5a80f5b4432aa70d3033af5a457b6cb3e924d7
Emanuel Albu <ealbu@sdl.com>
2024-05-22 09:59:26 +0200

    Your Productivity 4.0.0.0 / 2024   Omega compatibility   TellMe enhancements

---
+++ YourProductivity/Sdl.Community.YourProductivity/TellMe/Actions/DocumentationAction.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+using Sdl.TellMe.ProviderApi;
+
+namespace Sdl.Community.YourProductivity.TellMe.Actions
+{
+    public class DocumentationAction : AbstractTellMeAction
+    {
+        public override bool IsAvailable => true;
+
+        public override string Category => string.Format(PluginResources.TellMe_String_Results, PluginResources.Plugin_Name);
+
+        public override Icon Icon => PluginResources.TellmeDocumentation;
+
+        public DocumentationAction()
+        {
+            Name = $"{PluginResources.Plugin_Name} Documentation";
+        }
+
+        public override void Execute()
+        {
+            Process.Start("https://appstore.rws.com/Plugin/82?tab=documentation");
+        }
+    }
+}
