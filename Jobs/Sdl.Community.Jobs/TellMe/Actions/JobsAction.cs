32c12022c197ee55f8238eb884b76ae899327ebf
Emanuel Albu <ealbu@sdl.com>
2024-05-23 13:22:16 +0200

    Jobs 5.0.0.0 / 2024

---
+++ Jobs/Sdl.Community.Jobs/TellMe/Actions/JobsAction.cs
@@ -0,0 +1,20 @@
+﻿using Sdl.TellMe.ProviderApi;
+using Sdl.TranslationStudioAutomation.IntegrationApi;
+using System.Drawing;
+
+namespace Sdl.Community.Jobs.TellMe.Actions
+{
+    public class JobsAction : AbstractTellMeAction
+    {
+        public JobsAction()
+        {
+            Name = $"{PluginResources.Plugin_Name} View";
+        }
+
+        public override string Category => string.Format(PluginResources.TellMe_Provider_Results, PluginResources.Plugin_Name);
+        public override Icon Icon => PluginResources.icon;
+        public override bool IsAvailable => true;
+
+        public override void Execute() => SdlTradosStudio.Application.GetController<JobsView>().Activate();
+    }
+}
--- Jobs/Sdl.Community.Jobs/TellMe/TellMeProvider.cs
+++ Jobs/Sdl.Community.Jobs/TellMe/TellMeProvider.cs
@@ -1,25 +1,19 @@
-﻿using Sdl.TellMe.ProviderApi;
+﻿using Sdl.Community.Jobs.TellMe.Actions;
+using Sdl.TellMe.ProviderApi;
 
 namespace Sdl.Community.Jobs.TellMe
 {
-	[TellMeProvider]
-	public class TellMeProvider: ITellMeProvider
-	{
-		public string Name => string.Format(PluginResources.TellMe_Provider, PluginResources.Plugin_Name);
+    [TellMeProvider]
+    public class TellMeProvider : ITellMeProvider
+    {
+        public string Name => string.Format(PluginResources.TellMe_Provider, PluginResources.Plugin_Name);
 
-		public AbstractTellMeAction[] ProviderActions => new AbstractTellMeAction[]
-		{
-			new CommunityWikiAction
-			{
-				Keywords = new[] {"jobs", "trados jobs", "community", "support", "wiki" }
-			},
-			new AppStoreForumAction
-			{
-				Keywords = new[] { "jobs", "trados jobs", "support", "forum" }
-			},
-			new AppStoreDownloadAction
-			{
-				Keywords = new[] { "jobs", "trados jobs", "store", "download", "appstore" }}
-		};
-	}
+        public AbstractTellMeAction[] ProviderActions =>
+        [
+            new SourceCodeAction { Keywords = ["jobs", "trados jobs source code"] },
+            new JobsAction { Keywords = ["jobs", "trados jobs view"] },
+            new DocumentationAction { Keywords = ["jobs", "trados jobs", "community", "support", "documentation"] },
+            new AppStoreForumAction { Keywords = ["jobs", "trados jobs", "support", "forum"] }
+        ];
+    }
 }
