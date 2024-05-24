314e6e4bdbf00743cd93e7121d772af57ac91dc6
ancaia <91186154+ancaia@users.noreply.github.com>
2024-05-24 17:16:58 +0300

    Add LC login to StandAloneConsoleApp_PretranslateUsingProvider

---
+++ Code samples/StandAloneConsoleApp_PretranslateUsingProvider/LC/AuthenticationRequestModel.cs
@@ -0,0 +1,23 @@
+﻿namespace StandAloneConsoleApp_PretranslateUsingProvider.LC
+{
+    public class AuthenticationRequestModel
+    {
+        public string Audience { get; set; }
+
+        public string Password { get; set; }
+
+        public string GrantType { get; set; }
+
+        public string Scope { get; set; }
+
+        public string Realm { get; set; }
+
+        public string ClientSecret { get; set; }
+
+        public string ClientId { get; set; }
+
+        public string Username { get; set; }
+
+        public string TenantId { get; set; }
+    }
+}
---
+++ Code samples/StandAloneConsoleApp_PretranslateUsingProvider/LC/AuthenticationResponseModel.cs
@@ -0,0 +1,12 @@
+﻿namespace StandAloneConsoleApp_PretranslateUsingProvider.LC
+{
+    public class AuthenticationResponseModel
+    {
+        public string Access_token { get; set; }
+        public string Refresh_token { get; set; }
+        public string Id_token { get; set; }
+        public string Scope { get; set; }
+        public int Expires_in { get; set; }
+        public string Token_type { get; set; }
+    }
+}
