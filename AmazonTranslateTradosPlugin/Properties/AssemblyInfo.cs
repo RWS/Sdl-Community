b91d79885a99a47f9eefce612db441a50f27e776
Andrei Birleanu <abirleanu@sdl.com>
2024-05-28 09:03:09 +0300

    SDLCOM-5653

--- AmazonTranslateTradosPlugin/Properties/AssemblyInfo.cs
+++ AmazonTranslateTradosPlugin/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 [assembly: AssemblyVersion("5.0.0.0")]
-[assembly: AssemblyFileVersion("5.0.0.2")]
+[assembly: AssemblyFileVersion("5.0.0.3")]
 
