7150a4287be2fb1283e8656e0ebb08b490efa8cf
Flaviu Tritean <ftritean@sdl.com>
2024-06-10 15:11:15 +0300

    Add TellMeConstatnts to project

---
+++ Legit/Sdl.Community.Legit/TellMe/TellMeConstants.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sdl.Community.Legit.TellMe
+{
+    public static class TellMeConstants
+    {
+        public const string PluginName = "";
+
+        public const string TellMe_Provider_Name = $"{PluginName} Tell Me";
+
+        public const string TellMe_Forum_Name = "RWS Community AppStore Forum ";
+        public const string TellMe_Forum_Url = "https://community.rws.com/product-groups/trados-portfolio/rws-appstore/f";
+
+        public const string TellMe_Documentation_Name = $"{PluginName} Documentation";
+        public const string TellMe_Documentation_Url = "https://appstore.rws.com/Plugin/XX?tab=documentation";
+
+        public const string TellMe_SourceCode_Name = $"{PluginName} Source Code";
+        public const string TellMe_SourceCode_Url = "https://github.com/RWS/Sdl-Community/tree/master/Code%20samples";
+
+        public const string TellMe_Settings_Name = $"{PluginName} Settings";
+    }
+}
