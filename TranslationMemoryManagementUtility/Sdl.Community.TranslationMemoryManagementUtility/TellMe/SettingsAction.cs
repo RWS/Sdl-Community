80e553407fb40964a82419635e9b6581706a1169
FlaviuTritean <107054734+FlaviuTritean@users.noreply.github.com>
2024-06-12 17:09:15 +0300

    TMManagementUtility: Update to Omega, update Tell Me features (#1571)

---
+++ TranslationMemoryManagementUtility/Sdl.Community.TranslationMemoryManagementUtility/TellMe/SettingsAction.cs
@@ -0,0 +1,24 @@
+﻿using Sdl.TellMe.ProviderApi;
+using Sdl.TranslationStudioAutomation.IntegrationApi;
+using System.Drawing;
+using static Sdl.Community.TranslationMemoryManagementUtility.TranslationMemoryProviderRibbon;
+
+namespace Sdl.Community.TranslationMemoryManagementUtility
+{
+    public class SettingsAction : AbstractTellMeAction
+    {
+        public SettingsAction()
+        {
+            Name = $"{PluginResources.Plugin_Name} Settings";
+        }
+
+        public override string Category => $"{PluginResources.Plugin_Name} results";
+        public override Icon Icon => PluginResources.TellMe_Settings;
+        public override bool IsAvailable => true;
+
+        public override void Execute()
+        {
+            SdlTradosStudio.Application.ExecuteAction<TMProviderAction>();
+        }
+    }
+}
