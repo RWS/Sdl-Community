fe7846ace4443e3263b332efda85c6a3ac72b162
Flaviu Tritean <ftritean@sdl.com>
2024-06-19 14:32:53 +0300

    StudioViews: fix required changes

--- StudioViews/StudioViews/TellMe/CommunityAppStoreForumAction.cs
+++ StudioViews/StudioViews/TellMe/CommunityAppStoreForumAction.cs
@@ -10,7 +10,7 @@ namespace Sdl.Community.StudioViews.TellMe
 
 		public override string Category => string.Format(PluginResources.TellMe_String_Results, PluginResources.Plugin_Name);
 
-		public override Icon Icon => PluginResources.ForumIcon;
+		public override Icon Icon => PluginResources.Question;
 
 		public CommunityAppStoreForumAction()
 		{
