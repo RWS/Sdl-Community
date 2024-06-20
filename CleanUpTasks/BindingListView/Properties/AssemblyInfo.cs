abcd88ef2dee071e00b7c7d56bc7cbbc78abded9
Flaviu Tritean <ftritean@sdl.com>
2024-06-20 09:55:04 +0300

    CleanUpTasks: fix issue reported

--- CleanUpTasks/BindingListView/Properties/AssemblyInfo.cs
+++ CleanUpTasks/BindingListView/Properties/AssemblyInfo.cs
@@ -27,5 +27,5 @@ using System.Runtime.InteropServices;
 //      Revision
 //
 [assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.0.0.0")]
+[assembly: AssemblyFileVersion("4.0.0.1")]
 [assembly: CLSCompliant(true)]
