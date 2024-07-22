5b2bd11bccd14b210607edf14aa8fc342e53d59c
Emanuel Albu <ealbu@sdl.com>
2024-07-22 09:18:40 +0300

    Add GlobalSettings to persist latest used glossaries

--- DeepLMTProvider/Sdl.Community.DeelLMTProvider/ViewModel/ViewModel.cs
+++ DeepLMTProvider/Sdl.Community.DeelLMTProvider/ViewModel/ViewModel.cs
@@ -6,9 +6,9 @@ namespace Sdl.Community.DeepLMTProvider.ViewModel
 {
 	public class ViewModel : INotifyPropertyChanged
 	{
-		public event PropertyChangedEventHandler PropertyChanged;
+        public event PropertyChangedEventHandler PropertyChanged;
 
-		protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
 		{
 			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 		}
