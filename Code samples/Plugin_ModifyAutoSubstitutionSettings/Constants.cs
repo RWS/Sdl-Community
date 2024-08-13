4b832134a418e2c73ba344f5d957025bb8c589c2
Emanuel Albu <ealbu@sdl.com>
2024-08-13 18:26:36 +0300

    Add basic plug-in for changing Auto-substitution settings

---
+++ Code samples/Plugin_ModifyAutoSubstitutionSettings/Constants.cs
@@ -0,0 +1,13 @@
+﻿namespace Plugin_ModifyAutoSubstitutionSettings
+{
+    public static class Constants
+    {
+        public const string AcronymsAutoLocalizationEnabled = "AcronymsAutoLocalizationEnabled";
+        public const string AlphaNumericAutoLocalizationEnabled = "AlphaNumericAutoLocalizationEnabled";
+        public const string DatesAutoLocalizationEnabled = "DatesAutoLocalizationEnabled";
+        public const string MeasurementsAutoLocalizationEnabled = "MeasurementsAutoLocalizationEnabled";
+        public const string NumbersAutoLocalizationEnabled = "NumbersAutoLocalizationEnabled";
+        public const string TimesAutoLocalizationEnabled = "TimesAutoLocalizationEnabled";
+        public const string VariablesAutoLocalizationEnabled = "VariablesAutoLocalizationEnabled";
+    }
+}
---
+++ Code samples/Plugin_ModifyAutoSubstitutionSettings/Ribbon.cs
@@ -0,0 +1,39 @@
+﻿using Sdl.Desktop.IntegrationApi;
+using Sdl.Desktop.IntegrationApi.Extensions;
+using Sdl.ProjectAutomation.Settings;
+using Sdl.TranslationStudioAutomation.IntegrationApi;
+using System.Linq;
+
+namespace Plugin_ModifyAutoSubstitutionSettings
+{
+    [Action(nameof(TestRibbonAction), Icon = "Nuclear", Name = "Do/test something",
+        Description = "Describing...")]
+    [ActionLayout(typeof(TestRibbonGroup), 10, DisplayType.Large)]
+    public class TestRibbonAction : AbstractAction
+    {
+        protected override void Execute()
+        {
+            var projectsController = SdlTradosStudio.Application.GetController<ProjectsController>();
+
+            var project = projectsController.CurrentProject;
+
+            var lang = project.GetProjectInfo().TargetLanguages.First();
+
+            var settings = project.GetSettings(lang);
+
+            var translationMemorySettings = settings.GetSettingsGroup<TranslationMemorySettings>();
+
+            translationMemorySettings.SetSetting(Constants.AlphaNumericAutoLocalizationEnabled, false);
+
+            project.UpdateSettings(lang, settings);
+            project.Save();
+        }
+    }
+
+    [RibbonGroup(nameof(TestRibbonGroup), Name = "Test")]
+    [RibbonGroupLayout(LocationByType =
+        typeof(Sdl.Desktop.IntegrationApi.DefaultLocations.StudioDefaultRibbonTabs.AddinsRibbonTabLocation))]
+    public class TestRibbonGroup : AbstractRibbonGroup
+    {
+    }
+}
