ca97a4e9c8bf4f570fcb777cece3e412f45304cf
Emanuel Albu <42834028+ealbu@users.noreply.github.com>
2024-08-13 18:29:31 +0300

    Add basic plug-in for changing Auto-substitution settings

---
+++ Code samples/Plugin_ModifyAutoSubstitutionSettings/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using Sdl.ProjectAutomation.Settings;
+
+namespace Plugin_ModifyAutoSubstitutionSettings
+{
+    public static class Extensions
+    {
+        public static void SetSetting(this TranslationMemorySettings translationMemorySettings, string settingId, bool value)
+        {
+            if (value)
+                translationMemorySettings.RemoveSetting(settingId);
+            else
+            {
+                var setting = translationMemorySettings.GetSetting<bool>(settingId);
+                setting.Value = false;
+            }
+        }
+    }
+}
