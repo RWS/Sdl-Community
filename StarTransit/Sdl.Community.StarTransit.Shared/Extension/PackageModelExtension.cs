277ee1152887113e82cd18dabbdaabc098c54239
Emanuel Albu <ealbu@sdl.com>
2024-08-23 12:55:10 +0300

    StarTransit 5.0.1.X: Preserve folder structure of opened transit package

---
+++ StarTransit/Sdl.Community.StarTransit.Shared/Extension/PackageModelExtension.cs
@@ -0,0 +1,12 @@
+﻿using Sdl.Community.StarTransit.Shared.Models;
+using System.IO;
+using System.Linq;
+
+namespace Sdl.Community.StarTransit.Shared.Extension
+{
+    public static class PackageModelExtension
+    {
+        public static string GetTempProjectName(this PackageModel packageModel) =>
+            Path.GetDirectoryName(packageModel.PathToPrjFile)?.Split('\\').LastOrDefault();
+    }
+}
