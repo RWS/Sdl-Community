f77c17592b4b199cba89d013174414a1fe0ff5aa
Emanuel Albu <ealbu@sdl.com>
2024-09-08 22:01:29 +0300

    Antidote 4.0.3.0: Fix replacement issue

--- Antidote Verifier/Sdl.Community.AntidoteVerifier/Sdl.Community.AntidoteVerifier/Extensions/SegmentExtensions.cs
+++ Antidote Verifier/Sdl.Community.AntidoteVerifier/Sdl.Community.AntidoteVerifier/Extensions/SegmentExtensions.cs
@@ -1,11 +1,13 @@
-﻿using Sdl.FileTypeSupport.Framework.BilingualApi;
+﻿using Sdl.Community.AntidoteVerifier.Utils;
+using Sdl.FileTypeSupport.Framework.BilingualApi;
+using System;
 
 namespace Sdl.Community.AntidoteVerifier.Extensions
 {
     public static class SegmentExtensions
     {
 	    public static string GetString(this ISegment segment, bool includeSegments = false) =>
-		    segment.ToString().RemoveTags();
+		    segment.ToString();
 
         //displayLanguage = language of the message or the explication 
         public static bool CanReplace(this ISegment segment, int startIndex, int endPosition, string origString, string displayLanguage, ref string message, ref string explication)
@@ -68,11 +70,34 @@ namespace Sdl.Community.AntidoteVerifier.Extensions
 
         public static void Replace(this ISegment segment, int startIndex, int endPosition, string replacementText)
         {
-            var textVisitor = new CustomTextCollectionVisitor(segment, startIndex, endPosition);
-            foreach (var item in segment)
-                item.AcceptVisitor(textVisitor);
+            if (segment == null)
+                throw new ArgumentNullException(nameof(segment));
 
-            textVisitor.ReplaceText(replacementText);
+            var currentIndex = 0;
+
+            foreach (var element in segment)
+            {
+                if (element is IText textElement)
+                {
+                    var elementLength = textElement.Properties.Text.Length;
+
+                    // Check if the replacement range falls within this text element
+                    if (currentIndex <= startIndex && currentIndex + elementLength > endPosition)
+                    {
+                        // Split the text within this element
+                        var beforeText = textElement.Properties.Text.Substring(0, startIndex - currentIndex);
+                        var afterText = textElement.Properties.Text.Substring(endPosition - currentIndex);
+
+                        // Set the new text with the replacement
+                        textElement.Properties.Text = beforeText + replacementText + afterText;
+
+                        break;
+                    }
+                    currentIndex += elementLength;
+                }
+                else
+                    currentIndex += element.ToString().Length;
+            }
         }
     }
 }
