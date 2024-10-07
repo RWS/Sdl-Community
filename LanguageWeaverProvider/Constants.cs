d271770c59877133429b24d9b30549c307261a65
Emanuel Albu <ealbu@sdl.com>
2024-10-07 18:27:43 +0300

    ...

--- LanguageWeaverProvider/Constants.cs
+++ LanguageWeaverProvider/Constants.cs
@@ -3,12 +3,12 @@
 	public class Constants
 	{
         public const string METADATA_EVALUATED_AT_FORMAT = "M/d/yyyy HH:mm:ss";
-        public const string METADATA_EVALUATED_AT_PREFIX = "tqe_evaluated_at_";
-        public const string METADATA_DESCRIPTION_PREFIX = "tqe_description_";
+        public const string METADATA_EVALUATED_AT_PREFIX = "tqe-evaluated-at-";
+        public const string METADATA_DESCRIPTION_PREFIX = "tqe-description-";
         public const string METADATA_DESCRIPTION = "This was evaluated using the TQE system {0} with the {1} model";
-        public const string METADATA_MODEL_PREFIX = "tqe_model_";
-        public const string METADATA_SCORE_PREFIX = "tqe_score_";
-        public const string METADATA_SYSTEM_PREFIX = "tqe_system_";
+        public const string METADATA_MODEL_PREFIX = "tqe-model-";
+        public const string METADATA_SCORE_PREFIX = "tqe-score-";
+        public const string METADATA_SYSTEM_PREFIX = "tqe-system-";
         public const string METADATA_SYSTEM_NAME = "lw-qe";
 
         public const string TraceAppKey = "Trace-App";
