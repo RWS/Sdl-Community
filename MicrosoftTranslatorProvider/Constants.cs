2f21c5b5ef2e3af06549b5de683dea37c609a121
Emanuel Albu <ealbu@sdl.com>
2024-10-09 10:59:06 +0300

    Microsoft Translator 2.0.3.0 - SDLCOM-6254: Added "Ocp-Apim-Subscription-Region" header

--- MicrosoftTranslatorProvider/Constants.cs
+++ MicrosoftTranslatorProvider/Constants.cs
@@ -19,6 +19,7 @@ namespace MicrosoftTranslatorProvider
         public const string MicrosoftNaming_FullName = "Microsoft Translator Provider";
 		public const string MicrosoftTranslatorString = "Microsoft Translator";
 		public const string OcpApimSubscriptionKeyHeader = "Ocp-Apim-Subscription-Key";
+		public const string OcpApimSubscriptionRegionHeader = "Ocp-Apim-Subscription-Region";
 
 		public const string TellMe_StoreUrl = "https://appstore.rws.com/plugin/179/";
 		public const string TellMe_HelpUrl = "https://community.rws.com/product-groups/trados-portfolio/rws-appstore/w/wiki/6546/microsoft-machine-translation-provider";
