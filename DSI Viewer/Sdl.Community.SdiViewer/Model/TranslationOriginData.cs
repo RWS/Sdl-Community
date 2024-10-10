01f32ce3975f56f30d54934493c7231196185154
Emanuel Albu <ealbu@sdl.com>
2024-10-10 15:09:19 +0300

    Add TqeCard user control Add score color to TqeCard

--- DSI Viewer/Sdl.Community.SdiViewer/Model/TranslationOriginData.cs
+++ DSI Viewer/Sdl.Community.SdiViewer/Model/TranslationOriginData.cs
@@ -1,33 +1,15 @@
 ﻿namespace Sdl.Community.DsiViewer.Model
 {
-	public class TranslationOriginData : ModelBase
-	{
-		private string _model;
-		private string _qualityEstimation;
-        private string _system;
+    public class TranslationOriginData : ModelBase
+    {
         private string _description;
+        private string _model;
+        private string _qualityEstimation;
+        private string _score;
+        private string _system;
+        public string AccessibilityModelLabel => $"Model: {Model}";
 
-        public string Model
-		{
-			get => _model;
-			set
-			{
-				_model = value;
-				OnPropertyChanged(nameof(Model));
-				OnPropertyChanged(nameof(AccessibilityModelLabel));
-			}
-		}
-
-		public string QualityEstimation
-		{
-			get => _qualityEstimation;
-			set
-			{
-				_qualityEstimation = value;
-				OnPropertyChanged(nameof(QualityEstimation));
-				OnPropertyChanged(nameof(AccessibilityQELabel));
-			}
-		}
+        public string AccessibilityQELabel => $"Quality Estimation: {QualityEstimation}";
 
         public string Description
         {
@@ -35,6 +17,38 @@
             set
             {
                 _description = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string Model
+        {
+            get => _model;
+            set
+            {
+                _model = value;
+                OnPropertyChanged(nameof(Model));
+                OnPropertyChanged(nameof(AccessibilityModelLabel));
+            }
+        }
+
+        public string QualityEstimation
+        {
+            get => _qualityEstimation;
+            set
+            {
+                _qualityEstimation = value;
+                OnPropertyChanged(nameof(QualityEstimation));
+                OnPropertyChanged(nameof(AccessibilityQELabel));
+            }
+        }
+
+        public string Score
+        {
+            get => _score;
+            set
+            {
+                _score = value;
                 OnPropertyChanged();
             }
         }
@@ -48,9 +62,5 @@
                 OnPropertyChanged();
             }
         }
-
-        public string AccessibilityQELabel => $"Quality Estimation: {QualityEstimation}";
-
-		public string AccessibilityModelLabel => $"Model: {Model}";
-	}
+    }
 }
