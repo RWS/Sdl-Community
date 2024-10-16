416bc50bae665bcfe0336ee718f3cc9f92ef622a
FlaviuTritean <107054734+FlaviuTritean@users.noreply.github.com>
2024-10-16 09:09:44 +0300

    Add Ps1 script (#1671)

---
+++ build/PowerShellScripts/BuildSlnForCTE.ps1
@@ -0,0 +1,57 @@
+﻿param(
+[string]$directoryPath = ""
+)
+
+if (-not $directoryPath) {
+    $directoryPath = $env:SYSTEM_DEFAULTWORKINGDIRECTORY
+}
+
+# Define the root directory of your repository
+$repoPath = "D:\a\1\s\"
+
+# Get all .csproj files in the repository
+$csprojFiles = Get-ChildItem -Path $repoPath -Recurse -Filter *.sln | Select-Object -ExpandProperty FullName
+
+# Function to find MSBuild location by checking common paths
+function Get-MSBuildLocation {
+    # Array of possible paths where MSBuild might be located
+    $possiblePaths = @(
+        "$env:ProgramFiles(x86)\Microsoft Visual Studio\2019\*\MSBuild\Current\Bin\MSBuild.exe",
+        "$env:ProgramFiles(x86)\Microsoft Visual Studio\2022\*\MSBuild\Current\Bin\MSBuild.exe",
+        "$env:ProgramFiles(x86)\Microsoft Visual Studio\2017\*\MSBuild\15.0\Bin\MSBuild.exe",
+        "$env:ProgramFiles\Microsoft Visual Studio\2022\*\MSBuild\Current\Bin\MSBuild.exe",
+        "$env:ProgramFiles\Microsoft Visual Studio\2019\*\MSBuild\Current\Bin\MSBuild.exe",
+        "$env:ProgramFiles\dotnet\sdk\*\MSBuild.dll"
+    )
+
+    # Loop through each possible path to check if MSBuild.exe exists
+    foreach ($path in $possiblePaths) {
+        $msbuild = Get-ChildItem -Path $path -ErrorAction SilentlyContinue
+
+        # If MSBuild is found, return the path
+        if ($msbuild) {
+            return $msbuild.FullName
+        }
+    }
+
+    # If MSBuild was not found in the common paths, display a message
+    Write-Host "MSBuild.exe not found in the common locations." -ForegroundColor Red
+    return $null
+}
+
+# Call the function and store the MSBuild location
+$msbuildLocation = Get-MSBuildLocation
+
+Set-Alias MSBuild -Value $msbuildLocation;
+
+
+$feedName = 'Multitrans'
+$nugetRestoreArguments = "/p:RestoreSources=https://pkgs.dev.azure.com/sdl/_packaging/$feedName/nuget/v3/index.json"
+$msbuildArguments = "/flp:logfile=$directoryPath/AzureLogs/MyLog.log;append=true"
+
+foreach ($project in $csprojFiles) {
+  #  MSBuild "/bl" -m "$project" "/t:Restore" /p:nugetInteractive=true $nugetRestoreArguments
+    MSBuild "/bl" -m "$project" "/t:Restore" $nugetRestoreArguments
+    MSBuild  -m "$project" "/t:Rebuild" $msbuildArguments
+    if (! $?) {  write-Host "msbuild failed" -ForegroundColor Red ; }
+}
