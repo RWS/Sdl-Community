13fe538797fb289606a7728cb33cede2acf3cff8
aflorescu579774 <aflorescu@sdl.com>
2024-10-29 17:58:47 +0200

    GoogleCloudTranslationProvider 2.0.1.0 - CRQ-40113: Added Sample Codes for Pretranslating using Google Provider

---
+++ Code samples/StandAloneConsoleApp_PreTranslateUsingGoogleProvider/StandAloneConsoleApp_PreTranslateUsingGoogleProvider/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("StandAloneConsoleApp_PreTranslateUsingGoogleProvider")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SDL")]
+[assembly: AssemblyProduct("StandAloneConsoleApp_PreTranslateUsingGoogleProvider")]
+[assembly: AssemblyCopyright("Copyright © SDL 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ae2ac3d0-a442-4722-aa2e-f847d8d72953")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
