0b75bcebdb93a25f22166c288782009de72e17dc
Emanuel Albu <ealbu@sdl.com>
2024-11-07 17:36:18 +0200

    Microsoft Translation Provider 2.0.6.0: Validate before adding to project

--- MicrosoftTranslatorProvider/ViewModel/PrivateEndpointConfigurationViewModel.cs
+++ MicrosoftTranslatorProvider/ViewModel/PrivateEndpointConfigurationViewModel.cs
@@ -1,249 +1,244 @@
-﻿using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows.Input;
-using LanguageMappingProvider.Database.Interface;
+﻿using LanguageMappingProvider.Database.Interface;
 using MicrosoftTranslatorProvider.Commands;
 using MicrosoftTranslatorProvider.Interfaces;
 using MicrosoftTranslatorProvider.Model;
 using Sdl.LanguagePlatform.Core;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace MicrosoftTranslatorProvider.ViewModel
 {
-	public class PrivateEndpointConfigurationViewModel : BaseViewModel
+    public class PrivateEndpointConfigurationViewModel : BaseViewModel
     {
-		readonly ILanguageMappingDatabase _languageMappingDatabase;
-		readonly ITranslationOptions _translationOptions;
-		readonly LanguagePair[] _languagePairs;
+        private readonly ILanguageMappingDatabase _languageMappingDatabase;
+        private readonly LanguagePair[] _languagePairs;
+        private readonly ITranslationOptions _translationOptions;
+        private string _headerKey;
+        private string _headerValue;
+        private string _loadingAction;
+        private string _parameterKey;
+        private string _parameterValue;
 
-		string _loadingAction;
+        public PrivateEndpointConfigurationViewModel(ITranslationOptions translationOptions,
+            LanguagePair[] languagePairs, ILanguageMappingDatabase languageMappingDatabase)
+        {
+            _languagePairs = languagePairs;
+            _translationOptions = translationOptions;
+            _languageMappingDatabase = languageMappingDatabase;
+            InitializeCommands();
+            LoadPairMapping();
+            LoadConfigurations();
+        }
 
-		string _headerKey;
-		string _headerValue;
-		string _parameterKey;
-		string _parameterValue;
+        public ICommand AddHeaderCommand { get; set; }
+        public ICommand AddParameterCommand { get; set; }
+        public ICommand DeletePairCommand { get; set; }
 
-		public PrivateEndpointConfigurationViewModel(ITranslationOptions translationOptions, LanguagePair[] languagePairs, ILanguageMappingDatabase languageMappingDatabase)
-		{
-			_languagePairs = languagePairs;
-			_translationOptions = translationOptions;
-			_languageMappingDatabase = languageMappingDatabase;
-			InitializeCommands();
-			LoadPairMapping();
-			LoadConfigurations();
-		}
+        public string HeaderKey
+        {
+            get => _headerKey;
+            set
+            {
+                _headerKey = value;
+                OnPropertyChanged();
+            }
+        }
 
-		public ObservableCollection<UrlMetadata> Headers { get; set; }
+        public ObservableCollection<UrlMetadata> Headers { get; set; }
 
-		public ObservableCollection<UrlMetadata> Parameters { get; set; }
+        public string HeaderValue
+        {
+            get => _headerValue;
+            set
+            {
+                _headerValue = value;
+                OnPropertyChanged();
+            }
+        }
 
-		public ObservableCollection<PairModel> PairMappings { get; set; }
+        public string LoadingAction
+        {
+            get => _loadingAction;
+            set
+            {
+                _loadingAction = value;
+                OnPropertyChanged();
+            }
+        }
 
-		public string HeaderKey
-		{
-			get => _headerKey;
-			set
-			{
-				_headerKey = value;
-				OnPropertyChanged();
-			}
-		}
+        public ObservableCollection<PairModel> PairMappings { get; set; }
 
-		public string HeaderValue
-		{
-			get => _headerValue;
-			set
-			{
-				_headerValue = value;
-				OnPropertyChanged();
-			}
-		}
+        public string ParameterKey
+        {
+            get => _parameterKey;
+            set
+            {
+                _parameterKey = value;
+                OnPropertyChanged();
+            }
+        }
 
-		public string ParameterKey
-		{
-			get => _parameterKey;
-			set
-			{
-				_parameterKey = value;
-				OnPropertyChanged();
-			}
-		}
+        public ObservableCollection<UrlMetadata> Parameters { get; set; }
 
-		public string ParameterValue
-		{
-			get => _parameterValue;
-			set
-			{
-				_parameterValue = value;
-				OnPropertyChanged();
-			}
-		}
+        public string ParameterValue
+        {
+            get => _parameterValue;
+            set
+            {
+                _parameterValue = value;
+                OnPropertyChanged();
+            }
+        }
 
-		public string LoadingAction
-		{
-			get => _loadingAction;
-			set
-			{
-				_loadingAction = value;
-				OnPropertyChanged();
-			}
-		}
+        private void AddDefaultParameters()
+        {
+            Parameters =
+            [
+                new UrlMetadata()
+                {
+                    Key = "from",
+                    Value = "sourceLanguage",
+                    IsReadOnly = true
+                },
+                new UrlMetadata()
+                {
+                    Key = "to",
+                    Value = "targetLanguage",
+                    IsReadOnly = true
+                },
+            ];
+        }
 
-		public ICommand AddHeaderCommand { get; set; }
-		public ICommand AddParameterCommand { get; set; }
-		public ICommand DeletePairCommand { get; set; }
+        private void AddHeader(object parameter)
+        {
+            var newHeader = new UrlMetadata()
+            {
+                Key = HeaderKey,
+                Value = HeaderValue,
+            };
 
-		private void InitializeCommands()
-		{
-			AddHeaderCommand = new RelayCommand(AddHeader);
-			AddParameterCommand = new RelayCommand(AddParameter);
-			DeletePairCommand = new RelayCommand(DeletePair);
-		}
+            AddItem(Headers, newHeader);
+            HeaderKey = string.Empty;
+            HeaderValue = string.Empty;
+        }
 
-		private void LoadConfigurations()
-		{
-			Headers = [];
-			Parameters = [];
-			if (_translationOptions.PrivateEndpoint is null
-			 || _translationOptions.PrivateEndpoint.Parameters is null)
-			{
-				AddDefaultParemeters();
-			}
+        private void AddItem(ObservableCollection<UrlMetadata> items, UrlMetadata item)
+        {
+            items.Add(item);
+        }
 
-			Headers = LoadUrlMetadataCollection(_translationOptions.PrivateEndpoint.Headers) ?? [];
-			Parameters = LoadUrlMetadataCollection(_translationOptions.PrivateEndpoint.Parameters) ?? Parameters ?? [];
-		}
+        private void AddParameter(object parameter)
+        {
+            var newParameter = new UrlMetadata()
+            {
+                Key = ParameterKey,
+                Value = ParameterValue,
+            };
 
-		private void LoadPairMapping()
-		{
-			if (_translationOptions.PairMappings is null)
-			{
-				CreatePairMappings();
-				return;
-			}
+            AddItem(Parameters, newParameter);
+            ParameterKey = string.Empty;
+            ParameterValue = string.Empty;
+        }
 
+        private async void CreatePairMappings()
+        {
+            var originalPairMappings = PairMappings;
+            PairMappings = [];
+            LoadingAction = "Getting mapped languages...";
+            await Task.Delay(0);
+            var mappedLanguages = _languageMappingDatabase.GetMappedLanguages();
+            foreach (var languagePair in _languagePairs)
+            {
+                var mappedLanguagePairs = mappedLanguages.Where(mappedLang => mappedLang.TradosCode.Equals(languagePair.SourceCultureName) || mappedLang.TradosCode.Equals(languagePair.TargetCultureName));
+                var mappedSource = mappedLanguagePairs.FirstOrDefault(mappedLang => mappedLang.TradosCode.Equals(languagePair.SourceCultureName));
+                var mappedTarget = mappedLanguagePairs.FirstOrDefault(mappedLang => mappedLang.TradosCode.Equals(languagePair.TargetCultureName));
+                var displayName = $"{mappedSource.Name} ({mappedSource.Region}) - {mappedTarget?.Name} ({mappedTarget?.Region})";
+                var sourceLanguageCode = mappedSource.LanguageCode;
+                var targetLanguageCode = mappedTarget.LanguageCode;
 
-			PairMappings = new(_translationOptions.PairModels.Select(pm => pm.Clone()));
-		}
+                var currentModel = originalPairMappings?.FirstOrDefault(pair => pair.DisplayName.Equals(displayName));
+                if (currentModel is not null
+                 && sourceLanguageCode.Equals(currentModel.SourceLanguageCode)
+                 && targetLanguageCode.Equals(currentModel.TargetLanguageCode))
+                {
+                    PairMappings.Add(currentModel.Clone());
+                    continue;
+                }
 
-		private async void CreatePairMappings()
-		{
-			var originalPairMappings = PairMappings;
-			PairMappings = [];
-			LoadingAction = "Getting mapped languages...";
-			await Task.Delay(0);
-			var mappedLanguages = _languageMappingDatabase.GetMappedLanguages();
-			foreach (var languagePair in _languagePairs)
-			{
-				var mappedLanguagePairs = mappedLanguages.Where(mappedLang => mappedLang.TradosCode.Equals(languagePair.SourceCultureName) || mappedLang.TradosCode.Equals(languagePair.TargetCultureName));
-				var mappedSource = mappedLanguagePairs.FirstOrDefault(mappedLang => mappedLang.TradosCode.Equals(languagePair.SourceCultureName));
-				var mappedTarget = mappedLanguagePairs.FirstOrDefault(mappedLang => mappedLang.TradosCode.Equals(languagePair.TargetCultureName));
-				var displayName = $"{mappedSource.Name} ({mappedSource.Region}) - {mappedTarget?.Name} ({mappedTarget?.Region})";
-				var sourceLanguageCode = mappedSource.LanguageCode;
-				var targetLanguageCode = mappedTarget.LanguageCode;
+                var pairModel = new PairModel()
+                {
+                    DisplayName = displayName,
+                    IsSupported = true,
+                    SourceLanguageCode = sourceLanguageCode,
+                    SourceLanguageName = mappedSource.Name,
+                    TargetLanguageCode = targetLanguageCode,
+                    TargetLanguageName = mappedTarget.Name,
+                    TradosLanguagePair = languagePair
+                };
 
-				var currentModel = originalPairMappings?.FirstOrDefault(pair => pair.DisplayName.Equals(displayName));
-				if (currentModel is not null
-				 && sourceLanguageCode.Equals(currentModel.SourceLanguageCode)
-				 && targetLanguageCode.Equals(currentModel.TargetLanguageCode))
-				{
-					PairMappings.Add(currentModel.Clone());
-					continue;
-				}
+                PairMappings.Add(pairModel);
+            }
 
-				var pairModel = new PairModel()
-				{
-					DisplayName = displayName,
-					IsSupported = true,
-					SourceLanguageCode = sourceLanguageCode,
-					SourceLanguageName = mappedSource.Name,
-					TargetLanguageCode = targetLanguageCode,
-					TargetLanguageName = mappedTarget.Name,
-					TradosLanguagePair = languagePair
-				};
+            OnPropertyChanged(nameof(PairMappings));
+            LoadingAction = string.Empty;
+        }
 
-				PairMappings.Add(pairModel);
-			}
+        private void DeletePair(object parameter)
+        {
+            if (parameter is not UrlMetadata pair)
+            {
+                return;
+            }
 
-			OnPropertyChanged(nameof(PairMappings));
-			LoadingAction = string.Empty;
-		}
+            Headers.Remove(pair);
+            Parameters.Remove(pair);
+        }
 
-		private ObservableCollection<UrlMetadata> LoadUrlMetadataCollection(IEnumerable<UrlMetadata> source)
-		{
-			if (source?.Any() != true)
-			{
-				return null;
-			}
+        private void InitializeCommands()
+        {
+            AddHeaderCommand = new RelayCommand(AddHeader);
+            AddParameterCommand = new RelayCommand(AddParameter);
+            DeletePairCommand = new RelayCommand(DeletePair);
+        }
 
-			return new ObservableCollection<UrlMetadata>(source.Select(parameter => new UrlMetadata()
-			{
-				Key = parameter.Key,
-				Value = parameter.Value,
-				IsReadOnly = parameter.IsReadOnly
-			}));
-		}
+        private void LoadConfigurations()
+        {
+            Headers = [];
+            Parameters = [];
 
-		private void AddDefaultParemeters()
-		{
-			Parameters =
-			[
-				new UrlMetadata()
-				{
-					Key = "from",
-					Value = "sourceLanguage",
-					IsReadOnly = true
-				},
-				new UrlMetadata()
-				{
-					Key = "to",
-					Value = "targetLanguage",
-					IsReadOnly = true
-				},
-			];
-		}
+            if (_translationOptions.PrivateEndpoint?.Parameters is null) AddDefaultParameters();
 
-		private void AddHeader(object parameter)
-		{
-			var newHeader = new UrlMetadata()
-			{
-				Key = HeaderKey,
-				Value = HeaderValue,
-			};
+            Headers = LoadUrlMetadataCollection(_translationOptions.PrivateEndpoint?.Headers) ?? [];
+            Parameters = LoadUrlMetadataCollection(_translationOptions.PrivateEndpoint?.Parameters) ?? Parameters ?? [];
+        }
 
-			AddItem(Headers, newHeader);
-			HeaderKey = string.Empty;
-			HeaderValue = string.Empty;
-		}
+        private void LoadPairMapping()
+        {
+            if (_translationOptions.PairMappings is null)
+            {
+                CreatePairMappings();
+                return;
+            }
 
-		private void AddParameter(object parameter)
-		{
-			var newParameter = new UrlMetadata()
-			{
-				Key = ParameterKey,
-				Value = ParameterValue,
-			};
+            PairMappings = new(_translationOptions.PairModels.Select(pm => pm.Clone()));
+        }
 
-			AddItem(Parameters, newParameter);
-			ParameterKey = string.Empty;
-			ParameterValue = string.Empty;
-		}
+        private ObservableCollection<UrlMetadata> LoadUrlMetadataCollection(IEnumerable<UrlMetadata> source)
+        {
+            if (source?.Any() != true)
+            {
+                return null;
+            }
 
-		private void AddItem(ObservableCollection<UrlMetadata> items, UrlMetadata item)
-		{
-			items.Add(item);
-		}
-
-		private void DeletePair(object parameter)
-		{
-			if (parameter is not UrlMetadata pair)
-			{
-				return;
-			}
-
-			Headers.Remove(pair);
-			Parameters.Remove(pair);
-		}
-	}
+            return new ObservableCollection<UrlMetadata>(source.Select(parameter => new UrlMetadata()
+            {
+                Key = parameter.Key,
+                Value = parameter.Value,
+                IsReadOnly = parameter.IsReadOnly
+            }));
+        }
+    }
 }
--- MicrosoftTranslatorProvider/ViewModel/ProviderConfigurationViewModel.cs
+++ MicrosoftTranslatorProvider/ViewModel/ProviderConfigurationViewModel.cs
@@ -4,12 +4,11 @@ using MicrosoftTranslatorProvider.Interfaces;
 using MicrosoftTranslatorProvider.LanguageMappingProvider;
 using MicrosoftTranslatorProvider.LanguageMappingProvider.View;
 using MicrosoftTranslatorProvider.LanguageMappingProvider.ViewModel;
+using MicrosoftTranslatorProvider.Service;
 using MicrosoftTranslatorProvider.View;
 using Sdl.LanguagePlatform.Core;
 using System;
-using System.Linq;
 using System.Windows.Input;
-using MicrosoftTranslatorProvider.Service;
 using TradosProxySettings.View;
 using TradosProxySettings.ViewModel;
 
@@ -17,90 +16,58 @@ namespace MicrosoftTranslatorProvider.ViewModel
 {
     public class ProviderConfigurationViewModel : BaseViewModel
     {
-		readonly ILanguageMappingDatabase _languageMappingDatabase;
-		readonly ITranslationOptions _translationOptions;
-		readonly LanguagePair[] _languagePairs;
+        private readonly ILanguageMappingDatabase _languageMappingDatabase;
+        private readonly LanguagePair[] _languagePairs;
+        private readonly ITranslationOptions _translationOptions;
 
-		public ProviderConfigurationViewModel(ITranslationOptions translationOptions, LanguagePair[] languagePairs)
-		{
-			_languagePairs = languagePairs;
-			_translationOptions = translationOptions;
-			_languageMappingDatabase = DatabaseControl.InitializeDatabase();
-			AuthenticationType = _translationOptions.AuthenticationType;
-			InitializeCommands();
-			InitializeInternalView();
-		}
+        public ProviderConfigurationViewModel(ITranslationOptions translationOptions, LanguagePair[] languagePairs)
+        {
+            _languagePairs = languagePairs;
+            _translationOptions = translationOptions;
+            _languageMappingDatabase = DatabaseControl.InitializeDatabase();
+            AuthenticationType = _translationOptions.AuthenticationType;
+            InitializeCommands();
+            InitializeInternalView();
+        }
 
-		public bool SaveChanges { get; private set; }
+        public delegate void CloseWindowEventRaiser();
 
-		public AuthenticationType AuthenticationType { get; private set; }
+        public event CloseWindowEventRaiser CloseEventRaised;
 
-        public MicrosoftConfigurationViewModel MicrosoftConfigurationViewModel { get; set; }
-
-        public PrivateEndpointConfigurationViewModel PrivateEndpointConfigurationViewModel { get; set; }
-
+        public AuthenticationType AuthenticationType { get; private set; }
         public ICommand ManageChangesCommand { get; private set; }
-		public ICommand OpenLanguageMappingCommand { get; private set; }
-		public ICommand OpenProviderSettingsCommand { get; private set; }
-		public ICommand OpenProxySettingsCommand { get; private set; }
-
+        public MicrosoftConfigurationViewModel MicrosoftConfigurationViewModel { get; set; }
+        public ICommand OpenLanguageMappingCommand { get; private set; }
+        public ICommand OpenProviderSettingsCommand { get; private set; }
+        public ICommand OpenProxySettingsCommand { get; private set; }
+        public PrivateEndpointConfigurationViewModel PrivateEndpointConfigurationViewModel { get; set; }
         public ICommand ResetAndIdentifyPairsCommand { get; private set; }
+        public bool SaveChanges { get; private set; }
 
-		public delegate void CloseWindowEventRaiser();
-
-		public event CloseWindowEventRaiser CloseEventRaised;
-
-		private void InitializeCommands()
-		{
-			ManageChangesCommand = new RelayCommand(ManageChanges);
-			OpenLanguageMappingCommand = new RelayCommand(OpenLanguageMapping);
-			OpenProviderSettingsCommand = new RelayCommand(OpenProviderSettings);
+        private void InitializeCommands()
+        {
+            ManageChangesCommand = new RelayCommand(ManageChanges);
+            OpenLanguageMappingCommand = new RelayCommand(OpenLanguageMapping);
+            OpenProviderSettingsCommand = new RelayCommand(OpenProviderSettings);
             OpenProxySettingsCommand = new RelayCommand(OpenProxySettings);
         }
 
-		private void InitializeInternalView()
-		{
-			if (AuthenticationType == AuthenticationType.Microsoft)
-			{
-				MicrosoftConfigurationViewModel = new(_translationOptions, _languagePairs, _languageMappingDatabase);
-			}
-
-			if (AuthenticationType == AuthenticationType.PrivateEndpoint)
-			{
-				PrivateEndpointConfigurationViewModel = new(_translationOptions, _languagePairs, _languageMappingDatabase);
-			}
-		}
-
-		private void OpenLanguageMapping(object parameter)
-		{
-			var lmpViewModel = new LanguageMappingProviderViewModel(_languageMappingDatabase);
-			lmpViewModel.LanguageMappingUpdated += LanguageMappingUpdated;
-			var lmpView = new LanguageMappingProviderView() { DataContext = lmpViewModel };
-			lmpViewModel.CloseEventRaised += lmpView.Close;
-			lmpView.ShowDialog();
-		}
-
-		private void LanguageMappingUpdated(object sender, EventArgs e)
-		{
-			MicrosoftConfigurationViewModel.CreatePairMappings();
-		}
-
-		private void OpenProviderSettings(object parameter)
-		{
-			var settingsViewModel = new SettingsViewModel(_translationOptions);
-			var settingsView = new SettingsView() { DataContext = settingsViewModel };
-			settingsViewModel.CloseEventRaised += settingsView.Close;
-			settingsView.ShowDialog();
-		}
-
-        private void OpenProxySettings(object parameter)
+        private void InitializeInternalView()
         {
-            var view = new ProxySettingsWindow();
-            var viewModel = new ProxySettingsViewModel(view, _translationOptions.ProxySettings);
-            view.DataContext = viewModel;
-            view.ShowDialog();
-            _translationOptions.ProxySettings = viewModel.ProxySettings;
-            CredentialsManager.UpdateProxySettings(_translationOptions.ProxySettings);
+            if (AuthenticationType == AuthenticationType.Microsoft)
+            {
+                MicrosoftConfigurationViewModel = new(_translationOptions, _languagePairs, _languageMappingDatabase);
+            }
+
+            if (AuthenticationType == AuthenticationType.PrivateEndpoint)
+            {
+                PrivateEndpointConfigurationViewModel = new(_translationOptions, _languagePairs, _languageMappingDatabase);
+            }
+        }
+
+        private void LanguageMappingUpdated(object sender, EventArgs e)
+        {
+            MicrosoftConfigurationViewModel.CreatePairMappings();
         }
 
         private void ManageChanges(object parameter)
@@ -112,21 +79,50 @@ namespace MicrosoftTranslatorProvider.ViewModel
 
             if (saveChanges)
             {
-				_translationOptions.PairModels = AuthenticationType switch
-				{
-					AuthenticationType.Microsoft => [.. MicrosoftConfigurationViewModel.PairModels],
-					AuthenticationType.PrivateEndpoint => [.. PrivateEndpointConfigurationViewModel.PairMappings]
-				};
+                _translationOptions.PairModels = AuthenticationType switch
+                {
+                    AuthenticationType.Microsoft => [.. MicrosoftConfigurationViewModel.PairModels],
+                    AuthenticationType.PrivateEndpoint => [.. PrivateEndpointConfigurationViewModel.PairMappings]
+                };
 
-				if (AuthenticationType == AuthenticationType.PrivateEndpoint)
-				{
-					_translationOptions.PrivateEndpoint.Headers = [.. PrivateEndpointConfigurationViewModel.Headers];
-					_translationOptions.PrivateEndpoint.Parameters = [.. PrivateEndpointConfigurationViewModel.Parameters];
-				}
+                if (AuthenticationType == AuthenticationType.PrivateEndpoint)
+                {
+                    _translationOptions.PrivateEndpoint.Headers = [.. PrivateEndpointConfigurationViewModel.Headers];
+                    _translationOptions.PrivateEndpoint.Parameters = [.. PrivateEndpointConfigurationViewModel.Parameters];
+
+                    saveChanges = PrivateEndpointService.ValidateSettings(_translationOptions.PrivateEndpoint);
+                }
             }
 
             SaveChanges = saveChanges;
             CloseEventRaised.Invoke();
+        }
+
+        private void OpenLanguageMapping(object parameter)
+        {
+            var lmpViewModel = new LanguageMappingProviderViewModel(_languageMappingDatabase);
+            lmpViewModel.LanguageMappingUpdated += LanguageMappingUpdated;
+            var lmpView = new LanguageMappingProviderView() { DataContext = lmpViewModel };
+            lmpViewModel.CloseEventRaised += lmpView.Close;
+            lmpView.ShowDialog();
+        }
+
+        private void OpenProviderSettings(object parameter)
+        {
+            var settingsViewModel = new SettingsViewModel(_translationOptions);
+            var settingsView = new SettingsView() { DataContext = settingsViewModel };
+            settingsViewModel.CloseEventRaised += settingsView.Close;
+            settingsView.ShowDialog();
+        }
+
+        private void OpenProxySettings(object parameter)
+        {
+            var view = new ProxySettingsWindow();
+            var viewModel = new ProxySettingsViewModel(view, _translationOptions.ProxySettings);
+            view.DataContext = viewModel;
+            view.ShowDialog();
+            _translationOptions.ProxySettings = viewModel.ProxySettings;
+            CredentialsManager.UpdateProxySettings(_translationOptions.ProxySettings);
         }
     }
 }
