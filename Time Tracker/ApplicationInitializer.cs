165961f99dad415a92c55c3f8c59c7529c7b7665
Emanuel Albu <ealbu@sdl.com>
2024-12-19 20:56:24 +0200

    Time Tracker 5.0.2.0: Fix activity tracker not starting from editor if Studio Time Tracker View not opened first

---
+++ Time Tracker/ApplicationInitializer.cs
@@ -0,0 +1,25 @@
+﻿using Sdl.Community.Studio.Time.Tracker.Panels.Main;
+using Sdl.Desktop.IntegrationApi;
+using Sdl.Desktop.IntegrationApi.Extensions;
+using Sdl.Desktop.IntegrationApi.Interfaces;
+using Sdl.Desktop.IntegrationApi.Notifications.Events;
+using Sdl.TranslationStudioAutomation.IntegrationApi;
+using System;
+
+namespace Sdl.Community.Studio.Time.Tracker
+{
+    [ApplicationInitializer]
+    public class ApplicationInitializer : IApplicationInitializer
+    {
+        private static StudioTimeTrackerViewController _studioTimeTrackerViewController;
+
+        private StudioTimeTrackerViewController StudioTimeTrackerViewController => _studioTimeTrackerViewController ??=
+                    SdlTradosStudio.Application.GetController<StudioTimeTrackerViewController>();
+
+        public void Execute() =>
+            SdlTradosStudio.Application.GetService<IStudioEventAggregator>()
+                .GetEvent<StudioWindowCreatedNotificationEvent>().Subscribe(OnStudioWindowCreated);
+
+        private void OnStudioWindowCreated(StudioWindowCreatedNotificationEvent @event) => StudioTimeTrackerViewController.Initialize();
+    }
+}
