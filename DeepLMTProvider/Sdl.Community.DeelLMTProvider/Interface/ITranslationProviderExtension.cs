4a2832f840befc20f03a97dbb5e20f6276236dee
aflorescu579774 <165287938+aflorescu579774@users.noreply.github.com>
2025-03-18 15:52:41 +0200

    DeepL Translation Provider 6.2.4.0 - SDLCOM-6521: (#1719)
    - Added support for the ITranslationProviderExtension interface to ensure compatibility with the MT Comparison Tool

---
+++ DeepLMTProvider/Sdl.Community.DeelLMTProvider/Interface/ITranslationProviderExtension.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sdl.Community.DeepLMTProvider.Interface
+{
+    public interface ITranslationProviderExtension
+    {
+        public Dictionary<string, string> LanguagesSupported { get; set; }
+    }
+}
