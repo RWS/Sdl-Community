7de1cf9a2e02119767524b203c3380569f57321b
Alexandru Florescu <aflorescu@sdl.com>
2025-03-20 19:10:32 +0200

    Microsoft Translator Provider 1.2.4.0 - SDLCOM-6527: - Added support for the ITranslationProviderExtension interface to ensure compatibility with the MT Comparison Tool

---
+++ MicrosoftTranslatorProvider/Interface/ITranslationProviderExtension.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace MicrosoftTranslatorProvider.Interface
+{
+    public interface ITranslationProviderExtension
+    {
+        Dictionary<string, string> LanguagesSupported { get; set; }
+    }
+}
