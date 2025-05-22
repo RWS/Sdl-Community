024c9434968bee9ba39cfb7d27b42231d537037a
Emanuel Albu <ealbu@sdl.com>
2025-05-22 09:54:55 +0200

    MXLIFF File Type 5.1.0.X / 2024 SR1

--- MXLIFF File Type/Properties/AssemblyInfo.cs
+++ MXLIFF File Type/Properties/AssemblyInfo.cs
@@ -29,7 +29,7 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("5.0.0.0")]
-[assembly: AssemblyFileVersion("5.0.0.1")]
+[assembly: AssemblyFileVersion("5.1.0.0")]
 [assembly: Sdl.Core.PluginFramework.Plugin("PluginName")]
 [assembly: NeutralResourcesLanguage("en-US")]
 
