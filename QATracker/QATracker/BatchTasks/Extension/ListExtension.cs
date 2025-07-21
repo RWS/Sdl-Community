a2501301f67f90d5c55d2b563f5592abe9478ac2
Emanuel Albu <ealbu@sdl.com>
2025-07-21 15:44:38 +0200

    Refactor

---
+++ QATracker/QATracker/BatchTasks/Extension/ListExtension.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace QATracker.BatchTasks.Extension;
+
+public static class ListExtension
+{
+    public static List<T> AddIfNotPresent<T>(this List<T> list, T item)
+    {
+        if (!list.Contains(item))
+            list.Add(item);
+        return list;
+    }
+}
