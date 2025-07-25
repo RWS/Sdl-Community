a23560690262aeb2c2b02d376fe06aad8e14b432
Patrick Andrew Hartnett <phartnett@sdl.com>
2025-07-25 09:17:17 +0200

    Updated app.config

--- Code samples/ProjectAutomation/ProjectAutomation/Program.cs
+++ Code samples/ProjectAutomation/ProjectAutomation/Program.cs
@@ -25,13 +25,19 @@ namespace ProjectAutomation
 			// TODO: Set the memory resource
 			var memory = new MemoryResource
 			{
-				// Example - FileBased TM 
-                Path = @"C:\Users\phartnett\Documents\Studio 2024\Projects\Samples\SampleProject\TMs\English-German.sdltm",
+				// Example: connect to a FileBased TM 
+                // Path = @"C:\...\Projects\Samples\SampleProject\TMs\English-German.sdltm",
 
-                //Example to connect to deepL provider (https://appstore.rws.com/Plugin/24)
-                //Uri = new Uri("deepltranslationprovider:///"),
-                //UserNameOrClientId = "[your DeepL account e-mail]",
-                //Credential = "[your DeepL account API key]"
+                // Example: connect to deepL provider (https://appstore.rws.com/Plugin/24)
+                // Uri = new Uri("deepltranslationprovider:///"),
+                // UserNameOrClientId = "[your DeepL account e-mail]",
+                // Credential = "[your DeepL account API key]"
+
+                // Example: connect to Groupshare resource
+                // Uri = new Uri("sdltm.http://URL/?orgPath=/Appstore&tmName=TM_NAME"),
+                // UserNameOrClientId = "",
+                // UserPasswordOrClientSecret = "",
+                // IsWindowsUser = false
             };
 
 			CreateProject(sourceFilesDirectory, projectsDirectory, sourceLanguage, targetLanguage, memory);
