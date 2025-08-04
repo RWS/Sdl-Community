e4180f924f7b21169084e5b9a917291b6a906d50
Alexandru Florescu <aflorescu@sdl.com>
2025-08-04 14:08:12 +0300

    Trados Number Verifier 5.0.2.0: Implement the IVerificationCustomMessageData interface in NumberVerifierMessageData and AlignmentErrorExtendedData. This allows the source and target text to be displayed in the Verify File reports.

--- Trados Number Verifier/MessageUI/NumberVerifierMessageData.cs
+++ Trados Number Verifier/MessageUI/NumberVerifierMessageData.cs
@@ -2,34 +2,39 @@
 using Sdl.Community.NumberVerifier.Model;
 using Sdl.FileTypeSupport.Framework.BilingualApi;
 using Sdl.FileTypeSupport.Framework.IntegrationApi;
+using Sdl.Verification.Api;
+using System;
 
 namespace Sdl.Community.Extended.MessageUI
 {
-	public class NumberVerifierMessageData : ExtendedMessageEventData
+    public class NumberVerifierMessageData : ExtendedMessageEventData, IVerificationCustomMessageData
     {
-		public NumberVerifierMessageData(){}
-		public NumberVerifierMessageData(MessageDataModel messageDataModel)
-		{
-			SourceIssues = messageDataModel.SourceIssues;
-			InitialSourceIssues = messageDataModel.InitialSourceIssues;
-			InitialTargetIssues = messageDataModel.InitialTargetIssues;
-			TargetIssues = messageDataModel.TargetIssues;
-			ReplacementSuggestion = messageDataModel.ReplacementSuggestion;
+        public NumberVerifierMessageData() { }
+        public NumberVerifierMessageData(
+            MessageDataModel messageDataModel)
+        {
+            SourceIssues = messageDataModel.SourceIssues;
+            InitialSourceIssues = messageDataModel.InitialSourceIssues;
+            InitialTargetIssues = messageDataModel.InitialTargetIssues;
+            TargetIssues = messageDataModel.TargetIssues;
+            ReplacementSuggestion = messageDataModel.ReplacementSuggestion;
 
-			//Identifier for this custom message type
-			if (messageDataModel.ErrorMessage.Equals(Constants.AlphanumericMessage))
-			{
-				MessageType = Constants.AlphanumericIssue;
-			}
-			else if (messageDataModel.IsHindiVerification)
-			{
-				MessageType = Constants.HindiIssue;
-			}
-			else
-			{
-				MessageType = Constants.NumberIssue;
-			}
-		}
+            //Identifier for this custom message type
+            if (messageDataModel.ErrorMessage.Equals(Constants.AlphanumericMessage))
+            {
+                MessageType = Constants.AlphanumericIssue;
+            }
+            else if (messageDataModel.IsHindiVerification)
+            {
+                MessageType = Constants.HindiIssue;
+            }
+            else
+            {
+                MessageType = Constants.NumberIssue;
+            }
+
+            DetailedDescription = messageDataModel.ErrorMessage;
+        }
 
         /// <summary>
         /// Information which will be displayed in our custom UI.
@@ -46,25 +51,30 @@ namespace Sdl.Community.Extended.MessageUI
             set;
         }
 
-		public string InitialSourceIssues
-		{
-			get;
-			set;
-		}
-
-		public string InitialTargetIssues
-		{
-			get;
-			set;
-		}
-
-		/// <summary>
-		/// Suggestion which will be used in the custom UI for target segment replacement.
-		/// </summary>
-		public ISegment ReplacementSuggestion
+        public string InitialSourceIssues
         {
-            get; 
+            get;
             set;
         }
+
+        public string InitialTargetIssues
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Suggestion which will be used in the custom UI for target segment replacement.
+        /// </summary>
+        public ISegment ReplacementSuggestion
+        {
+            get;
+            set;
+        }
+
+        public string DetailedDescription { get; }
+
+        public string SourceSegmentPlainText { get; set; } = "Hello World";
+        public string TargetSegmentPlainText { get; set; } = "Hallo Welt!";
     }
 }
