4ba4ba9882356e2f03deaf388ffae49aa2481e89
aflorescu579774 <165287938+aflorescu579774@users.noreply.github.com>
2025-09-18 14:24:34 +0300

    Target Word Count 5.1.1.0 (#1779)
    - Implemented a safeguard to ensure InvoiceRates are never null or empty.

---
+++ Target Word Count/Sdl.Community.TargetWordCount/Helpers/DefaultInvoiceRateService.cs
@@ -0,0 +1,28 @@
+﻿using Sdl.Community.TargetWordCount.Models;
+using System.ComponentModel;
+
+namespace Sdl.Community.TargetWordCount.Helpers
+{
+    public static class DefaultInvoiceRateService
+    {
+        public static BindingList<InvoiceItem> CreateDefaultInvoiceRates()
+        {
+            return new BindingList<InvoiceItem>()
+            {
+                new InvoiceItem(RateType.Locked, string.Empty),
+                new InvoiceItem(RateType.PerfectMatch, string.Empty),
+                new InvoiceItem(RateType.ContextMatch, string.Empty),
+                new InvoiceItem(RateType.Repetitions, string.Empty),
+                new InvoiceItem(RateType.CrossFileRepetitions, string.Empty),
+                new InvoiceItem(RateType.OneHundred, string.Empty),
+                new InvoiceItem(RateType.NinetyFive, string.Empty),
+                new InvoiceItem(RateType.EightyFive, string.Empty),
+                new InvoiceItem(RateType.SeventyFive, string.Empty),
+                new InvoiceItem(RateType.Fifty, string.Empty),
+                new InvoiceItem(RateType.New, string.Empty),
+                new InvoiceItem(RateType.Total, string.Empty)
+            };
+        }
+
+    }
+}
