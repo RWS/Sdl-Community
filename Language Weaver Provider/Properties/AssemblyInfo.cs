33630441ffa9be75ec4f86558b5a4d2892369c93
flowAlexS <florescu.alex579774@gmail.com>
2025-10-03 14:23:11 +0300

    Language Weaver Provider 2.1.4.0: - Updated TranslationProvider factory to call ValidateAndUpdateTokenAsync without awaiting - Fire-and-forget token validation now updates credentials asynchronously

--- Language Weaver Provider/Properties/AssemblyInfo.cs
+++ Language Weaver Provider/Properties/AssemblyInfo.cs
@@ -29,4 +29,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 [assembly: AssemblyVersion("2.0.0.0")]
-[assembly: AssemblyFileVersion("2.1.3.0")]
+[assembly: AssemblyFileVersion("2.1.4.0")]
