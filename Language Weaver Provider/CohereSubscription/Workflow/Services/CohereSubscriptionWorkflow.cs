07bc764e12d38e05c24e5f37c28a79a34ec2011c
aflorescu579774 <aflorescu@sdl.com>
2026-02-09 14:22:01 +0200

    Added LanguageCloudIdentityAPI reference

--- Language Weaver Provider/CohereSubscription/Workflow/Services/CohereSubscriptionWorkflow.cs
+++ Language Weaver Provider/CohereSubscription/Workflow/Services/CohereSubscriptionWorkflow.cs
@@ -1,13 +1,35 @@
 ﻿using LanguageWeaverProvider.CohereSubscription.Workflow.Interfaces;
 using LanguageWeaverProvider.CohereSubscription.Workflow.Model;
+using LanguageWeaverProvider.Infrastructure.Http.Model;
+using LanguageWeaverProvider.Infrastructure.Http.Services;
+using LanguageWeaverProvider.Model;
+using Sdl.LanguageCloud.IdentityApi;
+using System;
+using System.Net.Http;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Navigation;
 
 namespace LanguageWeaverProvider.CohereSubscription.Workflow.Services
 {
     public class CohereSubscriptionWorkflow : ICohereSubscriptionWorkflowService
     {
-        public Task<CohereSubscriptionData> ExecuteAsync()
+        public async Task<CohereSubscriptionData> ExecuteAsync()
         {
+            var lcInstance = LanguageCloudIdentityApi.Instance;
+            if (lcInstance is null)
+            {
+                // Log
+                return null;
+            }
+
+            var languageCloudToken = $"Bearer {lcInstance.AccessToken}";
+            var tenantId = lcInstance.ActiveTenantId;
+
+            // TODO
+            // Create the genericHttpService with the instances that we'll use 
+            // When the final request is done, return the CohereSubscriptionData
+            // And add logic for the URIs
             var fakeData = new CohereSubscriptionData
             {
                 IsCohereDetected = false,
@@ -18,7 +40,7 @@ namespace LanguageWeaverProvider.CohereSubscription.Workflow.Services
                 TrialRemainingDays = 3 // example
             };
 
-            return Task.FromResult(fakeData);
+            return fakeData;
         }
     }
 }
