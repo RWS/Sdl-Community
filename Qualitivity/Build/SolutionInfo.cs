ba3be04384e4e2c4f38c132d3317b8a5dc4d5537
Emanuel Albu <ealbu@sdl.com>
2026-02-10 14:35:11 +0100

    Qualitivity 7.0.0.1: Update for Studio 2026 Beta

--- Qualitivity/Build/SolutionInfo.cs
+++ Qualitivity/Build/SolutionInfo.cs
@@ -2,16 +2,16 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyProductAttribute("Qualitivity")]
-[assembly: AssemblyVersionAttribute("6.0.0.0")]
-[assembly: AssemblyFileVersionAttribute("6.1.1.1")]
+[assembly: AssemblyVersionAttribute("7.0.0.0")]
+[assembly: AssemblyFileVersionAttribute("7.0.0.1")]
 [assembly: ComVisibleAttribute(false)]
 namespace System
 {
 	internal static class AssemblyVersionInformation
 	{
 		internal const string AssemblyProduct = "Qualitivity";
-		internal const string AssemblyVersion = "6.0.0.0";
-		internal const string AssemblyFileVersion = "6.1.1.1";
+		internal const string AssemblyVersion = "7.0.0.0";
+		internal const string AssemblyFileVersion = "7.0.0.1";
 		internal const bool ComVisible = false;
 	}
 }
