2d7a8efeddf162e46ad9e5dc102e043cbef71554
Emanuel Albu <ealbu@sdl.com>
2026-02-11 12:35:18 +0100

    Amazon Translate Provider 6.0.0.1: Update for Studio 2026 Beta

--- Amazon Translate MT provider/Properties/AssemblyInfo.cs
+++ Amazon Translate MT provider/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 [assembly: AssemblyVersion("6.0.0.0")]
-[assembly: AssemblyFileVersion("6.0.0.0")]
+[assembly: AssemblyFileVersion("6.0.0.1")]
 
