0089738d9fcb5b7f276c69ecb9704c37bcbf55a5
Emanuel Albu <ealbu@sdl.com>
2026-02-13 17:03:00 +0100

    DeepL: Add possiblity to choose a translation style

---
+++ DeepL Translation Provider/Sdl.Community.DeelLMTProvider/Model/DeepLStyle.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Sdl.Community.DeepLMTProvider.Model
+{
+    public class StyleRulesResponse
+    {
+        [JsonProperty("style_rules")]
+        public List<DeepLStyle> StyleRules { get; set; }
+    }
+    public class DeepLStyle
+    {
+        [JsonProperty("style_id")]
+        public string ID { get; set; }
+        public string Name { get; set; }
+    }
+}
