30ac3b9243c76be68df70652d9791c0e75bcc330
Emanuel Albu <ealbu@sdl.com>
2026-02-27 12:01:50 +0100

    Remove Capture QA Rule State

--- Capture QA Rule State/CaptureQARuleState/Components/SegmentMetadata_Provider/Model/Segment.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CaptureQARuleState.Components.SegmentMetadata_Provider.Model
-{
-    public class Segment
-    {
-        public string Id { get; set; }
-        public string Status { get; set; }
-    }
-}
