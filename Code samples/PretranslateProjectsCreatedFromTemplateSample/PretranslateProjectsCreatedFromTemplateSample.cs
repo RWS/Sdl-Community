8f4ca7c300c1121ffaa4338fa7308ea5fb85f6fd
Emanuel Albu <ealbu@sdl.com>
2026-06-01 13:11:00 +0200

    [Update] PretranslateProjectsCreatedFromTemplateSample

--- Code samples/PretranslateProjectsCreatedFromTemplateSample/PretranslateProjectsCreatedFromTemplateSample.cs
+++ Code samples/PretranslateProjectsCreatedFromTemplateSample/PretranslateProjectsCreatedFromTemplateSample.cs
@@ -1,64 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using PretranslateProjectsCreatedFromTemplateSample.Helpers;
 using Sdl.ProjectAutomation.Core;
 using Sdl.ProjectAutomation.FileBased;
 
 namespace PretranslateProjectsCreatedFromTemplateSample
 {
-	public static class PretranslateProjectsCreatedFromTemplateSample
+	public static class Program
 	{
-		private static void Main(string[] args)
+		private const string TemplatePath      = @"";
+		private const string SourceFilesFolder = @"";
+		private const string OutputFolder      = @"";
+
+		private static void Main()
 		{
-			if (args.Length < 4) return;
+			var template = new ProjectTemplateReference(TemplatePath);
 
-			if (!TryGetTemplate(args[0], out var template)) return;
-			if (!int.TryParse(args[args.Length - 1], out var cyclesTotal)) return;
-
-			Run(args, template, cyclesTotal);
-		}
-
-		private static void Run(string[] args, ProjectTemplateReference template, int cyclesTotal)
-		{
-			var pretranslator = new Pretranslator();
-			var projects = new List<FileBasedProject>();
-			for (var i = 1; i < args.Length - 2; i++)
+			var projectInfo = new ProjectInfo
 			{
-				projects.Add(pretranslator.CreateProjectFromTemplate(template, args[i], $"{args[args.Length - 2]}"));
-				Console.WriteLine($@"Project with files from ""{args[i]}"" created succesfully");
-			}
+				Name = new DirectoryInfo(SourceFilesFolder).Name + "_" + Guid.NewGuid(),
+				LocalProjectFolder = Path.Combine(OutputFolder, new DirectoryInfo(SourceFilesFolder).Name)
+			};
 
-			for (var cycleNo = 0; cycleNo < cyclesTotal; cycleNo++)
-			{
-				foreach (var project in projects)
-				{
-					using (var pretranslator2 = new Pretranslator())
-					{
-						var task = pretranslator2.Pretranslate(project);
-						project.Save();
+			var project = new FileBasedProject(projectInfo, template);
+			project.AddFolderWithFiles(SourceFilesFolder, recursive: false);
+			project.RunAutomaticTasks(
+				project.GetSourceLanguageFiles().GetIds(),
+                [
+                    AutomaticTaskTemplateIds.Scan,
+					AutomaticTaskTemplateIds.ConvertToTranslatableFormat,
+					AutomaticTaskTemplateIds.CopyToTargetLanguages
+                ]);
 
-						var message = "";
-						if (task.Status == TaskStatus.Failed)
-							message = string.Join(Environment.NewLine, task.Messages.Select(m => m.Message));
-						Console.WriteLine($"Pretranslation {cycleNo + 1}: {task.Status} {message}");
-					}
-				}
-			}
-		}
-
-		private static bool TryGetTemplate(string templatePath, out ProjectTemplateReference template)
-		{
-			template = null;
-
-			if (File.Exists(templatePath) && Path.GetExtension(templatePath) == ".sdltpl")
-			{
-				template = new ProjectTemplateReference(templatePath);
-				return true;
-			}
-
-			return false;
+			project.Save();
 		}
 	}
 }
