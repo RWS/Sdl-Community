d7f8700f30a1e75b00a58b69f04625604e8bc7a0
Emanuel Albu <ealbu@sdl.com>
2026-07-14 10:55:54 +0200

    [Fix] report not generated

--- Verify Files – Audit Report/VerifyFilesAuditReport/Components/Report_Extender/ReportExtender.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace VerifyFilesAuditReport.Components.Report_Extender;
-
-public class ReportExtender
-{
-    public IExtendedReport CreateReport(string xmlString)
-    {
-        return new ExtendedReport(xmlString);
-    }
-}
